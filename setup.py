16e3947b96129fd313b6929cb36eef948498366b
Valentin Sulzer <sulzer@maths.ox.ac.uk>
2018-11-11 22:58:41 +0000

    add description, name, email, author to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from distutils.core import setup
 
-setup(name='pybamm',
-      )
+setup(name="pybamm")
+setup(
+    name="pybamm",
+    description="Python Battery Mathematical Modelling.",
+    author="Valentin Sulzer",
+    author_email="valentinsulzer@hotmail.com",
+    url="https://github.com/tinosulzer/PyBaMM",
+)
