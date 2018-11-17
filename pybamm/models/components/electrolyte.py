d8c4acc1355ca050979bc2b08971a0421551fbe4
Valentin Sulzer <valentinsulzer@hotmail.com>
2018-11-17 15:06:45 +0000

    add Electrolyte component class and tests

--- pybamm/models/components/electrolyte.py
+++ pybamm/models/components/electrolyte.py
@@ -8,105 +8,112 @@ import numpy as np
 
 
 class Electrolyte:
-    def __init__(self):
-        pass
+    def set_simulation(self, param, operators, mesh):
+        self.param = param
+        self.operators = operators
+        self.mesh = mesh
 
-    def initial_conditions(param, mesh):
-        pass
+    def initial_conditions():
+        c0 = self.param.c0 * np.ones_like(self.mesh.xc)
 
-    def cation_conservation(self, c, j, bcs=None):
-        """The 1D diffusion equation.
+        return {"c": c0}
+
+    def cation_conservation(self, c, j, flux_bcs=None):
+        """Conservation of cations.
 
         Parameters
         ----------
-        param : pybamm.parameters.Parameter() instance
-            The parameters of the simulation
         c : array_like, shape (n,)
             The electrolyte concentration.
-        operators : pybamm.operators.Operators() instance
-            The spatial operators.
-        flux_bcs : 2-tuple of array_like, shape (1,)
-            Flux at the boundaries (Neumann BC).
         j : array_like, shape (n,)
             The interfacial current density.
+        flux_bcs : 2-tuple of array_like, shape (1,)
+            Flux at the boundaries (Neumann BC).
 
         Returns
         -------
         dcdt : array_like, shape (n,)
-            The time derivative.
+            The time derivative of c.
 
         """
         # Calculate internal flux
-        N_internal = -operators.grad(c)
+        N_internal = -self.operators.grad(c)
+
+        # Default boundary conditions is no flux
+        if flux_bcs == None:
+            flux_bcs = self._bcs_cation_flux()
 
         # Add boundary conditions (Neumann)
         flux_bc_left, flux_bc_right = flux_bcs
         N = np.concatenate([flux_bc_left, N_internal, flux_bc_right])
 
         # Calculate time derivative
-        dcdt = -operators.div(N) + param.s * j
+        dcdt = -self.operators.div(N) + self.param.s * j
 
         return dcdt
 
-    def current_conservation(self, c, e, j, bcs=None):
-        """The 1D diffusion equation.
+    def _bcs_cation_flux(self):
+        return (np.array([0]), np.array([0]))
 
-        Parameters
-        ----------
-        param : pybamm.parameters.Parameter() instance
-            The parameters of the simulation
-        variables : 2-tuple (c, e) of array_like, shape (n,)
-            The concentration, and potential difference.
-        operators : pybamm.operators.Operators() instance
-            The spatial operators.
-        current_bcs : 2-tuple of array_like, shape (1,)
-            Flux at the boundaries (Neumann BC).
-        j : array_like, shape (n,)
-            Interfacial current density.
-
-        Returns
-        -------
-        dedt : array_like, shape (n,)
-            The time derivative of the potential.
-
-        """
-        # Calculate current density
-        i = self.macinnes(variables, operators, current_bcs)
-
-        # Calculate time derivative
-        dedt = 1 / param.gamma_dl * (operators.div(i) - j)
-
-        return dedt
-
-    def macinnes(self, c, e, bcs=None):
-        """The 1D current.
-
-        Parameters
-        ----------
-        param : pybamm.parameters.Parameter() instance
-            The parameters of the simulation
-        variables : 2-tuple (c, e) of array_like, shape (n,)
-            The concentration, and potential difference.
-        operators : pybamm.operators.Operators() instance
-            The spatial operators.
-        current_bcs : 2-tuple of array_like, shape (1,)
-            Flux at the boundaries (Neumann BC).
-
-        Returns
-        -------
-        i : array_like, shape (n+1,)
-            The current density.
-        """
-        c, e = variables
-
-        kappa_over_c = 1
-        kappa = 1
-
-        # Calculate inner current
-        i_inner = kappa_over_c * operators.grad(c) + kappa * operators.grad(e)
-
-        # Add boundary conditions
-        lbc, rbc = current_bcs
-        i = np.concatenate([lbc, i_inner, rbc])
-
-        return i
+    # def current_conservation(self, c, e, j, bcs=None):
+    #     """The 1D diffusion equation.
+    #
+    #     Parameters
+    #     ----------
+    #     param : pybamm.parameters.Parameter() instance
+    #         The parameters of the simulation
+    #     variables : 2-tuple (c, e) of array_like, shape (n,)
+    #         The concentration, and potential difference.
+    #     operators : pybamm.operators.Operators() instance
+    #         The spatial operators.
+    #     current_bcs : 2-tuple of array_like, shape (1,)
+    #         Flux at the boundaries (Neumann BC).
+    #     j : array_like, shape (n,)
+    #         Interfacial current density.
+    #
+    #     Returns
+    #     -------
+    #     dedt : array_like, shape (n,)
+    #         The time derivative of the potential.
+    #
+    #     """
+    #     # Calculate current density
+    #     i = self.macinnes(variables, operators, current_bcs)
+    #
+    #     # Calculate time derivative
+    #     dedt = 1 / param.gamma_dl * (operators.div(i) - j)
+    #
+    #     return dedt
+    #
+    # def macinnes(self, c, e, bcs=None):
+    #     """The 1D current.
+    #
+    #     Parameters
+    #     ----------
+    #     param : pybamm.parameters.Parameter() instance
+    #         The parameters of the simulation
+    #     variables : 2-tuple (c, e) of array_like, shape (n,)
+    #         The concentration, and potential difference.
+    #     operators : pybamm.operators.Operators() instance
+    #         The spatial operators.
+    #     current_bcs : 2-tuple of array_like, shape (1,)
+    #         Flux at the boundaries (Neumann BC).
+    #
+    #     Returns
+    #     -------
+    #     i : array_like, shape (n+1,)
+    #         The current density.
+    #     """
+    #     c, e = variables
+    #
+    #     kappa_over_c = 1
+    #     kappa = 1
+    #
+    #     # Calculate inner current
+    #     i_inner = kappa_over_c * operators.grad(c) + kappa * operators.grad(e)
+    #
+    #     # Add boundary conditions
+    #     lbc, rbc = current_bcs
+    #     i = np.concatenate([lbc, i_inner, rbc])
+    #
+    #     return i
---
+++ tests/test_electrolyte.py
@@ -0,0 +1,59 @@
+import pybamm
+
+import numpy as np
+from numpy.linalg import norm
+
+import unittest
+
+
+class TestComponents(unittest.TestCase):
+    def test_electrolyte_diffusion_finite_volumes_convergence(self):
+        electrolyte = pybamm.Electrolyte()
+
+        # Finite volume only has h**2 convergence if the mesh is uniform?
+        uniform_lengths = {"Ln": 1e-3, "Ls": 1e-3, "Lp": 1e-3}
+        param = pybamm.Parameters(optional_parameters=uniform_lengths)
+
+        # Test convergence
+        ns = [100, 200, 400]
+        errs = [0] * len(ns)
+        for i, n in enumerate(ns):
+            # Set up
+            mesh = pybamm.Mesh(param, n)
+            param.set_mesh_dependent_parameters(mesh)
+            c0 = np.cos(2 * np.pi * mesh.xc)
+            operators = pybamm.Operators("Finite Volumes", "xc", mesh)
+            lbc = np.array([0])
+            rbc = np.array([0])
+            dcdt_exact = -4 * np.pi ** 2 * c0
+
+            # Calculate solution and errors
+            electrolyte.set_simulation(param, operators, mesh)
+
+            dcdt = electrolyte.cation_conservation(c0, 0, (lbc, rbc))
+            errs[i] = norm(dcdt - dcdt_exact) / norm(dcdt_exact)
+
+        # Expect h**2 convergence
+        [
+            self.assertLess(errs[i + 1] / errs[i], 0.26)
+            for i in range(len(errs) - 1)
+        ]
+
+    # def test_butler_volmer(self):
+    #     param = pybamm.Parameters()
+    #     I = np.array([1])
+    #     cn = np.array([0.4])
+    #     cs = np.array([0.5])
+    #     cp = np.array([0.56])
+    #     en = np.arcsinh(I / (param.iota_ref_n * cn)) + param.U_Pb(cn)
+    #     ep = np.arcsinh(
+    #         -I / (param.iota_ref_p * cp ** 2 * param.cw(cp))
+    #     ) + param.U_PbO2(cp)
+    #     j, jn, jp = models.components.butler_volmer(param, cn, cs, cp, en, ep)
+    #     self.assertTrue(np.allclose(jn, I))
+    #     self.assertTrue(np.allclose(jp, -I))
+
+
+if __name__ == "__main__":
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestComponents)
+    unittest.TextTestRunner(verbosity=2).run(suite)
