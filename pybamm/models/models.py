0f36a2e0c7a9237bf8bdf9be9af9c64b3ca7c541
Scottmar93 <marquis@maths.ox.ac.uk>
2018-11-18 14:35:31 +0000

    example of potential class structure of Models and Submodels

--- pybamm/models/models.py
+++ pybamm/models/models.py
@@ -1,60 +1,66 @@
 import numpy as np
+from pybamm.models import particles, electrolytes
 
 
-class SPM:
-
-    def __init__(self, param, mesh):
-        self.param = param
-        self.mesh = mesh
-
-        # create initial conditions
-        cn = param.cn0 * np.ones_like(mesh.rn)
-        cp = param.cp0 * np.ones_like(mesh.rp)
-        self.y0 = np.concatenate(cn, cp)
-
-    def pdes_rhs(self, vars, param, operators):
-        """Calculates the spatial derivates of the spatial terms in the PDEs
-           and returns the right-hand side to be used by the ODE solver
-           (Method of Lines).
-
-        Parameters
-        ----------
-        vars : pybamm.variables.Variables() instance
-            The variables of the model.
-        param : pybamm.parameters.Parameters() instance
-            The model parameters.
-        grad : function
-            The gradient operator.
-        div : function
-            The divergence operator.
-
-        Returns
-        -------
-        dydt : array_like
-            A concatenated vector of all the derivatives.
+class Model:
+    """The parent model class. This contains variables and methods common to all Models
 
         """
-        bcs = self.boundary_conditions(vars, param)
-        dcndt =
+    #   will be useful to have current and BV stored somewhere for reference as needed for RHS
 
-    def boundary_conditions(self, vars, param):
-        """Returns the boundary conditions for the model (fluxes only).
+    def __init__(self):
+        self.sub_models = {}
 
-               Parameters
-               ----------
-               vars : pybamm.variables.Variables() instance
-                   The variables of the model.
-               param : pybamm.parameters.Parameters() instance
-                   The model parameters.
+    def initial_conditions(self, param, mesh):
+        for i in self.sub_models.keys():
+            self.sub_models[i].initial_conditions(list(self.sub_models.keys())[i], param, mesh)
 
-               Returns
-               -------
-               bcs : dict of 2-tuples
-                   Dictionary of flux boundary conditions:
-                       {name: (left-hand flux bc, right-hand flux bc)}.
+    def rhs(self, operators):
+        dydt = []
+        for i in self.sub_models.keys():
+            dydt = np.concatenate((dydt, self.sub_models[i].rhs(operators)))
+        return dydt
 
-               """
-        bcs = {}
-        bcs["cn"] = (0, vars.current/param.Ln)
-        bcs["cp"] = (0, -vars.current/param.Lp)
-        return bcs
+    def update(self, y):
+        # TODO: find a way to deconcatenate u independent of sub_models
+        return
+
+
+# Each specific model should just contain a list of the domains and the
+# type of model to use on each domain
+class SPM(Model):
+    # should also put an input so that user can change type of particle model
+    def __init__(self):
+        super().__init__()
+        self.sub_models = {
+            "negative particle": particles.StandardParticle(),
+            "positive particle": particles.StandardParticle()
+        }
+
+
+class SPMe(Model):
+
+    def __init__(self):
+        super().__init__()
+        self.sub_models = {
+            "negative particle": particles.StandardParticle(),
+            "positive particle": particles.StandardParticle(),
+            "electrolyte": electrolytes.StefanMaxwell1D()
+        }
+
+
+class LeadAcid(Model):
+
+    def __init__(self):
+        super().__init__()
+        self.sub_models = {
+            "electrolyte": electrolytes.StefanMaxwell1D()
+        }
+
+
+class UserModel(Model):
+    """A class so that a user can mix and match domains and submodels by only entering
+    a dictionary of sub_models. All the functionality of Model is then retained."""
+    def __init__(self, sub_models):
+        super().__init__()
+        self.sub_models = sub_models
