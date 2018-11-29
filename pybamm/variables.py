645644e34b775bfc800f249962182b7111f14b55
Valentin Sulzer <valentinsulzer@hotmail.com>
2018-11-29 20:32:57 +0000

    make variables compatible with new model format

--- pybamm/variables.py
+++ pybamm/variables.py
@@ -19,9 +19,9 @@ class Variables(object):
         The simulation mesh.
     """
 
-    def __init__(self, model, mesh):
+    def __init__(self, model):
         self.model = model
-        self.mesh = mesh
+        self.mesh = model.mesh
 
     def update(self, t, y):
         """Update variables with a new t and y.
@@ -38,41 +38,71 @@ class Variables(object):
         self.t = t
         # Unpack y iteratively
         start = 0
-        for var, domain in self.model.variables:
-            end = start + len(self.mesh.__dict__[domain])
+        for var, mesh in self.model.pde_variables:
+            end = start + mesh.npts
             self.__dict__[var] = y[start:end]
             start = end
-            # Split 'tot' variables further into n, s and p
-            if domain == "xc":
+            # Split 'x' variables further into n, s and p
+            if mesh == self.mesh.x:
                 (
                     self.__dict__[var + "n"],
                     self.__dict__[var + "s"],
                     self.__dict__[var + "p"],
                 ) = np.split(
-                    self.__dict__[var], np.cumsum([self.mesh.nn - 1, self.mesh.ns + 1])
+                    self.__dict__[var],
+                    np.cumsum([self.mesh.xn.npts, self.mesh.xs.npts]),
                 )
+
+    @property
+    def neg(self):
+        """Variables in the negative electrode."""
+        neg_vars = {}
+        for attr, value in self.__dict__.items():
+            if attr[-1] == "n":
+                neg_vars[attr[:-1]] = value
+        return neg_vars
+
+    @property
+    def pos(self):
+        """Variables for the positive electrode."""
+        pos_vars = {}
+        for attr, value in self.__dict__.items():
+            if attr[-1] == "p":
+                pos_vars[attr[:-1]] = value
+        return pos_vars
+
+    def set_reaction_vars(self, reaction_vars):
+        for name, value in reaction_vars.items():
+            self.__dict__[name] = value
+            (
+                self.__dict__[name + "n"],
+                self.__dict__[name + "s"],
+                self.__dict__[name + "p"],
+            ) = np.split(
+                self.__dict__[name], np.cumsum([self.mesh.nn - 1, self.mesh.ns + 1])
+            )
 
     def average(self):
         """Average variable attributes over the relevant (sub)domain."""
-        param = self.model.param
+        param = self.model.param.geometric
         mesh = self.mesh
         old_keys = list(self.__dict__.keys())
         for attr in old_keys:
             if attr in ["c"]:
-                avg = np.dot(self.__dict__[attr].T, mesh.dx)
+                avg = np.dot(self.__dict__[attr].T, mesh.x.d_edges)
                 self.__dict__[attr + "_avg"] = avg
 
             elif attr[-1] == "n":
                 # Negative
-                avg = np.sum(self.__dict__[attr], axis=0) * mesh.dxn / param.ln
+                avg = np.dot(self.__dict__[attr].T, mesh.xn.d_edges) / param.ln
                 self.__dict__[attr + "_avg"] = avg
 
             elif attr[-1] == "s":
                 # Separator
-                avg = np.sum(self.__dict__[attr], axis=0) * mesh.dxs / param.ls
+                avg = np.dot(self.__dict__[attr].T, mesh.xs.d_edges) / param.ls
                 self.__dict__[attr + "_avg"] = avg
 
             elif attr[-1] == "p":
                 # Positive
-                avg = np.sum(self.__dict__[attr], axis=0) * mesh.dxp / param.lp
+                avg = np.dot(self.__dict__[attr].T, mesh.xp.d_edges) / param.lp
                 self.__dict__[attr + "_avg"] = avg
--- tests/test_variables.py
+++ tests/test_variables.py
@@ -9,33 +9,42 @@ from numpy.linalg import norm
 import unittest
 
 
+class ModelForTesting(object):
+    def __init__(self, param, mesh):
+        self.param = param
+        self.mesh = mesh
+        self.pde_variables = [("c", mesh.x)]
+
+
 class TestVariables(unittest.TestCase):
     """Test the variables class."""
 
     def setUp(self):
-        self.param = pybamm.Parameters()
-        self.mesh = pybamm.Mesh(self.param, 50)
-        self.param.set_mesh(self.mesh)
-        self.model = pybamm.ReactionDiffusionModel()
-        self.model.set_simulation(self.param, None, None)
-        self.vars = pybamm.Variables(self.model, self.mesh)
+        # Set up with a model and default simulation
+        param = pybamm.Parameters()
+        self.mesh = pybamm.Mesh(param)
+        model = ModelForTesting(param, self.mesh)
+        self.vars = pybamm.Variables(model)
+        self.mesh = self.mesh
+        y = np.ones_like(self.mesh.x.centres)
+        self.vars.update(self.mesh.time, y)
 
     def tearDown(self):
-        del self.param
-        del self.mesh
         del self.vars
+        del self.mesh
 
     def test_variables_shapes(self):
-        y = np.ones_like(self.mesh.xc)
-        self.vars.update(self.mesh.time, y)
-        self.assertEqual(self.vars.c.shape, self.mesh.xc.shape)
-        self.assertEqual(self.vars.cn.shape, self.mesh.xcn.shape)
-        self.assertEqual(self.vars.cs.shape, self.mesh.xcs.shape)
-        self.assertEqual(self.vars.cp.shape, self.mesh.xcp.shape)
+        # Test
+        self.assertEqual(self.vars.c.shape, self.mesh.x.centres.shape)
+        self.assertEqual(self.vars.cn.shape, self.mesh.xn.centres.shape)
+        self.assertEqual(self.vars.cs.shape, self.mesh.xs.centres.shape)
+        self.assertEqual(self.vars.cp.shape, self.mesh.xp.centres.shape)
+
+    def test_variables_neg_pos(self):
+        np.testing.assert_array_equal(self.vars.cn, self.vars.neg["c"])
+        np.testing.assert_array_equal(self.vars.cp, self.vars.pos["c"])
 
     def test_variables_average_basic(self):
-        y = np.ones_like(self.mesh.xc)
-        self.vars.update(self.mesh.time, y)
         self.vars.average()
         self.assertAlmostEqual(self.vars.c_avg, 1.0, places=10)
         self.assertAlmostEqual(self.vars.cn_avg, 1.0, places=10)
@@ -43,23 +52,31 @@ class TestVariables(unittest.TestCase):
         self.assertAlmostEqual(self.vars.cp_avg, 1.0, places=10)
 
     def test_variables_average_convergence(self):
+        # Set up
+        param = pybamm.Parameters()
+
         ns = [50, 100, 200]
         err = [0] * len(ns)
         errn = [0] * len(ns)
         errs = [0] * len(ns)
         errp = [0] * len(ns)
         for i, n in enumerate(ns):
-            mesh = pybamm.Mesh(self.param, n)
-            y = mesh.xc ** 2
-            vars = pybamm.Variables(self.model, mesh)
+            # Set up
+            mesh = pybamm.Mesh(param, n)
+            model = ModelForTesting(param, mesh)
+            y = mesh.x.centres ** 2
+            vars = pybamm.Variables(model)
             vars.update(mesh.time, y)
             vars.average()
+
+            # Exact solution
+            ln, ls, lp = [param.geometric.__dict__[l] for l in ["ln", "ls", "lp"]]
             c_avg_exact = 1 / 3
-            cn_avg_exact = self.param.ln ** 2 / 3
-            cs_avg_exact = ((1 - self.param.lp) ** 3 - self.param.ln ** 3) / (
-                3 * self.param.ls
-            )
-            cp_avg_exact = (1 - (1 - self.param.lp) ** 3) / (3 * self.param.lp)
+            cn_avg_exact = ln ** 2 / 3
+            cs_avg_exact = ((1 - lp) ** 3 - ln ** 3) / (3 * ls)
+            cp_avg_exact = (1 - (1 - lp) ** 3) / (3 * lp)
+
+            # Compare
             err[i] = norm(vars.c_avg - c_avg_exact) / norm(c_avg_exact)
             errn[i] = norm(vars.cn_avg - cn_avg_exact) / norm(cn_avg_exact)
             errs[i] = norm(vars.cs_avg - cs_avg_exact) / norm(cs_avg_exact)
