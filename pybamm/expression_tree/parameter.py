cba44c8067f513d469a69e2f17c0e38b71d5630a
Martin Robinson <martinjrobins@gmail.com>
2018-12-17 14:08:15 +0000

    #16 doc Parameter

--- pybamm/expression_tree/parameter.py
+++ pybamm/expression_tree/parameter.py
@@ -7,7 +7,22 @@ import pybamm
 
 
 class Parameter(pybamm.Domain, pybamm.Symbol):
-    def __init__(self, name, family=None, domain=[], parent=None):
+    """A node in the expression tree representing a parameter
+
+       This node will be replaced by a :class:`.Scalar` node by :class`.Parameter`
+
+       A variable has a list of domains (text) that it is valid over
+       (inherits from :class:`.Domain`)
+
+    """
+
+    def __init__(self, name, family=None, domain=[]):
+        """
+        Args:
+            name (str): name of the node
+            domain (iterable of str): list of domains the parameter is valid
+                over
+        """
         super().__init__(name, parent=parent, domain=domain)
         self.family = family
 
