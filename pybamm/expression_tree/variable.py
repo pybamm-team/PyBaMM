1156487c01f9622af8a956f58ee2ebd91a4160c5
Valentin Sulzer <valentinsulzer@hotmail.com>
2018-12-17 15:17:51 +0000

    #20 add __eq__ and __hash__ for variable class - this will allow recognition of y_slices

--- pybamm/expression_tree/variable.py
+++ pybamm/expression_tree/variable.py
@@ -9,3 +9,13 @@ import pybamm
 class Variable(pybamm.Domain, pybamm.Symbol):
     def __init__(self, name, domain=[], parent=None):
         super().__init__(name, parent=parent, domain=domain)
+
+    def __eq__(self, other):
+        return (
+            self.__class__ == other.__class__
+            and self.name == other.name
+            and self.domain == other.domain
+        )
+
+    def __hash__(self):
+        return hash((self.__class__, self.name, tuple(self.domain)))
