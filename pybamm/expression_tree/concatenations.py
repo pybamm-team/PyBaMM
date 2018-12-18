a72dc813084ae433388950fff6f424d556036fb4
Valentin Sulzer <valentinsulzer@hotmail.com>
2018-12-18 20:21:08 +0100

    #20 remove parent from concatenations

--- pybamm/expression_tree/concatenations.py
+++ pybamm/expression_tree/concatenations.py
@@ -9,22 +9,22 @@ import numpy as np
 
 
 class Concatenation(pybamm.Symbol):
-    def __init__(self, *children, name=None, parent=None):
-        super().__init__(name, children, parent)
+    def __init__(self, *children, name=None):
+        super().__init__(name, children)
 
-    def evaluate(self, y):
+    def evaluate(self, t, y):
         raise NotImplementedError
 
 
 class NumpyConcatenation(Concatenation):
-    def __init__(self, *children, name=None, parent=None):
+    def __init__(self, *children, name=None):
         # Convert any Scalar symbols in children to Vector for concatenation
         children = list(children)
         for i, child in enumerate(children):
             if isinstance(child, pybamm.Scalar):
                 children[i] = pybamm.Vector(np.array([child.value]))
 
-        super().__init__(*children, name=name, parent=parent)
+        super().__init__(*children, name=name)
 
-    def evaluate(self, y):
-        return np.concatenate([child.evaluate(y) for child in self.children])
+    def evaluate(self, t, y):
+        return np.concatenate([child.evaluate(t, y) for child in self.children])
