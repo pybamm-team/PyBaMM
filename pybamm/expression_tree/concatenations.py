c3470df8fca45e75186287bea8898062edb40bc9
Valentin Sulzer <valentinsulzer@hotmail.com>
2018-12-20 10:55:46 +0100

    #20 doc concatenation

--- pybamm/expression_tree/concatenations.py
+++ pybamm/expression_tree/concatenations.py
@@ -9,16 +9,40 @@ import numpy as np
 
 
 class Concatenation(pybamm.Symbol):
+    """A node in the expression tree representing a concatenation of symbols
+
+    **Extends**: :class:`pybamm.Symbol`
+
+    Parameters
+    ----------
+    children : iterable of :class:`pybamm.Symbol`
+        The symbols to concatenate
+
+    """
+
     def __init__(self, *children, name=None):
         if name is None:
             name = "concatenation"
         super().__init__(name, children)
 
     def evaluate(self, t, y):
+        """ See :meth:`pybamm.Symbol.evaluate()`. """
         raise NotImplementedError
 
 
 class NumpyConcatenation(Concatenation):
+    """A node in the expression tree representing a concatenation of symbols.
+    Upon evaluation, symbols are concatenated using numpy concatenation.
+
+    **Extends**: :class:`pybamm.Concatenation`
+
+    Parameters
+    ----------
+    children : iterable of :class:`pybamm.Symbol`
+        The symbols to concatenate
+
+    """
+
     def __init__(self, *children):
         # Convert any Scalar symbols in children to Vector for concatenation
         children = list(children)
@@ -29,4 +53,5 @@ class NumpyConcatenation(Concatenation):
         super().__init__(*children, name="numpy concatenation")
 
     def evaluate(self, t, y):
+        """ See :meth:`pybamm.Symbol.evaluate()`. """
         return np.concatenate([child.evaluate(t, y) for child in self.children])
