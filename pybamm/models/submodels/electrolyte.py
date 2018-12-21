db21821aa5fc1a054c097ca4965e624c4d528527
marquis <marquis@maths.ox.ac.uk>
2018-12-21 16:26:23 +0000

    Refactored StefanMaxwellDiffusion to inherit from BaseModel and generates an expression tree from the equations.
    Still unsure as to how to test this though

--- pybamm/models/submodels/electrolyte.py
+++ pybamm/models/submodels/electrolyte.py
@@ -3,180 +3,62 @@
 #
 from __future__ import absolute_import, division
 from __future__ import print_function, unicode_literals
+import pybamm
 
 import numpy as np
 
 
-class StefanMaxwellDiffusion(object):
-    """Equations for the electrolyte."""
+class StefanMaxwellDiffusion(pybamm.BaseModel):
+    """A class that generates the expression tree for Stefan-Maxwell Diffusion in the
+    electrolyte.
 
-    def __init__(self, subparam, suboperators, submesh, tests):
-        """
-        Assign simulation-specific objects as attributes.
+    Attributes
+    ----------
 
-        Parameters
-        ----------
-        subparam : :class:`pybamm.BaseParameterValues.Parameters` subclass instance
-            The parameters of the simulation
-        suboperators : :class:`pybamm.operators.Operators` subclass instance
-            The spatial operators.
-        submesh : :class:`pybamm.mesh.Mesh` subclass instance
-            The spatial and temporal discretisation.
-        tests : dict
-            A dictionary for testing the convergence of the numerical solution:
-                * {} (default): We are not running in test mode, use built-ins.
-                * {'inits': dict of initial conditions,
-                   'bcs': dict of boundary conditions,
-                   'sources': dict of source terms
-                   }: To be used for testing convergence to an exact solution.
-        """
-        # Apply attributes
-        self.subparam = subparam
-        self.suboperators = suboperators
-        self.submesh = submesh
-        self.tests = tests
+    rhs: dict
+        A dictionary that maps expressions (variables) to expressions that represent
+        the rhs
+    initial_conditions: dict
+        A dictionary that maps expressions (variables) to expressions that represent
+        the initial conditions
+    boundary_conditions: dict
+        A dictionary that maps expressions (variables) to expressions that represent
+        the boundary conditions
 
-    def initial_conditions(self):
-        """Calculates initial conditions for variables in the electrolyte.
+    *Extends:* :class:`BaseModel`
+    """
 
-        Returns
-        -------
-        array_like
-            The initial conditions
-        """
-        if not self.tests:
-            return self.subparam.c0 * np.ones_like(self.submesh.nodes)
-        else:
-            return self.tests["inits"]["concentration"]
+    def __init__(self):
+        super().__init__()
 
-    def pdes_rhs(self, vars):
-        """Conservation of cations.
+        electrolyte_domain = [
+            "negative electrode",
+            "separator",
+            "positive " "electrode",
+        ]
 
-        Parameters
-        ----------
-        vars : array_like, shape (n,)
-            The variables of the simulation
-        flux_bcs : 2-tuple of array_like, shape (1,)
-            Flux at the boundaries (Neumann BC).
+        electrode_domain = ["negative electrode", "positive electrode"]
 
-        Returns
-        -------
-        dcdt : array_like, shape (n,)
-            The time derivative of c.
+        c_e = pybamm.Variable("c_e", domain=electrolyte_domain)
 
-        """
-        if not self.tests:
-            flux_bcs = self.bcs()
-            j = vars.j
-        else:
-            flux_bcs = self.tests["bcs"](vars.t)["concentration"]
-            j = self.tests["sources"](vars.t)["concentration"]
+        # Should these also be variables?
+        N_e = pybamm.Variable("N_e", domain=electrolyte_domain)
+        G = pybamm.Variable("G", domain=electrode_domain)
 
-        # Calculate internal flux
-        N_internal = -self.suboperators.grad(vars.c)
+        delta = pybamm.Parameter("delta")
+        epsilon = pybamm.Parameter("epsilon", domain=electrolyte_domain)
+        b = pybamm.Parameter("b")
+        D_e = pybamm.Parameter("D_e")
+        nu = pybamm.Parameter("nu")
+        t_plus = pybamm.Parameter("t_plus")
 
-        # Add boundary conditions (Neumann)
-        flux_bc_left, flux_bc_right = flux_bcs
-        N = np.concatenate([flux_bc_left, N_internal, flux_bc_right])
+        c_e0 = pybamm.Parameter("c_e0")  # Should this be a parameter?
 
-        # Calculate time derivative
-        dcdt = -self.suboperators.div(N) + self.subparam.s * j
-
-        return dcdt
-
-    def bcs(self):
-        """Flux boundary conditions for the cation conservation equation.
-
-        Returns
-        -------
-        2-tuple of array_like, shape(1,)
-            The boundary conditions.
-
-        """
-        flux_bc_left = np.array([0])
-        flux_bc_right = np.array([0])
-        return (flux_bc_left, flux_bc_right)
-
-    # def current_conservation(self, domain, c, e, j, current_bcs):
-    #     """The 1D diffusion equation.
-    #
-    #     Parameters
-    #     ----------
-    #     subparam : pybamm.BaseParameterValues.Parameter() instance
-    #         The parameters of the simulation
-    #     variables : 2-tuple (c, e) of array_like, shape (n,)
-    #         The concentration, and potential difference.
-    #     operators : pybamm.operators.Operators() instance
-    #         The spatial operators.
-    #     j : array_like, shape (n,)
-    #         Interfacial current density.
-    #     current_bcs : 2-tuple of array_like, shape (1,)
-    #         Flux at the boundaries (Neumann BC).
-    #
-    #     Returns
-    #     -------
-    #     dedt : array_like, shape (n,)
-    #         The time derivative of the potential.
-    #
-    #     """
-    #     # Calculate current density
-    #     i = self.macinnes(domain, c, e, current_bcs)
-    #
-    #     # Calculate time derivative
-    #     if domain == "xcn":
-    #         gamma_dl = self.subparam.gamma_dl_n
-    #     elif domain == "xcp":
-    #         gamma_dl = self.subparam.gamma_dl_p
-    #
-    #     dedt = 1 / gamma_dl * (self.operators[domain].div(i) - j)
-    #
-    #     return dedt
-    #
-    # def macinnes(self, domain, c, e, current_bcs):
-    #     """MacInnes equation for the electrolyte current density.
-    #
-    #     Parameters
-    #     ----------
-    #     domain : string
-    #         The domain in which to calculate the electrolyte current density.
-    #     c : array_like, shape (n,)
-    #         The electrolyte concentration.
-    #     e : array_like, shape (n,)
-    #         The potential difference.
-    #     current_bcs : 2-tuple of array_like, shape (1,)
-    #         Flux at the boundaries (Neumann BC).
-    #
-    #     Returns
-    #     -------
-    #     i : array_like, shape (n+1,)
-    #         The current density.
-    #     """
-    #     operators = self.operators[domain]
-    #     kappa_over_c = 1
-    #     kappa = 1
-    #
-    #     # Calculate inner current
-    #     i_inner = kappa_over_c * operators.grad(c) + kappa * operators.grad(e)
-    #
-    #     # Add boundary conditions
-    #     lbc, rbc = current_bcs
-    #     i = np.concatenate([lbc, i_inner, rbc])
-    #
-    #     return i
-    #
-    # def bcs_current(self, domain, t):
-    #     """Boundary conditions for the current conservation equation.
-    #
-    #     Returns
-    #     -------
-    #     2-tuple of array_like, shape(1,)
-    #         The boundary conditions.
-    #
-    #     """
-    #     if domain == "xcn":
-    #         current_bc_left = np.array([0])
-    #         current_bc_right = np.array([self.subparam.icell(t)])
-    #     elif domain == "xcp":
-    #         current_bc_left = np.array([self.subparam.icell(t)])
-    #         current_bc_right = np.array([0])
-    #     return (current_bc_left, current_bc_right)
+        # Change expression once Binary operations can cope with None input
+        self.rhs = {
+            c_e: pybamm.Scalar(0)
+            - pybamm.Divergence(N_e) / delta / epsilon
+            + nu * (pybamm.Scalar(1) - t_plus) * G
+        }
+        self.initial_conditions = {c_e: c_e0}
+        self.boundary_conditions = {N_e: {"left": 0, "right": 0}}
