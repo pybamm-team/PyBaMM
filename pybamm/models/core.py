fc594b45dd66f1b0f1cda101ec80483c9287970e
marquis <marquis@maths.ox.ac.uk>
2018-12-21 11:25:54 +0000

    added tests and documentation for BaseModel. The set check setters and getters are functioning properly as well as that model behaves like a dictionary to access rhs

--- pybamm/models/core.py
+++ pybamm/models/core.py
@@ -8,17 +8,18 @@ from __future__ import print_function, unicode_literals
 class BaseModel(object):
     """Base model class for other models to extend.
 
-    Parameters
+    Attributes
     ----------
 
-    rhs: dict
-        A dictionary that maps expressions (variables) to
-        A dictionary for testing the convergence of the numerical solution:
-            * {} (default): We are not running in test mode, use built-ins.
-            * {'inits': dict of initial conditions,
-               'bcs': dict of boundary conditions,
-               'sources': dict of source terms
-               }: To be used for testing convergence to an exact solution.
+    _rhs: dict
+        A dictionary that maps expressions (variables) to expressions that represent
+        the rhs
+    _initial_conditions: dict
+        A dictionary that maps expressions (variables) to expressions that represent
+        the initial conditions
+    _boundary_conditions: dict
+        A dictionary that maps expressions (variables) to expressions that represent
+        the boundary conditions
     """
 
     def __init__(self):
@@ -47,7 +48,7 @@ class BaseModel(object):
         return self._boundary_conditions
 
     @boundary_conditions.setter
-    def initial_conditions(self, boundary_conditions):
+    def boundary_conditions(self, boundary_conditions):
         self._boundary_conditions = boundary_conditions
 
     def __getitem__(self, key):
--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -7,26 +7,28 @@ import unittest
 
 
 class TestBaseModel(unittest.TestCase):
-    def test_base_model_basic(self):
+
+    def test_rhs_set_get(self):
         model = pybamm.BaseModel()
-        # __str__
-        self.assertEqual(str(model), "Base Model")
+        rhs = {"c": "alpha", "d": "beta"}
+        model.rhs = rhs
+        self.assertEqual(rhs, model.rhs)
 
-        # set simulation
-        model.set_simulation(1, 2, 3)
-        self.assertEqual(model.param, 1)
-        self.assertEqual(model.operators, 2)
-        self.assertEqual(model.mesh, 3)
+    def test_initial_conditions_set_get(self):
+        model = pybamm.BaseModel()
+        initial_conditions = {"c0": "gamma", "d0": "delta"}
+        model.initial_conditions = initial_conditions
+        self.assertEqual(initial_conditions, model.initial_conditions)
 
-        # NotImplementedErrors
-        with self.assertRaises(NotImplementedError):
-            model.submodels
+    def test_boundary_conditions_set_get(self):
+        model = pybamm.BaseModel()
+        boundary_conditions = {"c_left": "epsilon", "c_right": "eta"}
+        model.boundary_conditions = boundary_conditions
+        self.assertEqual(boundary_conditions, model.boundary_conditions)
 
-
-if __name__ == "__main__":
-    print("Add -v for more debug output")
-    import sys
-
-    if "-v" in sys.argv:
-        debug = True
-    unittest.main()
+    def test_model_dict_behaviour(self):
+        model = pybamm.BaseModel()
+        rhs = {"c": "alpha", "d": "beta"}
+        model.rhs = rhs
+        self.assertEqual(model["c"], rhs["c"])
+        self.assertEqual(model["c"], model.rhs["c"])
