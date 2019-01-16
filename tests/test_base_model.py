102ebb25f3ade09c06955ceeb595ec5f721c8ce2
marquis <marquis@maths.ox.ac.uk>
2019-01-16 16:29:33 +0000

    merging from master

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -7,16 +7,39 @@ import unittest
 
 
 class TestBaseModel(unittest.TestCase):
-
     def test_rhs_set_get(self):
         model = pybamm.BaseModel()
-        rhs = {"c": "alpha", "d": "beta"}
+        rhs = {
+            pybamm.Symbol("c"): pybamm.Symbol("alpha"),
+            pybamm.Symbol("d"): pybamm.Symbol("beta"),
+        }
         model.rhs = rhs
         self.assertEqual(rhs, model.rhs)
+        # test domains
+        rhs = {
+            pybamm.Symbol("c", domain=["negative electrode"]): pybamm.Symbol(
+                "alpha", domain=["negative electrode"]
+            ),
+            pybamm.Symbol("d", domain=["positive electrode"]): pybamm.Symbol(
+                "beta", domain=["positive electrode"]
+            ),
+        }
+        model.rhs = rhs
+        self.assertEqual(rhs, model.rhs)
+        # non-matching domains should fail
+        with self.assertRaises(pybamm.DomainError):
+            model.rhs = {
+                pybamm.Symbol("c", domain=["positive electrode"]): pybamm.Symbol(
+                    "alpha", domain=["negative electrode"]
+                )
+            }
 
     def test_initial_conditions_set_get(self):
         model = pybamm.BaseModel()
-        initial_conditions = {"c0": "gamma", "d0": "delta"}
+        initial_conditions = {
+            pybamm.Symbol("c0"): pybamm.Symbol("gamma"),
+            pybamm.Symbol("d0"): pybamm.Symbol("delta"),
+        }
         model.initial_conditions = initial_conditions
         self.assertEqual(initial_conditions, model.initial_conditions)
 
@@ -26,9 +49,25 @@ class TestBaseModel(unittest.TestCase):
         model.boundary_conditions = boundary_conditions
         self.assertEqual(boundary_conditions, model.boundary_conditions)
 
+    def test_variables_set_get(self):
+        model = pybamm.BaseModel()
+        variables = {"c": "alpha", "d": "beta"}
+        model.variables = variables
+        self.assertEqual(variables, model.variables)
+
     def test_model_dict_behaviour(self):
         model = pybamm.BaseModel()
-        rhs = {"c": "alpha", "d": "beta"}
+        key = pybamm.Symbol("c")
+        rhs = {key: pybamm.Symbol("alpha")}
         model.rhs = rhs
-        self.assertEqual(model["c"], rhs["c"])
-        self.assertEqual(model["c"], model.rhs["c"])
+        self.assertEqual(model[key], rhs[key])
+        self.assertEqual(model[key], model.rhs[key])
+
+
+if __name__ == "__main__":
+    print("Add -v for more debug output")
+    import sys
+
+    if "-v" in sys.argv:
+        debug = True
+    unittest.main()
