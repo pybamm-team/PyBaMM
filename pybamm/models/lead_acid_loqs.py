cf4885a23c81ce7dc36b231f0141d2c6420a53ff
Valentin Sulzer <valentinsulzer@hotmail.com>
2019-02-03 01:20:32 +0000

    #103 clean up loqs a bit (missing functions)

--- pybamm/models/lead_acid_loqs.py
+++ pybamm/models/lead_acid_loqs.py
@@ -21,6 +21,10 @@ class LeadAcidLOQS(pybamm.BaseModel):
     boundary_conditions: dict
         A dictionary that maps expressions (variables) to expressions that represent
         the boundary conditions
+    variables: dict
+        A dictionary that maps strings to expressions that represent
+        the useful variables
+
     """
 
     def __init__(self):
@@ -29,26 +33,30 @@ class LeadAcidLOQS(pybamm.BaseModel):
         epsn = pybamm.Variable("epsn", domain=["time only"])
         epss = pybamm.Variable("epss", domain=["time only"])
         epsp = pybamm.Variable("epsp", domain=["time only"])
-        jn = pybamm.interface.HomogeneousReactions()
-        jp = pybamm.interface.HomogeneousReactions()
 
         # Parameters
         ln = pybamm.standard_parameters.ln
         ls = pybamm.standard_parameters.ls
         lp = pybamm.standard_parameters.lp
-        sn = pybamm.standard_parameters.sn
-        sp = pybamm.standard_parameters.sp
-        beta_surf_n = pybamm.standard_parameters.beta_surf_n
-        beta_surf_p = pybamm.standard_parameters.beta_surf_p
+        sn = pybamm.standard_parameters_lead_acid.sn
+        sp = pybamm.standard_parameters_lead_acid.sp
+        beta_surf_n = pybamm.standard_parameters_lead_acid.beta_surf_n
+        beta_surf_p = pybamm.standard_parameters_lead_acid.beta_surf_p
+        iota_ref_n = pybamm.standard_parameters_lead_acid.iota_ref_n
+        iota_ref_p = pybamm.standard_parameters_lead_acid.iota_ref_p
+        U_Pb = pybamm.standard_parameters_lead_acid.U_Pb_ref
+        U_PbO2 = pybamm.standard_parameters_lead_acid.U_PbO2_ref
         # Initial conditions
-        cinit = pybamm.standard_parameters.cinit
-        epsninit = pybamm.standard_parameters.epsninit
-        epssinit = pybamm.standard_parameters.epssinit
-        epspinit = pybamm.standard_parameters.epspinit
+        c_init = pybamm.standard_parameters_lead_acid.c_init
+        epsn_init = pybamm.standard_parameters_lead_acid.epsn_init
+        epss_init = pybamm.standard_parameters_lead_acid.epss_init
+        epsp_init = pybamm.standard_parameters_lead_acid.epsp_init
 
         # ODEs
-        depsndt = -beta_surf_n * j
-        depssdt = -beta_surf_p * j
+        jn = pybamm.standard_parameters.icell / ln
+        jp = -pybamm.standard_parameters.icell / lp
+        depsndt = -beta_surf_n * jn
+        depspdt = -beta_surf_p * jp
         dcdt = (
             1
             / (ln * epsn + ls * epss + lp * epsp)
@@ -57,13 +65,17 @@ class LeadAcidLOQS(pybamm.BaseModel):
         self.rhs = {c: dcdt, epsn: depsndt, epss: pybamm.Scalar(0), epsp: depspdt}
         # Initial conditions
         self.initial_conditions = {
-            c: cinit,
-            epsn: epsninit,
-            epss: epssinit,
-            epsp: epspinit,
+            c: c_init,
+            epsn: epsn_init,
+            epss: epss_init,
+            epsp: epsp_init,
         }
         # ODE model -> no boundary conditions
         self.boundary_conditions = {}
 
         # Variables
+        eps = pybamm.concatenate(epsn, epss, epsp)
+        Phisn = pybamm.Scalar(0)
+        Phi = -U_Pb_ref - jn / (2 * iota_ref_n * c)
+        V = -U_PbO2_ref - jp / (2 * iota_ref_p * c)
         self.variables = {"c": c, "eps": eps, "Phi": Phi, "Phis": Phis, "V": V}
