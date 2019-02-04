cd1226ab54d3a772721977d97178a58d8d1e46d4
Valentin Sulzer <valentinsulzer@hotmail.com>
2019-02-04 21:39:34 +0000

    #68 add suggested unit test for setting parameters

---
+++ input/parameters/lithium-ion/example_function.py
@@ -0,0 +1,11 @@
+#
+# Example function, for testing
+#
+from __future__ import absolute_import, division
+from __future__ import print_function, unicode_literals
+
+import numpy as np
+
+
+def example_function(x):
+    return x * np.exp(x - 1)
