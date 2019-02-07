6281dc55a62bcb0266cbccc97091626877426a2a
Scottmar93 <marquis@maths.ox.ac.uk>
2019-02-07 15:13:37 +0000

    Added test for custom geometry

---
+++ tests/test_geometry/test_geometry.py
@@ -0,0 +1,20 @@
+#
+# Tests for the base model class
+#
+import pybamm
+
+
+class TestGeometry:
+    def test_add_custom_geometry(self):
+        custom_geometry = {
+            "negative electrode": {
+                "x": {"min": pybamm.Scalar(1), "max": pybamm.Scalar(2)}
+            }
+        }
+        geometry = pybamm.Geometry(custom_geometry)
+
+        self.assertEqual(
+            geometry["negative electrode"], custom_geometry["negative electrode"]
+        )
+
+    # process geometry test conducted within TestParameterValues
