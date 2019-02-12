77dfb10a211ac61f63cb01ef1af675adb17edec7
Scottmar93 <marquis@maths.ox.ac.uk>
2019-02-12 12:06:28 +0000

    Changed geometry keys to indepedent variables and added tests for each type of geometry

--- tests/test_geometry/test_geometry.py
+++ tests/test_geometry/test_geometry.py
@@ -4,17 +4,58 @@
 import pybamm
 
 
-class TestGeometry:
-    def test_add_custom_geometry(self):
+class StandardGeometryTests:
+    def test_add_custom_geometry(self, geometry):
+
+        whole_cell = ["negative electrode", "separator", "positive electrode"]
+        x = pybamm.IndependentVariable("x", whole_cell)
         custom_geometry = {
             "negative electrode": {
-                "x": {"min": pybamm.Scalar(1), "max": pybamm.Scalar(2)}
+                x: {"min": pybamm.Scalar(1), "max": pybamm.Scalar(2)}
             }
         }
-        geometry = pybamm.Geometry1DMacro(custom_geometry)
 
         self.assertEqual(
             geometry["negative electrode"], custom_geometry["negative electrode"]
         )
 
-    # process geometry test conducted within TestParameterValues
+    def test_geometry_keys(self, geometry):
+        # checks that keys are pybamm.IndependentVariables
+        keys = list(geometry.keys())
+        all(self.assertIsInstance(key, pybamm.IndependentVariable) for key in keys)
+
+
+class TestGeometry1DMacro:
+    def test_add_custom_geometry(self):
+        geometry = pybamm.Geometry1DMacro()
+        tests = StandardGeometryTests()
+        tests.test_add_custom_geometry(geometry)
+
+    def test_geometry_keys(self):
+        geometry = pybamm.Geometry1DMacro()
+        tests = StandardGeometryTests(geometry)
+        tests.test_add_custom_geometry(geometry)
+
+
+class TestGeometry1DMicro:
+    def test_add_custom_geometry(self):
+        geometry = pybamm.Geometry1DMicro()
+        tests = StandardGeometryTests()
+        tests.test_add_custom_geometry(geometry)
+
+    def test_geometry_keys(self):
+        geometry = pybamm.Geometry1DMicro()
+        tests = StandardGeometryTests(geometry)
+        tests.test_add_custom_geometry(geometry)
+
+
+class TestGeometry3DMacro:
+    def test_add_custom_geometry(self):
+        geometry = pybamm.Geometry3DMacro()
+        tests = StandardGeometryTests()
+        tests.test_add_custom_geometry(geometry)
+
+    def test_geometry_keys(self):
+        geometry = pybamm.Geometry3DMacro()
+        tests = StandardGeometryTests(geometry)
+        tests.test_add_custom_geometry(geometry)
