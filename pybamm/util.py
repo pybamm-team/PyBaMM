fad55044f120972f210a02c25cc4daa15e10bf78
Fergus Cooper <fcooper8472@gmail.com>
2019-02-14 16:32:18 +0000

    #68 Finish implementing load_function

--- pybamm/util.py
+++ pybamm/util.py
@@ -7,10 +7,12 @@
 from __future__ import absolute_import, division
 from __future__ import print_function, unicode_literals
 
-import timeit
 import cProfile
-import pstats
+import importlib
 import os
+import pstats
+import sys
+import timeit
 
 
 class Timer(object):
@@ -84,6 +86,8 @@ def profile(code, sort="cumulative", num=30):
 def load_function(filename):
     """
     Load a python function from a file "function_name.py" called "function_name".
+    The filename might either be an absolute path, in which case that specific file will
+    be used, or the file will be searched for relative to PyBaMM root.
 
     Arguments
     ---------
@@ -99,6 +103,52 @@ def load_function(filename):
     if not filename.endswith('.py'):
         raise ValueError("Expected filename.py, but got {}".format(filename))
 
-    func_to_return = abs
+    # If it's an absolute path, find that exact file
+    if os.path.isabs(filename):
+        if not os.path.isfile(filename):
+            raise ValueError(
+                "{} is an absolute path, but the file is not found".format(filename))
 
-    return func_to_return
+        valid_filename = filename
+
+    # Else, search in the whole PyBaMM directory for matches
+    else:
+        search_path = os.path.commonpath([pth for pth in sys.path if 'PyBaMM' in pth])
+        head, tail = os.path.split(filename)
+
+        matching_files = []
+
+        for root, _, files in os.walk(search_path):
+            for file in files:
+                if file == tail:
+                    full_path = os.path.join(root, file)
+                    if full_path.endswith(filename):
+                        matching_files.append(full_path)
+
+        if len(matching_files) == 0:
+            raise ValueError(
+                "{} cannot be found in the PyBaMM directory".format(filename))
+        elif len(matching_files) > 1:
+            raise ValueError(
+                "{} found multiple times in the PyBaMM directory".format(filename))
+
+        valid_filename = matching_files[0]
+
+    # Now: we have some /path/to/valid/filename.py
+    # Add "/path/to/vaid" to the python path, and load the module "filename".
+    # Then, check "filename" module contains "filename" function.  If it does, return
+    # that function object, or raise an exception
+
+    valid_path, valid_leaf = os.path.split(valid_filename)
+    sys.path.append(valid_path)
+
+    # Load the module, which must be the leaf of filename, minus the .py extension
+    valid_module = valid_leaf.replace('.py', '')
+    module_object = importlib.import_module(valid_module)
+
+    # Check that a function of the same name exists in the loaded module
+    if valid_module not in dir(module_object):
+        raise ValueError(
+            "No function {} found in module {}".format(valid_module, valid_module))
+
+    return getattr(module_object, valid_module)
--- tests/test_util.py
+++ tests/test_util.py
@@ -4,6 +4,7 @@
 # The code in this file is adapted from Pints
 # (see https://github.com/pints-team/pints)
 #
+import os
 import pybamm
 import unittest
 
@@ -14,14 +15,42 @@ class TestUtil(unittest.TestCase):
     """
 
     def test_load_function(self):
-
-        # Currently assumes it will receive a filename ending in '.py'
+        # Test filename ends in '.py'
         with self.assertRaisesRegex(ValueError,
                                     'Expected filename.py, but got doesnotendindotpy'):
             pybamm.load_function('doesnotendindotpy')
 
+        # Test exception if absolute file not found
+        with self.assertRaisesRegex(ValueError,
+                                    'is an absolute path, but the file is not found'):
+            nonexistent_abs_file = os.path.join(os.getcwd(), 'i_dont_exist.py')
+            pybamm.load_function(nonexistent_abs_file)
 
-        # self.assertEqual(my_func(-3), 3)
+        # Test exception if relative file not found
+        with self.assertRaisesRegex(ValueError,
+                                    'cannot be found in the PyBaMM directory'):
+            pybamm.load_function('i_dont_exist.py')
+
+        # Test exception if relative file found more than once
+        with self.assertRaisesRegex(ValueError,
+                                    'found multiple times in the PyBaMM directory'):
+            pybamm.load_function('__init__.py')
+
+        # Test exception if no matching function found in module
+        with self.assertRaisesRegex(ValueError,
+                                    'No function .+ found in module .+'):
+            pybamm.load_function('process_symbol_bad_function.py')
+
+        # Test function load with absolute path
+        abs_test_path = os.path.join(os.getcwd(), 'tests', 'test_parameters', 'data',
+                                     'process_symbol_test_function.py')
+        self.assertTrue(os.path.isfile(abs_test_path))
+        func = pybamm.load_function(abs_test_path)
+        self.assertEqual(func(2), 246)
+
+        # Test function load with relative path
+        func = pybamm.load_function('process_symbol_test_function.py')
+        self.assertEqual(func(3), 369)
 
 
 if __name__ == "__main__":
