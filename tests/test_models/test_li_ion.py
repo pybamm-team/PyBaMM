0ad26eebe1e6d3666bcc665bc6abc8776d9d1909
Robert <timms@maths.ax.ac.uk>
2019-02-18 15:06:56 +0000

    added file for li-ion tests

---
+++ tests/test_models/test_li_ion.py
@@ -0,0 +1,18 @@
+#
+# Tests for the li-ion models
+#
+from __future__ import absolute_import, division
+from __future__ import print_function, unicode_literals
+import pybamm
+import tests
+
+import unittest
+import numpy as np
+
+
+class TestLiIonSPM(unittest.TestCase):
+    def test_basic_processing(self):
+        model = pybamm.li_ion.SPM()
+        modeltest = tests.StandardModelTest(model)
+
+        modeltest.test_all()
