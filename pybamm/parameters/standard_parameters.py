6a80c35278dfbcb332e09ca34a51e17c708cded7
rtimms <user@nb-timms-01.maths.ox.ac.uk>
2019-02-18 10:47:30 +0000

    Updated parameters to include params for SPM, concentration dependent diffusivities, and OCP functions

--- pybamm/parameters/standard_parameters.py
+++ pybamm/parameters/standard_parameters.py
@@ -44,6 +44,8 @@ sigma_n, sigma_p
     Electrical conductivities of the negative and positive electrode
 cn_max, cp_max
     Maximum lithium concentration in the negative and positive electrode
+Dn_typ, Dp_typ
+    Typical diffusivitites in the solid electrode material
 
 Electrochemical Reactions
 --------------------------
@@ -56,6 +58,8 @@ voltage_low_cut, voltage_high_cut
     Low and high voltage cut-offs
 I_typ
     Typical current density
+Phi_typ
+    Typical voltage drop across the cell
 
 Initial Conditions
 -------------------
@@ -67,6 +71,7 @@ cn0_dimensional, cp0_dimensional
 from __future__ import absolute_import, division
 from __future__ import print_function, unicode_literals
 from pybamm import Parameter
+import numpy as np
 
 # --------------------------------------------------------------------------------------
 """Dimensional Parameters"""
@@ -101,6 +106,8 @@ sigma_n = Parameter("sigma_n")  # Conductivity in negative electrode
 sigma_p = Parameter("sigma_p")  # Conductivity in positive electrode
 cn_max = Parameter("cn_max")  # Max concentration in negative electrode
 cp_max = Parameter("cp_max")  # Max concentration in positive electrode
+Dn_typ = Parameter("Dn_typ")  # Typical negative particle diffusivity
+Dp_typ = Parameter("Dp_typ")  # Typical positive particle diffusivity
 
 # Electrochemical Reactions
 m_n = Parameter("m_n")  # Reaction rate in negative electrode
@@ -110,6 +117,7 @@ m_p = Parameter("m_p")  # Reaction rate in positive electrode
 voltage_low_cut = Parameter("voltage_low_cut")  # Lower voltage cut-off
 voltage_high_cut = Parameter("voltage_high_cut")  # Upper voltage cut-off
 I_typ = Parameter("I_typ")  # Typical current density
+Phi_typ = Parameter("Phi_typ")  # Typical voltage drop
 
 # Initial Conditions
 ce0_dimensional = Parameter("ce0")  # Initial li ion concentration in electrolyte
@@ -134,6 +142,21 @@ b = Parameter("b")  # Bruggeman coefficient
 beta_n = a_n * R_n
 beta_p = a_p * R_p
 
+# Discharge timescale
+tau_d = (F * cn_max * Lx) / I_typ
+
+# Particle diffusion timescales
+tau_n = R_n ** 2 / Dn_typ
+tau_p = R_n ** 2 / Dp_typ
+
+# Scaled maximum concentration in positive particle
+# (Note C_hat_n == 1, so not needed)
+C_hat_p = cp_max/cn_max
+
+# Ratio of discharge and solid diffusion timescales
+gamma_n = tau_d / tau_n
+gamma_p = tau_d / tau_p
+
 # Electrolyte Properties
 t_plus = Parameter("t_plus")  # cation transference number
 delta = (Lx ** 2 / De_typ) * (I_typ / (F * cn_max * Lx))
@@ -141,3 +164,53 @@ nu = cn_max / ce_typ
 
 # Initial conditions
 ce0 = ce0_dimensional / ce_typ
+
+# -----------------------------------------------------------------------------
+"""Functions"""
+
+
+def D_n_dim(c):
+    "Dimensional diffusivity in negative particle"
+    return Dn_typ
+
+
+def D_n(c):
+    "Dimensionless negative particle diffusivity"
+    return D_n_dim(c) / Dn_typ
+
+
+def D_p_dim(c):
+    "Dimensional diffusivity in positive particle"
+    return Dp_typ
+
+
+def D_p(c):
+    "Dimensionless positive particle diffusivity"
+    return D_p_dim(c) / Dp_typ
+
+
+def U_n(c):
+    "Dimensionless open circuit potential in the negative electrode"
+    out = (0.194 + 1.5 * np.exp(-120.0 * c)
+           + 0.0351 * np.tanh((c - 0.286) / 0.083)
+           - 0.0045 * np.tanh((c - 0.849) / 0.119)
+           - 0.035 * np.tanh((c - 0.9233) / 0.05)
+           - 0.0147 * np.tanh((c - 0.5) / 0.034)
+           - 0.102 * np.tanh((c - 0.194) / 0.142)
+           - 0.022 * np.tanh((c - 0.9) / 0.0164)
+           - 0.011 * np.tanh((c - 0.124) / 0.0226)
+           + 0.0155 * np.tanh((c - 0.105) / 0.029))
+    return out / Phi_typ
+
+
+def U_p(c):
+    "Dimensionless open circuit potential in the positive electrode"
+    stretch = 1.062
+    sto = stretch * c
+    out = (2.16216 + 0.07645 * np.tanh(30.834 - 54.4806 * sto)
+           + 2.1581 * np.tanh(52.294 - 50.294 * sto)
+           - 0.14169 * np.tanh(11.0923 - 19.8543 * sto)
+           + 0.2051 * np.tanh(1.4684 - 5.4888 * sto)
+           + 0.2531 * np.tanh((-sto + 0.56478) / 0.1316)
+           - 0.02167 * np.tanh((sto - 0.525) / 0.006))
+    return out / Phi_typ
