#110 fix broadcasting, make more restrictive

pybamm/expression_tree/broadcasts.py
@@ -72,6 +72,11 @@ class NumpyBroadcast(Broadcast):
     """
 
     def __init__(self, child, domain, mesh):
+        # Only accept a 'constant' input if it evaluates to a number (i.e. no vectors
+        # and matrices)
+        if child.is_constant() and not child.evaluates_to_number():
+            raise TypeError("cannot Broadcast a constant Vector or Matrix")
+
         super().__init__(child, domain, name="numpy broadcast")
         # determine broadcasting vector size (size 1 if the domain is empty)
         if domain == []:
@@ -84,33 +89,44 @@ class NumpyBroadcast(Broadcast):
         # domain)
         self.broadcasting_vector = np.ones(self.broadcasting_vector_size)
 
+    @property
+    def shape(self):
+        """Update shape based on the shape of the child (either go to )"""
+        if self.children[0].shape == ():
+            return (self.broadcasting_vector_size,)
+        else:
+            return (self.children[0].size, self.broadcasting_vector_size)
+
     def evaluate(self, t=None, y=None):
         """ See :meth:`pybamm.Symbol.evaluate()`. """
         child = self.children[0]
         child_eval = child.evaluate(t, y)
+        # Note that child_eval_size can be different to child.size!!
         try:
             child_eval_size = child_eval.size
         except AttributeError:
             child_eval_size = 0
 
+        if child_eval_size <= 1:
+            return child_eval * self.broadcasting_vector
         if child_eval_size > 1:
-            if child_eval.shape[0] > 1:
+            # Possible shapes for a child with a shape:
+            # (n,) -> (e.g. time-like object) broadcast to (n, broadcasting_size)
+            # (1,m) -> (e.g. state-vector-like object) broadcast to
+            #          (n, broadcasting_size)
+            # (n,1) -> error
+            # (n,m) -> error
+            # (n,m,k,...) -> error
+            if len(child_eval.shape) == 1:
+                # shape (n,)
                 return np.repeat(
                     child_eval[np.newaxis, :], self.broadcasting_vector_size, axis=0
                 )
-            else:
-                return np.repeat(child_eval, self.broadcasting_vector_size, axis=0)
-        #
-        # # if child is a vector, add a dimension for broadcasting
-        # if isinstance(child, pybamm.Vector):
-        # # if child is a state vector, check that it has the right shape and then
-        # # broadcast
-        # elif isinstance(child, pybamm.StateVector):
-        #     assert child_eval.shape[0] == 1, ValueError(
-        #         """child_eval should have shape (1,n), not {}""".format(
-        #             child_eval.shape
-        #         )
-        #     )
-        # # otherwise just do normal multiplication
-        else:
-            return child_eval * self.broadcasting_vector
+            elif len(child_eval.shape) == 2:
+                if child_eval.shape[0] == 1:
+                    # shape (1, m) since size > 1
+                    return np.repeat(child_eval, self.broadcasting_vector_size, axis=0)
+            # All other cases
+            raise ValueError(
+                "cannot broadcast child with shape '{}'".format(child_eval.shape)
+            )

tests/test_expression_tree/test_broadcasts.py
@@ -38,16 +38,23 @@ class TestUnaryOperators(unittest.TestCase):
             broad.evaluate(), 7 * np.ones_like(combined_submeshes.nodes)
         )
         self.assertEqual(broad.domain, whole_cell)
+        self.assertEqual(broad.shape, broad.evaluate().shape)
 
-        # vector
-        vec = pybamm.Vector(np.linspace(0, 1))
-        broad = pybamm.NumpyBroadcast(vec, whole_cell, mesh)
-        np.testing.assert_array_equal(
-            broad.evaluate(),
-            np.repeat(np.linspace(0, 1)[np.newaxis, :], combined_submeshes.npts, axis=0),
-        )
-
+        # time
+        t = 3 * pybamm.t + 4
+        broad = pybamm.NumpyBroadcast(t, whole_cell, mesh)
         self.assertEqual(broad.domain, whole_cell)
+        np.testing.assert_array_equal(
+            broad.evaluate(t=3), 13 * np.ones_like(combined_submeshes.nodes)
+        )
+        self.assertEqual(broad.shape, broad.evaluate(t=3).shape)
+        np.testing.assert_array_equal(
+            broad.evaluate(t=np.linspace(0, 1)),
+            (
+                (3 * np.linspace(0, 1) + 4)[:, np.newaxis]
+                * np.ones_like(combined_submeshes.nodes)
+            ).T,
+        )
 
         # state vector
         state_vec = pybamm.StateVector(slice(1, 2))
@@ -61,9 +68,15 @@ class TestUnaryOperators(unittest.TestCase):
         state_vec = pybamm.StateVector(slice(1, 5))
         broad = pybamm.NumpyBroadcast(state_vec, whole_cell, mesh)
         y = np.vstack([np.linspace(0, 1), np.linspace(0, 2)]).T
-        with self.assertRaises(AssertionError):
+        with self.assertRaisesRegex(ValueError, "cannot broadcast child with shape"):
             broad.evaluate(y=y)
 
+        # vector - not accepted
+        vec = pybamm.Vector(np.ones(5))
+        with self.assertRaisesRegex(
+            TypeError, "cannot Broadcast a constant Vector or Matrix"
+        ):
+            broad = pybamm.NumpyBroadcast(vec, whole_cell, mesh)
 
 
 if __name__ == "__main__":