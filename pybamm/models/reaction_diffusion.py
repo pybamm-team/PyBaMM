d7b4649ebb856e494213fd7a4e700b89fe7583ce
Valentin Sulzer <valentinsulzer@hotmail.com>
2019-02-26 08:59:12 +0000

    #124 change current to a parameter

--- pybamm/models/reaction_diffusion.py
+++ pybamm/models/reaction_diffusion.py
@@ -30,8 +30,7 @@ class ReactionDiffusionModel(pybamm.BaseModel):
     def __init__(self):
         super().__init__()
         # Define current
-        t = pybamm.t
-        current = pybamm.standard_parameters.dimensionless_current(t)
+        current = pybamm.standard_parameters.current_with_time
         # Load reaction flux from submodels
         G = pybamm.interface.homogeneous_reaction(current)
         # Load diffusion model from submodels
