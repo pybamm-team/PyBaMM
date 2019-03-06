43f26682e225d7907e7feb752e954e33364601f8
Valentin Sulzer <valentinsulzer@hotmail.com>
2019-03-06 22:17:17 +0000

    #121 move potential equations to submodel

--- pybamm/models/lead_acid/composite.py
+++ pybamm/models/lead_acid/composite.py
@@ -5,8 +5,6 @@ from __future__ import absolute_import, division
 from __future__ import print_function, unicode_literals
 import pybamm
 
-import numpy as np
-
 
 class Composite(pybamm.LeadAcidBaseModel):
     """Composite model for lead-acid, from [1].
@@ -46,23 +44,21 @@ class Composite(pybamm.LeadAcidBaseModel):
         super().__init__()
 
         # Parameters
-        sp = pybamm.standard_parameters
-        spla = pybamm.standard_parameters_lead_acid
-        # Current function
-        i_cell = sp.current_with_time
+        param = pybamm.standard_parameters
+        param.__dict__.update(pybamm.standard_parameters_lead_acid.__dict__)
 
-        # Get leading-order model
-        loqs_model = pybamm.lead_acid.LOQS()
-        # Get model for the concentration
+        #
+        # Variables
+        #
         # Concentration variables
-        c_n = pybamm.Variable(
+        c_e_n = pybamm.Variable(
             "Negative electrode concentration", domain=["negative electrode"]
         )
-        c_s = pybamm.Variable("Separator concentration", domain=["separator"])
-        c_p = pybamm.Variable(
+        c_e_s = pybamm.Variable("Separator concentration", domain=["separator"])
+        c_e_p = pybamm.Variable(
             "Positive electrode concentration", domain=["positive electrode"]
         )
-        c = pybamm.Concatenation(c_n, c_s, c_p)
+        c_e = pybamm.Concatenation(c_e_n, c_e_s, c_e_p)
         # Porosity variables
         eps_n = pybamm.Variable(
             "Negative electrode porosity", domain=["negative electrode"]
@@ -72,89 +68,25 @@ class Composite(pybamm.LeadAcidBaseModel):
             "Positive electrode porosity", domain=["positive electrode"]
         )
         eps = pybamm.Concatenation(eps_n, eps_s, eps_p)
+
+        #
+        # Submodels
+        #
+        # Leading-order model
+        loqs_model = pybamm.lead_acid.LOQS()
         # Interfacial current density
         whole_cell = ["negative electrode", "separator", "positive electrode"]
         j = pybamm.interface.homogeneous_reaction(whole_cell)
         # Concentration model (reaction diffusion with homogeneous reaction)
         conc_model = pybamm.electrolyte_diffusion.StefanMaxwellWithPorosity(
-            c, eps, j, spla
+            c_e, eps, j, param
         )
-        porosity_model = pybamm.porosity.Standard(eps, j)
+        # Porosity model
+        porosity_model = pybamm.porosity.Standard(eps, j, param)
+        # Electrolyte potential model (solve ODE analytically)
+        electrolyte_potential_model = pybamm.electrolyte_current.FirstOrderPotential(
+            loqs_model, c_e, param
+        )
 
         # Update own model with submodels
-        self.update(loqs_model, conc_model, porosity_model)
-
-        # Extract leading-order variables, taking orphans to remove domains
-        c_0 = loqs_model.variables["Concentration"].orphans[0]
-        eps_0 = loqs_model.variables["Porosity"]
-        eps_0n, eps_0s, eps_0p = [e.orphans[0] for e in eps_0.orphans]
-        eta_0n = loqs_model.variables["Negative electrode overpotential"].orphans[0]
-        eta_0p = loqs_model.variables["Positive electrode overpotential"].orphans[0]
-        Phi_0 = loqs_model.variables["Electrolyte potential"].orphans[0]
-        V_0 = loqs_model.variables["Voltage"].orphans[0]
-
-        # Pre-define functions of leading-order variables
-        chi_0 = spla.chi(c_0)
-        kappa_0n = sp.kappa_e(c_0) * eps_0n ** sp.b
-        kappa_0s = sp.kappa_e(c_0) * eps_0s ** sp.b
-        kappa_0p = sp.kappa_e(c_0) * eps_0p ** sp.b
-        j0_0n = pybamm.Scalar(1)
-        j0_0p = pybamm.Scalar(1)
-        j0_1n = pybamm.Scalar(1)
-        j0_1p = pybamm.Scalar(1)
-        dUPbdc = pybamm.Scalar(1)
-        dUPbO2dc = pybamm.Scalar(1)
-
-        # Independent variables
-        x_n = pybamm.SpatialVariable("x", ["negative electrode"])
-        x_s = pybamm.SpatialVariable("x", ["separator"])
-        x_p = pybamm.SpatialVariable("x", ["positive electrode"])
-
-        # First-order concentration (c = c0 + C_e * c_1)
-        c_1 = (c - c_0) / spla.C_e
-        c_1n = (c_n - c_0) / spla.C_e
-        c_1p = (c_p - c_0) / spla.C_e
-
-        # Potential
-        cbar_1n = pybamm.Scalar(1)  # pybamm.Integral(c_1n, x_n) / sp.l_n
-        j0bar_1n = pybamm.Scalar(1)  # pybamm.Integral(j0_1n, x_n) / sp.l_n
-        An = (
-            j0bar_1n * pybamm.Function(np.tanh, eta_0n) / j0_0n
-            - dUPbdc * cbar_1n
-            - chi_0 / c_0 * cbar_1n
-            + i_cell * sp.l_n / (6 * kappa_0n)
-        )
-
-        Phi_1n = -i_cell * x_n ** 2 / (2 * sp.l_n * kappa_0n)
-        Phi_1s = -i_cell * ((x_s - sp.l_n) / kappa_0s + sp.l_n / (2 * kappa_0n))
-        Phi_1p = -i_cell * (
-            sp.l_n / (2 * kappa_0n)
-            + sp.l_s / (kappa_0s)
-            + (sp.l_p ** 2 - (1 - x_p) ** 2) / (2 * sp.l_p * kappa_0p)
-        )
-        Phi1 = (
-            chi_0 / c_0 * c_1
-            + pybamm.Concatenation(
-                pybamm.Broadcast(Phi_1n, ["negative electrode"]),
-                pybamm.Broadcast(Phi_1s, ["separator"]),
-                pybamm.Broadcast(Phi_1p, ["positive electrode"]),
-            )
-            + An
-        )
-
-        # Voltage
-        cbar_1p = pybamm.Scalar(1)  # pybamm.Integral(c_1p, x_p) / sp.l_p
-        Phibar_1p = pybamm.Scalar(1)  # pybamm.Integral(Phi1, x_p) / sp.l_p
-        j0bar_1p = pybamm.Scalar(1)  # pybamm.Integral(j0_1p, x_p) / sp.l_p
-        V1 = (
-            Phibar_1p
-            + dUPbO2dc * cbar_1p
-            - j0bar_1p * pybamm.Function(np.tanh, eta_0p) / j0_0p
-        )
-
-        # Variables
-        self.variables = {
-            "c": c,
-            "Phi": Phi_0 + spla.C_e * Phi1,
-            "V": V_0 + spla.C_e * V1,
-        }
+        self.update(loqs_model, conc_model, porosity_model, electrolyte_potential_model)
--- pybamm/models/submodels/electrolyte_current.py
+++ pybamm/models/submodels/electrolyte_current.py
@@ -1,50 +1,112 @@
 #
-# Equation classes for the electrolyte
+# Equation classes for the electrolyte current
 #
 from __future__ import absolute_import, division
 from __future__ import print_function, unicode_literals
 import pybamm
 
+import numpy as np
 
-class StefanMaxwell(pybamm.BaseModel):
+
+class FirstOrderPotential(pybamm.BaseModel):
     """A class that generates the expression tree for Stefan-Maxwell Current in the
     electrolyte.
 
     Parameters
     ----------
+    leading_order_model : Model class
+        The leading-order model for the asymptotics
     c_e: :class:`pybamm.Variable`
         A variable representing the concentration of ions in the electrolyte
-    phi_e: :class:`pybamm.Variable`
-        A variable representing the electric potential in the electrolyte
-    G : :class:`pybamm.Symbol`
-        An expression tree that represents the concentration flux at the
-        electrode-electrolyte interface
+    param : parameter class
+        The parameters to use for this submodel
 
     *Extends:* :class:`BaseModel`
     """
 
-    def __init__(self, c_e, phi_e, G):
+    def __init__(self, leading_order_model, c_e, param):
         super().__init__()
 
-        epsilon = pybamm.standard_parameters.epsilon_s  # make issue for spatially
-        # dependent parameters
-        b = pybamm.standard_parameters.b
-        delta = pybamm.standard_parameters.delta
-        nu = pybamm.standard_parameters.nu
-        t_plus = pybamm.standard_parameters.t_plus
-        Lambda = pybamm.standard_parameters.Lambda
-        sigma = pybamm.Scalar(1)  # leave as a constant for now (should be sigma(c_e))
+        # Current function
+        i_cell = param.current_with_time
 
-        i_e = (
-            ((epsilon ** b) / delta / nu)
-            * sigma
-            * (-Lambda * pybamm.grad(phi_e) + 2 * (1 - t_plus) * pybamm.grad(c_e) / c_e)
+        # Extract leading-order variables, taking orphans to remove domains
+        c_e_0 = leading_order_model.variables["Concentration"].orphans[0]
+        eps_0 = leading_order_model.variables["Porosity"]
+        eps_0n, eps_0s, eps_0p = [e.orphans[0] for e in eps_0.orphans]
+        eta_0n = leading_order_model.variables[
+            "Negative electrode overpotential"
+        ].orphans[0]
+        eta_0p = leading_order_model.variables[
+            "Positive electrode overpotential"
+        ].orphans[0]
+        Phi_0 = leading_order_model.variables["Electrolyte potential"].orphans[0]
+        V_0 = leading_order_model.variables["Voltage"].orphans[0]
+
+        # Pre-define functions of leading-order variables
+        chi_0 = param.chi(c_e_0)
+        kappa_0n = param.kappa_e(c_e_0) * eps_0n ** param.b
+        kappa_0s = param.kappa_e(c_e_0) * eps_0s ** param.b
+        kappa_0p = param.kappa_e(c_e_0) * eps_0p ** param.b
+        j0_0n = pybamm.Scalar(1)
+        j0_0p = pybamm.Scalar(1)
+        j0_1n = pybamm.Scalar(1)
+        j0_1p = pybamm.Scalar(1)
+        dUPbdc = pybamm.Scalar(1)
+        dUPbO2dc = pybamm.Scalar(1)
+
+        # Independent variables
+        x_n = pybamm.SpatialVariable("x", ["negative electrode"])
+        x_s = pybamm.SpatialVariable("x", ["separator"])
+        x_p = pybamm.SpatialVariable("x", ["positive electrode"])
+
+        # First-order concentration (c = c_e_0 + C_e * c_e_1)
+        c_e_1 = (c_e - c_e_0) / param.C_e
+        c_e_n = c_e.orphans[0]
+        c_e_1n = (c_e_n - c_e_0) / param.C_e
+        c_e_p = c_e.orphans[2]
+        c_e_1p = (c_e_p - c_e_0) / param.C_e
+
+        # Potential
+        cbar_1n = pybamm.Scalar(1)  # pybamm.Integral(c_e_1n, x_n) / param.l_n
+        j0bar_1n = pybamm.Scalar(1)  # pybamm.Integral(j0_1n, x_n) / param.l_n
+        A_n = (
+            j0bar_1n * pybamm.Function(np.tanh, eta_0n) / j0_0n
+            - dUPbdc * cbar_1n
+            - chi_0 / c_e_0 * cbar_1n
+            + i_cell * param.l_n / (6 * kappa_0n)
         )
 
-        self.rhs = {}
-        self.algebraic = {phi_e: pybamm.grad(i_e) - G}
-        self.initial_conditions = {}
-        self.boundary_conditions = {
-            i_e: {"left": pybamm.Scalar(0), "right": pybamm.Scalar(0)}
+        Phi_1n = -i_cell * x_n ** 2 / (2 * param.l_n * kappa_0n)
+        Phi_1s = -i_cell * ((x_s - param.l_n) / kappa_0s + param.l_n / (2 * kappa_0n))
+        Phi_1p = -i_cell * (
+            param.l_n / (2 * kappa_0n)
+            + param.l_s / (kappa_0s)
+            + (param.l_p ** 2 - (1 - x_p) ** 2) / (2 * param.l_p * kappa_0p)
+        )
+        Phi_1 = (
+            chi_0 / c_e_0 * c_e_1
+            + pybamm.Concatenation(
+                pybamm.Broadcast(Phi_1n, ["negative electrode"]),
+                pybamm.Broadcast(Phi_1s, ["separator"]),
+                pybamm.Broadcast(Phi_1p, ["positive electrode"]),
+            )
+            + A_n
+        )
+
+        # Voltage
+        cbar_1p = pybamm.Scalar(1)  # pybamm.Integral(c_e_1p, x_p) / param.l_p
+        Phibar_1p = pybamm.Scalar(1)  # pybamm.Integral(Phi1, x_p) / param.l_p
+        j0bar_1p = pybamm.Scalar(1)  # pybamm.Integral(j0_1p, x_p) / param.l_p
+        V_1 = (
+            Phibar_1p
+            + dUPbO2dc * cbar_1p
+            - j0bar_1p * pybamm.Function(np.tanh, eta_0p) / j0_0p
+        )
+
+        # Variables
+        self.variables = {
+            "c": c_e,
+            "Phi": Phi_0 + param.C_e * Phi_1,
+            "V": V_0 + param.C_e * V_1,
         }
-        self.variables = {"phi_e": phi_e, "i_e": i_e}
