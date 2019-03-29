b98a1e903cb4efa78ead6c959512c0db33324369
Valentin Sulzer <valentinsulzer@hotmail.com>
2019-03-29 12:56:49 +0000

    #227 use mesh instead of x_sol

--- pybamm/processed_variable.py
+++ pybamm/processed_variable.py
@@ -1,14 +1,14 @@
 #
 # Processed Variable class
 #
-
 import numpy as np
+import scipy.interpolate as interp
 
 
 class ProcessedVariable(object):
     """
-    An object that can be evaluated at arbitrary (scalars or vectors) t and x, and
-    returns the (interpolated) value of the base variable at that t and x
+    An object that can be evaluated at arbitrary (scalars or vectors) x and t, and
+    returns the (interpolated) value of the base variable at that x and t.
 
     Parameters
     ----------
@@ -17,23 +17,45 @@ class ProcessedVariable(object):
         variable. Note that this can be any kind of node in the expression tree, not
         just a :class:`pybamm.Variable`.
         When evaluated, returns an array of size (m,n)
-    x_sol : array_like, size (n,)
-
     t_sol : array_like, size (m,)
         The time vector returned by the solver
     y_sol : array_like, size (m, k)
         The solution vector returned by the solver. Can include solution values that
         other than those that get read by base_variable.evaluate() (i.e. k>=n)
+    mesh : :class:`pybamm.Mesh`
+        The mesh used to solve, used here to calculate the reference x values for
+        interpolation
+    interp_kind : string
+        The method to use for interpolation
+
     """
 
-    def __init__(self, base_variable, t_sol, y_sol, x_sol=(None,)):
+    def __init__(self, base_variable, t_sol, y_sol, mesh=None, interp_kind="linear"):
+        if mesh is not None and base_variable.domain != []:
+            # Process the discretisation to get x values
+            x_sol = np.concatenate([mesh[dom][0].nodes for dom in base_variable.domain])
+            len_x = len(x_sol)
+        else:
+            # No discretisation provided, or variable has no domain (function of t only)
+            x_sol = None
+            len_x = 1
+
         # initialise empty array of the correct size
-        entries = np.empty((len(x_sol), len(t_sol)))
+        entries = np.empty((len_x, len(t_sol)))
         # Evaluate the base_variable index-by-index
         for idx in range(len(t_sol)):
             entries[:, idx] = base_variable.evaluate(t_sol[idx], y_sol[:, idx])
 
-        # assign attributes
-        self._entries = entries
-        self._x_sol = x_sol
-        self._t_sol = t_sol
+        # assign attributes for reference
+        self.entries = entries
+        self.x_sol = x_sol
+        self.t_sol = t_sol
+
+        # set up interpolation
+        # self._interpolation_function = interp.interp2d(
+        #     x_sol, t_sol, entries, kind=interp_kind
+        # )
+
+    def evaluate(self, x, t):
+        "Evaluate the variable at arbitrary x and t, using interpolation"
+        return self._interpolation_function(x, t)
--- tests/test_processed_variable.py
+++ tests/test_processed_variable.py
@@ -19,29 +19,27 @@ class TestProcessedVariable(unittest.TestCase):
         t_sol = np.linspace(0, 1)
         y_sol = np.array([np.linspace(0, 5)])
         processed_var = pybamm.ProcessedVariable(var, t_sol, y_sol)
-        np.testing.assert_array_equal(processed_var._entries, t_sol * y_sol)
+        np.testing.assert_array_equal(processed_var.entries, t_sol * y_sol)
 
     def test_processed_var_space(self):
         t = pybamm.t
-        var = pybamm.Variable("var", domain=["negative electrode"])
-        x = pybamm.SpatialVariable("x", domain=["negative electrode"])
+        var = pybamm.Variable("var", domain=["negative electrode", "separator"])
+        x = pybamm.SpatialVariable("x", domain=["negative electrode", "separator"])
         eqn = t * var + x
 
         disc = tests.get_discretisation_for_testing()
         disc.set_variable_slices([var])
-        x_sol = disc.process_symbol(x)._entries
+        x_sol = disc.process_symbol(x).entries
         var_sol = disc.process_symbol(var)
         eqn_sol = disc.process_symbol(eqn)
         t_sol = np.linspace(0, 1)
-        y_sol = np.ones_like(disc.mesh["negative electrode"][0].nodes)[
-            :, np.newaxis
-        ] * np.linspace(0, 5)
+        y_sol = np.ones_like(x_sol)[:, np.newaxis] * np.linspace(0, 5)
 
-        processed_var = pybamm.ProcessedVariable(var_sol, t_sol, y_sol, x_sol=x_sol)
-        np.testing.assert_array_equal(processed_var._entries, y_sol)
-        processed_eqn = pybamm.ProcessedVariable(eqn_sol, t_sol, y_sol, x_sol=x_sol)
+        processed_var = pybamm.ProcessedVariable(var_sol, t_sol, y_sol, mesh=disc.mesh)
+        np.testing.assert_array_equal(processed_var.entries, y_sol)
+        processed_eqn = pybamm.ProcessedVariable(eqn_sol, t_sol, y_sol, mesh=disc.mesh)
         np.testing.assert_array_equal(
-            processed_eqn._entries, t_sol * y_sol + x_sol[:, np.newaxis]
+            processed_eqn.entries, t_sol * y_sol + x_sol[:, np.newaxis]
         )
 
     def test_processed_var_interpolation(self):
@@ -58,7 +56,29 @@ class TestProcessedVariable(unittest.TestCase):
         modeltest.test_all()
         t_sol, y_sol = modeltest.solver.t, modeltest.solver.y
         processed_var = pybamm.ProcessedVariable(model.variables["c"], t_sol, y_sol)
-        np.testing.assert_array_almost_equal(processed_var._entries[0], np.exp(-t_sol))
+        np.testing.assert_array_almost_equal(processed_var.entries[0], np.exp(-t_sol))
+
+        # with space
+        # set up and solve model
+        whole_cell = ["negative electrode", "separator", "positive electrode"]
+        model = pybamm.BaseModel()
+        c = pybamm.Variable("conc", domain=whole_cell)
+        model.rhs = {c: -c}
+        model.initial_conditions = {c: 1}
+        model.variables = {"c": c}
+        modeltest = tests.StandardModelTest(model)
+        modeltest.test_all()
+        # set up testing
+        t_sol, y_sol = modeltest.solver.t, modeltest.solver.y
+        x = pybamm.SpatialVariable("x", domain=whole_cell)
+        x_sol = modeltest.disc.process_symbol(x).entries
+        processed_var = pybamm.ProcessedVariable(
+            model.variables["c"], t_sol, y_sol, mesh=modeltest.disc.mesh
+        )
+        # test
+        np.testing.assert_array_almost_equal(
+            processed_var.entries, np.ones_like(x_sol)[:, np.newaxis] * np.exp(-t_sol)
+        )
 
 
 if __name__ == "__main__":
