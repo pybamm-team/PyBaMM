dd4966cd919219216c9901b07989cf3adde025a8
Valentin Sulzer <valentinsulzer@hotmail.com>
2019-04-03 17:57:13 +0100

    #243 use scipy constants

--- pybamm/parameters/standard_parameters_lithium_ion.py
+++ pybamm/parameters/standard_parameters_lithium_ion.py
@@ -58,6 +58,7 @@ cn0_dimensional, cp0_dimensional
 from __future__ import absolute_import, division
 from __future__ import print_function, unicode_literals
 import pybamm
+from scipy import constants
 
 # --------------------------------------------------------------------------------------
 "File Layout:"
@@ -71,8 +72,8 @@ import pybamm
 "1. Dimensional Parameters"
 
 # Physical constants
-R = pybamm.Parameter("Ideal gas constant")
-F = pybamm.Parameter("Faraday's constant")
+R = constants.R
+F = constants.physical_constants["Faraday constant"][0]
 T_ref = pybamm.Parameter("Reference temperature")
 
 # Macroscale geometry
