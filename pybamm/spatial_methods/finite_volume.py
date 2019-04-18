0834daca8267fc3b4fb27865abf5b48931e2281e
Robert Timms <timms@maths.ox.ac.uk>
2019-04-18 09:40:12 +0100

    #197 fix node_to_edge

--- pybamm/spatial_methods/finite_volume.py
+++ pybamm/spatial_methods/finite_volume.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import, division
 from __future__ import print_function, unicode_literals
 import pybamm
 
-import numpy as np
+import autograd.numpy as np
 from scipy.sparse import diags, eye, kron
 from autograd.builtins import isinstance
 
