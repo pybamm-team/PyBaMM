5655b2c3643ec21b0a35606796de8310a5cbb30f
Valentin Sulzer <valentinsulzer@hotmail.com>
2019-05-05 12:20:30 +0100

    #119 add functionality to solve loqs as DAEs

--- tests/unit/test_models/test_lead_acid/test_loqs_capacitance.py
+++ tests/unit/test_models/test_lead_acid/test_loqs_capacitance.py
@@ -4,6 +4,7 @@
 from __future__ import absolute_import, division
 from __future__ import print_function, unicode_literals
 import pybamm
+from pybamm.solvers.scikits_ode_solver import scikits_odes_spec
 import tests
 
 import unittest
@@ -13,6 +14,13 @@ import numpy as np
 class TestLeadAcidLOQSCapacitance(unittest.TestCase):
     def test_basic_processing(self):
         model = pybamm.lead_acid.LOQSCapacitance()
+        modeltest = tests.StandardModelTest(model)
+
+        modeltest.test_all()
+
+    @unittest.skipIf(scikits_odes_spec is None, "scikits.odes not installed")
+    def test_basic_processing_no_capacitance(self):
+        model = pybamm.lead_acid.LOQSCapacitance(use_capacitance=False)
         modeltest = tests.StandardModelTest(model)
 
         modeltest.test_all()
