0abba72c8f8162efd32a5fe8175e4df3183b5a69
Martin Robinson <martinjrobins@gmail.com>
2019-05-13 23:45:43 +0100

    #384 start testing scripts

--- run-tests.py
+++ run-tests.py
@@ -99,7 +99,7 @@ def run_doc_tests():
         sys.exit(ret)
 
 
-def run_notebook_tests(skip_slow_books=False, executable="python"):
+def run_notebook_and_scripts(skip_slow_books=False, executable="python"):
     """
     Runs Jupyter notebook tests. Exits if they fail.
     """
@@ -120,14 +120,14 @@ def run_notebook_tests(skip_slow_books=False, executable="python"):
                 ignore_list.append(line)
 
     # Scan and run
-    print("Testing notebooks with executable `" + str(executable) + "`")
-    if not scan_for_notebooks("examples", True, executable, ignore_list):
+    print("Testing notebooks and scripts with executable `" + str(executable) + "`")
+    if not scan_for_notebooks_and_scripts("examples", True, executable, ignore_list):
         print("\nErrors encountered in notebooks")
         sys.exit(1)
     print("\nOK")
 
 
-def scan_for_notebooks(root, recursive=True, executable="python", ignore_list=[]):
+def scan_for_notebooks_and_scripts(root, recursive=True, executable="python", ignore_list=[]):
     """
     Scans for, and tests, all notebooks in a directory.
     """
@@ -146,7 +146,7 @@ def scan_for_notebooks(root, recursive=True, executable="python", ignore_list=[]
             # Ignore hidden directories
             if filename[:1] == ".":
                 continue
-            ok &= scan_for_notebooks(path, recursive, executable)
+            ok &= scan_for_notebooks_and_scripts(path, recursive, executable)
 
         # Test notebooks
         if os.path.splitext(path)[1] == ".ipynb":
@@ -154,6 +154,12 @@ def scan_for_notebooks(root, recursive=True, executable="python", ignore_list=[]
                 print(path)
             else:
                 ok &= test_notebook(path, executable)
+        # Test scripts
+        elif os.path.splitext(path)[1] == ".py":
+            if debug:
+                print(path)
+            else:
+                ok &= test_script(path, executable)
 
     # Return True if every notebook is ok
     return ok
@@ -197,9 +203,51 @@ def test_notebook(path, executable="python"):
                 j = str(1 + i)
                 print(j + " " * (5 - len(j)) + line)
             print("-- stdout " + "-" * (79 - 10))
-            print(stdout)
+            print(str(stdout, 'utf-8'))
             print("-- stderr " + "-" * (79 - 10))
-            print(stderr)
+            print(str(stderr, 'utf-8'))
+            print("-" * 79)
+            return False
+    except KeyboardInterrupt:
+        p.terminate()
+        print("ABORTED")
+        sys.exit(1)
+
+    # Sucessfully run
+    print("ok (" + b.format() + ")")
+    return True
+
+
+def test_script(path, executable="python"):
+    """
+    Tests a single notebook, exists if it doesn't finish.
+    """
+    import nbconvert
+    import pybamm
+
+    b = pybamm.Timer()
+    print("Test " + path + " ... ", end="")
+    sys.stdout.flush()
+
+    # Tell matplotlib not to produce any figures
+    env = dict(os.environ)
+    env["MPLBACKEND"] = "Template"
+
+    # Run in subprocess
+    cmd = [executable] + [path]
+    try:
+        p = subprocess.Popen(
+            cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env
+        )
+        stdout, stderr = p.communicate()
+        # TODO: Use p.communicate(timeout=3600) if Python3 only
+        if p.returncode != 0:
+            # Show failing code, output and errors before returning
+            print("ERROR")
+            print("-- stdout " + "-" * (79 - 10))
+            print(str(stdout, 'utf-8'))
+            print("-- stderr " + "-" * (79 - 10))
+            print(str(stderr, 'utf-8'))
             print("-" * 79)
             return False
     except KeyboardInterrupt:
@@ -275,14 +323,14 @@ if __name__ == "__main__":
     )
     # Notebook tests
     parser.add_argument(
-        "--books",
+        "--examples",
         action="store_true",
-        help="Test only the fast Jupyter notebooks in `examples`.",
+        help="Test only the fast Jupyter notebooks and scripts in `examples`.",
     )
     parser.add_argument(
-        "--allbooks",
+        "--allexamples",
         action="store_true",
-        help="Test all Jupyter notebooks in `examples`.",
+        help="Test all Jupyter notebooks and scripts in `examples`.",
     )
     parser.add_argument(
         "-debook",
@@ -337,12 +385,12 @@ if __name__ == "__main__":
         has_run = True
         run_doc_tests()
     # Notebook tests
-    if args.allbooks:
+    if args.allexamples:
         has_run = True
-        run_notebook_tests()
-    elif args.books:
+        run_notebook_and_scripts()
+    elif args.examples:
         has_run = True
-        run_notebook_tests(True)
+        run_notebook_and_scripts(True)
     if args.debook:
         has_run = True
         export_notebook(*args.debook)
