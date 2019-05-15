2287c49479ed27c474d48f617f19afc451e723eb
Robert Timms <timms@maths.ox.ac.uk>
2019-05-15 14:18:07 +0100

    added error to discharge and more comsol results

--- comsol/comparison.py
+++ comsol/comparison.py
@@ -8,16 +8,17 @@ import numpy as np
 "Pick C_rate and load comsol data"
 
 # C_rate
-C_rate = 1
+C_rates = {"01": 0.1, "05": 0.5, "1": 1, "2": 2, "3": 3}
+C_rate = "2"  # choose the key from the above dictionary of available results
 
 # time-voltage
-comsol = pd.read_csv("comsol/data/Voltage_C{}.csv".format(C_rate), sep=",", header=None)
+comsol = pd.read_csv("comsol/data/{}C/Voltage.csv".format(C_rate), sep=",", header=None)
 comsol_time = comsol[0].values
 comsol_time_npts = len(comsol_time)
 comsol_voltage = comsol[1].values
 
 # negative electrode potential
-comsol = pd.read_csv("comsol/data/phi_n_C{}.csv".format(C_rate), sep=",", header=None)
+comsol = pd.read_csv("comsol/data/{}C/phi_n.csv".format(C_rate), sep=",", header=None)
 comsol_x_n_npts = int(len(comsol[0].values) / comsol_time_npts)
 comsol_x_n = comsol[0].values[0:comsol_x_n_npts]
 comsol_phi_n_vals = np.reshape(
@@ -26,14 +27,14 @@ comsol_phi_n_vals = np.reshape(
 
 # negative particle surface concentration
 comsol = pd.read_csv(
-    "comsol/data/c_n_surf_C{}.csv".format(C_rate), sep=",", header=None
+    "comsol/data/{}C/c_n_surf.csv".format(C_rate), sep=",", header=None
 )
 comsol_c_n_surf_vals = np.reshape(
     comsol[1].values, (comsol_x_n_npts, comsol_time_npts), order="F"
 )
 
 # positive electrode potential
-comsol = pd.read_csv("comsol/data/phi_p_C{}.csv".format(C_rate), sep=",", header=None)
+comsol = pd.read_csv("comsol/data/{}C/phi_p.csv".format(C_rate), sep=",", header=None)
 comsol_x_p_npts = int(len(comsol[0].values) / comsol_time_npts)
 comsol_x_p = comsol[0].values[0:comsol_x_p_npts]
 comsol_phi_p_vals = np.reshape(
@@ -42,14 +43,14 @@ comsol_phi_p_vals = np.reshape(
 
 # positive particle surface concentration
 comsol = pd.read_csv(
-    "comsol/data/c_p_surf_C{}.csv".format(C_rate), sep=",", header=None
+    "comsol/data/{}C/c_p_surf.csv".format(C_rate), sep=",", header=None
 )
 comsol_c_p_surf_vals = np.reshape(
     comsol[1].values, (comsol_x_p_npts, comsol_time_npts), order="F"
 )
 
 # electrolyte concentration
-comsol = pd.read_csv("comsol/data/c_e_C{}.csv".format(C_rate), sep=",", header=None)
+comsol = pd.read_csv("comsol/data/{}C/c_e.csv".format(C_rate), sep=",", header=None)
 comsol_x_npts = int(len(comsol[0].values) / comsol_time_npts)
 comsol_x = comsol[0].values[0:comsol_x_npts]
 comsol_c_e_vals = np.reshape(
@@ -57,7 +58,7 @@ comsol_c_e_vals = np.reshape(
 )
 
 # electrolyte potential
-comsol = pd.read_csv("comsol/data/phi_e_C{}.csv".format(C_rate), sep=",", header=None)
+comsol = pd.read_csv("comsol/data/{}C/phi_e.csv".format(C_rate), sep=",", header=None)
 comsol_phi_e_vals = np.reshape(
     comsol[1].values, (comsol_x_npts, comsol_time_npts), order="F"
 )
@@ -74,7 +75,7 @@ geometry = model.default_geometry
 param = model.default_parameter_values
 param["Electrode depth [m]"] = 1
 param["Electrode height [m]"] = 1
-param["Typical current [A]"] = 24 * C_rate
+param["Typical current [A]"] = 24 * C_rates[C_rate]
 param.process_model(model)
 param.process_geometry(geometry)
 
@@ -87,19 +88,18 @@ mesh = pybamm.Mesh(geometry, model.default_submesh_types, var_pts)
 disc = pybamm.Discretisation(mesh, model.default_spatial_methods)
 disc.process_model(model)
 
-# solve model
-solver = model.default_solver
-t = np.linspace(0, 1, 500)
-solver.solve(model, t)
-
-"-----------------------------------------------------------------------------"
-"Get variables for comparison"
-
 # discharge timescale
 tau = param.process_symbol(
     pybamm.standard_parameters_lithium_ion.tau_discharge
 ).evaluate(0, 0)
+
+# solve model at comsol times
+solver = model.default_solver
 time = comsol_time / tau
+solver.solve(model, time)
+
+"-----------------------------------------------------------------------------"
+"Get variables for comparison"
 
 # discharge capacity
 discharge_capacity = pybamm.ProcessedVariable(
@@ -256,8 +256,8 @@ plt.subplot(248)
 discharge_capacity_tracer, = plt.plot(
     [comsol_discharge_capacity[0], comsol_discharge_capacity[0]], [v_min, v_max], "k--"
 )
-plt.plot(comsol_discharge_capacity, comsol_voltage, "r:", label="Comsol")
 plt.plot(discharge_capacity_sol, voltage_sol, "b-", label="PyBaMM")
+plt.plot(comsol_discharge_capacity, comsol_voltage, "r:", label="Comsol")
 plt.axis(
     [comsol_discharge_capacity[0], comsol_discharge_capacity[-1] * 1.1, v_min, v_max]
 )
--- comsol/discharge_curve.py
+++ comsol/discharge_curve.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-# C_rates dict
+# dictionary of available comsol results
 C_rates = {"01": 0.1, "05": 0.5, "1": 1, "2": 2, "3": 3}
 
 # load model and geometry
@@ -19,7 +19,7 @@ param.process_geometry(geometry)
 
 # create mesh
 var = pybamm.standard_spatial_vars
-var_pts = {var.x_n: 11, var.x_s: 7, var.x_p: 11, var.r_n: 11, var.r_p: 11}
+var_pts = {var.x_n: 31, var.x_s: 11, var.x_p: 31, var.r_n: 11, var.r_p: 11}
 mesh = pybamm.Mesh(geometry, model.default_submesh_types, var_pts)
 
 # discretise model
@@ -27,14 +27,27 @@ disc = pybamm.Discretisation(mesh, model.default_spatial_methods)
 disc.process_model(model)
 
 # loop over C_rates dict to create plot
-plt.figure(figsize=(15, 8))
-ax = plt.gca()
+fig, ax = plt.subplots(figsize=(15, 8))
+plt.tight_layout()
+plt.subplots_adjust(left=-0.1)
+discharge_curve = plt.subplot(211)
+plt.xlim([0, 26])
+plt.ylim([3.2, 3.9])
+plt.xlabel(r"Discharge Capacity (Ah)")
+plt.ylabel("Voltage (V)")
+plt.title(r"Comsol $\cdots$ PyBaMM $-$")
+error_plot = plt.subplot(212)
+plt.xlim([0, 26])
+plt.yscale('log')
+plt.grid(True)
+plt.xlabel(r"Discharge Capacity (Ah)")
+plt.ylabel(r"$\vert V - V_{comsol} \vert$")
 
 for key, C_rate in C_rates.items():
 
     # load the comsol voltage data
     comsol = pd.read_csv(
-        "comsol/data/Voltage_C{}.csv".format(key), sep=",", header=None
+        "comsol/data/{}C/Voltage.csv".format(key), sep=",", header=None
     )
     comsol_time = comsol[0].values
     comsol_voltage = comsol[1].values
@@ -43,10 +56,22 @@ for key, C_rate in C_rates.items():
     param["Typical current [A]"] = 24 * C_rate
     param.update_model(model, disc)
 
-    # solve model
+    # discharge timescale
+    tau = param.process_symbol(
+        pybamm.standard_parameters_lithium_ion.tau_discharge
+    ).evaluate(0, 0)
+
+    # solve model at comsol times
     solver = model.default_solver
-    t = np.linspace(0, 1, 500)
+    t = comsol_time / tau
     solver.solve(model, t)
+
+    # discharge capacity
+    discharge_capacity = pybamm.ProcessedVariable(
+        model.variables["Discharge capacity [A.h]"], solver.t, solver.y, mesh=mesh
+    )
+    discharge_capacity_sol = discharge_capacity(solver.t)
+    comsol_discharge_capacity = comsol_time * param["Typical current [A]"] / 3600
 
     # extract the voltage
     voltage = pybamm.ProcessedVariable(
@@ -54,31 +79,23 @@ for key, C_rate in C_rates.items():
     )
     voltage_sol = voltage(solver.t)
 
-    # convert to discharge capacity
-    discharge_capacity = pybamm.ProcessedVariable(
-        model.variables["Discharge capacity [A.h]"], solver.t, solver.y, mesh=mesh
-    )
-    discharge_capacity_sol = discharge_capacity(solver.t)
-    comsol_discharge_capacity = comsol_time * param["Typical current [A]"] / 3600
+    # calculate the error
+    end_index = min(len(solver.t), len(comsol_time))
+    error = np.abs(voltage_sol[0:end_index] - comsol_voltage[0:end_index])
 
-    # plot discharge curves
+    # plot discharge curves and absolute error
     color = next(ax._get_lines.prop_cycler)["color"]
-    plt.plot(comsol_discharge_capacity, comsol_voltage, color=color, linestyle=":")
-    plt.plot(
+    discharge_curve.plot(comsol_discharge_capacity, comsol_voltage, color=color, linestyle=":")
+    discharge_curve.plot(
         discharge_capacity_sol,
         voltage_sol,
         color=color,
         linestyle="-",
         label="{} C".format(C_rate),
     )
+    error_plot.plot(discharge_capacity_sol[0:end_index], error, color=color)
 
-# add labels etc.
-plt.xlim([0, 26])
-plt.ylim([3.2, 3.9])
-plt.legend(loc="best")
-plt.xlabel(r"Discharge Capacity (Ah)")
-plt.ylabel("Voltage (V)")
-plt.title(r"Comsol $\cdots$ PyBaMM $-$")
-plt.tight_layout()
+discharge_curve.legend(loc="best")
+plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.25, wspace=0.35)
 plt.savefig("comsol/DischargeCurve.eps", format="eps", dpi=1000)
 plt.show()
