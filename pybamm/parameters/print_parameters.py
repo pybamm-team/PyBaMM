d085d39a36d16ee7db6ca1974a5eebc63f956c7e
Valentin Sulzer <valentinsulzer@hotmail.com>
2019-05-24 16:26:30 +0100

    #431 change tol for 3.5

--- pybamm/parameters/print_parameters.py
+++ pybamm/parameters/print_parameters.py
@@ -79,11 +79,11 @@ def print_parameters(parameters, parameter_values, output_file=None):
     # Calculate C-dependence of the parameters based on the difference between the
     # value at 1C and the value at C / 10
     for name, values in evaluated_parameters.items():
-        if abs(values[0] - values[1]) < 1e-9:
+        if abs(values[0] - values[1]) < 1e-10:
             C_dependence = ""
-        elif abs(values[0] - 10 * values[1]) < 1e-9:
+        elif abs(values[0] - 10 * values[1]) < 1e-10:
             C_dependence = " * Crate"
-        elif abs(10 * values[0] - values[1]) < 1e-9:
+        elif abs(10 * values[0] - values[1]) < 1e-10:
             C_dependence = " / Crate"
         evaluated_parameters[name] = (values[0], C_dependence)
 
