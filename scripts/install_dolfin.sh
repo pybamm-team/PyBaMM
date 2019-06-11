a5baf96696a17e0759509f87e22510f39546ac46
Robert Timms <timms@maths.ox.ac.uk>
2019-06-11 10:06:27 +0100

    petsc and h5py

--- scripts/install_dolfin.sh
+++ scripts/install_dolfin.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 pip install --prefix=$VIRTUAL_ENV fenics-ffc --upgrade
+pip install --prefix=$VIRTUAL_ENV numpy mpi4py
+pip install --prefix=$VIRTUAL_ENV petsc petsc4py
+pip install --prefix=$VIRTUAL_ENV h5py
 
 FENICS_VERSION=$(python3 -c"import ffc; print(ffc.__version__)")
 git clone --branch=$FENICS_VERSION https://bitbucket.org/fenics-project/dolfin
