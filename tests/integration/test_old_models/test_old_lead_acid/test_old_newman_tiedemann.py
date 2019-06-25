ede9d501a6fbbfd73df503d824d6eca655e3f470
Scottmar93 <marquis@maths.ox.ac.uk>
2019-06-25 14:53:02 +0100

    #457 old_newman_tiedemann integration tests run

--- tests/integration/test_old_models/test_old_lead_acid/test_old_newman_tiedemann.py
+++ tests/integration/test_old_models/test_old_lead_acid/test_old_newman_tiedemann.py
@@ -11,23 +11,21 @@ import numpy as np
 class TestOldLeadAcidNewmanTiedemann(unittest.TestCase):
     @unittest.skipIf(pybamm.have_scikits_odes(), "scikits.odes not installed")
     def test_basic_processing(self):
-        options = {"thermal": None, "Voltage": "On"}
-        model = pybamm.lead_acid.NewmanTiedemann(options)
+        model = pybamm.old_lead_acid.OldNewmanTiedemann()
         modeltest = tests.StandardModelTest(model)
         modeltest.test_all(t_eval=np.linspace(0, 0.6))
 
     @unittest.skipIf(pybamm.have_scikits_odes(), "scikits.odes not installed")
     def test_basic_processing_with_convection(self):
-        options = {"thermal": None, "Voltage": "On", "convection": True}
-        model = pybamm.lead_acid.NewmanTiedemann(options)
+        options = {"convection": True}
+        model = pybamm.old_lead_acid.OldNewmanTiedemann()
         var = pybamm.standard_spatial_vars
         var_pts = {var.x_n: 10, var.x_s: 10, var.x_p: 10}
         modeltest = tests.StandardModelTest(model, var_pts=var_pts)
         modeltest.test_all(t_eval=np.linspace(0, 0.6))
 
     def test_optimisations(self):
-        options = {"thermal": None, "Voltage": "On"}
-        model = pybamm.lead_acid.NewmanTiedemann(options)
+        model = pybamm.old_lead_acid.OldNewmanTiedemann()
         optimtest = tests.OptimisationsTest(model)
 
         original = optimtest.evaluate_model()
@@ -44,20 +42,20 @@ class TestOldLeadAcidNewmanTiedemann(unittest.TestCase):
 class TestOldLeadAcidNewmanTiedemannCapacitance(unittest.TestCase):
     def test_basic_processing_differential(self):
         options = {"capacitance": "differential"}
-        model = pybamm.lead_acid.NewmanTiedemann(options)
+        model = pybamm.old_lead_acid.OldNewmanTiedemann(options)
         modeltest = tests.StandardModelTest(model)
         modeltest.test_all()
 
     @unittest.skipIf(pybamm.have_scikits_odes(), "scikits.odes not installed")
     def test_basic_processing_algebraic(self):
         options = {"capacitance": "algebraic"}
-        model = pybamm.lead_acid.NewmanTiedemann(options)
+        model = pybamm.old_lead_acid.OldNewmanTiedemann(options)
         modeltest = tests.StandardModelTest(model)
         modeltest.test_all()
 
     def test_optimisations(self):
         options = {"capacitance": "differential"}
-        model = pybamm.lead_acid.NewmanTiedemann(options)
+        model = pybamm.old_lead_acid.OldNewmanTiedemann(options)
         optimtest = tests.OptimisationsTest(model)
 
         original = optimtest.evaluate_model()
