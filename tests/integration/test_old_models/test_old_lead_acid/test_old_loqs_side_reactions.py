a123cd010e152b59e8ad55ed949e00dfb1d389b3
Scottmar93 <marquis@maths.ox.ac.uk>
2019-06-25 14:39:44 +0100

    #457 passes loqs with side reactions

--- tests/integration/test_old_models/test_old_lead_acid/test_old_loqs_side_reactions.py
+++ tests/integration/test_old_models/test_old_lead_acid/test_old_loqs_side_reactions.py
@@ -9,7 +9,7 @@ import unittest
 class TestOldLeadAcidLOQSWithSideReactions(unittest.TestCase):
     def test_discharge_differential(self):
         options = {"capacitance": "differential", "side reactions": ["oxygen"]}
-        model = pybamm.lead_acid.LOQS(options)
+        model = pybamm.old_lead_acid.OldLOQS(options)
         modeltest = tests.StandardModelTest(model)
         modeltest.test_all()
 
@@ -19,20 +19,20 @@ class TestOldLeadAcidLOQSWithSideReactions(unittest.TestCase):
             "side reactions": ["oxygen"],
             "interfacial surface area": "varying",
         }
-        model = pybamm.lead_acid.LOQS(options)
+        model = pybamm.old_lead_acid.OldLOQS(options)
         modeltest = tests.StandardModelTest(model)
         modeltest.test_all()
 
     @unittest.skipIf(pybamm.have_scikits_odes(), "scikits.odes not installed")
     def test_discharge_algebraic(self):
         options = {"capacitance": "algebraic", "side reactions": ["oxygen"]}
-        model = pybamm.lead_acid.LOQS(options)
+        model = pybamm.old_lead_acid.OldLOQS(options)
         modeltest = tests.StandardModelTest(model)
         modeltest.test_all(skip_output_tests=True)
 
     def test_charge(self):
         options = {"capacitance": "differential", "side reactions": ["oxygen"]}
-        model = pybamm.lead_acid.LOQS(options)
+        model = pybamm.old_lead_acid.OldLOQS(options)
         parameter_values = model.default_parameter_values
         parameter_values.update(
             {"Typical current [A]": -1, "Initial State of Charge": 0.5}
@@ -42,7 +42,7 @@ class TestOldLeadAcidLOQSWithSideReactions(unittest.TestCase):
 
     def test_zero_current(self):
         options = {"capacitance": "differential", "side reactions": ["oxygen"]}
-        model = pybamm.lead_acid.LOQS(options)
+        model = pybamm.old_lead_acid.OldLOQS(options)
         parameter_values = model.default_parameter_values
         parameter_values.update({"Typical current [A]": 0})
         modeltest = tests.StandardModelTest(model, parameter_values=parameter_values)
