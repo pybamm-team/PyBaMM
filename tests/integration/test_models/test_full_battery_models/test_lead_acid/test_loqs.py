5a0e6da274aa2ea0ba5fadd9d59318873ee3fa04
Scottmar93 <marquis@maths.ox.ac.uk>
2019-06-26 10:30:15 +0100

    #457 loqs passes integration and unit tests

--- tests/integration/test_models/test_full_battery_models/test_lead_acid/test_loqs.py
+++ tests/integration/test_models/test_full_battery_models/test_lead_acid/test_loqs.py
@@ -8,7 +8,7 @@ import unittest
 import numpy as np
 
 
-class TestLeadAcidLOQS(unittest.TestCase):
+class TestLOQS(unittest.TestCase):
     @unittest.skipIf(pybamm.have_scikits_odes(), "scikits.odes not installed")
     def test_basic_processing(self):
         options = {"thermal": None, "Voltage": "On"}
