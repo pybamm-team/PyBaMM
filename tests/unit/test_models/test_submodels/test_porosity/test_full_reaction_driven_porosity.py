7c2593126aa2ca18d395f0e1d006c10febc09b2a
Scottmar93 <marquis@maths.ox.ac.uk>
2019-06-28 19:18:31 +0100

    #457 added unit tests for porosity submodels

---
+++ tests/unit/test_models/test_submodels/test_porosity/test_full_reaction_driven_porosity.py
@@ -0,0 +1,30 @@
+#
+# Test full porosity submodel
+#
+
+import pybamm
+import tests
+import unittest
+
+
+class TestFullModel(unittest.TestCase):
+    def test_public_functions(self):
+        param = pybamm.standard_parameters_lead_acid
+        a = pybamm.Scalar(0)
+        variables = {
+            "Negative electrode interfacial current density": a,
+            "Positive electrode interfacial current density": a,
+        }
+        submodel = pybamm.porosity.FullModel(param)
+        std_tests = tests.StandardSubModelTests(submodel, variables)
+        std_tests.test_all()
+
+
+if __name__ == "__main__":
+    print("Add -v for more debug output")
+    import sys
+
+    if "-v" in sys.argv:
+        debug = True
+    unittest.main()
+
