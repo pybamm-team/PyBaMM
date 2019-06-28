ab76c6b23e636785a8e979e2434bde227657b6a3
Scottmar93 <marquis@maths.ox.ac.uk>
2019-06-28 20:53:19 +0100

    #457 made submodels dict and OrderedDi ct for 3.5

--- pybamm/models/full_battery_models/base_battery_model.py
+++ pybamm/models/full_battery_models/base_battery_model.py
@@ -4,6 +4,7 @@
 
 import pybamm
 import os
+from collections import OrderedDict
 
 
 class BaseBatteryModel(pybamm.BaseModel):
@@ -17,7 +18,7 @@ class BaseBatteryModel(pybamm.BaseModel):
         super().__init__()
         self._extra_options = options
         self.set_standard_output_variables()
-        self.submodels = {}
+        self.submodels = OrderedDict()  # ordered dict not default in 3.5
 
     @property
     def default_parameter_values(self):
