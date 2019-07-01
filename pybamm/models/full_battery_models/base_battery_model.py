fd3c531c21d55f20714998cff43b0f51d061a84b
Scottmar93 <marquis@maths.ox.ac.uk>
2019-07-01 10:22:26 +0100

    #457 removed unreachable if statement

--- pybamm/models/full_battery_models/base_battery_model.py
+++ pybamm/models/full_battery_models/base_battery_model.py
@@ -141,15 +141,6 @@ class BaseBatteryModel(pybamm.BaseModel):
                 "must use capacitance formulation to solve {!s} in 2D".format(self)
             )
 
-        if len(options["side reactions"]) > 0:
-            raise pybamm.ModelError(
-                """
-                must use capacitance formulation to solve {!s} with side reactions
-                """.format(
-                    self
-                )
-            )
-
         return options
 
     def set_standard_output_variables(self):
