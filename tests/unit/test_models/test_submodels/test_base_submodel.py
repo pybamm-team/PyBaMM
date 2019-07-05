5259bbd3f340017e5d10832153cf3135501999bb
Valentin Sulzer <valentinsulzer@hotmail.com>
2019-07-05 18:44:13 +0100

    #499 add test for coverage

--- tests/unit/test_models/test_submodels/test_base_submodel.py
+++ tests/unit/test_models/test_submodels/test_base_submodel.py
@@ -8,6 +8,20 @@ import unittest
 
 
 class TestBaseSubModel(unittest.TestCase):
+    def test_domain(self):
+        # Accepted string
+        submodel = pybamm.BaseSubModel(None, "Negative")
+        self.assertEqual(submodel.domain, "Negative")
+
+        # None (accepted but can't be called as an attribute)
+        submodel = pybamm.BaseSubModel(None, None)
+        with self.assertRaises(AttributeError):
+            submodel.domain
+
+        # bad string
+        with self.assertRaises(pybamm.DomainError):
+            submodel = pybamm.BaseSubModel(None, "bad string")
+
     def test_public_functions(self):
         submodel = pybamm.BaseSubModel(None)
         std_tests = tests.StandardSubModelTests(submodel)
