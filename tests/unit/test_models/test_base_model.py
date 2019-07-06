f1ca1b08e0517f277b48a9c965bb54257ab20659
Martin Robinson <martinjrobins@gmail.com>
2019-07-06 08:02:12 +0100

    #494 turn off warning check, raise separate issue

--- tests/unit/test_models/test_base_model.py
+++ tests/unit/test_models/test_base_model.py
@@ -331,7 +331,7 @@ class TestBaseModel(unittest.TestCase):
         # TODO: getting a strange bug here, related to CPython bug here:
         #    https://bugs.python.org/issue29620
         # with self.assertWarns(pybamm.ModelWarning):
-        #     model.check_well_posedness()
+        model.check_well_posedness()
 
         # Check None entries have been removed from the variables dictionary
         for key, item in model._variables.items():
