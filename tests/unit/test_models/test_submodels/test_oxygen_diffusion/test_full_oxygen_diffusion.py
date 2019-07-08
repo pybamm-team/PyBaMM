fd28e08697cf4c6f56c823a5ef4f5966b5c55191
Valentin Sulzer <valentinsulzer@hotmail.com>
2019-07-08 18:39:45 +0100

    #490 add docs and basic tests

---
+++ tests/unit/test_models/test_submodels/test_oxygen_diffusion/test_full_oxygen_diffusion.py
@@ -0,0 +1,31 @@
+#
+# Test full concentration submodel
+#
+
+import pybamm
+import tests
+import unittest
+
+
+class TestFull(unittest.TestCase):
+    def test_public_functions(self):
+        param = pybamm.standard_parameters_lithium_ion
+        a = pybamm.Scalar(0)
+        variables = {
+            "Porosity": a,
+            "Porosity change": a,
+            "Volume-averaged velocity": a,
+            "Interfacial current density": a,
+        }
+        submodel = pybamm.oxygen_diffusion.Full(param)
+        std_tests = tests.StandardSubModelTests(submodel, variables)
+        std_tests.test_all()
+
+
+if __name__ == "__main__":
+    print("Add -v for more debug output")
+    import sys
+
+    if "-v" in sys.argv:
+        debug = True
+    unittest.main()
---
+++ tests/unit/test_models/test_submodels/test_oxygen_diffusion/test_leading_oxygen_diffusion.py
@@ -0,0 +1,47 @@
+#
+# Test leading-order concentration submodel
+#
+
+import pybamm
+import tests
+import unittest
+
+
+class TestLeadingOrder(unittest.TestCase):
+    def test_public_functions(self):
+        param = pybamm.standard_parameters_lead_acid
+        reactions = {
+            "main": {
+                "neg": {
+                    "s_plus": param.s_n,
+                    "j": "Average negative electrode interfacial current density",
+                },
+                "pos": {
+                    "s_plus": param.s_p,
+                    "j": "Average positive electrode interfacial current density",
+                },
+            }
+        }
+        a = pybamm.Scalar(0)
+        variables = {
+            "Average negative electrode porosity": a,
+            "Average separator porosity": a,
+            "Average positive electrode porosity": a,
+            "Average negative electrode porosity change": a,
+            "Average separator porosity change": a,
+            "Average positive electrode porosity change": a,
+            "Average negative electrode interfacial current density": a,
+            "Average positive electrode interfacial current density": a,
+        }
+        submodel = pybamm.oxygen_diffusion.LeadingOrder(param, reactions)
+        std_tests = tests.StandardSubModelTests(submodel, variables)
+        std_tests.test_all()
+
+
+if __name__ == "__main__":
+    print("Add -v for more debug output")
+    import sys
+
+    if "-v" in sys.argv:
+        debug = True
+    unittest.main()
