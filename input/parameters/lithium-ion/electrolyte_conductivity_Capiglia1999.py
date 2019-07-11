1e1fa0288a9f6f821706be35e58b74d65e062121
Scottmar93 <marquis@maths.ox.ac.uk>
2019-07-11 15:26:26 +0100

    #488 added electrolyte conductivity arrhenius

--- input/parameters/lithium-ion/electrolyte_conductivity_Capiglia1999.py
+++ input/parameters/lithium-ion/electrolyte_conductivity_Capiglia1999.py
@@ -1,4 +1,7 @@
-def electrolyte_conductivity_Capiglia1999(c_e):
+import autograd.np as np
+
+
+def electrolyte_conductivity_Capiglia1999(c_e, T, T_inf, E_k_e, R_g):
     """
     Conductivity of LiPF6 in EC:DMC as a function of ion concentration. The original
     data is from [1]. The fit is from Dualfoil [2].
@@ -9,6 +12,23 @@ def electrolyte_conductivity_Capiglia1999(c_e):
     properties of non-aqueous electrolyte solutions for rechargeable lithium batteries.
     Journal of power sources 81 (1999): 859-862.
     .. [2] http://www.cchem.berkeley.edu/jsngrp/fortran.html
+    Parameters
+    ----------
+    c_e: :class: `pybamm.Symbol`
+        Dimensional electrolyte concentration
+    T: :class: `pybamm.Symbol`
+        Dimensional temperature
+    T_inf: :class: `pybamm.Parameter`
+        Reference temperature
+    E_k_e: :class: `pybamm.Parameter`
+        Electrolyte conductivity activation energy
+    R_g: :class: `pybamm.Parameter`
+        The ideal gas constant
+
+    Returns
+    -------
+    :`pybamm.Symbol`
+        Solid diffusivity
     """
 
     sigma_e = (
@@ -17,4 +37,7 @@ def electrolyte_conductivity_Capiglia1999(c_e):
         - 1.052 * (c_e / 1000) ** 2
         + 0.1554 * (c_e / 1000) ** 3
     )
-    return sigma_e
+
+    arrhenius = np.exp(E_k_e / R_g * (1 / T_inf - 1 / T))
+
+    return sigma_e * arrhenius
