cd2680526339e5a6e2e098b55284a8b3efc3a795
Valentin Sulzer <valentinsulzer@hotmail.com>
2019-07-18 12:40:01 +0200

    voltage breakdown

--- results/2019_08_sulzer_thesis/lead_acid_discharge.py
+++ results/2019_08_sulzer_thesis/lead_acid_discharge.py
@@ -98,35 +98,57 @@ def plot_voltage_breakdown(all_variables, t_eval):
     file_name = "discharge_voltage_breakdown.eps"
     n = int(len(Crates) // np.sqrt(len(Crates)))
     m = int(np.ceil(len(Crates) / n))
-    fig, axes = plt.subplots(n, m, figsize=(6.4, 4.5))
-    labels = []
+    fig, axes = plt.subplots(n, m, figsize=(6.4, 2.3))
+    labels = ["V", "$V_U$", "$V_k$", "$V_c$", "$V_o$"]
+    overpotentials = [
+        "Average reaction overpotential [V]",
+        "Average concentration overpotential [V]",
+        "Average electrolyte ohmic losses [V]",
+    ]
+    y_min = 0.95 * min(
+        np.nanmin(models_variables[model]["Terminal voltage [V]"](t_eval))
+        for models_variables in all_variables.values()
+    )
+    y_max = 1.05 * max(
+        np.nanmax(models_variables[model]["Terminal voltage [V]"](t_eval))
+        for models_variables in all_variables.values()
+    )
     for k, Crate in enumerate(Crates):
-        models_variables = all_variables[Crate]
+        variables = all_variables[Crate][model]
         ax = axes.flat[k]
-        t_max = max(
-            np.nanmax(var["Time [h]"](t_eval)) for var in models_variables.values()
-        )
+
+        # Set up
+        t_max = np.nanmax(variables["Time [h]"](t_eval))
         ax.set_xlim([0, t_max])
-        ax.set_ylim([10.5, 13])
+        ax.set_ylim([y_min, y_max])
         ax.set_xlabel("Time [h]")
         ax.set_title(
             "\\textbf{{({})}} {}C ($\\mathcal{{C}}_e={}$)".format(
                 chr(97 + k), Crate, Crate * 0.6
             )
         )
-
         ax.xaxis.set_major_locator(plt.MaxNLocator(3))
         if k % m == 0:
             ax.set_ylabel("Voltage [V]")
-        for j, (model, variables) in enumerate(models_variables.items()):
-            ax.plot(
-                variables["Time [h]"](t_eval),
-                variables["Terminal voltage [V]"](t_eval),
-                linestyles[j],
-            )
-    leg = fig.legend(labels, loc="right", frameon=True)
+
+        # Plot
+        # Initialise
+        time = variables["Time [h]"](t_eval)
+        initial_ocv = variables["Average open circuit voltage [V]"](0) * 6
+        ocv = variables["Average open circuit voltage [V]"](t_eval) * 6
+        ax.fill_between(time, ocv, initial_ocv)
+        top = ocv
+        # Plot
+        for j, overpotential in enumerate(overpotentials):
+            bottom = top + variables[overpotential](t_eval) * 6
+            ax.fill_between(time, bottom, top)
+            top = bottom
+        ax.plot(time, variables["Terminal voltage [V]"](t_eval), "k--")
+    leg = axes.flat[-1].legend(
+        labels, bbox_to_anchor=(1.05, 0.5), loc="center left", frameon=True
+    )
     leg.get_frame().set_edgecolor("k")
-    plt.subplots_adjust(bottom=0.25, right=0.95, hspace=1.1, wspace=0.4)
+    fig.tight_layout()
     if OUTPUT_DIR is not None:
         plt.savefig(OUTPUT_DIR + file_name, format="eps", dpi=1000, bbox_inches="tight")
 
@@ -156,9 +178,9 @@ def lead_acid_discharge(compute):
             raise FileNotFoundError(
                 "Run script with '--compute' first to generate results"
             )
-    plot_voltages(all_variables, t_eval)
+    # plot_voltages(all_variables, t_eval)
     # plot_variables(all_variables, t_eval)
-    # plot_voltage_breakdown(all_variables, t_eval)
+    plot_voltage_breakdown(all_variables, t_eval)
 
 
 if __name__ == "__main__":
