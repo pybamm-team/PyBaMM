319b9993402f1e92f9f58a6d1f19097fb235a718
Valentin Sulzer <valentinsulzer@hotmail.com>
2019-07-30 14:58:56 +0100

    #554 2D statevector jacobian

--- tests/unit/test_expression_tree/test_jac_2D.py
+++ tests/unit/test_expression_tree/test_jac_2D.py
@@ -22,64 +22,99 @@ class TestJacobian(unittest.TestCase):
         y0 = np.ones(8)
 
         func = u
-        jacobian = np.zeros((4, 8))
-        jacobian[:2, :2] = np.eye(2)
-        jacobian[2:4, 4:6] = np.eye(2)
+        jacobian = np.array(
+            [
+                [1, 0, 0, 0, 0, 0, 0, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1, 0, 0, 0],
+                [0, 0, 0, 0, 0, 1, 0, 0],
+            ]
+        )
         dfunc_dy = func.jac(y).evaluate(y=y0)
         np.testing.assert_array_equal(jacobian, dfunc_dy.toarray())
 
         func = -v
-        jacobian = np.zeros((4, 8))
-        jacobian[:2, 2:4] = -np.eye(2)
-        jacobian[2:4, 6:8] = -np.eye(2)
+        jacobian = np.array(
+            [
+                [0, 0, -1, 0, 0, 0, 0, 0],
+                [0, 0, 0, -1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, -1, 0],
+                [0, 0, 0, 0, 0, 0, 0, -1],
+            ]
+        )
         dfunc_dy = func.jac(y).evaluate(y=y0)
         np.testing.assert_array_equal(jacobian, dfunc_dy.toarray())
 
         func = 3 * u + 4 * v
-        jacobian = np.zeros((4, 8))
-        jacobian[:2, :2] = 3 * np.eye(2)
-        jacobian[2:4, 4:6] = 3 * np.eye(2)
-        jacobian[:2, 2:4] = 4 * np.eye(2)
-        jacobian[2:4, 6:8] = 4 * np.eye(2)
+        jacobian = np.array(
+            [
+                [3, 0, 4, 0, 0, 0, 0, 0],
+                [0, 3, 0, 4, 0, 0, 0, 0],
+                [0, 0, 0, 0, 3, 0, 4, 0],
+                [0, 0, 0, 0, 0, 3, 0, 4],
+            ]
+        )
         dfunc_dy = func.jac(y).evaluate(y=y0)
         np.testing.assert_array_equal(jacobian, dfunc_dy.toarray())
 
         func = 7 * u - v * 9
-        jacobian = np.zeros((4, 8))
-        jacobian[:2, :2] = 7 * np.eye(2)
-        jacobian[2:4, 4:6] = 7 * np.eye(2)
-        jacobian[:2, 2:4] = -9 * np.eye(2)
-        jacobian[2:4, 6:8] = -9 * np.eye(2)
+        jacobian = np.array(
+            [
+                [7, 0, -9, 0, 0, 0, 0, 0],
+                [0, 7, 0, -9, 0, 0, 0, 0],
+                [0, 0, 0, 0, 7, 0, -9, 0],
+                [0, 0, 0, 0, 0, 7, 0, -9],
+            ]
+        )
         dfunc_dy = func.jac(y).evaluate(y=y0)
         np.testing.assert_array_equal(jacobian, dfunc_dy.toarray())
 
-        A = pybamm.Matrix(2 * eye(2))
+        A = pybamm.Matrix(2 * eye(4))
         func = A @ u
-        jacobian = np.array([[2, 0, 0, 0], [0, 2, 0, 0]])
+        jacobian = np.array(
+            [
+                [2, 0, 0, 0, 0, 0, 0, 0],
+                [0, 2, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 2, 0, 0, 0],
+                [0, 0, 0, 0, 0, 2, 0, 0],
+            ]
+        )
         dfunc_dy = func.jac(y).simplify().evaluate(y=y0)
         np.testing.assert_array_equal(jacobian, dfunc_dy.toarray())
 
         # when differentiating by independent part of the state vector
-        jacobian = np.array([[0, 0], [0, 0]])
-        du_dv = u.jac(v).evaluate().toarray()
-        np.testing.assert_array_equal(du_dv, jacobian)
+        with self.assertRaises(NotImplementedError):
+            u.jac(v)
 
         # test Jacobian of Outer (must set domain to be 'current collector')
         u.domain = ["current collector"]
         func = pybamm.Outer(u, pybamm.Scalar(4))
-        jacobian = np.array([[4, 0, 0, 0], [0, 4, 0, 0]])
+        jacobian = np.array(
+            [
+                [4, 0, 0, 0, 0, 0, 0, 0],
+                [0, 4, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 4, 0, 0, 0],
+                [0, 0, 0, 0, 0, 4, 0, 0],
+            ]
+        )
         dfunc_dy = func.jac(y).evaluate(y=y0)
         np.testing.assert_array_equal(jacobian, dfunc_dy.toarray())
 
         func = pybamm.Outer(u, pybamm.Vector(np.array([1, 2, 3])))
         jacobian = np.array(
             [
-                [1, 0, 0, 0],
-                [2, 0, 0, 0],
-                [3, 0, 0, 0],
-                [0, 1, 0, 0],
-                [0, 2, 0, 0],
-                [0, 3, 0, 0],
+                [1, 0, 0, 0, 0, 0, 0, 0],
+                [2, 0, 0, 0, 0, 0, 0, 0],
+                [3, 0, 0, 0, 0, 0, 0, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0],
+                [0, 2, 0, 0, 0, 0, 0, 0],
+                [0, 3, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1, 0, 0, 0],
+                [0, 0, 0, 0, 2, 0, 0, 0],
+                [0, 0, 0, 0, 3, 0, 0, 0],
+                [0, 0, 0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0, 2, 0, 0],
+                [0, 0, 0, 0, 0, 3, 0, 0],
             ]
         )
         dfunc_dy = func.jac(y).evaluate(y=y0)
@@ -90,136 +125,121 @@ class TestJacobian(unittest.TestCase):
         u = pybamm.StateVector(slice(0, 2), slice(4, 6))
         v = pybamm.StateVector(slice(2, 4), slice(6, 8))
 
-        y0 = np.arange(9)
+        y0 = np.arange(1, 9)
 
         func = v ** 2
-        jacobian = np.array([[0, 0, 6, 0], [0, 0, 0, 8]])
+        jacobian = np.array(
+            [
+                [0, 0, 6, 0, 0, 0, 0, 0],
+                [0, 0, 0, 8, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 14, 0],
+                [0, 0, 0, 0, 0, 0, 0, 16],
+            ]
+        )
         dfunc_dy = func.jac(y).evaluate(y=y0)
         np.testing.assert_array_equal(jacobian, dfunc_dy.toarray())
 
         func = 2 ** v
         jacobian = np.array(
-            [[0, 0, 2 ** 3 * np.log(2), 0], [0, 0, 0, 2 ** 4 * np.log(2)]]
+            [
+                [0, 0, 2 ** 3 * np.log(2), 0, 0, 0, 0, 0],
+                [0, 0, 0, 2 ** 4 * np.log(2), 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 2 ** 7 * np.log(2), 0],
+                [0, 0, 0, 0, 0, 0, 0, 2 ** 8 * np.log(2)],
+            ]
         )
         dfunc_dy = func.jac(y).evaluate(y=y0)
         np.testing.assert_array_equal(jacobian, dfunc_dy.toarray())
 
         func = v ** v
-        jacobian = [[0, 0, 27 * (1 + np.log(3)), 0], [0, 0, 0, 256 * (1 + np.log(4))]]
+        jacobian = [
+            [0, 0, 27 * (1 + np.log(3)), 0, 0, 0, 0, 0],
+            [0, 0, 0, 256 * (1 + np.log(4)), 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 823543 * (1 + np.log(7)), 0],
+            [0, 0, 0, 0, 0, 0, 0, 16777216 * (1 + np.log(8))],
+        ]
         dfunc_dy = func.jac(y).evaluate(y=y0)
         np.testing.assert_array_almost_equal(jacobian, dfunc_dy.toarray())
 
         func = u * v
-        jacobian = np.array([[3, 0, 1, 0], [0, 4, 0, 2]])
+        jacobian = np.array(
+            [
+                [3, 0, 1, 0, 0, 0, 0, 0],
+                [0, 4, 0, 2, 0, 0, 0, 0],
+                [0, 0, 0, 0, 7, 0, 5, 0],
+                [0, 0, 0, 0, 0, 8, 0, 6],
+            ]
+        )
         dfunc_dy = func.jac(y).evaluate(y=y0)
         np.testing.assert_array_equal(jacobian, dfunc_dy.toarray())
 
         func = u * (u + v)
-        jacobian = np.array([[5, 0, 1, 0], [0, 8, 0, 2]])
+        jacobian = np.array(
+            [
+                [5, 0, 1, 0, 0, 0, 0, 0],
+                [0, 8, 0, 2, 0, 0, 0, 0],
+                [0, 0, 0, 0, 17, 0, 5, 0],
+                [0, 0, 0, 0, 0, 20, 0, 6],
+            ]
+        )
         dfunc_dy = func.jac(y).evaluate(y=y0)
         np.testing.assert_array_equal(jacobian, dfunc_dy.toarray())
-
-        func = 1 / u + v / 3
-        jacobian = np.array([[-1, 0, 1 / 3, 0], [0, -1 / 4, 0, 1 / 3]])
-        dfunc_dy = func.jac(y).evaluate(y=y0)
-        np.testing.assert_array_equal(jacobian, dfunc_dy.toarray())
-
-        func = u / v
-        jacobian = np.array([[1 / 3, 0, -1 / 9, 0], [0, 1 / 4, 0, -1 / 8]])
-        dfunc_dy = func.jac(y).evaluate(y=y0)
-        np.testing.assert_array_equal(jacobian, dfunc_dy.toarray())
-
-        func = v / (1 + v)
-        jacobian = np.array([[0, 0, 1 / 16, 0], [0, 0, 0, 1 / 25]])
-        dfunc_dy = func.jac(y).evaluate(y=y0)
-        np.testing.assert_array_equal(jacobian, dfunc_dy.toarray())
-
-        func = pybamm.AbsoluteValue(v)
-        with self.assertRaises(pybamm.UndefinedOperationError):
-            func.jac(y)
 
     def test_functions(self):
         y = pybamm.StateVector(slice(0, 8))
         u = pybamm.StateVector(slice(0, 2), slice(4, 6))
         v = pybamm.StateVector(slice(2, 4), slice(6, 8))
 
-        y0 = np.arange(9)
+        y0 = np.arange(1, 9)
         const = pybamm.Scalar(1)
 
-        func = pybamm.Function(auto_np.sin, u)
-        jacobian = np.array([[np.cos(1), 0, 0, 0], [0, np.cos(2), 0, 0]])
-        dfunc_dy = func.jac(y).evaluate(y=y0)
-        np.testing.assert_array_equal(jacobian, dfunc_dy.toarray())
-
-        func = pybamm.Function(auto_np.cos, v)
-        jacobian = np.array([[0, 0, -np.sin(3), 0], [0, 0, 0, -np.sin(4)]])
-        dfunc_dy = func.jac(y).evaluate(y=y0)
-        np.testing.assert_array_equal(jacobian, dfunc_dy.toarray())
-
-        func = pybamm.Function(auto_np.sin, 3 * u * v)
+        func = pybamm.sin(u)
         jacobian = np.array(
             [
-                [9 * np.cos(9), 0, 3 * np.cos(9), 0],
-                [0, 12 * np.cos(24), 0, 6 * np.cos(24)],
+                [np.cos(1), 0, 0, 0, 0, 0, 0, 0],
+                [0, np.cos(2), 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, np.cos(5), 0, 0, 0],
+                [0, 0, 0, 0, 0, np.cos(6), 0, 0],
             ]
         )
         dfunc_dy = func.jac(y).evaluate(y=y0)
         np.testing.assert_array_equal(jacobian, dfunc_dy.toarray())
 
-        func = pybamm.Function(auto_np.cos, 5 * pybamm.Function(auto_np.exp, u + v))
+        func = pybamm.cos(v)
         jacobian = np.array(
             [
-                [
-                    -5 * np.exp(4) * np.sin(5 * np.exp(4)),
-                    0,
-                    -5 * np.exp(4) * np.sin(5 * np.exp(4)),
-                    0,
-                ],
-                [
-                    0,
-                    -5 * np.exp(6) * np.sin(5 * np.exp(6)),
-                    0,
-                    -5 * np.exp(6) * np.sin(5 * np.exp(6)),
-                ],
+                [0, 0, -np.sin(3), 0, 0, 0, 0, 0],
+                [0, 0, 0, -np.sin(4), 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, -np.sin(7), 0],
+                [0, 0, 0, 0, 0, 0, 0, -np.sin(8)],
+            ]
+        )
+        dfunc_dy = func.jac(y).evaluate(y=y0)
+        np.testing.assert_array_equal(jacobian, dfunc_dy.toarray())
+
+        func = pybamm.sin(3 * u * v)
+        jacobian = np.array(
+            [
+                [9 * np.cos(9), 0, 3 * np.cos(9), 0, 0, 0, 0, 0],
+                [0, 12 * np.cos(24), 0, 6 * np.cos(24), 0, 0, 0, 0],
+                [0, 0, 0, 0, 21 * np.cos(105), 0, 15 * np.cos(105), 0],
+                [0, 0, 0, 0, 0, 24 * np.cos(144), 0, 18 * np.cos(144)],
             ]
         )
         dfunc_dy = func.jac(y).evaluate(y=y0)
         np.testing.assert_array_equal(jacobian, dfunc_dy.toarray())
 
         # when child evaluates to number
-        func = pybamm.Function(auto_np.sin, const)
-        jacobian = np.array([[0, 0, 0, 0]])
+        func = pybamm.sin(const)
+        jacobian = np.array([[0, 0, 0, 0, 0, 0, 0, 0]])
         dfunc_dy = func.jac(y).evaluate(y=y0)
         np.testing.assert_array_equal(jacobian, dfunc_dy.toarray())
 
         # several children
         func = pybamm.Function(test_multi_var_function, 2 * y, 3 * y)
-        jacobian = np.diag(5 * np.ones(4))
+        jacobian = np.diag(5 * np.ones(8))
         dfunc_dy = func.jac(y).evaluate(y=y0)
         np.testing.assert_array_equal(jacobian, dfunc_dy.toarray())
-
-    def test_index(self):
-        vec = pybamm.StateVector(slice(0, 5))
-        ind = pybamm.Index(vec, 3)
-        jac = ind.jac(vec).evaluate(y=np.linspace(0, 2, 5)).toarray()
-        np.testing.assert_array_equal(jac, np.array([[0, 0, 0, 1, 0]]))
-
-        # jac of ind of something that isn't a StateVector should return zeros
-        const_vec = pybamm.Vector(np.ones(3))
-        ind = pybamm.Index(const_vec, 2)
-        jac = ind.jac(vec).evaluate(y=np.linspace(0, 2, 5)).toarray()
-        np.testing.assert_array_equal(jac, np.array([[0, 0, 0, 0, 0]]))
-
-    def test_jac_of_inner(self):
-        a = pybamm.Scalar(1)
-        b = pybamm.Scalar(2)
-        y = pybamm.Variable("y")
-        self.assertEqual(pybamm.inner(a, b).jac(y).evaluate(), 0)
-        self.assertEqual(pybamm.inner(a, y).jac(y).evaluate(), 1)
-        self.assertEqual(pybamm.inner(y, b).jac(y).evaluate(), 2)
-        vec = pybamm.StateVector(slice(0, 2))
-        jac = pybamm.inner(a * vec, b * vec).jac(vec).evaluate(y=np.ones(2)).toarray()
-        np.testing.assert_array_equal(jac, 4 * np.eye(2))
 
 
 if __name__ == "__main__":
