#546 fix quite composite averaged model

results/2019_08_sulzer_thesis/2D/2D_lead_acid_discharge_poorly_conducting.py
@@ -87,7 +87,7 @@ def discharge_states(compute):
     if compute:
         models = [
             pybamm.lead_acid.NewmanTiedemann(
-                {"surface form": "differential", "dimensionality": 1}, name="1D Full"
+                {"surface form": "algebraic", "dimensionality": 1}, name="1D Full"
             ),
             pybamm.lead_acid.NewmanTiedemann(
                 {"dimensionality": 1, "current collector": "potential pair"},
@@ -107,7 +107,7 @@ def discharge_states(compute):
             ),
         ]
         Crates = [0.1, 1, 2]
-        sigmas = [10 * 8000]  # , 100 * 8000, 1000 * 8000]
+        sigmas = [8000, 10 * 8000, 100 * 8000]  # , 1000 * 8000]
 
         t_eval = np.linspace(0, 1, 100)
         extra_parameter_values = {}  # "Bruggeman coefficient": 0.001}
@@ -129,8 +129,8 @@ def discharge_states(compute):
             raise FileNotFoundError(
                 "Run script with '--compute' first to generate results"
             )
-    # plot_voltages(all_variables, t_eval)
-    # plot_variables(all_variables, t_eval)
+    plot_voltages(all_variables, t_eval)
+    plot_variables(all_variables, t_eval)
     plot_variables_x_z(all_variables, t_eval)
 
 

results/2019_08_sulzer_thesis/2D/2D_lead_acid_discharge_quite_conducting.py
@@ -19,7 +19,11 @@ except ImportError:
 def plot_voltages(all_variables, t_eval):
     Crates = [0.1, 1, 2]
     all_variables = {k: v for k, v in all_variables.items() if k in Crates}
-    shared_plotting_2D.plot_voltages(all_variables, t_eval)
+    linestyles = ["k:", "k-", "g--", "b-."]
+    linewidths = [0.7, 1.4, 1.4, 1.4]
+    shared_plotting_2D.plot_voltages(
+        all_variables, t_eval, linestyles=linestyles, linewidths=linewidths
+    )
     file_name = "2d_quite_discharge_voltage_comparison.eps"
     if OUTPUT_DIR is not None:
         plt.savefig(OUTPUT_DIR + file_name, format="eps", dpi=1000)
@@ -32,26 +36,55 @@ def plot_variables(all_variables, t_eval):
     var_file_names = {
         "X-averaged electrolyte concentration [Molar]"
         + "": "2d_quite_discharge_average_electrolyte_concentration_comparison.eps",
-        "X-averaged electrolyte potential [V]"
-        + "": "2d_quite_discharge_average_electrolyte_potential_comparison.eps",
+        # "X-averaged electrolyte potential [V]"
+        # + "": "2d_quite_discharge_average_electrolyte_potential_comparison.eps",
         "Current collector current density"
         + "": "2d_quite_current_collector_current_density_comparison.eps",
     }
     limits_exceptions = {"X-averaged electrolyte concentration [Molar]": {"min": 0}}
+    linestyles = ["k:", "k-", "g--", "b-."]
+    linewidths = [0.7, 1.4, 1.4, 1.4]
     all_variables = {k: v for k, v in all_variables.items() if k in Crates}
     for var, file_name in var_file_names.items():
         if var in limits_exceptions:
             exceptions = limits_exceptions[var]
         else:
             exceptions = {}
-        shared_plotting_2D.plot_variable(all_variables, times, var, exceptions)
+        shared_plotting_2D.plot_variable(
+            all_variables,
+            times,
+            var,
+            exceptions,
+            linestyles=linestyles,
+            linewidths=linewidths,
+        )
+        if OUTPUT_DIR is not None:
+            plt.savefig(OUTPUT_DIR + file_name, format="eps", dpi=1000)
+
+
+def plot_variables_x_z(all_variables, t_eval):
+    # Set up
+    Crates = [0.1, 1, 2]
+    times = np.array([0, 0.195, 0.375, 0.545])
+    var_file_names = {
+        "Electrolyte concentration [Molar]"
+        + "": "2d_quite_discharge_electrolyte_concentration_comparison.eps"
+    }
+    limits_exceptions = {"Electrolyte concentration [Molar]": {"min": 0}}
+    all_variables = {k: v for k, v in all_variables.items() if k in Crates}
+    for var, file_name in var_file_names.items():
+        if var in limits_exceptions:
+            exceptions = limits_exceptions[var]
+        else:
+            exceptions = {}
+        shared_plotting_2D.plot_variable_x_z(all_variables, times, var, exceptions)
         if OUTPUT_DIR is not None:
             plt.savefig(OUTPUT_DIR + file_name, format="eps", dpi=1000)
 
 
 def plot_voltage_components(all_variables, t_eval):
-    Crates = [0.1, 2, 5]
-    model = "1+1D Composite averaged"
+    Crates = [0.1, 1, 2]
+    model = "1+1D Composite"
     shared_plotting_2D.plot_voltage_components(all_variables, t_eval, model, Crates)
     file_name = "2d_discharge_voltage_components.eps"
     if OUTPUT_DIR is not None:
@@ -63,19 +96,19 @@ def discharge_states(compute):
     if compute:
         models = [
             pybamm.lead_acid.NewmanTiedemann(
+                {"surface form": "algebraic", "dimensionality": 1}, name="1D Full"
+            ),
+            pybamm.lead_acid.NewmanTiedemann(
+                {"dimensionality": 1, "current collector": "potential pair"},
+                name="1+1D Full",
+            ),
+            pybamm.lead_acid.LOQS(
                 {
                     "dimensionality": 1,
                     "current collector": "potential pair quite conductive",
                 },
-                name="1+1D Full",
+                name="1+1D LOQS",
             ),
-            # pybamm.lead_acid.LOQS(
-            #     {
-            #         "dimensionality": 1,
-            #         "current collector": "potential pair quite conductive",
-            #     },
-            #     name="1+1D LOQS",
-            # ),
             # pybamm.lead_acid.FOQS(
             #     {"dimensionality": 1, "current collector": "potential pair"},
             #     name="FOQS",
@@ -87,16 +120,16 @@ def discharge_states(compute):
                 },
                 name="1+1D Composite",
             ),
-            pybamm.lead_acid.CompositeExtended(
-                {
-                    "dimensionality": 1,
-                    "current collector": "potential pair quite conductive averaged",
-                },
-                name="1+1D Composite",
-            ),
+            # pybamm.lead_acid.CompositeExtended(
+            #     {
+            #         "dimensionality": 1,
+            #         "current collector": "potential pair quite conductive averaged",
+            #     },
+            #     name="1+1D Composite",
+            # ),
         ]
         Crates = [0.1, 1, 2]
-        sigmas = [80, 800, 8000]
+        sigmas = [8000, 10 * 8000, 100 * 8000]
 
         t_eval = np.linspace(0, 1, 100)
         extra_parameter_values = {}  # "Bruggeman coefficient": 0.001}
@@ -120,77 +153,8 @@ def discharge_states(compute):
             )
     plot_voltages(all_variables, t_eval)
     plot_variables(all_variables, t_eval)
-
-
-def plot_errors(models_times_and_voltages):
-    npts = 20
-    linestyles = ["k-", "g--", "r:", "b-."]
-    Crates = defaultdict(list)
-    voltage_errors = defaultdict(list)
-    fig, ax = plt.subplots(1, 1)
-    for i, (model, times_and_voltages) in enumerate(models_times_and_voltages.items()):
-        if model != "Full":
-            for Crate, variables in times_and_voltages[npts].items():
-                Crates[model].append(Crate)
-                full_voltage = models_times_and_voltages["Full"][npts][Crate][
-                    "Battery voltage [V]"
-                ]
-                reduced_voltage = variables["Battery voltage [V]"]
-                voltage_errors[model].append(pybamm.rmse(full_voltage, reduced_voltage))
-            ax.semilogx(
-                Crates[model], voltage_errors[model], linestyles[i], label=model
-            )
-    ax.set_xlabel("C-rate")
-    ax.set_ylabel("RMSE [V]")
-    ax.legend(loc="best")
-    fig.tight_layout()
-    file_name = "discharge_asymptotics_rmse.eps"
-    if OUTPUT_DIR is not None:
-        plt.savefig(OUTPUT_DIR + file_name, format="eps", dpi=1000)
-
-
-def plot_times(models_times_and_voltages):
-    shared_plotting_2D.plot_times(models_times_and_voltages, Crate=1)
-    file_name = "discharge_asymptotics_solver_times.eps"
-    if OUTPUT_DIR is not None:
-        plt.savefig(OUTPUT_DIR + file_name, format="eps", dpi=1000)
-
-
-def discharge_times_and_errors(compute):
-    savefile = "discharge_asymptotics_times_and_errors.pickle"
-    if compute:
-        try:
-            with open(savefile, "rb") as f:
-                models_times_and_voltages = pickle.load(f)
-        except FileNotFoundError:
-            models_times_and_voltages = pybamm.get_infinite_nested_dict()
-        models = [
-            pybamm.lead_acid.NewmanTiedemann(
-                {"surface form": "algebraic"}, name="Full"
-            ),
-            pybamm.lead_acid.LOQS(name="LOQS"),
-            # pybamm.lead_acid.FOQS(name="FOQS"),
-            # pybamm.lead_acid.Composite(name="Composite"),
-        ]
-        Crates = np.linspace(0.01, 5, 2)
-        all_npts = [20]
-        t_eval = np.linspace(0, 1, 100)
-        new_models_times_and_voltages = convergence_study(
-            models, Crates, all_npts, t_eval
-        )
-        models_times_and_voltages.update(new_models_times_and_voltages)
-        with open(savefile, "wb") as f:
-            pickle.dump(models_times_and_voltages, f, pickle.HIGHEST_PROTOCOL)
-    else:
-        try:
-            with open(savefile, "rb") as f:
-                models_times_and_voltages = pickle.load(f)
-        except FileNotFoundError:
-            raise FileNotFoundError(
-                "Run script with '--compute' first to generate results"
-            )
-    plot_errors(models_times_and_voltages)
-    plot_times(models_times_and_voltages)
+    # plot_variables_x_z(all_variables, t_eval)
+    plot_voltage_components(all_variables, t_eval)
 
 
 if __name__ == "__main__":

results/2019_08_sulzer_thesis/2D/shared_plotting_2D.py
@@ -22,7 +22,7 @@ def plot_voltages(
     linewidths = linewidths or [1.4, 1.4, 1.4, 1.4]
     n = len(list(all_variables.values())[0])
     m = len(all_variables)
-    fig, axes = plt.subplots(n, m, figsize=figsize)
+    fig, axes = plt.subplots(n, m, sharex=True, sharey=True, figsize=figsize)
     y_min = 0.98 * min(
         np.nanmin(variables["Battery voltage [V]"](t_eval))
         for allsigma_models_variables in all_variables.values()
@@ -49,7 +49,8 @@ def plot_voltages(
             )
             ax.set_xlim([0, t_max])
             ax.set_ylim([y_min, y_max])
-            ax.set_xlabel("Time [h]")
+            if l == len(allsigma_models_variables) - 1:
+                ax.set_xlabel("Time [h]")
             if l == 0:
                 ax.set_title(
                     "\\textbf{{({})}} {}C ($\\mathcal{{C}}_e={}$)".format(
@@ -65,12 +66,15 @@ def plot_voltages(
                 # ax.xaxis.set_ticks_position("bottom")
             ax.xaxis.set_major_locator(plt.MaxNLocator(3))
             if k % m == 0:
+                sigma_exponent = int(np.floor(np.log10(sigma)))
+                sigma_dash = 0.05 * sigma / 8000
                 ax.set_ylabel(
-                    "$\\hat{{\\sigma}}_p = {} S/m$\n$(\\sigma\\prime_p={})$".format(
-                        sigma, sigma
-                    ),
+                    (
+                        "$\\hat{{\\sigma}}_p = {}\\times 10^{{{}}}$ S/m"
+                        + "\n$(\\sigma'_p={}/\\mathcal{{C}})$"
+                    ).format(sigma / 10 ** sigma_exponent, sigma_exponent, sigma_dash),
                     rotation=0,
-                    labelpad=40,
+                    labelpad=50,
                 )
                 ax.yaxis.get_label().set_verticalalignment("center")
 
@@ -85,7 +89,7 @@ def plot_voltages(
                 )
     leg = fig.legend(labels, loc="lower center", ncol=len(labels))
     plt.subplots_adjust(
-        bottom=0.17, top=0.95, left=0.2, right=0.97, hspace=0.08, wspace=0.05
+        bottom=0.17, top=0.95, left=0.28, right=0.97, hspace=0.08, wspace=0.05
     )
     leg.get_frame().set_edgecolor("k")
     return fig, axes
@@ -203,22 +207,17 @@ def plot_variable(
 
 
 def plot_variable_x_z(
-    all_variables,
-    times,
-    variable,
-    limits_exceptions=None,
-    yaxis="SOC",
-    figsize=(6.4, 4),
+    all_variables, times, variable, limits_exceptions=None, figsize=(6.4, 6.4)
 ):
-    Crate = 0.1
-    sigma = 80000
+    Crate = 1
+    sigma = 8000
     models = ["1+1D LOQS", "1+1D Composite", "1+1D Full"]
     time = 0.1
     limits_exceptions = limits_exceptions or {}
     n = 1  # len(times)
     m = 3  # len(all_variables)
     models_variables = all_variables[Crate][sigma]
-    fig, axes = plt.subplots(n, m, figsize=figsize)
+    fig = plt.figure(figsize=figsize)
 
     x = models_variables[models[0]]["x"](0, x=np.linspace(0.1, 0.9))[:, 0]
     x_dim = models_variables[models[0]]["x [m]"](0, x=np.linspace(0.1, 0.9))[:, 0]
@@ -238,14 +237,32 @@ def plot_variable_x_z(
             for variables in models_variables.values()
         ]
     )
-    if "min" in limits_exceptions:
-        v_min = min(v_min, limits_exceptions["min"])
-    if "max" in limits_exceptions:
-        v_max = max(v_max, limits_exceptions["max"])
+    v_min_errors = pybamm.ax_min(
+        [
+            np.nanmin(
+                variables[variable](time, x=x, z=z)
+                - models_variables["1+1D Full"][variable](time, x=x, z=z)
+            )
+            for variables in models_variables.values()
+        ]
+    )
+    v_max_errors = pybamm.ax_max(
+        [
+            np.nanmax(
+                variables[variable](time, x=x, z=z)
+                - models_variables["1+1D Full"][variable](time, x=x, z=z)
+            )
+            for variables in models_variables.values()
+        ]
+    )
+    # if "min" in limits_exceptions:
+    #     v_min = min(v_min, limits_exceptions["min"])
+    # if "max" in limits_exceptions:
+    #     v_max = max(v_max, limits_exceptions["max"])
 
     # Plot
     for i, model in enumerate(models):
-        ax = axes.flat[i]
+        ax = fig.add_subplot(2, 3, i + 1)
 
         # Title
         ax.set_title(model)
@@ -256,15 +273,46 @@ def plot_variable_x_z(
             ax.set_ylabel("z [m]")
         else:
             ax.set_yticklabels([])
-        CS = ax.pcolormesh(
+        CS = ax.contourf(
             x_dim,
             z_dim,
             np.transpose(models_variables[model][variable](time, x=x, z=z)),
-            # vmin=v_min,
-            # vmax=v_max,
-            # levels=100,
+            vmin=v_min,
+            vmax=v_max,
+            levels=100,
         )
-    cbar = fig.colorbar(CS)
+        for c in CS.collections:
+            c.set_edgecolor("face")
+        ax1 = ax
+
+        if i < 2:
+            ax = fig.add_subplot(2, 2, i + 3)
+
+            # Title
+            ax.set_title(model + " (error)")
+            # x-axis
+            ax.set_xlabel("x [m]")
+            # y-axis
+            if i == 0:
+                ax.set_ylabel("z [m]")
+            else:
+                ax.set_yticklabels([])
+            error = models_variables["1+1D Full"][variable](
+                time, x=x, z=z
+            ) - models_variables[model][variable](time, x=x, z=z)
+            CS2 = ax.contourf(
+                x_dim,
+                z_dim,
+                np.transpose(error),
+                vmin=v_min_errors,
+                vmax=v_max_errors,
+                levels=100,
+            )
+            for c in CS2.collections:
+                c.set_edgecolor("face")
+            ax2 = ax
+    cbar = fig.colorbar(CS, ax=ax1)
+    cbar2 = fig.colorbar(CS2, ax=ax2)
 
     # Electrodes
     L_n = pybamm.lead_acid.BaseModel().default_parameter_values.process_symbol(
@@ -276,33 +324,36 @@ def plot_variable_x_z(
     # ax.plot([L_n, L_n], [0, np.max(z_dim)], "k--")
 
     # Plot
-    # plt.subplots_adjust(
-    #     bottom=0.17, top=0.95, left=0.18, right=0.97, hspace=0.08, wspace=0.05
-    # )
-    return fig, axes
+    plt.subplots_adjust(hspace=0.35, wspace=0.05)
+    return fig
 
 
 def plot_voltage_components(all_variables, t_eval, model, Crates):
+    sigma = 10 * 8000
     n = int(len(Crates) // np.sqrt(len(Crates)))
     m = int(np.ceil(len(Crates) / n))
     fig, axes = plt.subplots(n, m, figsize=(6.4, 2.3))
     labels = ["V", "$V_U$", "$V_k$", "$V_c$", "$V_o$", "$V_{cc}$"]
     overpotentials = [
-        "Average battery reaction overpotential [V]",
-        "Average battery concentration overpotential [V]",
-        "Average battery electrolyte ohmic losses [V]",
-        "Current collector losses [V]",
+        "X-averaged battery reaction overpotential [V]",
+        "X-averaged battery concentration overpotential [V]",
+        "X-averaged battery electrolyte ohmic losses [V]",
+        # "Current collector losses [V]",
     ]
     y_min = 0.95 * min(
-        np.nanmin(models_variables[model]["Battery voltage [V]"](t_eval))
-        for models_variables in all_variables.values()
+        np.nanmin(
+            allsigma_models_variables[sigma][model]["Battery voltage [V]"](t_eval)
+        )
+        for allsigma_models_variables in all_variables.values()
     )
     y_max = 1.05 * max(
-        np.nanmax(models_variables[model]["Battery voltage [V]"](t_eval))
-        for models_variables in all_variables.values()
+        np.nanmax(
+            allsigma_models_variables[sigma][model]["Battery voltage [V]"](t_eval)
+        )
+        for allsigma_models_variables in all_variables.values()
     )
     for k, Crate in enumerate(Crates):
-        variables = all_variables[Crate][model]
+        variables = all_variables[Crate][sigma][model]
         ax = axes.flat[k]
 
         # Set up
@@ -323,16 +374,16 @@ def plot_voltage_components(all_variables, t_eval, model, Crates):
         # Initialise
         # for lead-acid we multiply everything by 6 to
         time = variables["Time [h]"](t_eval)
-        initial_ocv = variables["Average battery open circuit voltage [V]"](0)
-        ocv = variables["Average battery open circuit voltage [V]"](t_eval)
+        initial_ocv = variables["X-averaged battery open circuit voltage [V]"](0, z=1)
+        ocv = variables["X-averaged battery open circuit voltage [V]"](t_eval, z=1)
         ax.fill_between(time, ocv, initial_ocv)
         top = ocv
         # Plot
         for overpotential in overpotentials:
-            bottom = top + variables[overpotential](t_eval)
+            bottom = top + variables[overpotential](t_eval, z=1)
             ax.fill_between(time, bottom, top)
             top = bottom
-        ax.plot(time, variables["Battery voltage [V]"](t_eval), "k--")
+        ax.plot(time, variables["Battery voltage [V]"](t_eval, z=1), "k--")
     leg = axes.flat[-1].legend(
         labels, bbox_to_anchor=(1.05, 0.5), loc="center left", frameon=True
     )