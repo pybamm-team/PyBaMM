11e85f5751dd60316036c4f42d72b0a020517c88
Robert Timms <timms@maths.ox.ac.uk>
2019-08-12 17:24:02 +0100

    #584 add discharge rest script

--- pybamm/parameters/parameter_values.py
+++ pybamm/parameters/parameter_values.py
@@ -333,7 +333,9 @@ class ParameterValues(dict):
                     # Need to update parameters dict to be that of the new current
                     # function and make new parameters_eval dict to be processed
                     x.function.parameters = self["Current function"].parameters
-                    x.function.parameters_eval = dict.fromkeys(x.function.parameters.keys())
+                    x.function.parameters_eval = dict.fromkeys(
+                        x.function.parameters.keys()
+                    )
                     for param, sym in x.function.parameters.items():
                         # Need to process again as new symbols may be passed
                         # e.g. may explicitly pass pybamm.Scalar(1) instead of
@@ -342,12 +344,6 @@ class ParameterValues(dict):
                             new_sym = self.process_symbol(sym)
                             x.function.parameters[param] = new_sym
                             x.function.parameters_eval[param] = new_sym.evaluate()
-                        #if isinstance(sym, pybamm.Scalar):
-                        #    try:
-                        #        x.function.parameters_eval[param] = self.process_symbol(sym)
-                        #    except KeyError:
-                        #        # KeyError -> name not in parameter dict, don't update
-                        #        continue
                     if isinstance(x.function, pybamm.GetCurrentData):
                         # update interpolant
                         x.function.interpolate()
--- results/drive_cycles/discharge_rest.py
+++ results/drive_cycles/discharge_rest.py
@@ -1,8 +1,12 @@
+#
+# Simulate discharge followed by rest
+#
 import pybamm
 import numpy as np
 import matplotlib.pyplot as plt
 
 # load model
+pybamm.set_logging_level("INFO")
 model = pybamm.lithium_ion.SPMe()
 
 # create geometry
@@ -10,6 +14,11 @@ geometry = model.default_geometry
 
 # load parameter values and process model and geometry
 param = model.default_parameter_values
+# adjust current to correspond to a typical current density of 24 [A.m-2]
+C_rate = 1
+param["Typical current [A]"] = (
+    C_rate * 24 * param.process_symbol(pybamm.geometric_parameters.A_cc).evaluate()
+)
 param.process_model(model)
 param.process_geometry(geometry)
 
@@ -20,12 +29,16 @@ mesh = pybamm.Mesh(geometry, model.default_submesh_types, model.default_var_pts)
 disc = pybamm.Discretisation(mesh, model.default_spatial_methods)
 disc.process_model(model)
 
-# solve model
-t_eval1 = np.linspace(0, 0.2, 100)
+# solve model during discharge stage (1 hour)
+tau = param.process_symbol(pybamm.standard_parameters_lithium_ion.tau_discharge)
+t_end = 3600 / tau.evaluate(0)
+t_eval1 = np.linspace(0, t_end, 120)
 solution1 = model.default_solver.solve(model, t_eval1)
 
-# process vars
-solution1 = model.default_solver.solve(model, t_eval1)
+# process variables for later plotting
+time1 = pybamm.ProcessedVariable(
+    model.variables["Time [h]"], solution1.t, solution1.y
+)
 voltage1 = pybamm.ProcessedVariable(
     model.variables["Terminal voltage [V]"], solution1.t, solution1.y, mesh=mesh
 )
@@ -34,15 +47,24 @@ current1 = pybamm.ProcessedVariable(
 )
 
 # solve again with zero current, using last step of solution1 as initial conditions
-new_ics = solution1.y[:, -1][:, np.newaxis]
-model.concatenated_initial_conditons = new_ics
-import ipdb; ipdb.set_trace()
+# update the current to be zero
 param["Current function"] = pybamm.GetConstantCurrent(current=pybamm.Scalar(0))
 param.update_model(model, disc)
+# Note: need to update model.concatenated_initial_conditions *after* update_model,
+# as update_model updates model.concatenated_initial_conditions, by concatenting
+# the (unmodified) initial conditions for each variable
+model.concatenated_initial_conditions = solution1.y[:, -1][:, np.newaxis]
 
-
-t_eval2 = np.linspace(solution1.t[-1], 1, 100)
+# simulate 1 hour of rest
+t_start = solution1.t[-1]
+t_end = t_start + 3600 / tau.evaluate(0)
+t_eval2 = np.linspace(t_start, t_end, 120)
 solution2 = model.default_solver.solve(model, t_eval2)
+
+# process variables for later plotting
+time2 = pybamm.ProcessedVariable(
+    model.variables["Time [h]"], solution2.t, solution2.y
+)
 voltage2 = pybamm.ProcessedVariable(
     model.variables["Terminal voltage [V]"], solution2.t, solution2.y, mesh=mesh
 )
@@ -52,12 +74,12 @@ current2 = pybamm.ProcessedVariable(
 
 # plot
 plt.subplot(121)
-plt.plot(t_eval1, voltage1(t_eval1), t_eval2, voltage2(t_eval2))
-plt.xlabel('t')
+plt.plot(time1(t_eval1), voltage1(t_eval1), time2(t_eval2), voltage2(t_eval2))
+plt.xlabel('Time [h]')
 plt.ylabel('Voltage [V]')
 plt.subplot(122)
 z = np.linspace(0, 1, 10)
-plt.plot(t_eval1, current1(t_eval1), t_eval2, current2(t_eval2))
-plt.xlabel('t')
+plt.plot(time1(t_eval1), current1(t_eval1), time2(t_eval2), current2(t_eval2))
+plt.xlabel('Time [h]')
 plt.ylabel('Current [A]')
 plt.show()
