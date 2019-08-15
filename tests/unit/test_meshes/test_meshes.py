08e148998f2803babb3b1a51ba1e12c018587dc6
Martin Robinson <martinjrobins@gmail.com>
2019-08-15 07:17:52 +0100

    #568 create test to reproduce bug

--- tests/unit/test_meshes/test_meshes.py
+++ tests/unit/test_meshes/test_meshes.py
@@ -7,6 +7,35 @@ import unittest
 
 
 class TestMesh(unittest.TestCase):
+    def test_mesh_creation_no_parameters(self):
+        r = pybamm.SpatialVariable(
+            "r", domain=["negative particle"], coord_sys="spherical polar"
+        )
+
+        geometry = {
+            "negative particle": {
+                "primary": {r: {"min": pybamm.Scalar(0), "max": pybamm.Scalar(1)}}
+            }
+        }
+
+        submesh_types = {"negative particle": pybamm.Uniform1DSubMesh}
+        var_pts = {r: 20}
+        mesh = pybamm.Mesh(geometry, submesh_types, var_pts)
+
+        # create mesh
+        mesh = pybamm.Mesh(geometry, submesh_types, var_pts)
+
+        # check boundary locations
+        self.assertEqual(mesh["negative particle"][0].edges[0], 0)
+        self.assertEqual(mesh["positive particle"][0].edges[-1], 1)
+
+        # check number of edges and nodes
+        self.assertEqual(len(mesh["negative particle"][0].nodes), var_pts[r])
+        self.assertEqual(
+            len(mesh["negative particle"][0].edges),
+            len(mesh["negative particle"][0].nodes) + 1
+            )
+
     def test_mesh_creation(self):
         param = pybamm.ParameterValues(
             base_parameters={
