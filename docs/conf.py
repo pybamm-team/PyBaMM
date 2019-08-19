3be085a2a501942c2edc6505986a407df6925802
Robert Timms <timms@maths.ox.ac.uk>
2019-08-19 15:10:31 +0100

    try to fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -101,7 +101,11 @@ pygments_style = None
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+
+# Note: changing this to [] seems to fix it and is discussed online. Not sure
+# why this has changed?
+# html_static_path = ["_static"]
+html_static_path = []
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
