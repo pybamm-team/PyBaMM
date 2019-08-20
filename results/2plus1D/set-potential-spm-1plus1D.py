45ec923f1cbe886b7363fc7ea33be678550703cb
tom <t.g.tranter@gmail.com>
2019-08-20 13:41:59 +0100

    take out non-dimensionalizing

--- results/2plus1D/set-potential-spm-1plus1D.py
+++ results/2plus1D/set-potential-spm-1plus1D.py
@@ -22,8 +22,9 @@ param.process_model(model)
 param.process_geometry(geometry)
 
 # set mesh
+nbat = 10
 var = pybamm.standard_spatial_vars
-var_pts = {var.x_n: 5, var.x_s: 5, var.x_p: 5, var.r_n: 5, var.r_p: 5, var.z: 10}
+var_pts = {var.x_n: 5, var.x_s: 5, var.x_p: 5, var.r_n: 5, var.r_p: 5, var.z: nbat}
 # depnding on number of points in y-z plane may need to increase recursion depth...
 sys.setrecursionlimit(10000)
 mesh = pybamm.Mesh(geometry, model.default_submesh_types, var_pts)
@@ -62,16 +63,19 @@ def non_dim_potential(phi_dim, domain):
 
 
 # solve model -- replace with step
-t_eval1 = np.linspace(0, 0.1, 10)
+t_eval1 = np.linspace(0, 0.1, 20)
 solution1 = model.default_solver.solve(model, t_eval1)
 voltage_step1 = pybamm.ProcessedVariable(
     model.variables["Terminal voltage [V]"], solution1.t, solution1.y, mesh=mesh
 )
 current_step1 = pybamm.ProcessedVariable(
-    model.variables["Terminal voltage [V]"], solution1.t, solution1.y, mesh=mesh
+    model.variables["Current [A]"], solution1.t, solution1.y, mesh=mesh
 )
 heating_step1 = pybamm.ProcessedVariable(
     model.variables["X-averaged total heating [A.V.m-3]"], solution1.t, solution1.y, mesh=mesh
+)
+particle_step1 = pybamm.ProcessedVariable(
+    model.variables["X-averaged positive particle surface concentration [mol.m-3]"], solution1.t, solution1.y, mesh=mesh
 )
 
 
@@ -84,44 +88,66 @@ phi_s_cn_dim_new = current_state[model.variables["Negative current collector pot
 
 #phi_s_cp_dim_new = 3.3 * np.ones(var_pts[var.z]) - 0.05 * np.linspace(0, 1, var_pts[var.z])
 #phi_s_cp_dim_new = 3.3 * np.ones(var_pts[var.z])
-sf_cp = 0.0
-phi_s_cp_dim_new = current_state[model.variables["Positive current collector potential"].y_slices] - sf * np.linspace(0, 1, var_pts[var.z])
+sf_cp = 1e-2
+phi_s_cp_dim_new = current_state[model.variables["Positive current collector potential"].y_slices] - sf_cp * np.linspace(0, 1, var_pts[var.z])
+#variables = {
+#    "Negative current collector potential": non_dim_potential(
+#        phi_s_cn_dim_new, "negative"
+#    ),
+#    "Positive current collector potential": non_dim_potential(
+#        phi_s_cp_dim_new, "positive"
+#    ),
+#}
+
 variables = {
-    "Negative current collector potential": non_dim_potential(
-        phi_s_cn_dim_new, "negative"
-    ),
-    "Positive current collector potential": non_dim_potential(
-        phi_s_cp_dim_new, "positive"
-    ),
+    "Negative current collector potential": phi_s_cn_dim_new,
+    "Positive current collector potential": phi_s_cp_dim_new,
 }
 
-#new_state = update_statevector(variables, current_state)
+new_state = update_statevector(variables, current_state)
 
 # solve again -- replace with step
 # use new state as initial condition
-#model.concatenated_initial_conditions = new_state
-model.concatenated_initial_conditions = current_state[np.newaxis, :]
-t_eval2 = np.linspace(0.1, 0.2, 10)
+model.concatenated_initial_conditions = new_state
+#model.concatenated_initial_conditions = current_state[:, np.newaxis]
+t_eval2 = np.linspace(0.1, 0.2, 20)
 solution2 = model.default_solver.solve(model, t_eval2)
 voltage_step2 = pybamm.ProcessedVariable(
     model.variables["Terminal voltage [V]"], solution2.t, solution2.y, mesh=mesh
 )
+current_step2 = pybamm.ProcessedVariable(
+    model.variables["Current [A]"], solution2.t, solution2.y, mesh=mesh
+)
 heating_step2 = pybamm.ProcessedVariable(
     model.variables["X-averaged total heating [A.V.m-3]"], solution2.t, solution2.y, mesh=mesh
 )
-
+particle_step2 = pybamm.ProcessedVariable(
+    model.variables["X-averaged positive particle surface concentration [mol.m-3]"], solution2.t, solution2.y, mesh=mesh
+)
 # plot
-plt.figure()
-plt.plot(t_eval1, voltage_step1(t_eval1), t_eval2, voltage_step2(t_eval2))
-plt.xlabel('t')
-plt.ylabel('Voltage [V]')
-plt.show()
-plt.figure()
+#plt.figure()
+#plt.plot(t_eval1, voltage_step1(t_eval1), t_eval2, voltage_step2(t_eval2))
+#plt.xlabel('t')
+#plt.ylabel('Voltage [V]')
+#plt.show()
+#plt.figure()
+#plt.plot(t_eval1, current_step1(t_eval1), t_eval2, current_step2(t_eval2))
+#plt.xlabel('t')
+#plt.ylabel('Current [A]')
+#plt.show()
+#plt.figure()
 z = np.linspace(0, 1, 10)
-plt.plot(t_eval1, heating_step1(t_eval1, z=z), t_eval2, heating_step2(t_eval2, z=z))
+for bat_id in range(nbat):
+    plt.plot(t_eval1, heating_step1(t_eval1, z=z)[bat_id, :], t_eval2, heating_step2(t_eval2, z=z)[bat_id, :])
 plt.xlabel('t')
 plt.ylabel('X-averaged total heating [A.V.m-3]')
 plt.yscale('log')
+plt.show()
+plt.figure()
+for bat_id in range(nbat):
+    plt.plot(t_eval1, particle_step1(t_eval1,z=z)[bat_id, :], t_eval2, particle_step2(t_eval2,z=z)[bat_id, :])
+plt.xlabel('t')
+plt.ylabel('X-averaged positive particle surface concentration [mol.m-3]')
 plt.show()
 
 
