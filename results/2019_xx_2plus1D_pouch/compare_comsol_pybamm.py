1bb0b96992e43fd9a3452d33c5a12940b26a25cd
Robert Timms <timms@maths.ox.ac.uk>
2019-09-18 15:30:47 +0100

    script for potentials and temperature

---
+++ results/2019_xx_2plus1D_pouch/compare_comsol_pybamm.py
@@ -0,0 +1,213 @@
+import pybamm
+import numpy as np
+import os
+import sys
+import pickle
+import scipy.interpolate as interp
+import matplotlib.pyplot as plt
+
+# change working directory to the root of pybamm
+os.chdir(pybamm.root_dir())
+
+# increase recursion limit for large expression trees
+sys.setrecursionlimit(10000)
+
+"-----------------------------------------------------------------------------"
+"Pick C_rate and load comsol data"
+
+# C_rate
+# NOTE: the results in pybamm stop when a voltage cutoff is reached, so
+# for higher C-rate the pybamm solution may stop before the comsol solution
+C_rates = {"01": 0.1, "05": 0.5, "1": 1, "2": 2, "3": 3}
+C_rate = "1"  # choose the key from the above dictionary of available results
+
+# load the comsol results
+comsol_variables = pickle.load(open("comsol_{}C.pickle".format(C_rate), "rb"))
+
+"-----------------------------------------------------------------------------"
+"Create and solve pybamm model"
+
+# load model and geometry
+pybamm.set_logging_level("INFO")
+options = {
+    "current collector": "potential pair",
+    "dimensionality": 2,
+    "thermal": "x-lumped",
+}
+pybamm_model = pybamm.lithium_ion.SPM(options)
+pybamm_model.use_simplify_jacobian = False
+geometry = pybamm_model.default_geometry
+
+# load parameters and process model and geometry
+param = pybamm_model.default_parameter_values
+# adjust current to correspond to a typical current density of 24 [A.m-2]
+param["Typical current [A]"] = (
+    C_rates[C_rate]
+    * 24
+    * param.process_symbol(pybamm.geometric_parameters.A_cc).evaluate()
+)
+param.process_model(pybamm_model)
+param.process_geometry(geometry)
+
+# create mesh
+var = pybamm.standard_spatial_vars
+var_pts = {
+    var.x_n: 5,
+    var.x_s: 5,
+    var.x_p: 5,
+    var.r_n: 5,
+    var.r_p: 5,
+    var.y: 5,
+    var.z: 5,
+}
+mesh = pybamm.Mesh(geometry, pybamm_model.default_submesh_types, var_pts)
+
+# discretise model
+disc = pybamm.Discretisation(mesh, pybamm_model.default_spatial_methods)
+disc.process_model(pybamm_model)
+
+# discharge timescale
+tau = param.process_symbol(
+    pybamm.standard_parameters_lithium_ion.tau_discharge
+).evaluate()
+
+# solve model at comsol times
+time = comsol_variables["time"] / tau
+solution = pybamm_model.default_solver.solve(pybamm_model, time)
+
+
+"-----------------------------------------------------------------------------"
+"Make Comsol 'model' for comparison"
+
+comsol_t = comsol_variables["time"]
+
+L_y = param.process_symbol(pybamm.standard_parameters_lithium_ion.L_y).evaluate()
+L_z = param.process_symbol(pybamm.standard_parameters_lithium_ion.L_z).evaluate()
+pybamm_y = mesh["current collector"][0].edges["y"]
+pybamm_z = mesh["current collector"][0].edges["z"]
+
+y_plot = pybamm_y * L_z  # np.linspace(0, L_y, 20)
+z_plot = pybamm_z * L_z  # np.linspace(0, L_z, 20)
+grid_y, grid_z = np.meshgrid(y_plot, z_plot)
+
+
+def get_interp_fun(variable, domain):
+    """
+    Interpolate in space to plotting nodes, and then create function to interpolate
+    in time that can be called for plotting at any t.
+    """
+    if domain == ["negative current collector"]:
+        comsol_y = comsol_variables["y_neg_cc"]
+        comsol_z = comsol_variables["z_neg_cc"]
+    elif domain == ["positive current collector"]:
+        comsol_y = comsol_variables["y_pos_cc"]
+        comsol_z = comsol_variables["z_pos_cc"]
+    elif domain == ["separator"]:
+        comsol_y = comsol_variables["y_sep"]
+        comsol_z = comsol_variables["z_sep"]
+
+    # Note order of rows and cols!
+    interp_var = np.zeros((len(z_plot), len(y_plot), variable.shape[1]))
+    for i in range(0, variable.shape[1]):
+        interp_var[:, :, i] = interp.griddata(
+            np.column_stack((comsol_y, comsol_z)),
+            variable[:, i],
+            (grid_y, grid_z),
+            method="cubic",
+        )
+
+    def myinterp(t):
+        return interp.interp1d(comsol_t, interp_var, axis=2)(t)
+
+    return myinterp
+
+
+# Create interpolating functions to put in comsol_model.variables dict
+def comsol_voltage(t):
+    return interp.interp1d(comsol_t, comsol_variables["voltage"])(t)
+
+
+comsol_phi_s_cn = get_interp_fun(
+    comsol_variables["phi_s_cn"], ["negative current collector"]
+)
+comsol_phi_s_cp = get_interp_fun(
+    comsol_variables["phi_s_cp"], ["positive current collector"]
+)
+comsol_temperature = get_interp_fun(comsol_variables["temperature"], ["separator"])
+
+# Create comsol model with dictionary of Matrix variables
+comsol_model = pybamm.BaseModel()
+comsol_model.variables = {
+    "Terminal voltage [V]": comsol_voltage,
+    "Negative current collector potential [V]": comsol_phi_s_cn,
+    "Positive current collector potential [V]": comsol_phi_s_cp,
+    "X-averaged cell temperature [K]": comsol_temperature,
+}
+
+# Process variables
+output_variables = {}
+for var in comsol_model.variables.keys():
+    output_variables[var] = pybamm.ProcessedVariable(
+        pybamm_model.variables[var], solution.t, solution.y, mesh=mesh
+    )
+
+# Plotting function
+def plot(var, t, cmap="viridis"):
+    fig, ax = plt.subplots(figsize=(15, 8))
+
+    # find t index (note: t is dimensional)
+    ind = (np.abs(solution.t - t / tau)).argmin()
+
+    # plot pybamm solution
+    plt.subplot(131)
+    pybamm_plot = plt.pcolormesh(
+        y_plot,
+        z_plot,
+        np.transpose(output_variables[var](y=y_plot/L_y, z=z_plot/L_z, t=solution.t[ind])),
+        shading="gouraud",
+    )
+    plt.axis([0, y_plot[-1], 0, z_plot[-1]])
+    plt.xlabel(r"$y$")
+    plt.ylabel(r"$z$")
+    plt.title(r"PyBaMM: " + var)
+    plt.set_cmap(cmap)
+    plt.colorbar(pybamm_plot)
+
+    # plot comsol solution
+    plt.subplot(132)
+    comsol_plot = plt.pcolormesh(
+        y_plot,
+        z_plot,
+        comsol_model.variables[var](t=solution.t[ind]),
+        shading="gouraud",
+    )
+    plt.axis([0, y_plot[-1], 0, z_plot[-1]])
+    plt.xlabel(r"$y$")
+    plt.ylabel(r"$z$")
+    plt.title(r"COMSOL: " + var)
+    plt.set_cmap(cmap)
+    plt.colorbar(comsol_plot)
+
+    # plot "error"
+    plt.subplot(133)
+    diff_plot = plt.pcolormesh(
+        y_plot,
+        z_plot,
+        np.abs(
+            np.transpose(output_variables[var](y=y_plot/L_y, z=z_plot/L_z, t=solution.t[ind]))
+            - comsol_model.variables[var](t=solution.t[ind])
+        ),
+        shading="gouraud",
+    )
+    plt.axis([0, y_plot[-1], 0, z_plot[-1]])
+    plt.xlabel(r"$y$")
+    plt.ylabel(r"$z$")
+    plt.title(r"Error: " + var)
+    plt.set_cmap(cmap)
+    plt.colorbar(diff_plot)
+
+
+plot("Negative current collector potential [V]", comsol_t[-1] / 2, cmap="cividis")
+#plot("Positive current collector potential [V]", comsol_t[-1] / 2, cmap="cividis")
+#plot("X-averaged cell temperature [K]", comsol_t[-1] / 2, cmap="cividis")
+plt.show()
--- results/2019_xx_2plus1D_pouch/load_comsol_data.py
+++ results/2019_xx_2plus1D_pouch/load_comsol_data.py
@@ -2,88 +2,97 @@ import pybamm
 import os
 import pandas as pd
 import pickle
+import numpy as np
 
 # change working directory the root of pybamm
 os.chdir(pybamm.root_dir())
 
-"-----------------------------------------------------------------------------"
-"Pick C_rate and load comsol data"
-
-# C_rate
-C_rates = {"01": 0.1, "05": 0.5, "1": 1, "2": 2, "3": 3}
+# pick C_rate and load comsol data
+C_rates = {"1": 1}
 C_rate = "1"  # choose the key from the above dictionary of available results
 
-# time-voltage
+# time-voltage (both just 1D arrays)
 comsol = pd.read_csv(
-    "input/data/comsol/{}C/Voltage.csv".format(C_rate), sep=",", header=None
+    "input/comsol_results_csv/2plus1D/{}C/voltage.csv".format(C_rate),
+    sep=",",
+    header=None,
 )
-comsol_time = comsol[0].values
-comsol_time_npts = len(comsol_time)
-comsol_voltage = comsol[1].values
+time = comsol[0].values
+time_npts = len(time)
+voltage = comsol[1].values
 
-# negative current collector potential
+# negative current collector potential (stored as a (yz_npts, time_npts) size
+# array)
+comsol = pd.read_csv(
+    "input/comsol_results_csv/2plus1D/{}C/phi_s_cn.csv".format(C_rate),
+    sep=",",
+    header=None,
+)
+yz_npts = int(len(comsol[0].values) / time_npts)  # get length of y-z pts
+z_neg_cc = comsol[0].values[::time_npts]  # first column z
+y_neg_cc = comsol[1].values[::time_npts]  # second column y
+phi_s_cn = np.reshape(
+    comsol[3].values, (yz_npts, time_npts), order="C"
+)  # fourth column is phi_s_cn vals
 
-# positive current collector potential
+## test interp onto regular grid
+# y = np.linspace(0, np.max(y_neg_cc), 5)
+# z = np.linspace(0, np.max(z_neg_cc), 4)
+# grid_y, grid_z = np.meshgrid(y, z)
+# interp_var = np.zeros((len(z), len(y), len(time)))
+# for i in range(0, phi_s_cn.shape[1]):
+#    interp_var[:, :, i] = interp.griddata(
+#        np.column_stack((y_neg_cc, z_neg_cc)), phi_s_cn[:, i],(grid_y, grid_z), method="cubic")
+#
+#
+# def myinterp(t):
+#    return interp.interp1d(time, interp_var, axis=2)(t)
+#
+#
+# plt.pcolormesh(y,z,myinterp(2),shading="gouraud")
+# plt.show()
 
-# x-averaged temperature
+# positive current collector potential (stored as a (yz_npts, time_npts) size
+# array)
+comsol = pd.read_csv(
+    "input/comsol_results_csv/2plus1D/{}C/phi_s_cp.csv".format(C_rate),
+    sep=",",
+    header=None,
+)
+yz_npts = int(len(comsol[0].values) / time_npts)  # get length of y-z pts
+z_pos_cc = comsol[0].values[::time_npts]  # first column z
+y_pos_cc = comsol[1].values[::time_npts]  # second column y
+phi_s_cp = np.reshape(
+    comsol[3].values, (yz_npts, time_npts), order="C"
+)  # fourth column is phi_s_cp vals
+
+# temperature (evaluated on separator nodes) (stored as a (yz_npts, time_npts)
+# size array)
+comsol = pd.read_csv(
+    "input/comsol_results_csv/2plus1D/{}C/T.csv".format(C_rate), sep=",", header=None
+)
+yz_npts = int(len(comsol[0].values) / time_npts)  # get length of y-z pts
+z_sep = comsol[0].values[::time_npts]  # first column z
+y_sep = comsol[1].values[::time_npts]  # second column y
+T = np.reshape(
+    comsol[3].values, (yz_npts, time_npts), order="C"
+)  # fourth column is T vals
 
 # add comsol variables to dict and pickle
-comsol_variables = {"time": comsol_time, "voltage": comsol_voltage}
-pickle.dump(
-    comsol_variables,
-    open("input/comsol_results/2plus1D/comsol_{}C.pickle".format(C_rate), "wb")
-)
-## negative electrode potential
-#comsol = pd.read_csv(
-#    "input/data/comsol/{}C/phi_n.csv".format(C_rate), sep=",", header=None
-#)
-#comsol_x_n_npts = int(len(comsol[0].values) / comsol_time_npts)
-#comsol_x_n = comsol[0].values[0:comsol_x_n_npts]
-#comsol_phi_n_vals = np.reshape(
-#    comsol[1].values, (comsol_x_n_npts, comsol_time_npts), order="F"
-#)
-#
-## negative particle surface concentration
-#comsol = pd.read_csv(
-#    "input/data/comsol/{}C/c_n_surf.csv".format(C_rate), sep=",", header=None
-#)
-#comsol_c_n_surf_vals = np.reshape(
-#    comsol[1].values, (comsol_x_n_npts, comsol_time_npts), order="F"
-#)
-#
-## positive electrode potential
-#comsol = pd.read_csv(
-#    "input/data/comsol/{}C/phi_p.csv".format(C_rate), sep=",", header=None
-#)
-#comsol_x_p_npts = int(len(comsol[0].values) / comsol_time_npts)
-#comsol_x_p = comsol[0].values[0:comsol_x_p_npts]
-#comsol_phi_p_vals = np.reshape(
-#    comsol[1].values, (comsol_x_p_npts, comsol_time_npts), order="F"
-#)
-#
-## positive particle surface concentration
-#comsol = pd.read_csv(
-#    "input/data/comsol/{}C/c_p_surf.csv".format(C_rate), sep=",", header=None
-#)
-#comsol_c_p_surf_vals = np.reshape(
-#    comsol[1].values, (comsol_x_p_npts, comsol_time_npts), order="F"
-#)
-#
-## electrolyte concentration
-#comsol = pd.read_csv(
-#    "input/data/comsol/{}C/c_e.csv".format(C_rate), sep=",", header=None
-#)
-#comsol_x_npts = int(len(comsol[0].values) / comsol_time_npts)
-#comsol_x = comsol[0].values[0:comsol_x_npts]
-#comsol_c_e_vals = np.reshape(
-#    comsol[1].values, (comsol_x_npts, comsol_time_npts), order="F"
-#)
-#
-## electrolyte potential
-#comsol = pd.read_csv(
-#    "input/data/comsol/{}C/phi_e.csv".format(C_rate), sep=",", header=None
-#)
-#comsol_phi_e_vals = np.reshape(
-#    comsol[1].values, (comsol_x_npts, comsol_time_npts), order="F"
-#)
-#
+comsol_variables = {
+    "time": time,
+    "voltage": voltage,
+    "y_neg_cc": y_neg_cc,
+    "z_neg_cc": z_neg_cc,
+    "phi_s_cn": phi_s_cn,
+    "y_pos_cc": y_pos_cc,
+    "z_pos_cc": z_pos_cc,
+    "phi_s_cp": phi_s_cp,
+    "y_sep": y_sep,
+    "z_sep": z_sep,
+    "temperature": T,
+}
+
+savefile = "comsol_{}C.pickle".format(C_rate)
+with open(savefile, "wb") as f:
+    pickle.dump(comsol_variables, f, pickle.HIGHEST_PROTOCOL)
