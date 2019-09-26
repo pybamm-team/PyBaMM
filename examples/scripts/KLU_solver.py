879a7aed04d560c6842b621cba7ab65785ff4492
Scott Marquis <marquis@maths.ox.ac.uk>
2019-09-26 16:57:04 +0100

    solver works (without checks) on DFN 1D

--- examples/scripts/KLU_solver.py
+++ examples/scripts/KLU_solver.py
@@ -17,7 +17,10 @@ param.process_model(model)
 param.process_geometry(geometry)
 
 # set mesh
-mesh = pybamm.Mesh(geometry, model.default_submesh_types, model.default_var_pts)
+var = pybamm.standard_spatial_vars
+
+var_pts = {var.x_n: 60, var.x_s: 100, var.x_p: 60, var.r_n: 50, var.r_p: 50}
+mesh = pybamm.Mesh(geometry, model.default_submesh_types, var_pts)
 
 # discretise model
 disc = pybamm.Discretisation(mesh, model.default_spatial_methods)
@@ -27,7 +30,6 @@ disc.process_model(model)
 t_eval = np.linspace(0, 0.1, 100)
 
 solutions = model.default_solver.solve(model, t_eval)
-# model.use_to_python = False
 
 solver = pybamm.KLU()
 solution = solver.solve(model, t_eval)
