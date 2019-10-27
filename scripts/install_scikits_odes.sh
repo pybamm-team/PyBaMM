102436d0c4b1e82edd02fa8d25093206f1b6f191
tinosulzer <valentinsulzer@hotmail.com>
2019-10-26 21:31:41 -0400

    #646 making scikits install faster

--- scripts/install_scikits_odes.sh
+++ scripts/install_scikits_odes.sh
@@ -13,7 +13,15 @@ tar -xvf $SUNDIALS_NAME
 mkdir build-sundials-3.1.1
 cd build-sundials-3.1.1/
 cmake -DLAPACK_ENABLE=ON -DSUNDIALS_INDEX_TYPE=int32_t -DBUILD_ARKODE:BOOL=OFF -DEXAMPLES_ENABLE:BOOL=OFF -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR ../sundials-3.1.1/
-make install
+if [ "$(uname)" == "Darwin" ]; then
+    # Mac OS X platform        
+    NUM_OF_CORES=$(sysctl -n hw.cpu)
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # GNU/Linux platform
+    NUM_OF_CORES=$(cat /proc/cpuinfo | grep processor | wc -l)
+fi
+make clean
+make -j$NUM_OF_CORES install
 cd $CURRENT_DIR
 rm -rf $TMP_DIR
 export LD_LIBRARY_PATH=$INSTALL_DIR/lib:$LD_LIBRARY_PATH # For Linux
