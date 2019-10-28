466a4c4dee9ebf945b8d96198e3b1c04becd8d81
tinosulzer <valentinsulzer@hotmail.com>
2019-10-28 08:10:45 -0400

    #664 remove termination from casadi solver

--- pybamm/solvers/casadi_solver.py
+++ pybamm/solvers/casadi_solver.py
@@ -55,8 +55,8 @@ class CasadiSolver(pybamm.DaeSolver):
         )
         solve_time = timer.time() - solve_start_time
 
-        # Identify the event that caused termination
-        termination = self.get_termination_reason(solution, self.events)
+        # Events not implemented, termination is always 'final time'
+        termination = "final time"
 
         return solution, solve_time, termination
 
