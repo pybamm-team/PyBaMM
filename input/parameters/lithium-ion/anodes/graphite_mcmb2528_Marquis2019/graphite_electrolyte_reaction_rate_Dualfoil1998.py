ffafd825fec5e02c8a607eee14e3d27c363c359f
Robert Timms <timms@maths.ox.ac.uk>
2019-11-01 09:00:13 +0000

    remove extra params

--- input/parameters/lithium-ion/anodes/graphite_mcmb2528_Marquis2019/graphite_electrolyte_reaction_rate_Dualfoil1998.py
+++ input/parameters/lithium-ion/anodes/graphite_mcmb2528_Marquis2019/graphite_electrolyte_reaction_rate_Dualfoil1998.py
@@ -25,7 +25,7 @@ def graphite_electrolyte_reaction_rate_Dualfoil1998(T, T_inf, E_r, R_g):
     :`numpy.Array`
         Reaction rate
     """
-    m_ref = 2 * 10 ** (-5)
+    m_ref = 2 * 10 ** (-5) / 2
     arrhenius = np.exp(E_r / R_g * (1 / T_inf - 1 / T))
 
     return m_ref * arrhenius
