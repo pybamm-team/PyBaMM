92aa22b2f5033f7158a12c7eb1a96c6bf9457523
Scott Marquis <marquis@maths.ox.ac.uk>
2019-11-07 22:24:55 +0000

    #704 added a short script to demonstrate comparison between extrapolation methods

---
+++ examples/scripts/compare_extrapolations.py
@@ -0,0 +1,34 @@
+import pybamm
+
+
+x_n = pybamm.standard_spatial_vars.x_n
+x_s = pybamm.standard_spatial_vars.x_s
+x_p = pybamm.standard_spatial_vars.x_p
+r_n = pybamm.standard_spatial_vars.r_n
+r_p = pybamm.standard_spatial_vars.r_p
+
+var_pts = {x_n: 10, x_s: 3, x_p: 10}
+model_lin = pybamm.lead_acid.Full()
+sim_lin = pybamm.Simulation(model_lin, var_pts=var_pts)
+sim_lin.solve()
+
+model_quad = pybamm.lead_acid.Full()
+method_options = {"extrapolation": {"order": "quadratic", "use bcs": False}}
+spatial_methods = {
+    "negative particle": pybamm.FiniteVolume(method_options),
+    "positive particle": pybamm.FiniteVolume(method_options),
+    "macroscale": pybamm.FiniteVolume(method_options),
+    "current collector": pybamm.ZeroDimensionalMethod(),
+}
+sim_quad = pybamm.Simulation(
+    model_quad, spatial_methods=spatial_methods, var_pts=var_pts
+)
+sim_quad.solve()
+
+
+# plot the two sols
+models = [sim_lin.built_model, sim_quad.built_model]
+solutions = [sim_lin.solution, sim_quad.solution]
+plot = pybamm.QuickPlot(models, sim_lin.mesh, solutions)
+plot.dynamic_plot()
+
