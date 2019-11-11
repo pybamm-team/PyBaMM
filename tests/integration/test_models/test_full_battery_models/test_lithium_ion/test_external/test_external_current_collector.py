d64511a51552ccdb71681ec7c1c2603ca687484c
Scott Marquis <marquis@maths.ox.ac.uk>
2019-11-11 17:53:03 +0000

    #692 moved integration tests

---
+++ tests/integration/test_models/test_full_battery_models/test_lithium_ion/test_external/test_external_current_collector.py
@@ -0,0 +1,15 @@
+#
+# Tests for external submodels
+#
+import pybamm
+import unittest
+import numpy as np
+
+
+if __name__ == "__main__":
+    print("Add -v for more debug output")
+    import sys
+
+    if "-v" in sys.argv:
+        debug = True
+    unittest.main()
