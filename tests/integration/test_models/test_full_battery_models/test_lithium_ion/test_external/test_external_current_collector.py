6b084be3f46da3f23e4327b1f75bd6cfc3edc505
Scott Marquis <marquis@maths.ox.ac.uk>
2019-11-12 10:43:12 +0000

    #692 begining to add cc tests

--- tests/integration/test_models/test_full_battery_models/test_lithium_ion/test_external/test_external_current_collector.py
+++ tests/integration/test_models/test_full_battery_models/test_lithium_ion/test_external/test_external_current_collector.py
@@ -6,6 +6,23 @@ import unittest
 import numpy as np
 
 
+class TestExternalCC(unittest.TestCase):
+
+    model_options = {
+        "current collector": "potential pair",
+        "dimensionality": 1,
+        "external submodels": ["current collector"],
+    }
+    model = pybamm.lithium_ion.SPM(model_options)
+    var_pts = model.default_var_pts
+    var_pts.update({pybamm.standard_spatial_vars.y: 5})
+    sim = pybamm.Simulation(model)
+
+    # provide phi_s_n and i_cc
+
+    # return phi_s_p
+
+
 if __name__ == "__main__":
     print("Add -v for more debug output")
     import sys
