faca42f0fe10b2aa06871a52adcfa1315973e898
Scott Marquis <marquis@maths.ox.ac.uk>
2019-11-19 11:56:14 +0000

    692 updated example

--- results/2plus1D/set_temperature_spm_1plus1D.py
+++ results/2plus1D/set_temperature_spm_1plus1D.py
@@ -17,18 +17,22 @@ model_options = {
 model = pybamm.lithium_ion.SPMe(model_options)
 
 var = pybamm.standard_spatial_vars
-z_pts = 5
+z_pts = 20
 var_pts = {var.x_n: 5, var.x_s: 5, var.x_p: 5, var.r_n: 5, var.r_p: 5, var.z: z_pts}
 
 sim = pybamm.Simulation(model, var_pts=var_pts, C_rate=2)
 
 # Set the temperature (in dimensionless form)
-T_av = np.linspace(0, 1, z_pts)[:, np.newaxis]
+# T_av = np.linspace(0, 1, z_pts)[:, np.newaxis]
 
+z = np.linspace(0, 1, z_pts)
 t_eval = np.linspace(0, 0.13, 50)
 # step through the solver, setting the temperature at each timestep
 for i in np.arange(1, len(t_eval) - 1):
     dt = t_eval[i + 1] - t_eval[i]
+    T_av = (np.sin(2 * np.pi * z) * np.sin(2 * np.pi * 100 * t_eval[i]))[
+        :, np.newaxis
+    ]
     external_variables = {"X-averaged cell temperature": T_av}
     sim.step(dt, external_variables=external_variables)
 
