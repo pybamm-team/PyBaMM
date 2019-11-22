557d34064f3e023761fcad1e41c75f1acb8005de
Valentin Sulzer <vsulzer@umich.edu>
2019-11-22 15:52:46 -0500

    #746 reformat lithium-ion models

--- pybamm/expression_tree/binary_operators.py
+++ pybamm/expression_tree/binary_operators.py
@@ -77,19 +77,8 @@ class BinaryOperator(pybamm.Symbol):
     """
 
     def __init__(self, name, left, right):
-        # Turn numbers into scalars
-        if isinstance(left, numbers.Number):
-            left = pybamm.Scalar(left)
-        if isinstance(right, numbers.Number):
-            right = pybamm.Scalar(right)
+        left, right = self.format(left, right)
 
-        # Check both left and right are pybamm Symbols
-        if not (isinstance(left, pybamm.Symbol) and isinstance(right, pybamm.Symbol)):
-            raise NotImplementedError(
-                """'{}' not implemented for symbols of type {} and {}""".format(
-                    self.__class__.__name__, type(left), type(right)
-                )
-            )
         # Check and process domains, except for Outer symbol which takes the outer
         # product of two smbols in different domains, and gives it the domain of the
         # right child.
@@ -107,6 +96,38 @@ class BinaryOperator(pybamm.Symbol):
         )
         self.left = self.children[0]
         self.right = self.children[1]
+
+    def format(self, left, right):
+        "Format children left and right into compatible form"
+        # Turn numbers into scalars
+        if isinstance(left, numbers.Number):
+            left = pybamm.Scalar(left)
+        if isinstance(right, numbers.Number):
+            right = pybamm.Scalar(right)
+
+        # Check both left and right are pybamm Symbols
+        if not (isinstance(left, pybamm.Symbol) and isinstance(right, pybamm.Symbol)):
+            raise NotImplementedError(
+                """'{}' not implemented for symbols of type {} and {}""".format(
+                    self.__class__.__name__, type(left), type(right)
+                )
+            )
+
+        # Do some broadcasting in special cases
+        if (
+            left.domain != right.domain
+            and "secondary" in right.auxiliary_domains
+            and left.domain == right.auxiliary_domains["secondary"]
+        ):
+            left = pybamm.PrimaryBroadcast(left, right.domain)
+        if (
+            right.domain != left.domain
+            and "secondary" in left.auxiliary_domains
+            and right.domain == left.auxiliary_domains["secondary"]
+        ):
+            right = pybamm.PrimaryBroadcast(right, left.domain)
+
+        return left, right
 
     def __str__(self):
         """ See :meth:`pybamm.Symbol.__str__()`. """
--- pybamm/geometry/standard_spatial_vars.py
+++ pybamm/geometry/standard_spatial_vars.py
@@ -4,35 +4,77 @@ whole_cell = ["negative electrode", "separator", "positive electrode"]
 
 # Domains at cell centres
 x_n = pybamm.SpatialVariable(
-    "x_n", domain=["negative electrode"], coord_sys="cartesian"
+    "x_n",
+    domain=["negative electrode"],
+    auxiliary_domains={"secondary": "current collector"},
+    coord_sys="cartesian",
 )
-x_s = pybamm.SpatialVariable("x_s", domain=["separator"], coord_sys="cartesian")
+x_s = pybamm.SpatialVariable(
+    "x_s",
+    domain=["separator"],
+    auxiliary_domains={"secondary": "current collector"},
+    coord_sys="cartesian",
+)
 x_p = pybamm.SpatialVariable(
-    "x_p", domain=["positive electrode"], coord_sys="cartesian"
+    "x_p",
+    domain=["positive electrode"],
+    auxiliary_domains={"secondary": "current collector"},
+    coord_sys="cartesian",
 )
-x = pybamm.SpatialVariable("x", domain=whole_cell, coord_sys="cartesian")
+x = pybamm.SpatialVariable(
+    "x",
+    domain=whole_cell,
+    auxiliary_domains={"secondary": "current collector"},
+    coord_sys="cartesian",
+)
 
 y = pybamm.SpatialVariable("y", domain="current collector", coord_sys="cartesian")
 z = pybamm.SpatialVariable("z", domain="current collector", coord_sys="cartesian")
 
 r_n = pybamm.SpatialVariable(
-    "r_n", domain=["negative particle"], coord_sys="spherical polar"
+    "r_n",
+    domain=["negative particle"],
+    auxiliary_domains={
+        "secondary": "negative electrode",
+        "tertiary": "current collector",
+    },
+    coord_sys="spherical polar",
 )
 r_p = pybamm.SpatialVariable(
-    "r_p", domain=["positive particle"], coord_sys="spherical polar"
+    "r_p",
+    domain=["positive particle"],
+    auxiliary_domains={
+        "secondary": "positive electrode",
+        "tertiary": "current collector",
+    },
+    coord_sys="spherical polar",
 )
 
 # Domains at cell edges
 x_n_edge = pybamm.SpatialVariable(
-    "x_n_edge", domain=["negative electrode"], coord_sys="cartesian"
+    "x_n_edge",
+    domain=["negative electrode"],
+    auxiliary_domains={"secondary": "current collector"},
+    coord_sys="cartesian",
 )
 x_s_edge = pybamm.SpatialVariable(
-    "x_s_edge", domain=["separator"], coord_sys="cartesian"
+    "x_s_edge",
+    domain=["separator"],
+    auxiliary_domains={"secondary": "current collector"},
+    coord_sys="cartesian",
 )
 x_p_edge = pybamm.SpatialVariable(
-    "x_p_edge", domain=["positive electrode"], coord_sys="cartesian"
+    "x_p_edge",
+    domain=["positive electrode"],
+    auxiliary_domains={"secondary": "current collector"},
+    coord_sys="cartesian",
 )
-x_edge = pybamm.SpatialVariable("x_edge", domain=whole_cell, coord_sys="cartesian")
+x_edge = pybamm.SpatialVariable(
+    "x_edge",
+    domain=whole_cell,
+    auxiliary_domains={"secondary": "current collector"},
+    coord_sys="cartesian",
+)
 
 y_edge = pybamm.SpatialVariable(
     "y_edge", domain="current collector", coord_sys="cartesian"
@@ -42,8 +84,20 @@ z_edge = pybamm.SpatialVariable(
 )
 
 r_n_edge = pybamm.SpatialVariable(
-    "r_n_edge", domain=["negative particle"], coord_sys="spherical polar"
+    "r_n_edge",
+    domain=["negative particle"],
+    auxiliary_domains={
+        "secondary": "negative electrode",
+        "tertiary": "current collector",
+    },
+    coord_sys="spherical polar",
 )
 r_p_edge = pybamm.SpatialVariable(
-    "r_p_edge", domain=["positive particle"], coord_sys="spherical polar"
+    "r_p_edge",
+    domain=["positive particle"],
+    auxiliary_domains={
+        "secondary": "positive electrode",
+        "tertiary": "current collector",
+    },
+    coord_sys="spherical polar",
 )
