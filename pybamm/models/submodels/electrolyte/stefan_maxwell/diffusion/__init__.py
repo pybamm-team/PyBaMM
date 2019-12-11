171de9294c2a7595ab59f9838323643112e63cb8
Valentin Sulzer <valentinsulzer@hotmail.com>
2019-12-11 18:28:14 -0500

    #546 fix import

--- pybamm/models/submodels/electrolyte/stefan_maxwell/diffusion/__init__.py
+++ pybamm/models/submodels/electrolyte/stefan_maxwell/diffusion/__init__.py
@@ -1,9 +1,6 @@
 from .base_stefan_maxwell_diffusion import BaseModel
 from .leading_stefan_maxwell_diffusion import LeadingOrder
 from .first_order_stefan_maxwell_diffusion import FirstOrder
-from .first_order_stefan_maxwell_diffusion_average_correction import (
-    FirstOrderAverageCorrection,
-)
 from .composite_stefan_maxwell_diffusion import Composite
 from .full_stefan_maxwell_diffusion import Full
 from .constant_stefan_maxwell_diffusion import ConstantConcentration
