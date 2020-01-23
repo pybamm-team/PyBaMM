fc6ec22c438a5803a565f48c7450e7cf1634a630
tomtranter <t.g.tranter@gmail.com>
2020-01-23 18:23:32 +0000

    Add test for solution evaluation with Electrode height input

--- tests/unit/test_solvers/test_solution.py
+++ tests/unit/test_solvers/test_solution.py
@@ -95,6 +95,36 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(solution.model.name, solution_load.model.name)
         np.testing.assert_array_equal(solution["c"].entries, solution_load["c"].entries)
 
+    def test_solution_evals_with_inputs(self):
+        model = pybamm.lithium_ion.SPM()
+        geometry = model.default_geometry
+        param = model.default_parameter_values
+        param.update(
+            {
+                "Electrode height [m]": "[input]",
+            }
+        )
+        param.process_model(model)
+        param.process_geometry(geometry)
+        var = pybamm.standard_spatial_vars
+        var_pts = {var.x_n: 5, var.x_s: 5, var.x_p: 5, var.r_n: 10, var.r_p: 10}
+        spatial_methods = model.default_spatial_methods
+        solver = model.default_solver
+        sim = pybamm.Simulation(
+            model=model,
+            geometry=geometry,
+            parameter_values=param,
+            var_pts=var_pts,
+            spatial_methods=spatial_methods,
+            solver=solver,
+        )
+        inputs = {
+                'Electrode height [m]': 0.1
+                }
+        sim.solve(t_eval=np.linspace(0, 0.01, 10), inputs=inputs)
+        time = sim.solution['Time [h]'](sim.solution.t)
+        self.assertEqual(len(time), 10)
+
 
 if __name__ == "__main__":
     print("Add -v for more debug output")
