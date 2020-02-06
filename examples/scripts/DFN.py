a8f43b525ec5b2770adb4c26372c6a341542e34f
Valentin Sulzer <valentinsulzer@hotmail.com>
2020-02-05 23:40:00 -0500

    revert dfn changes

--- examples/scripts/DFN.py
+++ examples/scripts/DFN.py
@@ -8,26 +8,14 @@ import numpy as np
 pybamm.set_logging_level("INFO")
 
 
-def CCCV(variables):
-    I = variables["Current [A]"]
-    V = variables["Terminal voltage [V]"]
-    s_I = pybamm.InputParameter("Current switch")
-    s_V = pybamm.InputParameter("Voltage switch")
-    # s_P = pybamm.InputParameter("Power switch")
-    return s_I * (I - pybamm.InputParameter("Current input [A]")) + s_V * (
-        V - pybamm.InputParameter("Voltage input [V]")
-    )
-
-
 # load model
-model = pybamm.lithium_ion.DFN({"operating mode": CCCV})
+model = pybamm.lithium_ion.DFN()
 
 # create geometry
 geometry = model.default_geometry
 
 # load parameter values and process model and geometry
 param = model.default_parameter_values
-param.update({"Voltage function [V]": 4.1}, check_already_exists=False)
 param.process_model(model)
 param.process_geometry(geometry)
 
@@ -45,44 +33,7 @@ t_eval = np.linspace(0, 0.2, 100)
 solver = model.default_solver
 solver.rtol = 1e-3
 solver.atol = 1e-6
-solution = None
-
-solution = solver.step(
-    solution,
-    model,
-    0.01,
-    npts=100,
-    inputs={
-        "Current switch": 1,
-        "Voltage switch": 0,
-        "Current input [A]": -1,
-        "Voltage input [V]": 0,
-    },
-)
-solution = solver.step(
-    solution,
-    model,
-    0.01,
-    npts=100,
-    inputs={
-        "Current switch": 1,
-        "Voltage switch": 0,
-        "Current input [A]": -0.5,
-        "Voltage input [V]": 0,
-    },
-)
-solution = solver.step(
-    solution,
-    model,
-    0.01,
-    npts=100,
-    inputs={
-        "Current switch": 0,
-        "Voltage switch": 1,
-        "Current input [A]": 0,
-        "Voltage input [V]": 4.1,
-    },
-)
+solution = solver.solve(model, t_eval)
 
 # plot
 plot = pybamm.QuickPlot(solution)
