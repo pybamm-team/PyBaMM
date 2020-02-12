f01d080d9ec26f5b1e5adfe9200d9ea3bba88589
Scott Marquis <marquis@maths.ox.ac.uk>
2020-02-12 16:28:14 +0000

    #821 added kim parameter set

---
+++ input/parameters/lithium-ion/anodes/graphite_Kim2011/graphite_entropic_change_NULL.py
@@ -0,0 +1,11 @@
+def graphite_entropic_change_NULL(sto, c_n_max):
+    """
+        NULL function for graphite entropic change.
+
+        Parameters
+        ----------
+        sto: double
+            Stochiometry of material (li-fraction)
+
+    """
+    return 0
---
+++ input/parameters/lithium-ion/cathodes/NCA_Kim2011/nca_entropic_change_NULL.py
@@ -0,0 +1,12 @@
+def nca_entropic_change_NULL(sto, c_p_max):
+    """
+        Null function for entropic change.
+
+        Parameters
+        ----------
+        sto: double
+            Stochiometry of material (li-fraction)
+
+    """
+
+    return 0
