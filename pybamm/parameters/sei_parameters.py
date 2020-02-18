f76e200f0880339fba16f9b0606ca4e709f6dbaa
Scott Marquis <marquis@maths.ox.ac.uk>
2020-02-18 19:05:42 +0000

    #834 added a standard sei parameter file

--- pybamm/parameters/sei_parameters.py
+++ pybamm/parameters/sei_parameters.py
@@ -13,3 +13,34 @@ def alpha(m_ratio, phi_s, phi_e, U_inner, U_outer):
         "Inner SEI reaction proportion", m_ratio, phi_s, phi_e, U_inner, U_outer
     )
 
+
+V_bar_inner_dimensional = pybamm.Parameter("Inner SEI partial molar volume [m3.mol-1]")
+V_bar_outer_dimensional = pybamm.Parameter("Outer SEI partial molar volume [m3.mol-1]")
+
+m_sei_dimensional = pybamm.Parameter("SEI reaction exchange current density [A.m-2]")
+
+R_sei_dimensional = pybamm.Parameter("SEI resistance per unit thickness [Ohm.m-1]")
+
+D_sol_dimensional = pybamm.Parameter("Outer SEI solvent diffusivity [m2.s-1]")
+c_sol_dimensional = pybamm.Parameter("Bulk solvent concentration [mol.m-3]")
+
+m_ratio = pybamm.Parameter("Ratio of inner and outer SEI exchange current densities")
+
+U_inner_dimensional = pybamm.Parameter("Inner SEI open-circuit potential [V]")
+U_outer_dimensional = pybamm.Parameter("Outer SEI open-circuit potential [V]")
+
+kappa_inner_dimensional = pybamm.Parameter("Inner SEI electron conductivity [S.m-1]")
+
+D_li_dimensional = pybamm.Parameter(
+    "Inner SEI lithium interstitial diffusivity [m2.s-1]"
+)
+
+c_li_0_dimensional = pybamm.Parameter(
+    "Lithium interstitial reference concentration [mol.m-3]"
+)
+
+# --------------------------------------------------------------------------------------
+# Dimensionless parameters
+
+# write as C_SEI_reaction etc ...
+
