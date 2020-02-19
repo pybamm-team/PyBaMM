d01ebd09bf830100dee3ea05127b605c0ee35d21
Scott Marquis <marquis@maths.ox.ac.uk>
2020-02-19 13:53:01 +0000

    #834 reaction limited runs but playing with parameters

--- examples/scripts/sei_growth.py
+++ examples/scripts/sei_growth.py
@@ -1,14 +1,24 @@
 import pybamm as pb
 
-pb.set_logging_level("INFO")
+pb.set_logging_level("DEBUG")
 # pb.settings.debug_mode = True
 
 options = {"sei": "reaction limited"}
 model = pb.lithium_ion.DFN(options)
 
-sim = pb.Simulation(model)
+experiment = pb.Experiment(["Rest for 100 hours"])
 
+parameter_values = model.default_parameter_values
+
+sim = pb.Simulation(model, experiment=experiment)
 
 solver = pb.CasadiSolver(mode="safe")
+
 sim.solve(solver=solver)
-sim.plot(["Terminal voltage [V]", "Total SEI thickness", "Loss of "])
+sim.plot(
+    [
+        "Terminal voltage [V]",
+        "X-averaged total SEI thickness",
+        "Loss of lithium to SEI [mols]",
+    ]
+)
