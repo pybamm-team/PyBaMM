7652c7f8645c23c92653b5d3cc35bc3a3a89fee3
Valentin Sulzer <valentinsulzer@hotmail.com>
2020-02-20 22:26:24 -0500

    #833 manual implementation of levenshtein

--- pybamm/util.py
+++ pybamm/util.py
@@ -12,7 +12,6 @@ import timeit
 import pathlib
 import pickle
 import pybamm
-import Levenshtein
 from collections import defaultdict
 
 
@@ -22,13 +21,51 @@ def root_dir():
 
 
 class FuzzyDict(dict):
+    def levenshtein_ratio(self, s, t):
+        """
+        Calculates levenshtein distance between two strings s and t.
+        Uses the formula from
+        https://www.datacamp.com/community/tutorials/fuzzy-string-python
+        """
+        # Initialize matrix of zeros
+        rows = len(s) + 1
+        cols = len(t) + 1
+        distance = np.zeros((rows, cols), dtype=int)
+
+        # Populate matrix of zeros with the indices of each character of both strings
+        for i in range(1, rows):
+            for k in range(1, cols):
+                distance[i][0] = i
+                distance[0][k] = k
+
+        # Iterate over the matrix to compute the cost of deletions, insertions and/or
+        # substitutions
+        for col in range(1, cols):
+            for row in range(1, rows):
+                if s[row - 1] == t[col - 1]:
+                    # If the characters are the same in the two strings in a given
+                    # position [i,j] then the cost is 0
+                    cost = 0
+                else:
+                    # In order to align the results with those of the Python Levenshtein
+                    # package, the cost of a substitution is 2.
+                    cost = 2
+                distance[row][col] = min(
+                    distance[row - 1][col] + 1,  # Cost of deletions
+                    distance[row][col - 1] + 1,  # Cost of insertions
+                    distance[row - 1][col - 1] + cost,  # Cost of substitutions
+                )
+        # Computation of the Levenshtein Distance Ratio
+        ratio = ((len(s) + len(t)) - distance[row][col]) / (len(s) + len(t))
+        return ratio
+
     def get_best_matches(self, key):
         "Get best matches from keys"
         key = key.lower()
         best_three = []
         lowest_score = 0
         for k in self.keys():
-            score = Levenshtein.ratio(k.lower(), key)
+            score = self.levenshtein_ratio(k.lower(), key)
             # Start filling out the list
             if len(best_three) < 3:
                 best_three.append((k, score))
