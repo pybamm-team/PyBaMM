fa7680a83df11d461a8b2ea3fd8fcc6e8e20f039
tomtranter <t.g.tranter@gmail.com>
2020-02-26 15:46:12 +0000

    Add new electrolyte functions from Landesfeind 2019

---
+++ pybamm/input/parameters/lithium-ion/electrolytes/lipf6_Landesfeind2019/electrolyte_diffusivity_Landesfeind2019_base.py
@@ -0,0 +1,32 @@
+from pybamm import exp
+
+
+def electrolyte_diffusivity_Landesfeind2019_base(c_e, T, coeffs):
+    """
+    Conductivity of LiPF6 in solvent_X as a function of ion concentration and
+    Temperature. The data comes from [1].
+    References
+    ----------
+    .. [1] Landesfeind, J. and Gasteiger, H.A., 2019. Temperature and Concentration
+    Dependence of the Ionic Transport Properties of Lithium-Ion Battery Electrolytes.
+    Journal of The Electrochemical Society, 166(14), pp.A3079-A3097.
+    ----------
+    c_e: :class: `numpy.Array`
+        Dimensional electrolyte concentration
+    T: :class: `numpy.Array`
+        Dimensional temperature
+    coeffs: :class: `numpy.Array`
+        Fitting parameter coefficients
+    Returns
+    -------
+    :`numpy.Array`
+        Electrolyte diffusivity
+    """
+    c = c_e/1000 # mol.m-3 -> mol.l
+    p1, p2, p3, p4  = coeffs
+    A = p1 * exp(p2 * c)
+    B = exp(p3 / T)
+    C = exp(p4 * c / T)
+    D_e = A * B * C * 1e-6  # m2/s
+
+    return D_e
---
+++ tests/unit/test_parameters/test_parameter_sets/test_Landesfeind2020.py
@@ -0,0 +1,70 @@
+#
+# Tests for LG M50 parameter set loads
+#
+import pybamm
+import unittest
+import os
+import numpy as np
+
+
+class TestLandesfeind(unittest.TestCase):
+
+    def test_electrolyte_conductivity(self):
+        root = pybamm.root_dir()
+        p = "pybamm/input/parameters/lithium-ion/electrolytes/lipf6_Landesfeind2019"
+        k_path = os.path.join(root, p)
+        files = [f for f in os.listdir(k_path) if '.py' in f
+                 and '_base' not in f
+                 and 'conductivity' in f]
+        funcs = [pybamm.load_function(os.path.join(k_path, f)) for f in files]
+        T_ref = 298.15
+        T = T_ref + 30.0
+        c = 1000.0
+        k = [np.around(f(c, T, np.nan, np.nan, np.nan).value, 6) for f in funcs]
+        self.assertEqual(k, [1.839786, 1.361015, 0.750259])
+        T += 20
+        k = [np.around(f(c, T, np.nan, np.nan, np.nan).value, 6) for f in funcs]
+        self.assertEqual(k, [2.292425, 1.664438, 0.880755])
+
+        chemistry = pybamm.parameter_sets.Chen2020
+        param = pybamm.ParameterValues(chemistry=chemistry)
+        param['Electrolyte conductivity [S.m-1]'] = funcs[0]
+        model = pybamm.lithium_ion.SPM()
+        sim = pybamm.Simulation(model, parameter_values=param)
+        sim.set_parameters()
+        sim.build()
+
+
+    def test_electrolyte_diffusivity(self):
+        root = pybamm.root_dir()
+        p = "pybamm/input/parameters/lithium-ion/electrolytes/lipf6_Landesfeind2019"
+        d_path = os.path.join(root, p)
+        files = [f for f in os.listdir(d_path) if '.py' in f
+                 and '_base' not in f
+                 and 'diffusivity' in f]
+        funcs = [pybamm.load_function(os.path.join(d_path, f)) for f in files]
+        T_ref = 298.15
+        T = T_ref + 30.0
+        c = 1000.0
+        D = [np.around(f(c, T, np.nan, np.nan, np.nan).value, 12) for f in funcs]
+        self.assertEqual(D, [5.796505e-06, 5.417881e-06, 5.608856e-06])
+        T += 20
+        D = [np.around(f(c, T, np.nan, np.nan, np.nan).value, 12) for f in funcs]
+        self.assertEqual(D, [8.5992e-06, 7.752815e-06, 7.907549e-06])
+        
+        chemistry = pybamm.parameter_sets.Chen2020
+        param = pybamm.ParameterValues(chemistry=chemistry)
+        param['Electrolyte diffusivity [m2.s-1]'] = funcs[0]
+        model = pybamm.lithium_ion.SPM()
+        sim = pybamm.Simulation(model, parameter_values=param)
+        sim.set_parameters()
+        sim.build()
+
+if __name__ == "__main__":
+    print("Add -v for more debug output")
+    import sys
+
+    if "-v" in sys.argv:
+        debug = True
+    pybamm.settings.debug_mode = True
+    unittest.main()
