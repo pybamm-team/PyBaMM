9225e00378b8aab1e6cc035f763efaf3b5e812cf
Valentin Sulzer <valentinsulzer@hotmail.com>
2020-02-26 16:35:02 -0500

    #861 remove external submodels

--- tests/unit/test_parameters/test_parameters_cli.py
+++ tests/unit/test_parameters/test_parameters_cli.py
@@ -119,3 +119,13 @@ class TestParametersCLI(unittest.TestCase):
         # ./input/parameters/lithium-ion/cathodes/tmp_dir
         # but must not intefere with existing input dir if it exists
         # in the current dir...
+
+
+if __name__ == "__main__":
+    print("Add -v for more debug output")
+    import sys
+
+    if "-v" in sys.argv:
+        debug = True
+    pybamm.settings.debug_mode = True
+    unittest.main()
