d9d758128f5d9c248ae33fc7b594507631e193d6
Valentin Sulzer <valentinsulzer@hotmail.com>
2020-02-25 19:38:41 -0500

    #820 fix cccv

--- examples/scripts/experimental_protocols/cccv.py
+++ examples/scripts/experimental_protocols/cccv.py
@@ -2,7 +2,9 @@
 # Constant-current constant-voltage charge
 #
 import pybamm
+import matplotlib.pyplot as plt
 
+pybamm.set_logging_level("INFO")
 experiment = pybamm.Experiment(
     [
         "Discharge at C/10 for 10 hours or until 3.3 V",
