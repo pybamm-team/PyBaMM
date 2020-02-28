7cea7b5790a7b434d82eb66cfc4d170b6802df8f
Valentin Sulzer <valentinsulzer@hotmail.com>
2020-02-28 15:49:18 -0500

    #865 coverage

--- pybamm/models/submodels/interface/first_order_kinetics/inverse_first_order_kinetics.py
+++ pybamm/models/submodels/interface/first_order_kinetics/inverse_first_order_kinetics.py
@@ -76,25 +76,3 @@ class InverseFirstOrderKinetics(BaseInterface):
         )
 
         return variables
-
-    def _get_j_diffusion_limited_first_order(self, variables):
-        """
-        First-order correction to the interfacial current density due to
-        diffusion-limited effects. For a general model the correction term is zero,
-        since the reaction is not diffusion-limited
-        """
-        j_leading_order = variables[
-            "Leading-order x-averaged "
-            + self.domain.lower()
-            + " electrode"
-            + self.reaction_name
-            + " interfacial current density"
-        ]
-        param = self.param
-        if self.domain == "Negative":
-            N_ox_s_p = variables["Oxygen flux"].orphans[1]
-            N_ox_neg_sep_interface = N_ox_s_p[0]
-
-            j = -N_ox_neg_sep_interface / param.C_e / param.s_ox_Ox / param.l_n
-
-        return (j - j_leading_order) / param.C_e
