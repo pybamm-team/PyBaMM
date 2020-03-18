4f9951faffe553710e86f9c74f766309fb0b9c48
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2020-03-18 10:28:15 +0000

    Fix minor bugs in setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,8 @@
 import os
 import sys
 import subprocess
-<<<<<<< HEAD
-from pathlib import Path
-=======
-import tarfile
 import glob
-import pathlib
-from shutil import copy
-from platform import python_version
->>>>>>> parent of 65a670cd... Remove package_data dict from setup.py
+from pathlib import Path
 
 import wheel.bdist_wheel as orig
 
@@ -103,7 +96,14 @@ class CMakeBuild(build_ext):
             self.move_output(ext)
 
     def move_output(self, ext):
+        # Copy built module to dist/ directory
         build_temp = Path(self.build_temp).resolve()
+        source_path = build_temp / self.get_ext_filename(ext.name)
+        # Get destination location
+        # self.get_ext_fullpath(ext.name) -->
+        # build/lib.linux-x86_64-3.5/idaklu.cpython-37m-x86_64-linux-gnu.so
+        # using resolve() with python < 3.6 will result in a FileNotFoundError
+        # since the location does not yet exists.
         dest_path = Path(self.get_ext_fullpath(ext.name)).resolve()
         source_path = build_temp / self.get_ext_filename(ext.name)
         dest_directory = dest_path.parents[0]
@@ -130,14 +130,14 @@ for file_ext in ["*.csv", "*.py", "*.md"]:
     #  'input/drive_cycles/US06.csv',
     # ...
     pybamm_data.extend(
-        [os.path.join(*pathlib.Path(filename).parts[1:]) for filename in list_of_files]
+        [os.path.join(*Path(filename).parts[1:]) for filename in list_of_files]
     )
 pybamm_data.append("./version")
 pybamm_data.append("./CITATIONS.txt")
 
 setup(
     name="pybamm",
-    version="1.0",
+    version="2.0",
     description="Python Battery Mathematical Modelling.",
     long_description="description",
     long_description_content_type="text/markdown",
