9a71bcb4b354f8ca68ed88b1a057f8ffb0b043cf
Valentin Sulzer <valentinsulzer@hotmail.com>
2020-04-03 18:23:26 -0400

    #937 add 1D tests

--- tests/unit/test_solvers/test_processed_casadi_variable.py
+++ tests/unit/test_solvers/test_processed_casadi_variable.py
@@ -163,7 +163,53 @@ class TestProcessedCasadiVariable(unittest.TestCase):
         )
 
     def test_processed_variable_1D_with_vector_inputs(self):
-        pass
+        var = pybamm.Variable("var", domain=["negative electrode", "separator"])
+        x = pybamm.SpatialVariable("x", domain=["negative electrode", "separator"])
+        p = pybamm.InputParameter("p")
+        q = pybamm.InputParameter("q")
+        eqn = (var * p) ** 2 + 2 * q
+
+        # On nodes
+        disc = tests.get_discretisation_for_testing()
+        disc.set_variable_slices([var])
+        x_sol = disc.process_symbol(x).entries[:, 0]
+        n = x_sol.size
+        eqn_sol = disc.process_symbol(eqn)
+
+        # Scalar t
+        t_sol = [0]
+        y_sol = np.ones_like(x_sol)[:, np.newaxis] * 5
+        sol = pybamm.CasadiSolution(t_sol, y_sol)
+        sol.inputs = {"p": casadi.MX.sym("p", n), "q": casadi.MX.sym("q")}
+        processed_eqn = pybamm.ProcessedCasadiVariable(eqn_sol, sol)
+
+        # Test values - constant p
+        np.testing.assert_array_equal(
+            processed_eqn.value(np.r_[2 * np.ones(n), [3]]), (2 * y_sol) ** 2 + 6
+        )
+        np.testing.assert_array_equal(
+            processed_eqn.value({"p": 27 * np.ones(n), "q": -42}),
+            (27 * y_sol) ** 2 - 84,
+        )
+        # Test values - varying p
+        p = np.linspace(0, 1, n)
+        np.testing.assert_array_equal(
+            processed_eqn.value(np.r_[p, [3]]), (p[:, np.newaxis] * y_sol) ** 2 + 6,
+        )
+
+        # Test sensitivities - constant p
+        np.testing.assert_array_equal(
+            processed_eqn.sensitivity({"p": 2 * np.ones(n), "q": -84}),
+            np.c_[100 * np.eye(y_sol.size), 2 * np.ones(n)],
+        )
+        # Test sensitivities - varying p
+        # d/dy((py)**2) = (2*p*y) * y
+        np.testing.assert_array_equal(
+            processed_eqn.sensitivity({"p": p, "q": -84}),
+            np.c_[
+                np.diag((2 * p[:, np.newaxis] * y_sol ** 2).flatten()), 2 * np.ones(n)
+            ],
+        )
 
 
 if __name__ == "__main__":
