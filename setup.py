85bc1fac4d1b5f5baecb75a04bf864161960e1e3
Valentin Sulzer <valentinsulzer@hotmail.com>
2020-04-06 11:55:10 -0400

    #546 remove levenshtein

--- setup.py
+++ setup.py
@@ -499,7 +499,6 @@ setup(
         "scikit-fem>=0.2.0",
         "casadi>=3.5.0",
         "jupyter",  # For example notebooks
-        "python-Levenshtein>=0.12.0",
         # Note: Matplotlib is loaded for debug plots, but to ensure pybamm runs
         # on systems without an attached display, it should never be imported
         # outside of plot() methods.
