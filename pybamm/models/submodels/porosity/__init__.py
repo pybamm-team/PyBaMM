ecf53a7c14c91c9d4dfe030a69839f97dfd129b6
felipe-salinas <64426781+felipe-salinas@users.noreply.github.com>
2020-05-25 13:51:01 +0200

    Creates new porosity class

--- pybamm/models/submodels/porosity/__init__.py
+++ pybamm/models/submodels/porosity/__init__.py
@@ -1,4 +1,5 @@
 from .base_porosity import BaseModel
 from .constant_porosity import Constant
 from .full_reaction_driven_porosity import Full
+from .sei_reaction_driven_porosity import SeiFull
 from .leading_reaction_driven_porosity import LeadingOrder
