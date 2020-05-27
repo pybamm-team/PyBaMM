50e6ad5d5a45009b833c8ede6544d1d4adb84749
felipe-salinas <64426781+felipe-salinas@users.noreply.github.com>
2020-05-27 11:45:43 +0200

    c_ec defined in get_coupled_variables

--- pybamm/models/submodels/interface/sei/ec_reaction_limited.py
+++ pybamm/models/submodels/interface/sei/ec_reaction_limited.py
@@ -34,19 +34,13 @@ class EcReactionLimited(BaseModel):
             domain=self.domain.lower() + " electrode",
             auxiliary_domains={"secondary": "current collector"},
         )
-        c_ec = pybamm.Variable(
-            self.domain + " electrode EC surface concentration",
-            domain=self.domain.lower() + " electrode",
-            auxiliary_domains={"secondary": "current collector"},
-        )
         L_sei_av = pybamm.x_average(L_sei)
         j_sei_av = pybamm.x_average(j_sei)
-        c_ec_av = pybamm.x_average(c_ec)
 
         L_scale = pybamm.sei_parameters.L_sei_0_dim
         # in this model the scale is identical to the intercalation current
         j_scale = pybamm.sei_parameters.j_scale_n
-        c_scale = pybamm.sei_parameters.c_ec_0_dim
+        R_sei_dim = pybamm.sei_parameters.R_sei_dimensional
 
         variables = {
             "Total " + self.domain.lower() +
@@ -54,7 +48,9 @@ class EcReactionLimited(BaseModel):
             "Total " + self.domain.lower() +
             " sei thickness [m]": L_sei * L_scale,
             "X-averaged total " + self.domain.lower() +
-            " sei thickness": L_sei_av,
+            " electrode sei thickness": L_sei_av,
+            "X-averaged total " + self.domain.lower() +
+            " electrode sei thickness [m]": L_sei_av * L_scale,
             self.domain +
             " electrode sei interfacial current density": j_sei,
             self.domain +
@@ -67,6 +63,24 @@ class EcReactionLimited(BaseModel):
             " electrode sei interfacial current density [A.m-2]": j_sei_av * j_scale,
             "X-averaged " + self.domain.lower() +
             " electrode scaled sei interfacial current density": j_sei_av,
+            "X-averaged " + self.domain.lower() +
+            " electrode resistance [Ohm.m2]": L_sei_av * L_scale * R_sei_dim,
+        }
+
+        return variables
+
+    def get_coupled_variables(self, variables):
+
+        j_sei = variables[self.domain +
+                          " electrode sei interfacial current density"]
+        L_sei = variables["Total " + self.domain.lower() +
+                          " electrode sei thickness"]
+        C_ec = pybamm.sei_parameters.C_ec
+        c_scale = pybamm.sei_parameters.c_ec_0_dim
+        # concentration of EC on graphite surface, base case = 1
+        c_ec = pybamm.Scalar(1) + j_sei * L_sei * C_ec
+        c_ec_av = pybamm.x_average(c_ec)
+        variables = {
             self.domain +
             " electrode EC surface concentration": c_ec,
             self.domain +
@@ -76,11 +90,6 @@ class EcReactionLimited(BaseModel):
             "X-averaged " + self.domain.lower() +
             " electrode EC surface concentration": c_ec_av * c_scale,
         }
-
-        return variables
-
-    def get_coupled_variables(self, variables):
-
         # Update whole cell variables, which also updates the "sum of" variables
         if (
             "Negative electrode sei interfacial current density" in variables
@@ -106,11 +115,11 @@ class EcReactionLimited(BaseModel):
     def set_algebraic(self, variables):
         phi_s_n = variables[self.domain + " electrode potential"]
         phi_e_n = variables[self.domain + " electrolyte potential"]
-        c_ec = variables[self.domain + " electrode EC surface concentration"]
         j_sei = variables[self.domain +
                           " electrode sei interfacial current density"]
         L_sei = variables["Total " + self.domain.lower() +
                           " electrode sei thickness"]
+        c_ec = variables[self.domain + " electrode EC surface concentration"]
 
         # Look for current that contributes to the -IR drop
         # If we can't find the interfacial current density from the main reaction, j,
@@ -125,25 +134,21 @@ class EcReactionLimited(BaseModel):
             j = variables["X-averaged " + self.domain.lower()
                           + " electrode total interfacial current density"]
         C_sei_ec = pybamm.sei_parameters.C_sei_ec
-        C_ec = pybamm.sei_parameters.C_ec
         R_sei = pybamm.sei_parameters.R_sei
 
         # need to revise for thermal case
 
         self.algebraic = {j_sei: j_sei + C_sei_ec * c_ec * pybamm.exp(- 0.5 * (
-            phi_s_n - phi_e_n - j * L_sei * R_sei)),
-            c_ec: c_ec - pybamm.Scalar(1) - j_sei * L_sei * C_ec}
+            phi_s_n - phi_e_n - j * L_sei * R_sei))}
 
     def set_initial_conditions(self, variables):
         L_sei = variables["Total " + self.domain.lower() +
                           " electrode sei thickness"]
-        c_ec = variables[self.domain + " electrode EC surface concentration"]
         j_sei = variables[self.domain +
                           " electrode sei interfacial current density"]
 
         L_sei_0 = pybamm.Scalar(1)
-        c_ec_0 = pybamm.Scalar(1)
         j_sei_0 = pybamm.Scalar(0)
 
         self.initial_conditions = {L_sei: L_sei_0,
-                                   c_ec: c_ec_0, j_sei: j_sei_0}
+                                   j_sei: j_sei_0}
