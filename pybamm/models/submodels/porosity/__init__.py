90162c5207279bd3b03c41b16d4deb5954348016
felipe-salinas <64426781+felipe-salinas@users.noreply.github.com>
2020-06-02 13:08:06 +0200

    Original file

--- pybamm/models/submodels/porosity/__init__.py
+++ pybamm/models/submodels/porosity/__init__.py
@@ -1,5 +1,4 @@
 from .base_porosity import BaseModel
 from .constant_porosity import Constant
 from .full_reaction_driven_porosity import Full
-from .sei_reaction_driven_porosity import SeiFull
 from .leading_reaction_driven_porosity import LeadingOrder
