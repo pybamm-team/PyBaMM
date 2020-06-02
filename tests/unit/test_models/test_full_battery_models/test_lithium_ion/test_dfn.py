f31be986096555dec9d8db33b80a1c594dba5b2c
felipe-salinas <64426781+felipe-salinas@users.noreply.github.com>
2020-06-02 13:18:16 +0200

    Adds test for ex reaction limited

--- tests/unit/test_models/test_full_battery_models/test_lithium_ion/test_dfn.py
+++ tests/unit/test_models/test_full_battery_models/test_lithium_ion/test_dfn.py
@@ -149,6 +149,11 @@ class TestDFNWithSEI(unittest.TestCase):
         model = pybamm.lithium_ion.DFN(options)
         model.check_well_posedness()
 
+    def test_well_posed_ec_reaction_limited(self):
+        options = {"sei": "ec reaction limited"}
+        model = pybamm.lithium_ion.DFN(options)
+        model.check_well_posedness()
+
 
 if __name__ == "__main__":
     print("Add -v for more debug output")
