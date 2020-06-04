a59f64bce44652c23244307f50f284920fabb07f
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2020-06-04 19:03:05 +0100

    Use homemade action to build manylinux wheels

---
+++ build_manylinux_wheels/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e -x
+
+# CLI arguments
+PY_VERSIONS=$1
+
+# Compile wheels
+arrPY_VERSIONS=(${PY_VERSIONS// / })
+for PY_VER in "${arrPY_VERSIONS[@]}"; do
+    # Update pip
+    /opt/python/"${PY_VER}"/bin/pip install --upgrade --no-cache-dir pip
+
+    # Build wheels
+    /opt/python/"${PY_VER}"/bin/pip wheel /github/workspace/ -w /github/workspace/wheelhouse/ --no-deps || { echo "Building wheels failed."; exit 1; }
+done
+
+# Bundle external shared libraries into the wheels
+for whl in /github/workspace/wheelhouse/*-linux*.whl; do
+    auditwheel repair "$whl" --plat "${PLAT}" -w /github/workspace/dist/ || { echo "Repairing wheels failed."; auditwheel show "$whl"; exit 1; }
+done
+
+echo "Succesfully built wheels:"
+ls -l /github/workspace/dist/
