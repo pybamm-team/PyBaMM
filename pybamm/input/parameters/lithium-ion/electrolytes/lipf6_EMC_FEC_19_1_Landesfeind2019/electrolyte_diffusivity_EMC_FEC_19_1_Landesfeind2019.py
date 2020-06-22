db2f502f063fc3f4618ea3b75cc8a684a60182e1
Ferran Brosa Planella <Ferran.Brosa-Planella@warwick.ac.uk>
2020-06-22 11:03:56 +0200

    #1048 fixed typos

--- pybamm/input/parameters/lithium-ion/electrolytes/lipf6_EMC_FEC_19_1_Landesfeind2019/electrolyte_diffusivity_EMC_FEC_19_1_Landesfeind2019.py
+++ pybamm/input/parameters/lithium-ion/electrolytes/lipf6_EMC_FEC_19_1_Landesfeind2019/electrolyte_diffusivity_EMC_FEC_19_1_Landesfeind2019.py
@@ -8,16 +8,20 @@ def electrolyte_diffusivity_EMC_FEC_19_1_Landesfeind2019(c_e, T):
     """
     Diffusivity of LiPF6 in EMC:FEC (19:1 w:w) as a function of ion concentration and
     temperature. The data comes from [1].
+
     References
     ----------
     .. [1] Landesfeind, J. and Gasteiger, H.A., 2019. Temperature and Concentration
     Dependence of the Ionic Transport Properties of Lithium-Ion Battery Electrolytes.
     Journal of The Electrochemical Society, 166(14), pp.A3079-A3097.
+
+    Parameters
     ----------
     c_e: :class:`pybamm.Symbol`
         Dimensional electrolyte concentration
     T: :class:`pybamm.Symbol`
         Dimensional temperature
+
     Returns
     -------
     :class:`pybamm.Symbol`
