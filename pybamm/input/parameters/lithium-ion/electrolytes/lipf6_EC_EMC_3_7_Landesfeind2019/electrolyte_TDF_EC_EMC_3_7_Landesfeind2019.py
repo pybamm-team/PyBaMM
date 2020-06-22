3a0313413cb103d8e477133bc495387ebd84f331
Ferran Brosa Planella <Ferran.Brosa-Planella@warwick.ac.uk>
2020-06-22 11:03:56 +0200

    #1048 fixed typos

--- pybamm/input/parameters/lithium-ion/electrolytes/lipf6_EC_EMC_3_7_Landesfeind2019/electrolyte_TDF_EC_EMC_3_7_Landesfeind2019.py
+++ pybamm/input/parameters/lithium-ion/electrolytes/lipf6_EC_EMC_3_7_Landesfeind2019/electrolyte_TDF_EC_EMC_3_7_Landesfeind2019.py
@@ -4,22 +4,26 @@ import numpy as np
 
 def electrolyte_TDF_EC_EMC_3_7_Landesfeind2019(c_e, T=298.15):
     """
-    Diffusivity of LiPF6 in EC:EMC (3:7 w:w) as a function of ion concentration and
-    temperature. The data comes from [1].
+    Thermodynamic factor of LiPF6 in EC:EMC (3:7 w:w) as a function of ion
+    concentration and temperature. The data comes from [1].
+
     References
     ----------
     .. [1] Landesfeind, J. and Gasteiger, H.A., 2019. Temperature and Concentration
     Dependence of the Ionic Transport Properties of Lithium-Ion Battery Electrolytes.
     Journal of The Electrochemical Society, 166(14), pp.A3079-A3097.
+
+    Parameters
     ----------
     c_e: :class:`pybamm.Symbol`
         Dimensional electrolyte concentration
     T: :class:`pybamm.Symbol`
         Dimensional temperature
+
     Returns
     -------
     :class:`pybamm.Symbol`
-        Electrolyte diffusivity
+        Electrolyte thermodynamic factor
     """
     coeffs = np.array(
         [2.57e1, -4.51e1, -1.77e-1, 1.94, 2.95e-1, 3.08e-4, 2.59e-1, -9.46e-3, -4.54e-4]
