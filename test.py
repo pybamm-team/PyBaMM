58936dca5213afe9138b12479bf8b9b8e54be674
Valentin Sulzer <valentinsulzer@hotmail.com>
2020-08-16 21:38:33 -0400

    #1126 implement higher-order method (LUDS)

--- test.py
+++ test.py
@@ -1,19 +1,25 @@
 import pybamm
 
-c = pybamm.Variable("c", domain="negative electrode")
+c = pybamm.Variable("c", domain=["negative electrode"])
 model = pybamm.BaseModel()
-# x = pybamm.SpatialVariableEdge("x", domain="negative electrode")
+# x = pybamm.SpatialVariableEdge("x", domain=["negative electrode"])
 # v = 0.5 + 0.5 * x
-v = pybamm.PrimaryBroadcastToEdges(1, "negative electrode")
-model.rhs = {c: -pybamm.div(pybamm.upwind(c) * v) + 2}
-model.initial_conditions = {c: 1}
-model.boundary_conditions = {c: {"left": (0, "Dirichlet"), "right": (0, "Neumann")}}
+v = pybamm.PrimaryBroadcastToEdges(1, ["negative electrode"])
+model.rhs = {c: -pybamm.div(-pybamm.downwind(c) * v) + 2}
+model.initial_conditions = {c: 0}
+model.boundary_conditions = {c: {"left": (0, "Neumann"), "right": (0, "Dirichlet")}}
+# model.boundary_conditions = {c: {"left": (0, "Dirichlet"), "right": (0, "Neumann")}}
 model.variables = {"c": c}
 
 spm = pybamm.lithium_ion.SPM()
 
+var = pybamm.standard_spatial_vars
+
 param = spm.default_parameter_values
-geometry = spm.default_geometry
+geometry = {
+    "negative electrode": {var.x_n: {"min": 0, "max": 1}},
+    "current collector": {var.z: {"position": 1}},
+}
 submesh_types = spm.default_submesh_types
 var_pts = spm.default_var_pts
 spatial_methods = spm.default_spatial_methods
