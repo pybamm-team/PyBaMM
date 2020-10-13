cf39650e281379747b95b861d60b4af2c900bcd3
Ferran Brosa Planella <Ferran.Brosa-Planella@warwick.ac.uk>
2020-10-13 12:21:21 +0200

    #884 removed SPME_FBP (for development only)

--- pybamm/models/full_battery_models/lithium_ion/__init__.py
+++ pybamm/models/full_battery_models/lithium_ion/__init__.py
@@ -4,7 +4,6 @@
 from .base_lithium_ion_model import BaseModel
 from .spm import SPM
 from .spme import SPMe
-from .spme_fbp import SPMeFBP
 from .dfn import DFN
 from .basic_dfn import BasicDFN
 from .basic_spm import BasicSPM
