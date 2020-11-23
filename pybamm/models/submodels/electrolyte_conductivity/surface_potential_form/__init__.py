d12df652e0f12ab04bb943334ffbf072ddc23fd2
Robert Timms <timms@maths.ox.ac.uk>
2020-11-23 14:37:55 +0000

    #1207 add composite surface form

--- pybamm/models/submodels/electrolyte_conductivity/surface_potential_form/__init__.py
+++ pybamm/models/submodels/electrolyte_conductivity/surface_potential_form/__init__.py
@@ -1,6 +1,12 @@
 # Full order models
 from .full_surface_form_conductivity import FullAlgebraic, FullDifferential
 
+# Composite models
+from .composite_surface_form_conductivity import (
+    CompositeDifferential,
+    CompositeAlgebraic,
+)
+
 # Leading-order models
 from .leading_surface_form_conductivity import (
     LeadingOrderDifferential,
