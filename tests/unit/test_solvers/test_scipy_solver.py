8577fea3d35c6814b6a4708819c9f30179ab008f
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2020-12-09 10:43:03 +0000

    Add test for ScipySolver.solve() with multiple inputs

--- tests/unit/test_solvers/test_scipy_solver.py
+++ tests/unit/test_solvers/test_scipy_solver.py
@@ -217,6 +217,37 @@ class TestScipySolver(unittest.TestCase):
         np.testing.assert_array_equal(solution.t, t_eval[: len(solution.t)])
         np.testing.assert_allclose(solution.y[0], np.exp(-0.1 * solution.t))
 
+    def test_model_solver_with_multiple_inputs(self):
+        # Create model
+        model = pybamm.BaseModel()
+        model.convert_to_format = "python"
+        domain = ["negative electrode", "separator", "positive electrode"]
+        var = pybamm.Variable("var", domain=domain)
+        model.rhs = {var: -pybamm.InputParameter("rate") * var}
+        model.initial_conditions = {var: 1}
+        # No need to set parameters; can use base discretisation (no spatial
+        # operators)
+        # create discretisation
+        mesh = get_mesh_for_testing()
+        spatial_methods = {"macroscale": pybamm.FiniteVolume()}
+        disc = pybamm.Discretisation(mesh, spatial_methods)
+        disc.process_model(model)
+
+        # Solve
+        solver = pybamm.ScipySolver(rtol=1e-8, atol=1e-8, method="RK45")
+        t_eval = np.linspace(0, 10, 100)
+        ninputs = 8
+        inputs_list = [{"rate": 0.01 * (i + 1)} for i in range(ninputs)]
+        solutions = solver.solve(model, t_eval, inputs=inputs_list, nproc=2)
+        for i in range(ninputs):
+            with self.subTest(i=i):
+                solution = solutions[i]
+                self.assertLess(len(solution.t), len(t_eval))
+                np.testing.assert_array_equal(solution.t, t_eval[: len(solution.t)])
+                np.testing.assert_allclose(
+                    solution.y[0], np.exp(-0.01 * (i + 1) * solution.t)
+                )
+
     def test_model_solver_with_external(self):
         # Create model
         model = pybamm.BaseModel()
