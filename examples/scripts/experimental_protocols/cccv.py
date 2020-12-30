eb2cbea6e25743422ab9fbaefc06fdac7802f9c8
jack <jack.lonnborn@gmail.com>
2020-12-30 13:58:43 +1100

    black

--- examples/scripts/experimental_protocols/cccv.py
+++ examples/scripts/experimental_protocols/cccv.py
@@ -7,11 +7,13 @@ import matplotlib.pyplot as plt
 pybamm.set_logging_level("INFO")
 experiment = pybamm.Experiment(
     [
-        ("Discharge at C/10 for 10 hours or until 3.3 V",
-        "Rest for 1 hour",
-        "Charge at 1 A until 4.1 V",
-        "Hold at 4.1 V until 50 mA",
-        "Rest for 1 hour"),
+        (
+            "Discharge at C/10 for 10 hours or until 3.3 V",
+            "Rest for 1 hour",
+            "Charge at 1 A until 4.1 V",
+            "Hold at 4.1 V until 50 mA",
+            "Rest for 1 hour",
+        ),
     ]
     * 3
 )
