46fba47f67fb735ff07e91c6135e60a594c4c042
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2021-02-11 17:56:49 +0000

    Extract prepare_model function at top-level

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -3,50 +3,51 @@
 
 import pybamm
 import numpy as np
-import sys
+
+
+def prepare_model(model):
+    geometry = model.default_geometry
+
+    # load parameter values and process model and geometry
+    chemistry = pybamm.parameter_sets.Marquis2019
+    param = pybamm.ParameterValues(chemistry=chemistry)
+    param.process_model(model)
+    param.process_geometry(geometry)
+
+    # set mesh
+    var = pybamm.standard_spatial_vars
+    var_pts = {
+        var.x_n: 20,
+        var.x_s: 20,
+        var.x_p: 20,
+        var.r_n: 30,
+        var.r_p: 30,
+        var.y: 10,
+        var.z: 10,
+    }
+    mesh = pybamm.Mesh(geometry, model.default_submesh_types, var_pts)
+
+    # discretise model
+    disc = pybamm.Discretisation(mesh, model.default_spatial_methods)
+    disc.process_model(model)
+
 
 class TimeSolveSPM:
     params = [True, False]
+    solver = pybamm.CasadiSolver()
 
     def setup(self, solve_first):
         self.model = pybamm.lithium_ion.SPM()
-        geometry = self.model.default_geometry
-
-        # load parameter values and process model and geometry
-        chemistry = pybamm.parameter_sets.Marquis2019
-        param = pybamm.ParameterValues(chemistry=chemistry)
-        param.process_model(self.model)
-        param.process_geometry(geometry)
-
-        # set mesh
-        var = pybamm.standard_spatial_vars
-        var_pts = {
-            var.x_n: 20,
-            var.x_s: 20,
-            var.x_p: 20,
-            var.r_n: 30,
-            var.r_p: 30,
-            var.y: 10,
-            var.z: 10,
-        }
-        mesh = pybamm.Mesh(geometry, self.model.default_submesh_types, var_pts)
-
-        # discretise model
-        disc = pybamm.Discretisation(mesh,self.model.default_spatial_methods)
-        disc.process_model(self.model)
-
         c_rate = 1
-        self.tmax = 4000/c_rate
-        self.nb_points = 500
+        tmax = 4000 / c_rate
+        nb_points = 500
+        self.t_eval = np.linspace(0, tmax, nb_points)
+        prepare_model(self.model)
         if solve_first:
-            solver = pybamm.CasadiSolver()
-            t_eval = np.linspace(0, self.tmax, self.nb_points)
-            solver.solve(self.model, t_eval=t_eval)
+            self.solve_model_once(True)
 
+    def solve_model_once(self, solve_first):
+        TimeSolveSPM.solver.solve(self.model, t_eval=self.t_eval)
 
     def time_solve_model(self, solve_first):
-        solver = pybamm.CasadiSolver()
-        t_eval = np.linspace(0, self.tmax, self.nb_points)
-        solver.solve(self.model, t_eval=t_eval)
-
-    
+        TimeSolveSPM.solver.solve(self.model, t_eval=self.t_eval)
