2378c0f146ef15107f2492ffc04d5db48d4f4818
Priyanshu Agarwal <priyanshuone6@gmail.com>
2021-02-11 11:43:39 +0530

    add tests

--- tests/unit/test_parameters/test_update_parameter_sets_doc.py
+++ tests/unit/test_parameters/test_update_parameter_sets_doc.py
@@ -1,7 +1,7 @@
 """
 Tests for the update_parameter_sets_doc.py
 """
-from pybamm.parameters.update_parameter_sets_doc import generate_ps_doc
+from pybamm.parameters.update_parameter_sets_doc import get_ps_dict, generate_ps_doc
 import pybamm
 import unittest
 
@@ -14,39 +14,43 @@ your simulation, add the line "pybamm.print_citations()" to your script.
 Lead-acid parameter sets
 ------------------------
     * Sulzer2019 :
-        Valentin Sulzer, S. Jon Chapman, Colin P. Please, David A. Howey, and Charles W.
+      - Valentin Sulzer, S. Jon Chapman, Colin P. Please, David A. Howey, and Charles W.
         Monroe. Faster Lead-Acid Battery Simulations from Porous-Electrode Theory: Part
         I. Physical Model. Journal of The Electrochemical Society, 166(12):A2363–A2371,
         2019. doi:10.1149/2.0301910jes.
 
 Lithium-ion parameter sets
 --------------------------
-    * Ecker2015 :
-        Madeleine Ecker, Thi Kim Dung Tran, Philipp Dechent, Stefan Käbitz, Alexander
-        Warnecke, and Dirk Uwe Sauer. Parameterization of a Physico-Chemical Model of a
-        Lithium-Ion Battery: I. Determination of Parameters. Journal of the
-        Electrochemical Society, 162(9):A1836–A1848, 2015. doi:10.1149/2.0551509jes.
-        Giles Richardson, Ivan Korotkin, Rahifa Ranom, Michael Castle, and Jamie M.
-        Foster. Generalised single particle models for high-rate operation of graded
-        lithium-ion electrodes: systematic derivation and validation. Electrochimica
-        Acta, 339:135862, 2020. doi:10.1016/j.electacta.2020.135862.
-    * NCA_Kim2011 :
-        Gi-Heon Kim, Kandler Smith, Kyu-Jin Lee, Shriram Santhanagopalan, and Ahmad
-        Pesaran. Multi-domain modeling of lithium-ion batteries encompassing
-        multi-physics in varied length scales. Journal of the Electrochemical Society,
-        158(8):A955–A969, 2011. doi:10.1149/1.3597614.
+    * Prada2013 :
+      - Chang-Hui Chen, Ferran Brosa Planella, Kieran O'Regan, Dominika Gastol, W.
+        Dhammika Widanage, and Emma Kendrick. Development of Experimental Techniques for
+        Parameterization of Multi-scale Lithium-ion Battery Models. Journal of The
+        Electrochemical Society, 167(8):080534, 2020. doi:10.1149/1945-7111/ab9050.
+      - Michael J. Lain, James Brandon, and Emma Kendrick. Design strategies for high
+        power vs. high energy lithium ion cells. Batteries, 5(4):64, 2019.
+        doi:10.3390/batteries5040064.
+      - Eric Prada, D. Di Domenico, Y. Creff, J. Bernard, Valérie Sauvant-Moynot, and
+        François Huet. A simplified electrochemical and thermal aging model of
+        LiFePO4-graphite Li-ion batteries: power and capacity fade simulations. Journal
+        of The Electrochemical Society, 160(4):A616, 2013. doi:10.1149/2.053304jes.
 """
 
 AUTHOR_YEAR_DICT = {
     "lithium-ion": [
-        ("NCA_Kim2011", ["Kim2011"]),
-        ("Ecker2015", ["Ecker2015i", "Richardson2020"]),
+        ("Prada2013", ["Chen2020", "Lain2019", "Prada2013"]),
     ],
     "lead-acid": [("Sulzer2019", ["Sulzer2019physical"])],
 }
 
 
 class TestUpdateParameterSetsDoc(unittest.TestCase):
+    def test_get_ps_dict(self):
+        output = get_ps_dict()
+        for key, values in AUTHOR_YEAR_DICT.items():
+            output_values = output.get(key, [])
+            for value in values:
+                self.assertIn(value, output_values)
+
     def test_generate_ps_doc(self):
         output = generate_ps_doc(AUTHOR_YEAR_DICT)
         self.assertEqual(output, DOC)
