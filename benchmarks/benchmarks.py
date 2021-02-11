a713acc3589f7393301139c06819c33bb9700c97
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2021-02-11 17:59:58 +0000

    move function solve_model_once to top-level

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -32,6 +32,10 @@ def prepare_model(model):
     disc.process_model(model)
 
 
+def solve_model_once(model, solver, t_eval):
+    solver.solve(model, t_eval=t_eval)
+
+
 class TimeSolveSPM:
     params = [True, False]
     solver = pybamm.CasadiSolver()
@@ -44,10 +48,7 @@ class TimeSolveSPM:
         self.t_eval = np.linspace(0, tmax, nb_points)
         prepare_model(self.model)
         if solve_first:
-            self.solve_model_once(True)
-
-    def solve_model_once(self, solve_first):
-        TimeSolveSPM.solver.solve(self.model, t_eval=self.t_eval)
+            solve_model_once(self.model, TimeSolveSPM.solver, self.t_eval)
 
     def time_solve_model(self, solve_first):
         TimeSolveSPM.solver.solve(self.model, t_eval=self.t_eval)
