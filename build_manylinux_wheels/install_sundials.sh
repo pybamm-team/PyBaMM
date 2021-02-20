60f2ac7e47f228a0e8962a44dad814095635c824
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2021-02-20 18:16:32 +0000

    Overhaul install_sundials script

--- build_manylinux_wheels/install_sundials.sh
+++ build_manylinux_wheels/install_sundials.sh
@@ -1,24 +1,53 @@
 #!/bin/bash
 
+# This script installs both SuiteSparse
+# (https://people.engr.tamu.edu/davis/suitesparse.html) and Sundials
+# (https://computing.llnl.gov/projects/sundials) from source. For each
+# two library:
+# - Archive downloaded and source code extrated in current working
+#   directory.
+# - Library is built and installed.
+#
+# Usage: ./install_sundials.sh suitesparse_version sundials_version
+
+function prepend_python_bin_dir_to_PATH {
+    python_bin_dir_cmd="print(os.path.split(sys.executable)[0])"
+    python_bin_dir=$(python -c "import sys, os;$python_bin_dir_cmd")
+    export PATH=$python_bin_dir:$PATH
+}
+
+function download {
+    ROOT_ADDR=$1
+    FILENAME=$2
+
+    wget -q $ROOT_ADDR/$FILENAME
+}
+
+function extract {
+    tar -xf $1
+}
+
+SUITESPARSE_VERSION=$1
+SUNDIALS_VERSION=$2
+
+SUITESPARSE_ROOT_ADDR=https://github.com/DrTimothyAldenDavis/SuiteSparse/archive
+SUNDIALS_ROOT_ADDR=https://computing.llnl.gov/projects/sundials/download
+
+SUITESPARSE_ARCHIVE_NAME=v$SUITESPARSE_VERSION.tar.gz
+SUNDIALS_ARCHIVE_NAME=sundials-$SUNDIALS_VERSION.tar.gz
+
 yum -y update
-yum -y install wget openblas-devel
+yum -y install wget
+download $SUITESPARSE_ROOT_ADDR $SUITESPARSE_ARCHIVE_NAME
+download $SUNDIALS_ROOT_ADDR $SUNDIALS_ARCHIVE_NAME
+extract $SUITESPARSE_ARCHIVE_NAME
+extract $SUNDIALS_ARCHIVE_NAME
 
-python_bin_dir_cmd="print(os.path.split(sys.executable)[0])"
-python_bin_dir=$(python -c "import sys, os;$python_bin_dir_cmd")
-export PATH=$python_bin_dir:$PATH
-python -m pip install cmake
+### Compile and install SUITESPARSE ###
+# SuiteSparse is required to compile SUNDIALS's
+# KLU solver.
 
-echo CMAKE_VERSION: $(cmake --version)
-
-mkdir /deps
-wget -q https://github.com/DrTimothyAldenDavis/SuiteSparse/archive/v5.7.2.tar.gz .
-wget -q https://computing.llnl.gov/projects/sundials/download/sundials-5.3.0.tar.gz .
-tar -xf v5.7.2.tar.gz --directory /deps
-tar -xf sundials-5.3.0.tar.gz --directory /deps
-
-SUITESPARSE_DIR=/deps/SuiteSparse-5.7.2
-SUNDIALS_DIR=/deps/sundials-5.3.0
-
+SUITESPARSE_DIR=SuiteSparse-$SUITESPARSE_VERSION
 for dir in SuiteSparse_config AMD COLAMD BTF KLU
 do
     cd $SUITESPARSE_DIR/$dir;
@@ -27,10 +56,20 @@ do
     cd ../
 done
 
+### Compile and install SUNDIALS ###
+
+# Building cmake requires cmake >= 3.5
+python -m pip install cmake
+prepend_python_bin_dir_to_PATH
+
+# Building SUNDIALS requires a BLAS library
+yum -y install openblas-devel
+
+mkdir -p build_sundials
+cd build_sundials
 KLU_INCLUDE_DIR=/usr/include
 KLU_LIBRARY_DIR=/usr/lib
-mkdir -p /deps/build_sundials
-cd /deps/build_sundials
+SUNDIALS_DIR=sundials-$SUNDIALS_VERSION
 cmake -DLAPACK_ENABLE=ON\
       -DSUNDIALS_INDEX_SIZE=32\
       -DEXAMPLES_ENABLE:BOOL=OFF\
