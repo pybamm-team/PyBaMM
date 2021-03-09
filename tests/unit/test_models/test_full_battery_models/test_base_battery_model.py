c37ba51032457c6a93ef340f53fef99874e7181f
Priyanshu Agarwal <priyanshuone6@gmail.com>
2021-03-09 17:16:12 +0530

    add test for print_options

--- tests/unit/test_models/test_full_battery_models/test_base_battery_model.py
+++ tests/unit/test_models/test_full_battery_models/test_base_battery_model.py
@@ -1,8 +1,41 @@
 #
 # Tests for the base battery model class
 #
+from pybamm.models.full_battery_models.base_battery_model import Options
 import pybamm
 import unittest
+import io
+from contextlib import redirect_stdout
+
+OPTIONS_DICT = {
+    "surface form": "differential",
+    "loss of active material": "negative",
+    "thermal": "x-full",
+}
+
+PRINT_OPTIONS_OUTPUT = """\
+'operating mode': 'current' (possible: ['current', 'voltage', 'power'])
+'dimensionality': 0 (possible: [0, 1, 2])
+'surface form': 'differential' (possible: ['false', 'differential', 'algebraic'])
+'convection': 'none' (possible: ['none', 'uniform transverse', 'full transverse'])
+'side reactions': []
+'interfacial surface area': 'constant' (possible: ['constant', 'varying'])
+'current collector': 'uniform' (possible: ['uniform', 'potential pair', 'potential pair quite conductive'])
+'particle': 'Fickian diffusion' (possible: ['Fickian diffusion', 'fast diffusion', 'uniform profile', 'quadratic profile', 'quartic profile'])
+'particle shape': 'spherical' (possible: ['spherical', 'user', 'no particles'])
+'electrolyte conductivity': 'default' (possible: ['default', 'full', 'leading order', 'composite', 'integrated'])
+'thermal': 'x-full' (possible: ['isothermal', 'lumped', 'x-lumped', 'x-full'])
+'cell geometry': 'pouch' (possible: ['arbitrary', 'pouch'])
+'external submodels': []
+'SEI': 'none' (possible: ['none', 'constant', 'reaction limited', 'solvent-diffusion limited', 'electron-migration limited', 'interstitial-diffusion limited', 'ec reaction limited'])
+'lithium plating': 'none' (possible: ['none', 'reversible', 'irreversible'])
+'SEI porosity change': 'false' (possible: ['true', 'false'])
+'loss of active material': 'negative' (possible: ['none', 'negative', 'positive', 'both'])
+'working electrode': 'none'
+'particle cracking': 'none' (possible: ['none', 'no cracking', 'negative', 'positive', 'both'])
+'total interfacial current density as a state': 'false' (possible: ['true', 'false'])
+'SEI film resistance': 'none' (possible: ['none', 'distributed', 'average'])
+"""  # noqa: E501
 
 
 class TestBaseBatteryModel(unittest.TestCase):
@@ -125,9 +158,7 @@ class TestBaseBatteryModel(unittest.TestCase):
             pybamm.BaseBatteryModel({"thermal": "bad thermal"})
         with self.assertRaisesRegex(pybamm.OptionError, "cell geometry"):
             pybamm.BaseBatteryModel({"cell geometry": "bad geometry"})
-        with self.assertRaisesRegex(
-            pybamm.OptionError, "dimensionality"
-        ):
+        with self.assertRaisesRegex(pybamm.OptionError, "dimensionality"):
             pybamm.BaseBatteryModel({"dimensionality": 5})
         with self.assertRaisesRegex(pybamm.OptionError, "current collector"):
             pybamm.BaseBatteryModel(
@@ -207,6 +238,14 @@ class TestBaseBatteryModel(unittest.TestCase):
         )
         with self.assertRaisesRegex(pybamm.ModelError, "Missing variable"):
             model.build_model()
+
+
+class TestOptions(unittest.TestCase):
+    def test_print_options(self):
+        with io.StringIO() as buffer, redirect_stdout(buffer):
+            Options(OPTIONS_DICT).print_options()
+            output = buffer.getvalue()
+        self.assertEqual(output, PRINT_OPTIONS_OUTPUT)
 
 
 if __name__ == "__main__":
