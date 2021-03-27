23301b2610b88f4cab0f0eda84e397b32da6b1c3
Valentin Sulzer <valentinsulzer@hotmail.com>
2021-03-27 18:14:01 -0400

    update author

--- docs/conf.py
+++ docs/conf.py
@@ -154,9 +154,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, "PyBaMM.tex", "PyBaMM Documentation", "Valentin Sulzer", "manual")
-]
+latex_documents = [(master_doc, "PyBaMM.tex", "PyBaMM Documentation", author, "manual")]
 
 
 # -- Options for manual page output ------------------------------------------
