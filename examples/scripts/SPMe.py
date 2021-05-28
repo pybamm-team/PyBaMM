ec6aaa64d4c7aa5c59e8dfb39edeef6eb8f8f2fd
Valentin Sulzer <valentinsulzer@hotmail.com>
2021-05-28 15:06:05 -0400

    revert SPMe example changes

--- examples/scripts/SPMe.py
+++ examples/scripts/SPMe.py
@@ -5,11 +5,11 @@
 import pybamm
 import numpy as np
 
-# pybamm.set_logging_level("INFO")
+pybamm.set_logging_level("INFO")
 
 # load model
 model = pybamm.lithium_ion.SPMe()
-# model.convert_to_format = "python"
+model.convert_to_format = "python"
 
 # create geometry
 geometry = model.default_geometry
@@ -20,9 +20,7 @@ param.process_model(model)
 param.process_geometry(geometry)
 
 # set mesh
-var = pybamm.standard_spatial_vars
-var_pts = {var.x_n: 10, var.x_s: 10, var.x_p: 10, var.r_n: 10, var.r_p: 10}
-mesh = pybamm.Mesh(geometry, model.default_submesh_types, var_pts)
+mesh = pybamm.Mesh(geometry, model.default_submesh_types, model.default_var_pts)
 
 # discretise model
 disc = pybamm.Discretisation(mesh, model.default_spatial_methods)
@@ -30,16 +28,7 @@ disc.process_model(model)
 
 # solve model for 1 hour
 t_eval = np.linspace(0, 3600, 100)
-solver = pybamm.CasadiSolver(mode="fast", rtol=1e-6, atol=1e-6)
-solution = solver.solve(model, t_eval)
-solve_time = 0
-int_time = 0
-for _ in range(1000):
-    solution = solver.solve(model, t_eval)
-    solve_time += solution.solve_time
-    int_time += solution.integration_time
-
-print(str(solve_time / 1000) + " (" + str(int_time / 1000) + ")")
+solution = model.default_solver.solve(model, t_eval)
 
 # plot
 plot = pybamm.QuickPlot(
@@ -57,4 +46,4 @@ plot = pybamm.QuickPlot(
     time_unit="seconds",
     spatial_unit="um",
 )
-# plot.dynamic_plot()
+plot.dynamic_plot()
