98291da96b32588cf6cf0f3b947fd8a029e8a02e
Priyanshu Agarwal <priyanshuone6@gmail.com>
2021-07-05 04:54:15 +0530

    Fix coverage

--- pybamm/expression_tree/sympy_overrides.py
+++ pybamm/expression_tree/sympy_overrides.py
@@ -1,10 +1,9 @@
 #
 # SymPy overrides
 #
-from sympy.core.function import _coeff_isneg
-from sympy.printing.conventions import requires_partial
+import re
+
 from sympy.printing.latex import LatexPrinter
-from sympy.printing.precedence import PRECEDENCE
 
 
 class CustomPrint(LatexPrinter):
@@ -12,37 +11,13 @@ class CustomPrint(LatexPrinter):
 
     def _print_Derivative(self, expr):
         """Override :meth:`sympy.printing.latex.LatexPrinter._print_Derivative`"""
-        if requires_partial(expr.expr) or getattr(expr, "force_partial", False):
-            diff_symbol = r'\partial'
-        else:
-            diff_symbol = r'd'
+        eqn = super()._print_Derivative(expr)
 
-        tex = ""
-        dim = 0
-        for x, num in reversed(expr.variable_count):
-            dim += num
-            if num == 1:
-                tex += r"%s %s" % (diff_symbol, self._print(x))
-            else:
-                tex += r"%s %s^{%s}" % (diff_symbol,
-                                        self.parenthesize_super(self._print(x)),
-                                        self._print(num))
+        if getattr(expr, "force_partial", False) and "partial" not in eqn:
+            a1, a2 = re.findall(r"^\\frac{(\w+)}{(\w+) .+", eqn)[0]
+            eqn = eqn.replace(a1, "\partial").replace(a2, "\partial")
 
-        if dim == 1:
-            tex = r"\frac{%s}{%s}" % (diff_symbol, tex)
-        else:
-            tex = r"\frac{%s^{%s}}{%s}" % (diff_symbol, self._print(dim), tex)
-
-        if any(_coeff_isneg(i) for i in expr.args):
-            return r"%s %s" % (tex, self.parenthesize(expr.expr,
-                                                      PRECEDENCE["Mul"],
-                                                      is_neg=True,
-                                                      strict=True))
-
-        return r"%s %s" % (tex, self.parenthesize(expr.expr,
-                                                  PRECEDENCE["Mul"],
-                                                  is_neg=False,
-                                                  strict=True))
+        return eqn
 
 
 def custom_print_func(expr, **settings):
