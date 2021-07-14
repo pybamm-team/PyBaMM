72ad54d1253c69000f7859decc6ef35584154d1b
Valentin Sulzer <valentinsulzer@hotmail.com>
2021-07-14 14:07:12 -0400

    #1547 fix esoh model in pouch cell

--- examples/scripts/DFN.py
+++ examples/scripts/DFN.py
@@ -1,50 +1,8 @@
-#
-# Example showing how to load and solve the DFN
-#
-
 import pybamm
-import numpy as np
 
 pybamm.set_logging_level("INFO")
-
-# load model
-model = pybamm.lithium_ion.DFN()
-# create geometry
-geometry = model.default_geometry
-
-# load parameter values and process model and geometry
-param = model.default_parameter_values
-param.process_geometry(geometry)
-param.process_model(model)
-
-# set mesh
-var = pybamm.standard_spatial_vars
-var_pts = {var.x_n: 30, var.x_s: 30, var.x_p: 30, var.r_n: 10, var.r_p: 10}
-mesh = pybamm.Mesh(geometry, model.default_submesh_types, var_pts)
-
-# discretise model
-disc = pybamm.Discretisation(mesh, model.default_spatial_methods)
-disc.process_model(model)
-
-# solve model
-t_eval = np.linspace(0, 3600, 100)
-solver = pybamm.CasadiSolver(mode="safe", atol=1e-6, rtol=1e-3)
-solution = solver.solve(model, t_eval)
-
-# plot
-plot = pybamm.QuickPlot(
-    solution,
-    [
-        "Negative particle concentration [mol.m-3]",
-        "Electrolyte concentration [mol.m-3]",
-        "Positive particle concentration [mol.m-3]",
-        "Current [A]",
-        "Negative electrode potential [V]",
-        "Electrolyte potential [V]",
-        "Positive electrode potential [V]",
-        "Terminal voltage [V]",
-    ],
-    time_unit="seconds",
-    spatial_unit="um",
-)
-plot.dynamic_plot()
+options = {"cell geometry": "pouch", "thermal": "x-lumped", "dimensionality": 2}
+model = pybamm.lithium_ion.SPMe(options=options)
+experiment = pybamm.Experiment(["Discharge with 5 C for 1 minute"])
+sim = pybamm.Simulation(model, experiment=experiment)
+sim.solve(solver=pybamm.CasadiSolver("fast with events"))
