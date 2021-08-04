bf59b7d1941ec0677b54f9408e73566c0575fde3
Martin Robinson <martinjrobins@gmail.com>
2021-08-04 07:56:54 +0100

    #1477 fix integration idaklu test

--- tests/integration/test_solvers/test_idaklu.py
+++ tests/integration/test_solvers/test_idaklu.py
@@ -20,20 +20,20 @@ class TestIDAKLUSolver(unittest.TestCase):
         np.testing.assert_array_less(1, solution.t.size)
 
     def test_on_spme_sensitivities(self):
-        param_name = "Negative electrode conductivity [S.m-1]"
-        neg_electrode_cond = 100.0
+        param_name = 'Typical current [A]'
+        param_value = 0.15652
         model = pybamm.lithium_ion.SPMe()
         geometry = model.default_geometry
         param = model.default_parameter_values
         param.update({param_name: "[input]"})
-        inputs = {param_name: neg_electrode_cond}
+        inputs = {param_name: param_value}
         param.process_model(model)
         param.process_geometry(geometry)
         mesh = pybamm.Mesh(geometry, model.default_submesh_types, model.default_var_pts)
         disc = pybamm.Discretisation(mesh, model.default_spatial_methods)
         disc.process_model(model)
         t_eval = np.linspace(0, 3600, 100)
-        solver = pybamm.IDAKLUSolver()
+        solver = pybamm.IDAKLUSolver(rtol=1e-10, atol=1e-10)
         solution = solver.solve(
             model, t_eval,
             inputs=inputs,
@@ -48,16 +48,18 @@ class TestIDAKLUSolver(unittest.TestCase):
         h = 1e-6
         sol_plus = solver.solve(
             model, t_eval,
-            inputs={param_name: neg_electrode_cond + 0.5 * h}
+            inputs={param_name: param_value + 0.5 * h}
         )
         sol_neg = solver.solve(
             model, t_eval,
-            inputs={param_name: neg_electrode_cond - 0.5 * h}
+            inputs={param_name: param_value - 0.5 * h}
         )
         dyda_fd = (sol_plus.y - sol_neg.y) / h
+        dyda_fd = dyda_fd.transpose().reshape(-1, 1)
 
-        np.testing.assert_array_almost_equal(
-            dyda_ida, dyda_fd
+        np.testing.assert_allclose(
+            dyda_ida, dyda_fd,
+            rtol=1e-2, atol=1e-3,
         )
 
     def test_set_tol_by_variable(self):
@@ -109,7 +111,6 @@ class TestIDAKLUSolver(unittest.TestCase):
 if __name__ == "__main__":
     print("Add -v for more debug output")
 
-    pybamm.set_logging_level('INFO')
     if "-v" in sys.argv:
         debug = True
     pybamm.settings.debug_mode = True
