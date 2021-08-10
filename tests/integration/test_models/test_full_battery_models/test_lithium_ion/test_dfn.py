9e408854c2c7cd02a87b6659e2f14d3c52320193
tobykirk <toby.kirk@maths.ox.ac.uk>
2021-08-10 18:00:24 +0100

    add more tests DFN size distributions

--- tests/integration/test_models/test_full_battery_models/test_lithium_ion/test_dfn.py
+++ tests/integration/test_models/test_full_battery_models/test_lithium_ion/test_dfn.py
@@ -304,6 +304,45 @@ class TestDFNWithSizeDistribution(unittest.TestCase):
         )
         modeltest.test_all(skip_output_tests=True)
 
+    def test_conservation_each_electrode(self):
+        # Test that surface areas are being calculated from the distribution correctly
+        # for any discretization in the size domain.
+        # We test that the amount of lithium removed or added to each electrode
+        # is the same as for the standard DFN with the same parameters
+        models = [
+            pybamm.lithium_ion.DFN(),
+            pybamm.lithium_ion.DFN(options={"particle size": "distribution"})
+        ]
+        var = pybamm.standard_spatial_vars
+
+        # reduce number of particle sizes, for a crude discretization
+        var_pts = {
+            var.R_n: 3,
+            var.R_p: 3,
+        }
+        solver = pybamm.CasadiSolver(mode="fast")
+
+        # solve
+        neg_Li = []
+        pos_Li = []
+        for model in models:
+            sim = pybamm.Simulation(
+                model,
+                parameter_values=self.params,
+                var_pts=self.var_pts,
+                solver=solver
+            )
+            sim.var_pts.update(var_pts)
+            solution = sim.solve([0, 3500])
+            neg = solution["Total lithium in negative electrode [mol]"].entries[-1]
+            pos = solution["Total lithium in positive electrode [mol]"].entries[-1]
+            neg_Li.append(neg)
+            pos_Li.append(pos)
+
+        # compare
+        np.testing.assert_array_almost_equal(neg_Li[0], neg_Li[1], decimal=12)
+        np.testing.assert_array_almost_equal(pos_Li[0], pos_Li[1], decimal=12)
+
 
 if __name__ == "__main__":
     print("Add -v for more debug output")
