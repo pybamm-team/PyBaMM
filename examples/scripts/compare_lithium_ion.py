1a72c320b3f57e13f0ee26593f747886f27c7306
Valentin Sulzer <valentinsulzer@hotmail.com>
2021-10-09 21:24:47 -0400

    revert example

--- examples/scripts/compare_lithium_ion.py
+++ examples/scripts/compare_lithium_ion.py
@@ -7,27 +7,18 @@ pybamm.set_logging_level("INFO")
 
 # load models
 models = [
-    pybamm.lithium_ion.SPM({"operating mode": "power"}),
-    # pybamm.lithium_ion.SPMe(),
-    # pybamm.lithium_ion.DFN(),
-    # pybamm.lithium_ion.NewmanTobias(),
+    pybamm.lithium_ion.SPM(),
+    pybamm.lithium_ion.SPMe(),
+    pybamm.lithium_ion.DFN(),
+    pybamm.lithium_ion.NewmanTobias(),
 ]
-
-parameter_values = models[0].default_parameter_values
-parameter_values.update({"Power function [W]": 2}, check_already_exists=False)
 
 # create and run simulations
 sims = []
 for model in models:
-    sim = pybamm.Simulation(model, parameter_values=parameter_values)
+    sim = pybamm.Simulation(model)
     sim.solve([0, 3600])
     sims.append(sim)
 
 # plot
-pybamm.dynamic_plot(
-    sims,
-    [
-        "X-averaged negative particle concentration",
-        "X-averaged positive particle concentration",
-    ],
-)
+pybamm.dynamic_plot(sims)
