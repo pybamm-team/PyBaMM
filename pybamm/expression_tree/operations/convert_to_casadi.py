21afc3811c5fb90ba3bbe29fcc908ffe9c153f67
Elias Hohl <elias.hohl@avl.com>
2021-11-05 20:42:08 +0100

    correctly pass multiple children to casadi interpolant in 2D

--- pybamm/expression_tree/operations/convert_to_casadi.py
+++ pybamm/expression_tree/operations/convert_to_casadi.py
@@ -132,9 +132,20 @@ class CasadiConverter(object):
             elif symbol.function == special.erf:
                 return casadi.erf(*converted_children)
             elif isinstance(symbol, pybamm.Interpolant):
-                return casadi.interpolant(
-                    "LUT", "bspline", symbol.x, symbol.y.flatten()
-                )(*converted_children)
+
+                if len(converted_children) == 1:
+                    return casadi.interpolant(
+                        "LUT", "bspline", symbol.x, symbol.y.flatten()
+                    )(*converted_children)
+
+                elif len(converted_children) == 2:
+                    return casadi.interpolant(
+                        "LUT", "bspline", symbol.x, symbol.y.flatten()
+                    )(converted_children)
+
+                else:
+                    raise Exception("Invalid converted_children count for Interpolant: {0}".format(len(converted_children)))
+
             elif symbol.function.__name__.startswith("elementwise_grad_of_"):
                 differentiating_child_idx = int(symbol.function.__name__[-1])
                 # Create dummy symbolic variables in order to differentiate using CasADi
