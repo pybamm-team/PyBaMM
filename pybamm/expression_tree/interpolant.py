50e8607942f0a39d12bb91a3e0a42e6d61a0ddd2
Elias Hohl <elias.hohl@avl.com>
2021-11-05 19:42:10 +0100

    flatten results of interpolating function

--- pybamm/expression_tree/interpolant.py
+++ pybamm/expression_tree/interpolant.py
@@ -108,7 +108,8 @@ class Interpolant(pybamm.Function):
                         x1, y.T, bounds_error=False, fill_value="extrapolate"
                     )
             elif len(x) == 2:
-                interpolating_function = interpolate.interp2d(x1, x2, y)
+                grid_interpolating_function = interpolate.interp2d(x1, x2, y)
+                interpolating_function = lambda *params: grid_interpolating_function(*params).flatten()
         elif interpolator == "pchip":
             interpolating_function = interpolate.PchipInterpolator(
                 x1, y, extrapolate=extrapolate
