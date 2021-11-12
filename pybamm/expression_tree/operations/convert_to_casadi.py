239b07c36f211ce50b0f7045fa50de30c70f81db
Elias Hohl <elias.hohl@avl.com>
2021-11-12 10:08:21 +0100

    transpose vector

--- pybamm/expression_tree/operations/convert_to_casadi.py
+++ pybamm/expression_tree/operations/convert_to_casadi.py
@@ -199,7 +199,7 @@ class CasadiConverter(object):
 
                     # res = LUT[converted_children[0], converted_children[1]]
 
-                    res = LUT(casadi.hcat(converted_children))
+                    res = LUT(casadi.hcat(converted_children).T)
 
                     return res
 
