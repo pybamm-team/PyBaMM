fe863b9bb2bad744d32c6015c360922f047c8421
Valentin Sulzer <valentinsulzer@hotmail.com>
2021-11-12 11:26:59 -0500

    revert example

--- examples/scripts/compare_lead_acid.py
+++ examples/scripts/compare_lead_acid.py
@@ -3,14 +3,14 @@
 #
 import pybamm
 
-pybamm.set_logging_level("DEBUG")
+pybamm.set_logging_level("INFO")
 
 # load models
 models = [
     pybamm.lead_acid.LOQS(),
     pybamm.lead_acid.FOQS(),
     pybamm.lead_acid.Composite(),
-    pybamm.lead_acid.Full({"surface form": "differential"}),
+    pybamm.lead_acid.Full(),
 ]
 
 # create and run simulations
