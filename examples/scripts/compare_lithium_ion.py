a203d6c90645ac4f3168941d0158e9ab0fb396ba
Valentin Sulzer <valentinsulzer@hotmail.com>
2021-11-17 13:18:20 -0500

    revert example

--- examples/scripts/compare_lithium_ion.py
+++ examples/scripts/compare_lithium_ion.py
@@ -7,13 +7,10 @@ pybamm.set_logging_level("INFO")
 
 # load models
 models = [
-    # pybamm.lithium_ion.SPM(),
-    # pybamm.lithium_ion.SPMe(),
-    # pybamm.lithium_ion.DFN(),
-    pybamm.lithium_ion.NewmanTobias(
-        {"particle": ("Fickian diffusion", "quartic profile")}
-        # {"particle": "quartic profile"}
-    ),
+    pybamm.lithium_ion.SPM(),
+    pybamm.lithium_ion.SPMe(),
+    pybamm.lithium_ion.DFN(),
+    pybamm.lithium_ion.NewmanTobias(),
 ]
 
 # create and run simulations
