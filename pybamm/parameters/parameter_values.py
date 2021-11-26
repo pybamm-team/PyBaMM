9f2d3f1cfb50c9d67b61ca71828860c1c48e4707
Elias Hohl <elias.hohl@avl.com>
2021-11-26 17:51:34 +0100

    add pragma: no cover

--- pybamm/parameters/parameter_values.py
+++ pybamm/parameters/parameter_values.py
@@ -692,6 +692,7 @@ class ParameterValues:
                         )
 
                 else:
+                    # pragma: no cover
                     raise ValueError("Invalid function name length: {0}"
                                      .format(len(function_name)))
 
