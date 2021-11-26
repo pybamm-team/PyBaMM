3195f4c1cf4c20a464cbe02a83df204a5c940d3c
Elias Hohl <elias.hohl@avl.com>
2021-11-26 19:54:42 +0100

    use standard parameter values

--- tests/unit/test_parameters/test_parameter_values.py
+++ tests/unit/test_parameters/test_parameter_values.py
@@ -616,7 +616,7 @@ class TestParameterValues(unittest.TestCase):
         #
         # pv['function'] = '[function]'
 
-        parameter_values = pybamm.ParameterValues("Ai2020")
+        parameter_values = pybamm.ParameterValues(chemistry=pybamm.parameter_sets.Ai2020)
         parameter_values.update(
             {
                 "function": "[function]lico2_diffusivity_Dualfoil1998",
