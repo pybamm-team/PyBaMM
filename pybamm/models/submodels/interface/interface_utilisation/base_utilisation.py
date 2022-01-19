d0772a4bf1359adce08ea0f77180624e3359d609
Valentin Sulzer <valentinsulzer@hotmail.com>
2022-01-19 11:13:17 -0500

    make utilisation self-limiting

--- pybamm/models/submodels/interface/interface_utilisation/base_utilisation.py
+++ pybamm/models/submodels/interface/interface_utilisation/base_utilisation.py
@@ -22,12 +22,21 @@ class BaseModel(pybamm.BaseSubModel):
     def __init__(self, param, domain, options):
         super().__init__(param, domain, options=options)
 
-    def _get_standard_interface_utilisation_variables(self, u):
-        u_av = pybamm.x_average(u)
+    def _get_standard_interface_utilisation_variables(self, u_var):
+        u = pybamm.maximum(u_var, 1e-8)
+        u_var_av = pybamm.x_average(u_var)
+        u_av = pybamm.maximum(u_var_av, 1e-8)
         if self.half_cell and self.domain == "Negative":
-            variables = {"Lithium metal interface utilisation": u_av}
+            variables = {
+                "Lithium metal interface utilisation variable": u_var_av,
+                "Lithium metal interface utilisation": u_av,
+            }
         else:
             variables = {
+                self.domain + " electrode interface utilisation variable": u_var,
+                "X-averaged "
+                + self.domain.lower()
+                + " electrode interface utilisation variable": u_var_av,
                 self.domain + " electrode interface utilisation": u,
                 "X-averaged "
                 + self.domain.lower()
