3d43eeb85eb122ebe04e78496570fcbcf3052282
Weilong Ai <weilong_ai@seu.edu.cn>
2022-02-28 14:23:01 +0800

    bug fixed to the test_basic_models

--- tests/unit/test_models/test_full_battery_models/test_lithium_ion/test_basic_models.py
+++ tests/unit/test_models/test_full_battery_models/test_lithium_ion/test_basic_models.py
@@ -12,6 +12,13 @@ class TestBasicModels(unittest.TestCase):
 
         copy = model.new_copy()
         copy.check_well_posedness()
+        
+    def test_dfn_composite_well_posed(self):
+        model = pybamm.lithium_ion.BasicDFNComposite()
+        model.check_well_posedness()
+
+        copy = model.new_copy()
+        copy.check_well_posedness()
 
     def test_spm_well_posed(self):
         model = pybamm.lithium_ion.BasicSPM()
