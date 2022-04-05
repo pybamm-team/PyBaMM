55b33bb890b12cfd3531a32952a0dd0aa82ea3e0
martinjrobins <martinjrobins@gmail.com>
2022-04-05 16:29:32 +0100

    '#1863 #2008 fix input_parameter size bug again gp

--- pybamm/discretisations/discretisation.py
+++ pybamm/discretisations/discretisation.py
@@ -1040,7 +1040,7 @@ class Discretisation(object):
                 expected_size = self._get_variable_size(symbol)
             else:
                 expected_size = None
-            symbol.expected_size = expected_size
+            symbol._expected_size = expected_size
             return symbol.create_copy()
         else:
             # Backup option: return the object
