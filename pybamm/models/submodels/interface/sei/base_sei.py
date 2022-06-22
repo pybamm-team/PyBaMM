6bfddd1c8d794937fadf544cd2e820b6df2215b9
DrSOKane <s.okane@imperial.ac.uk>
2022-06-22 14:52:55 +0100

    Revert "Added if statement to set roughness to 1 if not in variables"
    This reverts commit 5b321a72dd65613fd58318d969b1a11d452e9202.

--- pybamm/models/submodels/interface/sei/base_sei.py
+++ pybamm/models/submodels/interface/sei/base_sei.py
@@ -230,10 +230,7 @@ class BaseModel(BaseInterface):
         elif self.reaction == "SEI on cracks":
             L_inner_cr = variables["Inner SEI on cracks thickness"]
             L_outer_cr = variables["Outer SEI on cracks thickness"]
-            if self.domain + " electrode roughness ratio" in variables:
-                roughness = variables[self.domain + " electrode roughness ratio"]
-            else:
-                roughness = 1
+            roughness = variables[self.domain + " electrode roughness ratio"]
 
             n_inner_cr = L_inner_cr * (roughness - 1)  # inner SEI cracks concentration
             n_outer_cr = L_outer_cr * (roughness - 1)  # outer SEI cracks concentration
