d49f5968cd4285e4ac4c27032ed35f6e14a66056
Valentin Sulzer <valentinsulzer@hotmail.com>
2022-07-29 10:52:00 -0400

    #2193 update model out of place to avoid bug

--- pybamm/solvers/base_solver.py
+++ pybamm/solvers/base_solver.py
@@ -1297,7 +1297,7 @@ class BaseSolver(object):
                 model.y0 = old_solution.all_ys[-1][:, -1]
             else:
                 model.y0 = model.set_initial_conditions_from(
-                    old_solution
+                    old_solution, inplace=False
                 ).concatenated_initial_conditions.evaluate(0, inputs=ext_and_inputs)
         set_up_time = timer.time()
 
