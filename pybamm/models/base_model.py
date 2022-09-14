f72b4e514723a4aa2d59aae568a144293694423b
Alexander Bills <abills@andrew.cmu.edu>
2022-09-14 14:03:42 -0400

    typo

--- pybamm/models/base_model.py
+++ pybamm/models/base_model.py
@@ -1147,7 +1147,7 @@ class BaseModel:
                 
             converter = pybamm.JuliaConverter(dae_type=dae_type)
             converter.convert_tree_to_intermediate(pybamm.numpy_concatenation(self.concatenated_rhs,self.concatenated_algebraic),len_rhs=len_rhs)
-            eqn_string = converter.build_julia_code(funcname=name)
+            eqn_str = converter.build_julia_code(funcname=name)
 
         if get_consistent_ics_solver is None or self.algebraic == {}:
             ics = self.concatenated_initial_conditions
