d4ffe99be2be34a5757d2a3da38803ff56ebc88e
Sravan Pannala <57152030+sravanpannala@users.noreply.github.com>
2022-10-05 09:54:51 -0400

    add more diffusion hppc plots

--- pybamm/input/parameters/lithium_ion/negative_electrodes/graphite_UMBL_Siegel2022/graphite_ocp_Siegel.py
+++ pybamm/input/parameters/lithium_ion/negative_electrodes/graphite_UMBL_Siegel2022/graphite_ocp_Siegel.py
@@ -12,14 +12,12 @@ def graphite_ocp_Siegel(sto):
     """
 
     u_eq = (
-        0.063
-        + 0.8 * pybamm.exp(-75 * (sto + 0.001))
-        - 0.0120 * pybamm.tanh((sto - 0.127) / 0.016)
-        - 0.0118 * pybamm.tanh((sto - 0.155) / 0.016)
-        - 0.0035 * pybamm.tanh((sto - 0.220) / 0.020)
-        - 0.0095 * pybamm.tanh((sto - 0.190) / 0.013)
-        - 0.0145 * pybamm.tanh((sto - 0.490) / 0.020)
-        - 0.0800 * pybamm.tanh((sto - 1.030) / 0.055)
+        0.105
+        + 0.8000 * pybamm.exp(-75 * (sto - 0.0005))
+        - 0.0030 * pybamm.tanh((sto - 0.1503) / 0.0052)
+        - 0.0467 * pybamm.tanh((sto - 0.1804) / 0.0578)
+        - 0.0180 * pybamm.tanh((sto - 0.5150) / 0.0250)
+        - 0.0500 * pybamm.tanh((sto - 0.9900) / 0.0531)
     )
 
     return u_eq
