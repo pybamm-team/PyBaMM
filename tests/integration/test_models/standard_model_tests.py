4a873ffe42ea67e5940e4e32cee22890ae9acc1b
Alexander Bills <abills@andrew.cmu.edu>
2022-10-11 12:08:26 -0400

    add comment to force workflow

--- tests/integration/test_models/standard_model_tests.py
+++ tests/integration/test_models/standard_model_tests.py
@@ -72,6 +72,8 @@ class StandardModelTest(object):
         self.solver.rtol = 1e-8
         self.solver.atol = 1e-8
 
+        #Somehow removing an equation makes the solver fail at
+        # the low tolerances
         if isinstance(self.model, pybamm.lithium_ion.NewmanTobias):
             self.solver.rtol = 1e-7
 
