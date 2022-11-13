80e0e2c10dfc4e898a2716fc512440da263c05ec
Alec Bills <abills@andrew.cmu.edu>
2022-11-13 15:57:41 -0500

    symbol replacer

--- pybamm/__init__.py
+++ pybamm/__init__.py
@@ -95,6 +95,7 @@ from .expression_tree.operations.evaluate_python import JaxCooMatrix
 from .expression_tree.operations.jacobian import Jacobian
 from .expression_tree.operations.convert_to_casadi import CasadiConverter
 from .expression_tree.operations.unpack_symbols import SymbolUnpacker
+
 #
 # Model classes
 #
