db7bed5c383e54a3e41c4b95ef77473b7e91a60c
Valentin Sulzer <valentinsulzer@hotmail.com>
2022-11-30 15:41:04 -0500

    explicitly create interpolants from functions

--- examples/scripts/compare_lithium_ion.py
+++ examples/scripts/compare_lithium_ion.py
@@ -7,16 +7,17 @@ pybamm.set_logging_level("INFO")
 
 # load models
 models = [
-    pybamm.lithium_ion.SPM(),
-    pybamm.lithium_ion.SPMe(),
+    # pybamm.lithium_ion.SPM(),
+    # pybamm.lithium_ion.SPMe(),
     pybamm.lithium_ion.DFN(),
-    pybamm.lithium_ion.NewmanTobias(),
+    # pybamm.lithium_ion.NewmanTobias(),
 ]
+parameter_values = pybamm.ParameterValues("Ai2020")
 
 # create and run simulations
 sims = []
 for model in models:
-    sim = pybamm.Simulation(model)
+    sim = pybamm.Simulation(model, parameter_values=parameter_values)
     sim.solve([0, 3600])
     sims.append(sim)
 
