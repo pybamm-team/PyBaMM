#2418 fix unit tests, add scaling to some algebraic equations

pybamm/solvers/base_solver.py
@@ -988,7 +988,7 @@ class BaseSolver(object):
             end_indices.append(dindex + 1)
             start_indices.append(dindex + 1)
             if dtime * (1 - eps) < t_eval[dindex] < dtime * (1 + eps):
-                t_eval[dindex] += eps
+                t_eval[dindex] *= 1 + eps
                 t_eval = np.insert(t_eval, dindex, dtime * (1 - eps))
             else:
                 t_eval = np.insert(

tests/unit/test_solvers/test_casadi_solver.py
@@ -88,10 +88,10 @@ class TestCasadiSolver(unittest.TestCase):
         solution = solver.solve(model, t_eval)
         np.testing.assert_array_equal(solution.t, t_eval)
         np.testing.assert_array_almost_equal(
-            solution.y[0], np.exp(0.1 * t_eval), decimal=5
+            solution.y.full()[0], np.exp(0.1 * t_eval), decimal=5
         )
         np.testing.assert_array_almost_equal(
-            solution.y[1], np.ones_like(t_eval), decimal=5
+            solution.y.full()[1], np.ones_like(t_eval), decimal=5
         )
 
         # DAE model, errors
@@ -440,7 +440,6 @@ class TestCasadiSolver(unittest.TestCase):
             solution.y.full()[-1], 1 * np.exp(-0.1 * solution.t), decimal=5
         )
 
-    @unittest.skip("External variables will be removed")
     def test_model_solver_with_external(self):
         # Create model
         model = pybamm.BaseModel()