99494c8aa8278d4650b2339acc630179af059df7
Valentin Sulzer <valentinsulzer@hotmail.com>
2022-12-13 11:42:38 -0500

    loosen tol for split electrlyte concentration test

--- examples/scripts/compare_lithium_ion.py
+++ examples/scripts/compare_lithium_ion.py
@@ -4,34 +4,24 @@
 import pybamm
 
 pybamm.set_logging_level("INFO")
-options = {"interface utilisation": "constant"}
+options = {"thermal": "x-full"}
 # load models
 models = [
-    pybamm.lithium_ion.SPM(),
+    pybamm.lithium_ion.SPMe(),
     # pybamm.lithium_ion.SPM(options),
-    # pybamm.lithium_ion.SPMe(options),
+    pybamm.lithium_ion.SPMe(options),
     # pybamm.lithium_ion.DFN(options),
     # pybamm.lithium_ion.NewmanTobias(),
 ]
 
 
 parameter_values = pybamm.ParameterValues("Marquis2019")
-parameter_values.update(
-    {
-        "Initial negative electrode interface utilisation": 0.9,
-        "Initial positive electrode interface utilisation": 0.8,
-    },
-    check_already_exists=False,
-)
+
 sims = []
 for model in models:
     sim = pybamm.Simulation(model, parameter_values=parameter_values)
-    sim.solver.atol = 1e-8
-    sim.solver.rtol = 1e-8
-    sol = sim.solve([0, 3600])
+    sim.solve([0, 3600])
     sims.append(sim)
-    # print(sol.y[:, -1])
-    print(sim.built_model.concatenated_rhs.evaluate(y=sol.y[:, -1].full()))
 
 # plot
 pybamm.dynamic_plot(
