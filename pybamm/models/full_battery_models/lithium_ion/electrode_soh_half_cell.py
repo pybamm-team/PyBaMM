a00603eac3cd2277a5816f46539af0537106bf8a
js1tr3 <siegeljb@gmail.com>
2022-12-23 17:22:55 -0500

    updated examples to use new variables for stoich window calculation
    added new voltage limits to ESOH example and half cell

--- pybamm/models/full_battery_models/lithium_ion/electrode_soh_half_cell.py
+++ pybamm/models/full_battery_models/lithium_ion/electrode_soh_half_cell.py
@@ -44,8 +44,8 @@ class ElectrodeSOHHalfCell(pybamm.BaseModel):
             U_w = param.p.prim.U_dimensional
             Q = Q_w * (x_100 - x_0)
 
-        V_max = param.voltage_high_cut_dimensional
-        V_min = param.voltage_low_cut_dimensional
+        V_max = param.opc_soc_100_dimensional
+        V_min = param.opc_soc_0_dimensional
 
         self.algebraic = {
             x_100: U_w(x_100, T_ref) - V_max,
