4419603d790c99aa928599d111c3ce564cd88676
Ferran Brosa Planella <Ferran.Brosa-Planella@warwick.ac.uk>
2023-01-25 12:39:46 +0000

    flake8

--- pybamm/experiments/experiment.py
+++ pybamm/experiments/experiment.py
@@ -25,7 +25,9 @@ examples = """
     Run US06 (W) for 2 hours,
     """
 
-# TODO: Add examples of timestamps! (if I have forgotten this at the PR stage please bring it up)
+# TODO: Add examples of timestamps! (if I have forgotten this at the PR stage
+# please bring it up)
+
 
 class Experiment:
     """
@@ -78,10 +80,7 @@ class Experiment:
             cccv_handling,
         )
 
-        self.datetime_formats = [
-            "Day %j %H:%M:%S",
-            "%Y-%m-%d %H:%M:%S"
-        ]
+        self.datetime_formats = ["Day %j %H:%M:%S", "%Y-%m-%d %H:%M:%S"]
 
         self.period = self.convert_time_to_seconds(period.split())
         operating_conditions_cycles = []
@@ -178,7 +177,7 @@ class Experiment:
             tags = list(np.unique(tag_CC + tag_CV))
             if len(tags) == 0:
                 tags = None
-            
+
             current_timestamp = self._process_timestamp(cond)
             next_timestamp = self._process_timestamp(next_step)
 
@@ -497,11 +496,14 @@ class Experiment:
         elif cond[0] != "[":
             return None
         else:
-            timestamp = cond[cond.find("[") + 1: cond.find("]")]
+            timestamp = cond[cond.find("[") + 1 : cond.find("]")]
             for format in self.datetime_formats:
                 try:
                     return datetime.strptime(timestamp, format)
                 except ValueError:
                     pass
-                
-            raise ValueError(f"The timestamp [{timestamp}] does not match any of the supported formats.")
+
+            raise ValueError(
+                f"The timestamp [{timestamp}] does not match any "
+                "of the supported formats."
+            )
