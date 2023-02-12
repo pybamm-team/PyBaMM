a166a475c7e03f3645f29b5ff8656203b97ed0f0
Jerom Palimattom Tom <83979298+jerom-tom@users.noreply.github.com>
2023-02-12 16:07:57 +0530

    Update Ai2020

--- pybamm/input/parameters/lithium_ion/Ai2020.py
+++ pybamm/input/parameters/lithium_ion/Ai2020.py
@@ -8,10 +8,8 @@ def graphite_diffusivity_Dualfoil1998(sto, T):
 
     References
     ----------
-     .. [1] Ai, W., Kraft, L., Sturm, J., Jossen, A., & Wu, B. (2020).
-     Electrochemical Thermal-Mechanical Modelling of Stress Inhomogeneity in
-     Lithium-Ion Pouch Cells. Journal of The Electrochemical Society, 167(1), 013512
-      DOI: 10.1149/2.0122001JES.
+     :cite:t:`2019:Ai2019`
+    .. bibliography::
      .. [2] Rieger, B., Erhard, S. V., Rumpf, K., & Jossen, A. (2016).
      A new method to model the thickness change of a commercial pouch cell
      during discharge. Journal of The Electrochemical Society, 163(8), A1566-A1575.
