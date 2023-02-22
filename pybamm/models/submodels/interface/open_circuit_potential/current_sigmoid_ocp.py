29c07c0267faf27e7f788ae452c2f1b7e77a57bc
DrSOKane <s.okane@imperial.ac.uk>
2023-02-22 15:33:29 +0000

    Changed current_sigmoid_ocp to be valid for both electrodes

--- pybamm/models/submodels/interface/open_circuit_potential/current_sigmoid_ocp.py
+++ pybamm/models/submodels/interface/open_circuit_potential/current_sigmoid_ocp.py
@@ -7,12 +7,17 @@ from . import BaseOpenCircuitPotential
 
 class CurrentSigmoidOpenCircuitPotential(BaseOpenCircuitPotential):
     def get_coupled_variables(self, variables):
+        domain, Domain = self.domain_Domain
         current = variables["Total current density"]
         k = 100
-        m_lith = pybamm.sigmoid(current, 0, k)  # for lithation (current < 0)
-        m_delith = 1 - m_lith  # for delithiation (current > 0)
 
-        domain, Domain = self.domain_Domain
+        if Domain == "Negative":
+            m_lith = pybamm.sigmoid(current, 0, k)  # for lithation (current < 0)
+            m_delith = 1 - m_lith  # for delithiation (current > 0)
+        elif Domain == "Positive":
+            m_delith = pybamm.sigmoid(current, 0, k)  # for delithation (current < 0)
+            m_lith = 1 - m_delith  # for lithiation (current > 0)
+
         phase_name = self.phase_name
 
         if self.reaction == "lithium-ion main":
