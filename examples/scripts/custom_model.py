6a3c26c1e1198d14dc131375ba883b27b2beb0c7
Valentin Sulzer <valentinsulzer@hotmail.com>
2023-03-06 12:48:38 -0500

    fix examples

--- examples/scripts/custom_model.py
+++ examples/scripts/custom_model.py
@@ -44,6 +44,11 @@ for domain in ["negative", "positive"]:
         options={**model.options, "particle": "uniform profile"},
         phase="primary",
     )
+    model.submodels[
+        f"{domain} total particle concentration"
+    ] = pybamm.particle.TotalConcentration(
+        model.param, domain, model.options, phase="primary"
+    )
 
     model.submodels[
         f"{domain} open-circuit potential"
