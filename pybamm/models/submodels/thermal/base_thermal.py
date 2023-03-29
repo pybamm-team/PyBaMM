67a2e3436f3c67c4ae1ce8a0258c78195c4e2917
Ivan Korotkin <i.korotkin@soton.ac.uk>
2023-03-29 16:06:30 +0100

    Fixed domain in the integral

--- pybamm/models/submodels/thermal/base_thermal.py
+++ pybamm/models/submodels/thermal/base_thermal.py
@@ -169,8 +169,8 @@ class BaseThermal(pybamm.BaseSubModel):
             dUeq_p = Ueq_p.children[0].diff(c_p)
             integrand_r_n = D_n * dc_n_dr**2 * dUeq_n
             integrand_r_p = D_p * dc_p_dr**2 * dUeq_p
-            integration_variable_r_n = [pybamm.SpatialVariable("r", integrand_r_n)]
-            integration_variable_r_p = [pybamm.SpatialVariable("r", integrand_r_p)]
+            integration_variable_r_n = [pybamm.SpatialVariable("r", domain=integrand_r_n.domain)]
+            integration_variable_r_p = [pybamm.SpatialVariable("r", domain=integrand_r_p.domain)]
             integral_r_n = pybamm.Integral(integrand_r_n, integration_variable_r_n)
             integral_r_p = pybamm.Integral(integrand_r_p, integration_variable_r_p)
             Q_mix_s_n = F * N_n * integral_r_n
