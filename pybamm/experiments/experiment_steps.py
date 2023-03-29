d2f20b9e4e64ff5940162624dcc65f3f25f8b615
mbonkile <bonkile.mayur@gmail.com>
2023-03-29 12:49:29 +0100

    Experiment steps: CC is defined as a class

---
+++ pybamm/experiments/experiment_steps.py
@@ -0,0 +1,28 @@
+#
+# Experiment class
+#
+
+import numpy as np
+import re
+
+class CC:
+    """
+    Description:
+
+
+    """
+    def __init__(
+            self,
+            C_rate="",
+            temperature=None,
+            duration=None,
+            upper_cutoff=None,
+                 ):
+        self.c_rate = C_rate
+        self.temperature = temperature
+        self.duration = duration
+        self.upper_cutoff = upper_cutoff
+
+
+
+
