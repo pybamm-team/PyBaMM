3ad0c75b855b3f1d81fc8d812733d766f5200b59
Alexander Bills <abills@andrew.cmu.edu>
2023-03-30 20:47:55 -0400

    change ocv hack

--- pybamm/models/submodels/thermal/base_thermal.py
+++ pybamm/models/submodels/thermal/base_thermal.py
@@ -157,13 +157,13 @@ class BaseThermal(pybamm.BaseSubModel):
             else:
                 a_n = variables["Negative electrode surface area to volume ratio [m-1]"]
                 R_n = variables["Negative particle radius [m]"]
+                T_n = variables["Negative electrode temperature [K]"]
                 N_n = a_n / (4 * pi * R_n**2)
                 c_n = variables["Negative particle concentration [mol.m-3]"]
                 dc_n_dr = pybamm.grad_squared(c_n)
                 D_n = variables["Negative particle effective diffusivity [m2.s-1]"]
-                Ueq_n = variables["Negative electrode open-circuit potential [V]"]
-                # TODO: Drop terms as it has spatial operators and diff doesn't work
-                dUeq_n = Ueq_n.children[0].children[0].diff(c_n)
+                Ueq_n = param.n.prim.U(c_n, T_n)
+                dUeq_n = Ueq_n.diff(c_n)
                 integrand_r_n = D_n * dc_n_dr**2 * dUeq_n
                 integration_variable_r_n = [
                     pybamm.SpatialVariable("r", domain=integrand_r_n.domain)
@@ -172,12 +172,13 @@ class BaseThermal(pybamm.BaseSubModel):
                 Q_mix_s_n = F * N_n * integral_r_n
             a_p = variables["Positive electrode surface area to volume ratio [m-1]"]
             R_p = variables["Positive particle radius [m]"]
+            T_p = variables["Positive electrode temperature [K]"]
             N_p = a_p / (4 * pi * R_p**2)
             c_p = variables["Positive particle concentration [mol.m-3]"]
             dc_p_dr = pybamm.grad_squared(c_p)
             D_p = variables["Positive particle effective diffusivity [m2.s-1]"]
-            Ueq_p = variables["Positive electrode open-circuit potential [V]"]
-            dUeq_p = Ueq_p.children[0].children[0].diff(c_p)
+            Ueq_p = param.p.prim.U(c_p, T_p)
+            dUeq_p = Ueq_p.diff(c_p)
             integrand_r_p = D_p * dc_p_dr**2 * dUeq_p
             integration_variable_r_p = [
                 pybamm.SpatialVariable("r", domain=integrand_r_p.domain)
