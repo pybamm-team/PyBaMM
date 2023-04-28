2320bae65788c462704b2dd9bf6e62f9dd28b35a
Valentin Sulzer <valentinsulzer@hotmail.com>
2023-04-28 17:15:48 -0400

    #2670 add xarray as a dependency

--- setup.py
+++ setup.py
@@ -214,6 +214,7 @@ setup(
         "importlib-metadata",
         "pybtex>=0.24.0",
         "sympy>=1.8",
+        "xarray",
         "bpx",
         # Note: Matplotlib is loaded for debug plots, but to ensure pybamm runs
         # on systems without an attached display, it should never be imported
