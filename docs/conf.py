2fef1a11d8b230a0238c89fc18d68a6895499f80
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2023-06-18 16:03:09 +0530

    Ensure mathematical equations render correctly
    Also fix some links, lint some code blocks against trailing whitespaces, and remove empty code blocks at the end of notebooks

--- docs/conf.py
+++ docs/conf.py
@@ -95,7 +95,7 @@ language = "en"
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", ".ipynb_checkpoints"]
 
 # Suppress warnings generated if footnotes are not references in a docstring
 # suppress_warnings = ["ref.footnote"]
