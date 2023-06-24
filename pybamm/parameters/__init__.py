8151b2bc0f0b8c0e85e4caff9d9a33df761a448f
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2023-06-24 21:50:19 +0530

    Move parameters toctree to `__init__.py`

--- pybamm/parameters/__init__.py
+++ pybamm/parameters/__init__.py
@@ -1,3 +1,18 @@
+r"""
+
+.. toctree::
+  parameter_values
+  geometric_parameters
+  electrical_parameters
+  thermal_parameters
+  lithium_ion_parameters
+  lead_acid_parameters
+  parameter_sets
+  process_parameter_data
+
+"""
+
+
 from .process_parameter_data import (
     process_1D_data,
     process_2D_data,
