22ba8cc14962af507e8d5fc559a125658f0ff8b8
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2023-06-28 21:21:51 +0530

    Add a sphinx extension for inheritance diagrams
    generated using graphviz and sphinxext-inheritance_diagram

---
+++ docs/sphinxext/inheritance_diagram.py
@@ -0,0 +1,48 @@
+# Sphinx extension to add an inheritance diagram in the docstring of a class built upon
+# the built-in sphinxext.inheritance_diagram extension. The inheritance diagram is
+# generated via graphviz and the fully qualified name of the class.
+
+from inspect import getmro, isclass
+
+# Initialise empty list of classes for logging
+classes_to_add_diagram = []
+
+
+def add_diagram(app, what, name, obj, options, lines):
+    # if it is not a class, do nothing
+    if not isclass(obj):
+        return
+    # if it is a model or submodel class, add the inheritance diagram
+    else:
+        # get the fully qualified name of the class
+        cls_name = f"{obj.__module__}.{obj.__qualname__}"
+        # check if the class is a model or submodel
+        if "pybamm.models" in cls_name:
+            # check if the class derives from another class
+            if not len(getmro(obj)) > 2:
+                # do nothing if it is not a derived class
+                return
+
+            # Append the inheritance diagram to the docstring
+            lines.append("\n")
+            lines.append(".. dropdown:: View inheritance diagram")
+            lines.append("   :animate: fade-in-slide-down")
+            lines.append("   :icon: eye\n")
+            lines.append("   :class-title: sd-align-major-center sd-fs-6 \n")
+            lines.append("   :class-body: sd-text-info sd-shadow-none\n")
+            lines.append("   :class-container: sd-font-weight-lighter sd-text-info \n")
+            lines.append("\n")
+            lines.append("       .. inheritance-diagram:: " + cls_name)
+            lines.append("           :parts: 2\n")
+            lines.append("\n")
+
+            # Add the class name to the list of classes
+            classes_to_add_diagram.append(cls_name)
+
+    # Logging purposes
+    print("Successfully added inheritance diagrams for the following classes: ")
+    print(*classes_to_add_diagram, sep=", ")
+
+
+def setup(app):
+    app.connect("autodoc-add-inheritance-diagram", add_diagram)
