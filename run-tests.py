3a417ba467594e50ac4ee2c02e93317e59fce364
arjxn.py <arjxn.py@gmail.com>
2023-07-05 02:10:06 +0530

    Install `pybamm[all]` in run-tests

--- run-tests.py
+++ run-tests.py
@@ -187,15 +187,15 @@ def test_notebook(path, executable="python"):
     print("Test " + path + " ... ", end="")
     sys.stdout.flush()
 
-    # Make sure the notebook has a "%pip install pybamm -q" command, for using Google
+    # Make sure the notebook has a "%pip install pybamm[all] -q" command, for using Google
     # Colab
     with open(path, "r") as f:
-        if "%pip install pybamm -q" not in f.read():
+        if "%pip install pybamm[all] -q" not in f.read():
             # print error and exit
             print("\n" + "-" * 70)
             print("ERROR")
             print("-" * 70)
-            print("Installation command '%pip install pybamm -q' not found in notebook")
+            print("Installation command '%pip install pybamm[all] -q' not found in notebook")
             print("-" * 70)
             return False
 
