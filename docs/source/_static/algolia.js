a936428f775ce7a35d82e83b56a87a41747c7136
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2023-07-20 11:01:47 +0530

    Test DocSearch v2 via Algolia keys on readthedocs
    A potential solution based on https://github.com/pystatgen/sgkit/pull/668.

---
+++ docs/source/_static/algolia.js
@@ -0,0 +1,10 @@
+docsearch({
+    appId: 'R2IYF7ETH7',
+    apiKey: '599cec31baffa4868cae4e79f180729b',
+    indexName: 'docsearch',
+    // Replace inputSelector with a CSS selector
+    // matching your search input
+    inputSelector: '#search-input',
+    // Set debug to true if you want to inspect the dropdown
+    debug: false
+  });
