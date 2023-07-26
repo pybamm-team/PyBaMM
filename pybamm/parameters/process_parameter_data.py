d7a4346ae4a02cef7ad9dd43e34a7b5471a2c351
arjxn.py <arjxn.py@gmail.com>
2023-07-26 16:54:30 +0530

    Skip 1 row

--- pybamm/parameters/process_parameter_data.py
+++ pybamm/parameters/process_parameter_data.py
@@ -142,7 +142,7 @@ def process_3D_data_csv(name, path=None):
 
     filename, name = _process_name(name, path, ".csv")
 
-    df = np.loadtxt(filename)
+    df = np.loadtxt(filename, skiprows=1)
 
     x1 = np.array(list(set(df.iloc[:, 0])))
     x2 = np.array(list(set(df.iloc[:, 1])))
