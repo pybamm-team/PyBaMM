2e692d48089ed957f8ca7cba478e27772cf232e2
DrSOKane <s.okane@imperial.ac.uk>
2023-08-03 17:03:56 +0100

    Fixed calendar ageing example

--- examples/scripts/calendar_ageing.py
+++ examples/scripts/calendar_ageing.py
@@ -45,12 +45,12 @@ pb.dynamic_plot(
         "Negative particle surface concentration",
         "X-averaged negative particle surface concentration",
         "Electrolyte concentration [mol.m-3]",
-        "Total SEI thickness [m]",
-        "X-averaged total SEI thickness [m]",
-        "X-averaged SEI concentration [mol.m-3]",
+        "Negative total SEI thickness [m]",
+        "X-averaged negative total SEI thickness [m]",
+        "X-averaged negative SEI concentration [mol.m-3]",
         "Sum of x-averaged negative electrode volumetric "
         "interfacial current densities [A.m-3]",
         "Loss of lithium inventory [%]",
-        ["Total lithium lost [mol]", "Loss of lithium to SEI [mol]"],
+        ["Total lithium lost [mol]", "Loss of lithium to negative SEI [mol]"],
     ],
 )
