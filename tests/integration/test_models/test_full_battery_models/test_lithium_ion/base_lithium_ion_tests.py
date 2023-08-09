5176f4ecd77d88962539d31960d4558af84e09ab
Ferran Brosa Planella <Ferran.Brosa-Planella@warwick.ac.uk>
2023-08-09 09:57:57 +0100

    #3252 add integration test current-driven LAM

--- tests/integration/test_models/test_full_battery_models/test_lithium_ion/base_lithium_ion_tests.py
+++ tests/integration/test_models/test_full_battery_models/test_lithium_ion/base_lithium_ion_tests.py
@@ -236,11 +236,31 @@ class BaseIntegrationTestLithiumIon:
 
     def test_well_posed_loss_active_material_current_negative(self):
         options = {"loss of active material": ("current-driven", "none")}
-        self.run_basic_processing_test(options)
+        parameter_values = pybamm.ParameterValues("Chen2020")
+
+        def current_LAM(i, T):
+            return -1e-8 * abs(i)
+
+        parameter_values.update(
+            {"Negative electrode current-driven LAM rate": current_LAM},
+            check_already_exists=False,
+        )
+
+        self.run_basic_processing_test(options, parameter_values=parameter_values)
 
     def test_well_posed_loss_active_material_current_positive(self):
         options = {"loss of active material": ("none", "current-driven")}
-        self.run_basic_processing_test(options)
+        parameter_values = pybamm.ParameterValues("Chen2020")
+
+        def current_LAM(i, T):
+            return -1e-8 * abs(i)
+
+        parameter_values.update(
+            {"Positive electrode current-driven LAM rate": current_LAM},
+            check_already_exists=False,
+        )
+
+        self.run_basic_processing_test(options, parameter_values=parameter_values)
 
     def test_negative_cracking(self):
         options = {"particle mechanics": ("swelling and cracking", "none")}
