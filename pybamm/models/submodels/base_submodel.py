d1975758a721bfa8f18d43d878fab1d94a228886
Saransh Chopra <saransh0701@gmail.com>
2023-09-05 12:44:25 +0530

    Fix pre-commit

--- pybamm/models/submodels/base_submodel.py
+++ pybamm/models/submodels/base_submodel.py
@@ -73,7 +73,7 @@ class BaseSubModel(pybamm.BaseModel):
 
         self.external = external
 
-        if options is None or type(options) == dict:
+        if options is None or isinstance(options, dict):
             options = pybamm.BatteryModelOptions(options)
 
         self.options = options
