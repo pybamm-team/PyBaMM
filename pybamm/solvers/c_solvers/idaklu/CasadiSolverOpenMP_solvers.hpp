38e4a064d29958a8b6a377d65677c661181874bc
John Brittain <john.brittain@dtc.ox.ac.uk>
2023-09-11 09:50:48 +0000

    Refactor CasadiFunctionsOpenMP concrete implementations to macro generator

--- pybamm/solvers/c_solvers/idaklu/CasadiSolverOpenMP_solvers.hpp
+++ pybamm/solvers/c_solvers/idaklu/CasadiSolverOpenMP_solvers.hpp
@@ -5,248 +5,116 @@
 #include "casadi_solver.hpp"
 
 /**
+ * Macro to generate CasadiSolver OpenMP implementations with specified linear
+ * solvers
+ */
+#define CASADISOLVER_NEWCLASS(CLASSNAME, FCN_CALL) \
+class CasadiSolverOpenMP_##CLASSNAME : public CasadiSolverOpenMP { \
+public: \
+  CasadiSolverOpenMP_##CLASSNAME( \
+    np_array atol_np, \
+    double rel_tol, \
+    np_array rhs_alg_id, \
+    int number_of_parameters, \
+    int number_of_events, \
+    int jac_times_cjmass_nnz, \
+    int jac_bandwidth_lower, \
+    int jac_bandwidth_upper, \
+    std::unique_ptr<CasadiFunctions> functions, \
+    const Options& options \
+  ) : \
+    CasadiSolverOpenMP( \
+      atol_np, \
+      rel_tol, \
+      rhs_alg_id, \
+      number_of_parameters, \
+      number_of_events, \
+      jac_times_cjmass_nnz, \
+      jac_bandwidth_lower, \
+      jac_bandwidth_upper, \
+      std::move(functions), \
+      options \
+    ) \
+  { \
+    Initialize(); \
+  } \
+  void SetLinearSolver() override { LS = FCN_CALL; }; \
+};
+
+/**
  * @brief CasadiSolver Dense implementation with OpenMP class
  */
-class CasadiSolverOpenMP_Dense : public CasadiSolverOpenMP {
-public:
-  CasadiSolverOpenMP_Dense(
-    np_array atol_np,
-    double rel_tol,
-    np_array rhs_alg_id,
-    int number_of_parameters,
-    int number_of_events,
-    int jac_times_cjmass_nnz,
-    int jac_bandwidth_lower,
-    int jac_bandwidth_upper,
-    std::unique_ptr<CasadiFunctions> functions,
-    const Options& options
-  ) :
-    CasadiSolverOpenMP(
-      atol_np,
-      rel_tol,
-      rhs_alg_id,
-      number_of_parameters,
-      number_of_events,
-      jac_times_cjmass_nnz,
-      jac_bandwidth_lower,
-      jac_bandwidth_upper,
-      std::move(functions),
-      options
-    )
-  {
-    Initialize();
-  }
-  void SetLinearSolver() override;
-};
+CASADISOLVER_NEWCLASS(
+  Dense,
+  SUNLinSol_Dense(yy, J, sunctx)
+)
 
 /**
  * @brief CasadiSolver KLU implementation with OpenMP class
  */
-class CasadiSolverOpenMP_KLU : public CasadiSolverOpenMP {
-public:
-  CasadiSolverOpenMP_KLU(
-    np_array atol_np,
-    double rel_tol,
-    np_array rhs_alg_id,
-    int number_of_parameters,
-    int number_of_events,
-    int jac_times_cjmass_nnz,
-    int jac_bandwidth_lower,
-    int jac_bandwidth_upper,
-    std::unique_ptr<CasadiFunctions> functions,
-    const Options& options
-  ) :
-    CasadiSolverOpenMP(
-      atol_np,
-      rel_tol,
-      rhs_alg_id,
-      number_of_parameters,
-      number_of_events,
-      jac_times_cjmass_nnz,
-      jac_bandwidth_lower,
-      jac_bandwidth_upper,
-      std::move(functions),
-      options
-    )
-  {
-    Initialize();
-  }
-  void SetLinearSolver() override;
-};
+CASADISOLVER_NEWCLASS(
+  KLU,
+  SUNLinSol_KLU(yy, J, sunctx)
+)
 
 /**
  * @brief CasadiSolver Banded implementation with OpenMP class
  */
-class CasadiSolverOpenMP_Band : public CasadiSolverOpenMP {
-public:
-  CasadiSolverOpenMP_Band(
-    np_array atol_np,
-    double rel_tol,
-    np_array rhs_alg_id,
-    int number_of_parameters,
-    int number_of_events,
-    int jac_times_cjmass_nnz,
-    int jac_bandwidth_lower,
-    int jac_bandwidth_upper,
-    std::unique_ptr<CasadiFunctions> functions,
-    const Options& options
-  ) :
-    CasadiSolverOpenMP(
-      atol_np,
-      rel_tol,
-      rhs_alg_id,
-      number_of_parameters,
-      number_of_events,
-      jac_times_cjmass_nnz,
-      jac_bandwidth_lower,
-      jac_bandwidth_upper,
-      std::move(functions),
-      options
-    )
-  {
-    Initialize();
-  }
-  void SetLinearSolver() override;
-};
+CASADISOLVER_NEWCLASS(
+  Band,
+  SUNLinSol_Band(yy, J, sunctx)
+)
 
 /**
  * @brief CasadiSolver SPBCGS implementation with OpenMP class
  */
-class CasadiSolverOpenMP_SPBCGS : public CasadiSolverOpenMP {
-public:
-  CasadiSolverOpenMP_SPBCGS(
-    np_array atol_np,
-    double rel_tol,
-    np_array rhs_alg_id,
-    int number_of_parameters,
-    int number_of_events,
-    int jac_times_cjmass_nnz,
-    int jac_bandwidth_lower,
-    int jac_bandwidth_upper,
-    std::unique_ptr<CasadiFunctions> functions,
-    const Options& options
-  ) :
-    CasadiSolverOpenMP(
-      atol_np,
-      rel_tol,
-      rhs_alg_id,
-      number_of_parameters,
-      number_of_events,
-      jac_times_cjmass_nnz,
-      jac_bandwidth_lower,
-      jac_bandwidth_upper,
-      std::move(functions),
-      options
-    )
-  {
-    Initialize();
-  }
-  void SetLinearSolver() override;
-};
+CASADISOLVER_NEWCLASS(
+  SPBCGS,
+  LS = SUNLinSol_SPBCGS(
+    yy,
+    precon_type,
+    options.linsol_max_iterations,
+    sunctx
+  );
+)
 
 /**
  * @brief CasadiSolver SPFGMR implementation with OpenMP class
  */
-class CasadiSolverOpenMP_SPFGMR : public CasadiSolverOpenMP {
-public:
-  CasadiSolverOpenMP_SPFGMR(
-    np_array atol_np,
-    double rel_tol,
-    np_array rhs_alg_id,
-    int number_of_parameters,
-    int number_of_events,
-    int jac_times_cjmass_nnz,
-    int jac_bandwidth_lower,
-    int jac_bandwidth_upper,
-    std::unique_ptr<CasadiFunctions> functions,
-    const Options& options
-  ) :
-    CasadiSolverOpenMP(
-      atol_np,
-      rel_tol,
-      rhs_alg_id,
-      number_of_parameters,
-      number_of_events,
-      jac_times_cjmass_nnz,
-      jac_bandwidth_lower,
-      jac_bandwidth_upper,
-      std::move(functions),
-      options
-    )
-  {
-    Initialize();
-  }
-  void SetLinearSolver() override;
-};
+CASADISOLVER_NEWCLASS(
+  SPFGMR,
+  LS = SUNLinSol_SPFGMR(
+    yy,
+    precon_type,
+    options.linsol_max_iterations,
+    sunctx
+  );
+)
 
 /**
  * @brief CasadiSolver SPGMR implementation with OpenMP class
  */
-class CasadiSolverOpenMP_SPGMR : public CasadiSolverOpenMP {
-public:
-  CasadiSolverOpenMP_SPGMR(
-    np_array atol_np,
-    double rel_tol,
-    np_array rhs_alg_id,
-    int number_of_parameters,
-    int number_of_events,
-    int jac_times_cjmass_nnz,
-    int jac_bandwidth_lower,
-    int jac_bandwidth_upper,
-    std::unique_ptr<CasadiFunctions> functions,
-    const Options& options
-  ) :
-    CasadiSolverOpenMP(
-      atol_np,
-      rel_tol,
-      rhs_alg_id,
-      number_of_parameters,
-      number_of_events,
-      jac_times_cjmass_nnz,
-      jac_bandwidth_lower,
-      jac_bandwidth_upper,
-      std::move(functions),
-      options
-    )
-  {
-    Initialize();
-  }
-  void SetLinearSolver() override;
-};
+CASADISOLVER_NEWCLASS(
+  SPGMR,
+  LS = SUNLinSol_SPGMR(
+    yy,
+    precon_type,
+    options.linsol_max_iterations,
+    sunctx
+  );
+)
 
 /**
  * @brief CasadiSolver SPTFQMR implementation with OpenMP class
  */
-class CasadiSolverOpenMP_SPTFQMR : public CasadiSolverOpenMP {
-public:
-  CasadiSolverOpenMP_SPTFQMR(
-    np_array atol_np,
-    double rel_tol,
-    np_array rhs_alg_id,
-    int number_of_parameters,
-    int number_of_events,
-    int jac_times_cjmass_nnz,
-    int jac_bandwidth_lower,
-    int jac_bandwidth_upper,
-    std::unique_ptr<CasadiFunctions> functions,
-    const Options& options
-  ) :
-    CasadiSolverOpenMP(
-      atol_np,
-      rel_tol,
-      rhs_alg_id,
-      number_of_parameters,
-      number_of_events,
-      jac_times_cjmass_nnz,
-      jac_bandwidth_lower,
-      jac_bandwidth_upper,
-      std::move(functions),
-      options
-    )
-  {
-    Initialize();
-  }
-  void SetLinearSolver() override;
-};
+CASADISOLVER_NEWCLASS(
+  SPTFQMR,
+  LS = SUNLinSol_SPTFQMR(
+    yy,
+    precon_type,
+    options.linsol_max_iterations,
+    sunctx
+  );
+)
 
 #endif // PYBAMM_IDAKLU_CASADI_SOLVER_OPENMP_HPP
