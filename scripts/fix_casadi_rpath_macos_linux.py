9432e612f0e8273ddcc332cd4b0b63d9dc047f2a
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2023-09-12 08:11:06 +0530

    #3049 Run rpath fixes on macOS and Linux

--- scripts/fix_casadi_rpath_macos_linux.py
+++ scripts/fix_casadi_rpath_macos_linux.py
@@ -2,7 +2,7 @@
 Removes the rpath from libcasadi.dylib and libcasadi.3.7.dylib in the casadi python
 install and uses a fixed path
 
-Used when building the wheels for macos
+Used when building the wheels for macOS and GNU/Linux
 """
 import casadi
 import os
@@ -15,18 +15,21 @@ libcpp_name = "libc++.1.0.dylib"
 libcppabi_name = "libc++abi.dylib"
 libcasadi_name = "libcasadi.dylib"
 libcasadi_37_name = "libcasadi.3.7.dylib"
+
 install_name_tool_args_for_libcasadi_name = [
     "-change",
     os.path.join("@rpath", libcpp_name),
     os.path.join(casadi_dir, libcpp_name),
     os.path.join(casadi_dir, libcasadi_name),
 ]
+
 install_name_tool_args_for_libcasadi_37_name = [
     "-change",
     os.path.join("@rpath", libcpp_name),
     os.path.join(casadi_dir, libcpp_name),
     os.path.join(casadi_dir, libcasadi_37_name),
 ]
+
 subprocess.run(["otool"] + ["-L", os.path.join(casadi_dir, libcasadi_name)])
 
 print(" ".join(["install_name_tool"] + install_name_tool_args_for_libcasadi_name))
@@ -50,8 +53,8 @@ subprocess.run(["install_name_tool"] + install_name_tool_args)
 
 subprocess.run(["otool"] + ["-L", os.path.join(casadi_dir, libcpp_name)])
 
-# Copy libcasadi.3.7.dylib and libc++.1.0.dylib to LD_LIBRARY_PATH ($HOME/.local/lib)
-# This is needed for the casadi python bindings to work
+# Copy libcasadi.3.7.dylib and libc++.1.0.dylib to $HOME/.local/lib
+# This is needed for the casadi python bindings to work while repairing the wheel
 
 subprocess.run(
     ["cp",
