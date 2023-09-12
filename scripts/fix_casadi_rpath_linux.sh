78bff9a6f817552106af1e644130d6fa95d45768
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2023-09-13 00:14:21 +0530

    #3049 fix copying script for Linux

--- scripts/fix_casadi_rpath_linux.sh
+++ scripts/fix_casadi_rpath_linux.sh
@@ -3,5 +3,5 @@
 LD_LIBRARY_PATH=${HOME}/.local/lib
 CASADI_PATH=$(python -c "import casadi; print(casadi.__path__[0])")
 
-cp ${CASADI_PATH}/libcasadi.so.3.7 ${LD_LIBRARY_PATH}
-cp ${CASADI_PATH}/libcasadi.so ${LD_LIBRARY_PATH}
+sudo cp ${CASADI_PATH}/libcasadi.so.3.7 ${LD_LIBRARY_PATH}
+sudo cp ${CASADI_PATH}/libcasadi.so ${LD_LIBRARY_PATH}
