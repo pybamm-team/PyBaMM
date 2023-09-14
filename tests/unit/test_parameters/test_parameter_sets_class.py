07f7068d3b1916ad39e62c57b57188a86141578c
kratman <kratmandu@gmail.com>
2023-09-14 14:00:16 -0400

    Fix the selection

--- tests/unit/test_parameters/test_parameter_sets_class.py
+++ tests/unit/test_parameters/test_parameter_sets_class.py
@@ -25,7 +25,7 @@ class TestParameterSets(TestCase):
         """Check that all parameter sets have been registered with the
         ``pybamm_parameter_sets`` entry point"""
         known_entry_points = set(
-            ep.name for ep in il.entry_points().select(group="pybamm_parameter_sets")
+            ep.name for ep in il.entry_points()["pybamm_parameter_sets"]
         )
         self.assertEqual(set(pybamm.parameter_sets.keys()), known_entry_points)
         self.assertEqual(len(known_entry_points), len(pybamm.parameter_sets))
