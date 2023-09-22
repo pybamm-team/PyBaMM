90abbdf02717093f7980c79a9083221d2912518c
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2023-09-22 22:16:11 +0530

    Un-silence session install commands to stream `pip` output

--- noxfile.py
+++ noxfile.py
@@ -39,7 +39,7 @@ def run_pybamm_requires(session):
     """Download, compile, and install the build-time requirements for Linux and macOS: the SuiteSparse and SUNDIALS libraries."""  # noqa: E501
     set_environment_variables(PYBAMM_ENV, session=session)
     if sys.platform != "win32":
-        session.install("wget", "cmake")
+        session.install("wget", "cmake", silent=False)
         session.run("python", "scripts/install_KLU_Sundials.py")
         if not os.path.exists("./pybind11"):
             session.run(
@@ -50,18 +50,18 @@ def run_pybamm_requires(session):
                 external=True,
             )
     else:
-        session.error("nox -s pybamm-requires is only available on Linux & MacOS.")
+        session.error("nox -s pybamm-requires is only available on Linux & macOS.")
 
 
 @nox.session(name="coverage")
 def run_coverage(session):
     """Run the coverage tests and generate an XML report."""
     set_environment_variables(PYBAMM_ENV, session=session)
-    session.install("coverage")
-    session.install("-e", ".[all]")
+    session.install("coverage", silent=False)
+    session.install("-e", ".[all]", silent=False)
     if sys.platform != "win32":
-        session.install("-e", ".[odes]")
-        session.install("-e", ".[jax]")
+        session.install("-e", ".[odes]", silent=False)
+        session.install("-e", ".[jax]", silent=False)
     session.run("coverage", "run", "--rcfile=.coveragerc", "run-tests.py", "--nosub")
     session.run("coverage", "combine")
     session.run("coverage", "xml")
@@ -71,16 +71,16 @@ def run_coverage(session):
 def run_integration(session):
     """Run the integration tests."""
     set_environment_variables(PYBAMM_ENV, session=session)
-    session.install("-e", ".[all]")
+    session.install("-e", ".[all]", silent=False)
     if sys.platform == "linux":
-        session.install("-e", ".[odes]")
+        session.install("-e", ".[odes]", silent=False)
     session.run("python", "run-tests.py", "--integration")
 
 
 @nox.session(name="doctests")
 def run_doctests(session):
     """Run the doctests and generate the output(s) in the docs/build/ directory."""
-    session.install("-e", ".[all,docs]")
+    session.install("-e", ".[all,docs]", silent=False)
     session.run("python", "run-tests.py", "--doctest")
 
 
@@ -90,8 +90,8 @@ def run_unit(session):
     set_environment_variables(PYBAMM_ENV, session=session)
     session.install("-e", ".[all]")
     if sys.platform == "linux":
-        session.install("-e", ".[odes]")
-        session.install("-e", ".[jax]")
+        session.install("-e", ".[odes]", silent=False)
+        session.install("-e", ".[jax]", silent=False)
     session.run("python", "run-tests.py", "--unit")
 
 
@@ -99,7 +99,7 @@ def run_unit(session):
 def run_examples(session):
     """Run the examples tests for Jupyter notebooks."""
     set_environment_variables(PYBAMM_ENV, session=session)
-    session.install("-e", ".[all,dev]")
+    session.install("-e", ".[all,dev]", silent=False)
     notebooks_to_test = session.posargs if session.posargs else []
     session.run("pytest", "--nbmake", *notebooks_to_test, external=True)
 
@@ -108,7 +108,7 @@ def run_examples(session):
 def run_scripts(session):
     """Run the scripts tests for Python scripts."""
     set_environment_variables(PYBAMM_ENV, session=session)
-    session.install("-e", ".[all]")
+    session.install("-e", ".[all]", silent=False)
     session.run("python", "run-tests.py", "--scripts")
 
 
@@ -117,8 +117,8 @@ def set_dev(session):
     """Install PyBaMM in editable mode."""
     set_environment_variables(PYBAMM_ENV, session=session)
     envbindir = session.bin
-    session.install("-e", ".[all]")
-    session.install("cmake")
+    session.install("-e", ".[all]", silent=False)
+    session.install("cmake", silent=False)
     if sys.platform == "linux" or sys.platform == "darwin":
         session.run(
             "echo",
@@ -134,10 +134,10 @@ def set_dev(session):
 def run_tests(session):
     """Run the unit tests and integration tests sequentially."""
     set_environment_variables(PYBAMM_ENV, session=session)
-    session.install("-e", ".[all]")
+    session.install("-e", ".[all]", silent=False)
     if sys.platform == "linux" or sys.platform == "darwin":
-        session.install("-e", ".[odes]")
-        session.install("-e", ".[jax]")
+        session.install("-e", ".[odes]", silent=False)
+        session.install("-e", ".[jax]", silent=False)
     session.run("python", "run-tests.py", "--all")
 
 
@@ -145,7 +145,7 @@ def run_tests(session):
 def build_docs(session):
     """Build the documentation and load it in a browser tab, rebuilding on changes."""
     envbindir = session.bin
-    session.install("-e", ".[all,docs]")
+    session.install("-e", ".[all,docs]", silent=False)
     session.chdir("docs")
     # Local development
     if session.interactive:
@@ -176,7 +176,7 @@ def build_docs(session):
 @nox.session(name="pre-commit")
 def lint(session):
     """Check all files against the defined pre-commit hooks."""
-    session.install("pre-commit")
+    session.install("pre-commit", silent=False)
     session.run("pre-commit", "run", "--all-files")
 
 
