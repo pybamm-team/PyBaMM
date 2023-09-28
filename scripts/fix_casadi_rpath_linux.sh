f30d37288525ba4b08ea8866c7ad87686cf6b036
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2023-09-28 19:29:17 +0530

    #3049 remove Linux rpath fix script

--- scripts/fix_casadi_rpath_linux.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-LD_LIBRARY_PATH=${HOME}/.local/lib
-CASADI_PATH=$(python -c "import casadi; print(casadi.__path__[0])")
-
-sudo cp ${CASADI_PATH}/libcasadi.so.3.7 ${LD_LIBRARY_PATH}
-sudo cp ${CASADI_PATH}/libcasadi.so ${LD_LIBRARY_PATH}
