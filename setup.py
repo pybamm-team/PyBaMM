5c18e229b5f9a111dcbf46c0bd9c9fece2bfe069
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2023-10-07 03:27:38 +0530

    #3049 correctly specify inclusion of packages

--- setup.py
+++ setup.py
@@ -299,6 +299,8 @@ ext_modules = [idaklu_ext] if compile_KLU() else []
 # Project metadata was moved to pyproject.toml (which is read by pip). However, custom
 # build commands and setuptools extension modules are still defined here.
 setup(
+    # silence "Package would be ignored" warnings
+    include_package_data=True,
     ext_modules=ext_modules,
     cmdclass={
         "build_ext": CMakeBuild,
