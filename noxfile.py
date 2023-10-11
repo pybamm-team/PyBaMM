e8610dd3acabaed3185643a9424be9181c647146
Akhilender Bongirwar <112749383+akhilender-bongirwar@users.noreply.github.com>
2023-10-12 00:30:57 +0530

    Update noxfile.py
    - Altered and removed the darwin platform

--- noxfile.py
+++ noxfile.py
@@ -121,7 +121,7 @@ def set_dev(session):
     session.install("virtualenv", "cmake")
     session.run("virtualenv", os.fsdecode(VENV_DIR), silent=True)
     python = os.fsdecode(VENV_DIR.joinpath("bin/python"))
-    if sys.platform == "linux" or sys.platform == "darwin":
+    if sys.platform == "linux":
         session.run(python, "-m", "pip", "install", ".[all,dev,jax,odes]", external=True)
     else:
         session.run(python, "-m", "pip", "install", "-e", ".[all,dev]", external=True)
