eec0b5eab160bf14e2abce4b51d0303cd22d4c9a
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2023-10-24 15:06:55 +0530

    #2885 Don't read or open the CHANGELOG file
    Let Towncrier handle it after the version has been updated

--- scripts/update_version.py
+++ scripts/update_version.py
@@ -5,9 +5,6 @@ Automatically update the version number
 import json
 import os
 import re
-from datetime import date
-from dateutil.relativedelta import relativedelta
-
 
 import pybamm
 
@@ -17,7 +14,6 @@ def update_version():
     Opens file and updates the version number
     """
     release_version = os.getenv("VERSION")[1:]
-    last_day_of_month = date.today() + relativedelta(day=31)
 
 
     # pybamm/version.py
@@ -38,7 +34,7 @@ def update_version():
         file.seek(0)
         file.write(replace_version)
 
-    # docs/source/_static/versions.json for readthedocs build
+    # docs/_static/versions.json for readthedocs build
     if "rc" not in release_version:
         with open(
             os.path.join(pybamm.root_dir(), "docs", "_static", "versions.json"),
@@ -81,21 +77,6 @@ def update_version():
         file.truncate(0)
         file.seek(0)
         file.write(replace_commit_id)
-
-    changelog_line1 = "# [Unreleased](https://github.com/pybamm-team/PyBaMM/)\n"
-    changelog_line2 = f"# [v{release_version}](https://github.com/pybamm-team/PyBaMM/tree/v{release_version}) - {last_day_of_month}\n\n"  # noqa: E501
-
-    # CHANGELOG.md
-    with open(os.path.join(pybamm.root_dir(), "CHANGELOG.md"), "r+") as file:
-        output_list = file.readlines()
-        output_list[0] = changelog_line1
-        if "rc0" in release_version:
-            output_list.insert(2, changelog_line2)
-        else:
-            output_list[2] = changelog_line2
-        file.truncate(0)
-        file.seek(0)
-        file.writelines(output_list)
 
 
 if __name__ == "__main__":
