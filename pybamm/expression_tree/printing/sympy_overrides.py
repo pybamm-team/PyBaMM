2e30131d7ef35c19f538065fc894dc4bf32236f8
Saransh Chopra <saransh0701@gmail.com>
2023-11-13 23:05:22 +0530

    Fix CustomPrinter

--- pybamm/expression_tree/printing/sympy_overrides.py
+++ pybamm/expression_tree/printing/sympy_overrides.py
@@ -12,10 +12,10 @@ class CustomPrint(LatexPrinter):
         """Override :meth:`sympy.printing.latex.LatexPrinter._print_Derivative`"""
         eqn = super()._print_Derivative(expr)
         if getattr(expr, "force_partial", False) and "partial" not in eqn:
-                var1, var2 = re.findall(r"^\\frac{(\w+)}{(\w+) .+", eqn)[0]
-                eqn = eqn.replace(var1, "\partial").replace(var2, "\partial")
+            var1, var2 = re.findall(r"^\\frac{(\w+)}{(\w+) .+", eqn)[0]
+            eqn = eqn.replace(var1, "\partial").replace(var2, "\partial")
 
         return eqn
 
 def custom_print_func(expr, **settings):
-    return CustomPrint()._print_Derivative(expr)
+    return CustomPrint().doprint(expr)
