bd2d009c74d29a326857277129f2c713f8a1020e
Saransh Chopra <saransh0701@gmail.com>
2023-11-13 22:55:28 +0530

    Fix sympy overrides

--- pybamm/expression_tree/printing/sympy_overrides.py
+++ pybamm/expression_tree/printing/sympy_overrides.py
@@ -3,27 +3,19 @@
 #
 import re
 
-from pybamm.util import have_optional_dependency
+from sympy.printing.latex import LatexPrinter
 
 
-def custom_latex_printer(expr, **settings):
-    latex = have_optional_dependency("sympy","latex")
-    Derivative = have_optional_dependency("sympy","Derivative")
-    if isinstance(expr, Derivative) and getattr(expr, "force_partial", False):
-        latex_str = latex(expr, **settings)
-        var1, var2 = re.findall(r"^\\frac{(\w+)}{(\w+) .+", latex_str)[0]
-        latex_str = latex_str.replace(var1, "\partial").replace(var2, "\partial")
-        return latex_str
-    else:
-        return latex(expr, **settings)
+class CustomPrint(LatexPrinter):
+        """Override SymPy methods to match PyBaMM's requirements"""
+        def _print_Derivative(self, expr):
+                """Override :meth:`sympy.printing.latex.LatexPrinter._print_Derivative`"""
+                eqn = super()._print_Derivative(expr)
+                if getattr(expr, "force_partial", False) and "partial" not in eqn:
+                        var1, var2 = re.findall(r"^\\frac{(\w+)}{(\w+) .+", eqn)[0]
+                        eqn = eqn.replace(var1, "\partial").replace(var2, "\partial")
 
-class CustomPrint:
-    """Override SymPy methods to match PyBaMM's requirements"""
-
-    def _print_Derivative(self, expr):
-        """Override :meth:`sympy.printing.latex.LatexPrinter._print_Derivative`"""
-        return custom_latex_printer(expr)
+                return eqn
 
 def custom_print_func(expr, **settings):
-    have_optional_dependency("sympy.printing.latex", "LatexPrinter")
     return CustomPrint()._print_Derivative(expr)
