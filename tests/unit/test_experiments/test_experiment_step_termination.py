99ad3d06ae31a4c8e5151b47be34e2d4432d64c5
Valentin Sulzer <valentinsulzer@hotmail.com>
2023-12-08 10:46:55 -0500

    coverage again

--- tests/unit/test_experiments/test_experiment_step_termination.py
+++ tests/unit/test_experiments/test_experiment_step_termination.py
@@ -9,5 +9,10 @@ class TestExperimentStepTermination(unittest.TestCase):
     def test_base_termination(self):
         term = pybamm.step.BaseTermination(1)
         self.assertEqual(term.value, 1)
+
+        with self.assertRaises(NotImplementedError):
+            term.get_event(None, None)
+
+        self.assertEqual(term, pybamm.step.BaseTermination(1))
         self.assertNotEqual(term, pybamm.step.BaseTermination(2))
         self.assertNotEqual(term, pybamm.step.CurrentTermination(1))
