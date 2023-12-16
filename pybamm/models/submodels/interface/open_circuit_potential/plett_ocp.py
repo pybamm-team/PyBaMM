80a03d67be0817659851829ab4ec0a77acde78c5
Tanner Leo <tanner.m.leo@gmail.com>
2023-12-15 21:21:49 -0800

    now passes TestDFN unit test well posed

--- pybamm/models/submodels/interface/open_circuit_potential/plett_ocp.py
+++ pybamm/models/submodels/interface/open_circuit_potential/plett_ocp.py
@@ -9,10 +9,11 @@ class PlettOpenCircuitPotential(BaseOpenCircuitPotential):
     def get_fundamental_variables(self):
         domain, Domain = self.domain_Domain
         phase_name = self.phase_name
-        h = pybamm.Variable(f'{Domain} electrode {phase_name}hysteresis state',domains={
+        h = pybamm.Variable(f'{Domain} electrode {phase_name}hysteresis state',
+                            domains={
             'primary':f'{domain} electrode',
-            'secondary':'current collector',
-        })
+            'secondary':'current collector',}
+        )
         return {f'{Domain} electrode {phase_name}hysteresis state':h,}
 
     def get_coupled_variables(self, variables):
@@ -57,52 +58,33 @@ class PlettOpenCircuitPotential(BaseOpenCircuitPotential):
             T_bulk = pybamm.xyz_average(pybamm.size_average(T))
             ocp_bulk_eq = self.phase_param.U(sto_bulk, T_bulk)
 
-            c_s_rav = variables[
-            f"R-averaged {domain} {phase_name}particle concentration distribution"
-            ]
-            # eps_s = variables[
-            #     f"{Domain} electrode {phase_name}active material volume fraction"
-            # ]
-            # eps_s_av = pybamm.x_average(eps_s)
-            # c_s_vol_av = pybamm.x_average(eps_s * c_s_rav) / eps_s_av
-            # c_s_vol = eps_s * c_s_rav
-            c_scale = self.phase_param.c_max
-            # L = self.domain_param.L
-            # A = self.param.A_cc
-            sto_rav = c_s_rav / c_scale
-            sto_rav = variables[f'R-averaged {domain} particle stoichiometry distribution']
-            # sto_xav = variables[f'X-averaged {domain} particle stoichiometry distribution']
-            # eps_s = pybamm.PrimaryBroadcast(eps_s,[f'{domain} particle size'])
 
-            #! don't know how to broadcast this
+            c_scale = self.phase_param.c_max
             variables[f"Total lithium in {phase} phase in {domain} electrode [mol]"] =  sto_bulk* c_scale #c_s_vol * L * A
 
-
-            H = self.phase_param.H(sto_rav)
+            H = self.phase_param.H(sto_surf)
             variables[f'{Domain} electrode {phase_name}equilibrium OCP [V]'] = ocp_surf_eq
             variables[f'{Domain} electrode {phase_name}bulk equilibrium OCP [V]'] = ocp_bulk_eq
             variables[f'{Domain} electrode {phase_name}OCP hysteresis [V]'] = H
-            # dQdU = variables[f'{Domain} electrode {phase_name}differential capacity [A.s.V-1]']
 
-            dU = pybamm.source(1,ocp_bulk_eq.diff(sto_bulk))
-            # dU = pybamm.PrimaryBroadcast(dU,[f'{domain} electrode'])
-            # dU = pybamm.PrimaryBroadcast(dU,[f'{domain} particle size'])
-            # dU = pybamm.FullBroadcast(dU,broadcast_domains={'primary':f'{domain} particle size', 'secondary':f'{domain} electrode'})
-            Q = variables[f"Total lithium in {phase} phase in {domain} electrode [mol]"] * pybamm.constants.F / 3600
-            # Q = pybamm.PrimaryBroadcast(Q,f'{domain} electrode')
-            dQ = Q.diff(sto_bulk)
+            dU = self.phase_param.U(sto_surf,T_bulk).diff(sto_surf)
+            dQ = self.phase_param.Q(sto_surf).diff(sto_surf)
             dQdU = dQ/dU
             variables[f'{Domain} electrode {phase_name}differential capacity [A.s.V-1]'] = dQdU
             variables[f'{Domain} electrode {phase_name}hysteresis state distribution'] = h
-            # H = pybamm.PrimaryBroadcast(H,f'{domain} particle size')
+            # H = H.orphans[0]
+            # H = pybamm.SecondaryBroadcast(H,f'{domain} electrode')
+            # H = pybamm.TertiaryBroadcast(H,f'current collector')
             # h = pybamm.SecondaryBroadcast(h,f'current collector')
             # ocp_surf_eq = pybamm.SecondaryBroadcast(ocp_surf_eq,[f'{domain} electrode'])
             # ocp_bulk_eq = pybamm.PrimaryBroadcast(ocp_bulk_eq,[f'{domain} electrode'])
             # ocp_bulk_eq = pybamm.PrimaryBroadcast(ocp_bulk_eq, [f'{domain} particle size'])
             #! why is ocp surf not in electrode domain? why no x dimension?
+            # H = pybamm.PrimaryBroadcast(H.orphans[0],[f'current collector'])
+            # h = pybamm.PrimaryBroadcast(h,[f'{domain} electrode'])
             H_x_av = pybamm.x_average(H)
             h_x_av = pybamm.x_average(h)
-            ocp_surf = ocp_surf_eq + H_x_av * h_x_av
+            ocp_surf = ocp_surf_eq + H * h
             H_s_av = pybamm.size_average(H_x_av)
             h_s_av = pybamm.size_average(h_x_av)
 
