a309b1be359e3744c96adfcf0c226e36d6ed5f1f
AlessioBugetti <alessiobugetti98@gmail.com>
2024-01-15 21:57:33 +0100

    Fix test_plot_voltage_components.py

--- tests/unit/test_plotting/test_plot_voltage_components.py
+++ tests/unit/test_plotting/test_plot_voltage_components.py
@@ -11,6 +11,39 @@ class TestPlotVoltageComponents(TestCase):
         sim = pybamm.Simulation(model)
         sol = sim.solve([0, 3600])
         for split in [True, False]:
+            _, ax = pybamm.plot_voltage_components(
+                sol, testing=True, split_by_electrode=split
+            )
+            t, V = ax.get_lines()[0].get_data()
+            np.testing.assert_array_equal(t, sol["Time [h]"].data)
+            np.testing.assert_array_equal(V, sol["Battery voltage [V]"].data)
+
+        _, ax = plt.subplots()
+        _, ax_out = pybamm.plot_voltage_components(sol, ax=ax, show_legend=True)
+        self.assertEqual(ax_out, ax)
+
+    def test_plot_with_simulation(self):
+        model = pybamm.lithium_ion.SPM()
+        sim = pybamm.Simulation(model)
+        sim.solve([0, 3600])
+
+        for split in [True, False]:
+            _, ax = pybamm.plot_voltage_components(
+                sim, testing=True, split_by_electrode=split
+            )
+            t, V = ax.get_lines()[0].get_data()
+            np.testing.assert_array_equal(t, sim.solution["Time [h]"].data)
+            np.testing.assert_array_equal(V, sim.solution["Battery voltage [V]"].data)
+
+        _, ax = plt.subplots()
+        _, ax_out = pybamm.plot_voltage_components(sim, ax=ax, show_legend=True)
+        self.assertEqual(ax_out, ax)
+
+    def test_plot_from_solution(self):
+        model = pybamm.lithium_ion.SPM()
+        sim = pybamm.Simulation(model)
+        sol = sim.solve([0, 3600])
+        for split in [True, False]:
             _, ax = sol.plot_voltage_components(testing=True, split_by_electrode=split)
             t, V = ax.get_lines()[0].get_data()
             np.testing.assert_array_equal(t, sol["Time [h]"].data)
@@ -20,13 +53,12 @@ class TestPlotVoltageComponents(TestCase):
         _, ax_out = sol.plot_voltage_components(ax=ax, show_legend=True)
         self.assertEqual(ax_out, ax)
 
-    def test_plot_with_simulation(self):
+    def test_plot_from_simulation(self):
         model = pybamm.lithium_ion.SPM()
         sim = pybamm.Simulation(model)
         sim.solve([0, 3600])
 
         for split in [True, False]:
-            # Use the method from the Simulation instance
             _, ax = sim.plot_voltage_components(testing=True, split_by_electrode=split)
             t, V = ax.get_lines()[0].get_data()
             np.testing.assert_array_equal(t, sim.solution["Time [h]"].data)
