adda3365307349d14fa6775bd9a6744293f70a97
Robert Timms <43040151+rtimms@users.noreply.github.com>
2024-01-16 17:00:50 +0000

    #3611 use actual cell volume for average total heating (#3707)
    * #3611 use actual cell volume for average total heating
    
    * #3611 changelog
    
    * #3611 account for number of electrode pairs
    
    * #3611 update variable names

--- examples/scripts/thermal_lithium_ion.py
+++ examples/scripts/thermal_lithium_ion.py
@@ -6,11 +6,15 @@ import pybamm
 pybamm.set_logging_level("INFO")
 
 # load models
+# for the full model we use the "x-full" thermal submodel, which means that we solve
+# the thermal model in the x-direction for a single-layer pouch cell
+# for the lumped model we use the "arbitrary" cell geometry, which means that we can
+# specify the surface area for cooling and total heat transfer coefficient
 full_thermal_model = pybamm.lithium_ion.SPMe(
     {"thermal": "x-full"}, name="full thermal model"
 )
 lumped_thermal_model = pybamm.lithium_ion.SPMe(
-    {"thermal": "lumped"}, name="lumped thermal model"
+    {"cell geometry": "arbitrary", "thermal": "lumped"}, name="lumped thermal model"
 )
 models = [full_thermal_model, lumped_thermal_model]
 
@@ -31,26 +35,42 @@ full_params.update(
     }
 )
 # for the lumped model we set the "Total heat transfer coefficient [W.m-2.K-1]"
-# parameter as well as the "Cell cooling surface area [m2]" parameter. Since the "full"
-# model only accounts for cooling from the large surfaces of the pouch, we set the
-# "Surface area for cooling" parameter to the area of the large surfaces of the pouch,
-# and the total heat transfer coefficient to 5 W.m-2.K-1
+# parameter as well as the "Cell cooling surface area [m2]" and "Cell volume [m3]
+# parameters. Since the "full" model only accounts for cooling from the large surfaces
+# of the pouch, we set the "Surface area for cooling [m2]" parameter to the area of the
+# large surfaces of the pouch, and the total heat transfer coefficient to 5 W.m-2.K-1
 A = parameter_values["Electrode width [m]"] * parameter_values["Electrode height [m]"]
+contributing_layers = [
+    "Negative current collector",
+    "Negative electrode",
+    "Separator",
+    "Positive electrode",
+    "Positive current collector",
+]
+total_thickness = sum(
+    [parameter_values[layer + " thickness [m]"] for layer in contributing_layers]
+)
+electrode_volume = (
+    total_thickness
+    * parameter_values["Electrode height [m]"]
+    * parameter_values["Electrode width [m]"]
+)
 lumped_params = parameter_values.copy()
 lumped_params.update(
     {
         "Total heat transfer coefficient [W.m-2.K-1]": 5,
         "Cell cooling surface area [m2]": 2 * A,
+        "Cell volume [m3]": electrode_volume,
     }
 )
-params = [full_params, lumped_params]
+
 # loop over the models and solve
+params = [full_params, lumped_params]
 sols = []
 for model, param in zip(models, params):
     sim = pybamm.Simulation(model, parameter_values=param)
     sim.solve([0, 3600])
     sols.append(sim.solution)
-
 
 # plot
 output_variables = [
