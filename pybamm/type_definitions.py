ea03b4fd7e978084079fef6f222bdb5c4ee97de5
Pip Liggins <philippa.liggins@dtc.ox.ac.uk>
2024-02-09 10:58:51 +0000

    Use typing List/Dict

--- pybamm/type_definitions.py
+++ pybamm/type_definitions.py
@@ -3,7 +3,7 @@
 #
 from __future__ import annotations
 
-from typing import Union
+from typing import Union, List, Dict
 from typing_extensions import TypeAlias
 import numpy as np
 import pybamm
@@ -15,6 +15,6 @@ Numeric: TypeAlias = Union[int, float, np.number]
 ChildValue: TypeAlias = Union[float, np.ndarray]
 ChildSymbol: TypeAlias = Union[float, np.ndarray, pybamm.Symbol]
 
-DomainType: TypeAlias = Union[list[str], str, None]
-AuxiliaryDomainType: TypeAlias = Union[dict[str, str], None]
-DomainsType: TypeAlias = Union[dict[str, Union[list[str], str]], None]
+DomainType: TypeAlias = Union[List[str], str, None]
+AuxiliaryDomainType: TypeAlias = Union[Dict[str, str], None]
+DomainsType: TypeAlias = Union[Dict[str, Union[List[str], str]], None]
