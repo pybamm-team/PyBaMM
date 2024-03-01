c218078fa4bbaa0970dd6a0a87e67f892cf93841
Pip Liggins <philippa.liggins@dtc.ox.ac.uk>
2024-03-01 16:14:07 +0000

    Add test for copying with new children

--- tests/unit/test_expression_tree/test_operations/test_copy.py
+++ tests/unit/test_expression_tree/test_operations/test_copy.py
@@ -64,6 +64,139 @@ class TestCopy(TestCase):
         ]:
             self.assertEqual(symbol, symbol.new_copy())
 
+    def test_symbol_new_copy_new_children(self):
+        a = pybamm.Parameter("a")
+        b = pybamm.Parameter("b")
+
+        # binary operations
+        for symbol_ab, symbol_ba in zip(
+            [
+                a + b,
+                a - b,
+                a * b,
+                a / b,
+                a**b,
+                pybamm.minimum(a, b),
+                pybamm.maximum(a, b),
+                pybamm.Equality(a, b),
+            ],
+            [
+                b + a,
+                b - a,
+                b * a,
+                b / a,
+                b**a,
+                pybamm.minimum(b, a),
+                pybamm.maximum(b, a),
+                pybamm.Equality(b, a),
+            ],
+        ):
+            self.assertEqual(symbol_ab.new_copy(new_children=[b, a]), symbol_ba)
+
+        # unary operations
+        for symbol_a, symbol_b in zip(
+            [
+                -a,
+                abs(a),
+                pybamm.Function(np.sin, a),
+                pybamm.PrimaryBroadcast(a, "domain"),
+                pybamm.FullBroadcast(a, "domain", {"secondary": "other domain"}),
+                pybamm.NotConstant(a),
+                pybamm.EvaluateAt(a, 0),
+            ],
+            [
+                -b,
+                abs(b),
+                pybamm.Function(np.sin, b),
+                pybamm.PrimaryBroadcast(b, "domain"),
+                pybamm.FullBroadcast(b, "domain", {"secondary": "other domain"}),
+                pybamm.NotConstant(b),
+                pybamm.EvaluateAt(b, 0),
+            ],
+        ):
+            self.assertEqual(symbol_a.new_copy(new_children=[b]), symbol_b)
+
+        v_n = pybamm.Variable("v", "negative electrode")
+        w_n = pybamm.Variable("w", "negative electrode")
+        x_n = pybamm.standard_spatial_vars.x_n
+
+        for symbol_v, symbol_w in zip(
+            [
+                pybamm.grad(v_n),
+                pybamm.upwind(v_n),
+                pybamm.IndefiniteIntegral(v_n, x_n),
+                pybamm.BackwardIndefiniteIntegral(v_n, x_n),
+                pybamm.BoundaryValue(v_n, "right"),
+                pybamm.BoundaryGradient(v_n, "right"),
+                pybamm.SecondaryBroadcast(v_n, "current collector"),
+            ],
+            [
+                pybamm.grad(w_n),
+                pybamm.upwind(w_n),
+                pybamm.IndefiniteIntegral(w_n, x_n),
+                pybamm.BackwardIndefiniteIntegral(w_n, x_n),
+                pybamm.BoundaryValue(w_n, "right"),
+                pybamm.BoundaryGradient(w_n, "right"),
+                pybamm.SecondaryBroadcast(w_n, "current collector"),
+            ],
+        ):
+            self.assertEqual(symbol_v.new_copy(new_children=[w_n]), symbol_w)
+
+        self.assertEqual(
+            pybamm.div(pybamm.grad(v_n)).new_copy(new_children=[pybamm.grad(w_n)]),
+            pybamm.div(pybamm.grad(w_n)),
+        )
+
+        v_s = pybamm.Variable("v", "separator")
+        mesh = get_mesh_for_testing()
+
+        for symbol_n, symbol_s in zip(
+            [
+                pybamm.concatenation(v_n, v_s),
+                pybamm.DomainConcatenation([v_n, v_s], mesh),
+            ],
+            [
+                pybamm.concatenation(v_s, v_n),
+                pybamm.DomainConcatenation([v_s, v_n], mesh),
+            ],
+        ):
+            self.assertEqual(symbol_n.new_copy(new_children=[v_s, v_n]), symbol_s)
+
+        self.assertEqual(
+            pybamm.NumpyConcatenation(a, b, v_s).new_copy(new_children=[b, a, v_n]),
+            pybamm.NumpyConcatenation(b, a, v_n),
+        )
+
+        v_n_2D = pybamm.Variable(
+            "v",
+            domain="negative particle",
+            auxiliary_domains={"secondary": "negative electrode"},
+        )
+        w_n_2D = pybamm.Variable(
+            "w",
+            domain="negative particle",
+            auxiliary_domains={"secondary": "negative electrode"},
+        )
+        vec = pybamm.Vector([1, 2, 3, 4, 5])
+        vec_b = pybamm.Vector([6, 7, 8, 9, 10])
+        mat = pybamm.Matrix([[1, 2], [3, 4]])
+        mat_b = pybamm.Matrix([[5, 6], [7, 8]])
+
+        self.assertEqual(
+            pybamm.TertiaryBroadcast(v_n_2D, "current collector").new_copy(
+                new_children=[w_n_2D]
+            ),
+            pybamm.TertiaryBroadcast(w_n_2D, "current collector"),
+        )
+        self.assertEqual(
+            pybamm.Index(vec, 1).new_copy(new_children=[vec_b]),
+            pybamm.Index(vec_b, 1),
+        )
+        self.assertEqual(
+            pybamm.SparseStack(mat, mat).new_copy(new_children=[mat_b, mat_b]),
+            pybamm.SparseStack(mat_b, mat_b),
+        )
+
 
 if __name__ == "__main__":
     print("Add -v for more debug output")
