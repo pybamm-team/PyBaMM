6963216e5b1f0261b97058685537dda69bd03b98
kratman <kratmandu@gmail.com>
2024-04-03 12:22:44 -0400

    Remove python 3.8

--- pybamm/expression_tree/interpolant.py
+++ pybamm/expression_tree/interpolant.py
@@ -4,7 +4,7 @@
 from __future__ import annotations
 import numpy as np
 from scipy import interpolate
-from typing import Sequence
+from collections.abc import Sequence
 import numbers
 
 import pybamm
