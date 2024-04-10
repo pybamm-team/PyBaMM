55aeb55eb2627cd29fc14fe6f1ed58a7085579de
kratman <kratmandu@gmail.com>
2024-04-10 16:41:08 -0400

    A little extra cleanup

--- pybamm/__init__.py
+++ pybamm/__init__.py
@@ -1,38 +1,13 @@
 import sys
 import os
 
-
-#
-# Version info
-#
 from pybamm.version import __version__
-
-#
-# Constants
-#
-# Float format: a float can be converted to a 17 digit decimal and back without
-# loss of information
-FLOAT_FORMAT = "{: .17e}"
-# Absolute path to the PyBaMM repo
-script_path = os.path.abspath(__file__)
 
 from .util import root_dir
 
-ABSOLUTE_PATH = root_dir()
-PARAMETER_PATH = [
-    root_dir(),
-    os.getcwd(),
-    os.path.join(root_dir(), "pybamm", "input", "parameters"),
-]
-
-
-#
-# Utility classes and methods
-#
 from .util import Timer, TimerTime, FuzzyDict
 from .util import (
     root_dir,
-    rmse,
     load,
     is_constant_and_can_evaluate,
 )
@@ -48,9 +23,6 @@ from .logger import logger, set_logging_level, get_new_logger
 from .settings import settings
 from .citations import Citations, citations, print_citations
 
-#
-# Classes for the Expression Tree
-#
 from .expression_tree.symbol import *
 from .expression_tree.binary_operators import *
 from .expression_tree.concatenations import *
@@ -73,7 +45,6 @@ from .expression_tree.state_vector import StateVectorBase, StateVector, StateVec
 
 from .expression_tree.exceptions import *
 
-# Operations
 from .expression_tree.operations.evaluate_python import (
     find_symbols,
     id_to_python_variable,
@@ -88,14 +59,10 @@ from .expression_tree.operations.jacobian import Jacobian
 from .expression_tree.operations.convert_to_casadi import CasadiConverter
 from .expression_tree.operations.unpack_symbols import SymbolUnpacker
 
-#
-# Model classes
-#
 from .models.base_model import BaseModel
 from .models.event import Event
 from .models.event import EventType
 
-# Battery models
 from .models.full_battery_models.base_battery_model import (
     BaseBatteryModel,
     BatteryModelOptions,
@@ -104,9 +71,6 @@ from .models.full_battery_models import lead_acid
 from .models.full_battery_models import lithium_ion
 from .models.full_battery_models import equivalent_circuit
 
-#
-# Submodel classes
-#
 from .models.submodels.base_submodel import BaseSubModel
 
 from .models.submodels import (
@@ -132,18 +96,12 @@ from .models.submodels.interface import lithium_plating
 from .models.submodels.interface import interface_utilisation
 from .models.submodels.interface import open_circuit_potential
 
-#
-# Geometry
-#
 from .geometry.geometry import Geometry
 from .geometry.battery_geometry import battery_geometry
 
 from .expression_tree.independent_variable import KNOWN_COORD_SYS
 from .geometry import standard_spatial_vars
 
-#
-# Parameter classes and methods
-#
 from .parameters.parameter_values import ParameterValues
 from .parameters import constants
 from .parameters.geometric_parameters import geometric_parameters, GeometricParameters
@@ -158,9 +116,6 @@ from .parameters.ecm_parameters import EcmParameters
 from .parameters.size_distribution_parameters import *
 from .parameters.parameter_sets import parameter_sets
 
-#
-# Mesh and Discretisation classes
-#
 from .discretisations.discretisation import Discretisation
 from .discretisations.discretisation import has_bc_of_form
 from .meshes.meshes import Mesh, SubMesh, MeshGenerator
@@ -181,23 +136,14 @@ from .meshes.scikit_fem_submeshes import (
     UserSupplied2DSubMesh,
 )
 
-#
-# Serialisation
-#
 from .models.base_model import load_model
 
-#
-# Spatial Methods
-#
 from .spatial_methods.spatial_method import SpatialMethod
 from .spatial_methods.zero_dimensional_method import ZeroDimensionalSpatialMethod
 from .spatial_methods.finite_volume import FiniteVolume
 from .spatial_methods.spectral_volume import SpectralVolume
 from .spatial_methods.scikit_finite_element import ScikitFiniteElement
 
-#
-# Solver classes
-#
 from .solvers.solution import Solution, EmptySolution, make_cycle_solution
 from .solvers.processed_variable import ProcessedVariable
 from .solvers.processed_variable_computed import ProcessedVariableComputed
@@ -214,17 +160,10 @@ from .solvers.jax_bdf_solver import jax_bdf_integrate
 from .solvers.idaklu_jax import IDAKLUJax
 from .solvers.idaklu_solver import IDAKLUSolver, have_idaklu
 
-#
-# Experiments
-#
 from .experiment.experiment import Experiment
 from . import experiment
 from .experiment import step
 
-
-#
-# Plotting
-#
 from .plotting.quick_plot import QuickPlot, close_plots, QuickPlotAxes
 from .plotting.plot import plot
 from .plotting.plot2D import plot2D
@@ -232,20 +171,27 @@ from .plotting.plot_voltage_components import plot_voltage_components
 from .plotting.plot_summary_variables import plot_summary_variables
 from .plotting.dynamic_plot import dynamic_plot
 
-#
-# Simulation
-#
 from .simulation import Simulation, load_sim, is_notebook
 
-#
-# Batch Study
-#
 from .batch_study import BatchStudy
 
-#
-# Callbacks
-#
 from . import callbacks
+
+#
+# Constants
+#
+# Float format: a float can be converted to a 17 digit decimal and back without
+# loss of information
+FLOAT_FORMAT = "{: .17e}"
+# Absolute path to the PyBaMM repo
+script_path = os.path.abspath(__file__)
+
+ABSOLUTE_PATH = root_dir()
+PARAMETER_PATH = [
+    root_dir(),
+    os.getcwd(),
+    os.path.join(root_dir(), "pybamm", "input", "parameters"),
+]
 
 #
 # Remove any imported modules, so we don't expose them as part of pybamm
