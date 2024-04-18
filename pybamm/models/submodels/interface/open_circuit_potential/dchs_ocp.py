d0be6377af13bca1d6c5cca3e73f3e6b3faa34be
Tanner Leo <tleo@silanano.com>
2024-04-18 10:02:01 -0700

    clean up

--- pybamm/models/submodels/interface/open_circuit_potential/dchs_ocp.py
+++ pybamm/models/submodels/interface/open_circuit_potential/dchs_ocp.py
@@ -28,6 +28,7 @@ class DCHSOpenCircuitPotential(BaseOpenCircuitPotential):
         if self.reaction == "lithium-ion main":
             T = variables[f"{Domain} electrode temperature [K]"]
             h = variables[f"{Domain} electrode {phase_name}hysteresis state"]
+
             # For "particle-size distribution" models, take distribution version
             # of c_s_surf that depends on particle size.
             domain_options = getattr(self.options, domain)
@@ -54,51 +55,44 @@ class DCHSOpenCircuitPotential(BaseOpenCircuitPotential):
                     sto_surf = sto_surf.orphans[0]
                     T = T.orphans[0]
 
-            ocp_surf_eq = self.phase_param.U(sto_surf, T)
-            dUdT = self.phase_param.dUdT(sto_surf)
+            variables[
+                f"{Domain} electrode {phase_name}hysteresis state distribution"
+            ] = h
 
             # Bulk OCP is from the average SOC and temperature
             sto_bulk = variables[f"{Domain} electrode {phase_name}stoichiometry"]
-            T_bulk = pybamm.xyz_average(pybamm.size_average(T))
-            ocp_bulk_eq = self.phase_param.U(sto_bulk, T_bulk)
-
-            # # get dQ, change in capacity with respect to stoichiometry
-            # c_max = self.phase_param.c_max
-            # epsilon_s_av = self.phase_param.epsilon_s_av
-            # V_electrode = self.param.A_cc * self.domain_param.L
-            # Li_max = c_max * V_electrode * epsilon_s_av
-            # Q_max = Li_max * self.param.F / 3600
-            # dQ = Q_max
-
             c_scale = self.phase_param.c_max
             variables[f"Total lithium in {phase} phase in {domain} electrode [mol]"] = (
                 sto_bulk * c_scale
             )  # c_s_vol * L * A
 
-            H = self.phase_param.H(sto_surf)
+            ocp_surf_eq = self.phase_param.U(sto_surf, T)
             variables[f"{Domain} electrode {phase_name}equilibrium OCP [V]"] = (
                 ocp_surf_eq
             )
+
+            T_bulk = pybamm.xyz_average(pybamm.size_average(T))
+            ocp_bulk_eq = self.phase_param.U(sto_bulk, T_bulk)
             variables[f"{Domain} electrode {phase_name}bulk equilibrium OCP [V]"] = (
                 ocp_bulk_eq
             )
+
+            H = self.phase_param.H(sto_surf)
             variables[f"{Domain} electrode {phase_name}OCP hysteresis [V]"] = H
 
             dU = self.phase_param.U(sto_surf, T_bulk).diff(sto_surf)
-            # dQ = variables[f"{Domain} electrode {phase_name}capacity [A.h]"]
             dQ = self.phase_param.Q(sto_surf).diff(sto_surf)
             dQdU = dQ / dU
             variables[
                 f"{Domain} electrode {phase_name}differential capacity [A.s.V-1]"
             ] = dQdU
-            variables[
-                f"{Domain} electrode {phase_name}hysteresis state distribution"
-            ] = h
+
             H_x_av = pybamm.x_average(H)
             h_x_av = pybamm.x_average(h)
             variables[f"X-averaged {domain} electrode {phase_name}hysteresis state"] = (
                 h_x_av
             )
+
             # check if psd
             if domain_options["particle size"] == "distribution":
                 # should always be true
@@ -120,10 +114,13 @@ class DCHSOpenCircuitPotential(BaseOpenCircuitPotential):
             # must not be a psd
             else:
                 ocp_surf = ocp_surf_eq + H * h
+
             H_s_av = pybamm.size_average(H_x_av)
             h_s_av = pybamm.size_average(h_x_av)
 
             ocp_bulk = ocp_bulk_eq + H_s_av * h_s_av
+
+            dUdT = self.phase_param.dUdT(sto_surf)
 
         variables.update(self._get_standard_ocp_variables(ocp_surf, ocp_bulk, dUdT))
         return variables
@@ -132,11 +129,10 @@ class DCHSOpenCircuitPotential(BaseOpenCircuitPotential):
         domain, Domain = self.domain_Domain
         phase_name = self.phase_name
 
-        current = self.param.current_with_time
         current = variables[
             f"{Domain} electrode {phase_name}interfacial current density [A.m-2]"
         ]
-        Q_cell = variables[f"{Domain} electrode capacity [A.h]"]
+        Q_cell = variables[f"{Domain} electrode {phase_name}capacity [A.h]"]
         dQdU = variables[
             f"{Domain} electrode {phase_name}differential capacity [A.s.V-1]"
         ]
