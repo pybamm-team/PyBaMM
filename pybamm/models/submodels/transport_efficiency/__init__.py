d87955be420052391a621c4938a976011bc048ac
arjxn.py <arjxn.py@gmail.com>
2024-05-13 21:13:54 +0530

    Fix breaking import

--- pybamm/models/submodels/transport_efficiency/__init__.py
+++ pybamm/models/submodels/transport_efficiency/__init__.py
@@ -1,5 +1,4 @@
 from .base_transport_efficiency import BaseModel
-from .bruggeman_transport_efficiency import Bruggeman
 from .bruggeman import Bruggeman
 from .cation_exchange_membrane import CationExchangeMembrane
 from .heterogeneous_catalyst import HeterogeneousCatalyst
@@ -9,6 +8,6 @@ from .overlapping_spheres import OverlappingSpheres
 from .random_overlapping_cylinders import RandomOverlappingCylinders
 from .tortuosity_factor import TortuosityFactor
 
-__all__ = ['base_transport_efficiency', 'bruggeman_transport_efficiency', 'bruggeman', 'cation_exchange_membrane',
+__all__ = ['base_transport_efficiency', 'bruggeman', 'cation_exchange_membrane',
            'heterogeneous_catalyst', 'hyperbola_of_revolution', 'ordered_packing', 'overlapping_spheres',
            'random_overlapping_cylinders', 'tortuosity_factor']
