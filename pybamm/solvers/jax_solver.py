8575bf06ddadb6cfc377ffbbc54c2f80e9b8d541
Eric G. Kratz <kratman@users.noreply.github.com>
2024-05-31 14:44:06 -0400

    Update pybamm/solvers/jax_solver.py
    Co-authored-by: Arjun Verma <arjunverma.oc@gmail.com>

--- pybamm/solvers/jax_solver.py
+++ pybamm/solvers/jax_solver.py
@@ -30,7 +30,7 @@ class JaxSolver(pybamm.BaseSolver):
 
     Parameters
     ----------
-    method: str, optional (see :ref:`jax.experimental.ode.odeint` for details)
+    method: str, optional (see `jax.experimental.ode.odeint` for details)
         * 'RK45' (default) uses jax.experimental.ode.odeint
         * 'BDF' uses custom jax_bdf_integrate (see :func:`jax_bdf_integrate.py` for details)
     root_method: str, optional
