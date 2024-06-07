8200b8fbbd6ccd3f56aee020d141e4ef86e6b927
Caitlin D. Parke <caitlin.parke93@gmail.com>
2024-06-07 16:45:39 -0400

    Issue 4123 fix: SEI + Composite Integrations and lithium plating functions updated (#4153)
    * #4123 fix
    
    * Updated plating.py with same bug fix from issue 4123
    
    * Bug fix combined to one line
    
    * style: pre-commit fixes
    
    ---------
    
    Co-authored-by: Caitlin Parke <cat@Caitlins-MacBook-Pro.local>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- pybamm/models/submodels/interface/sei/sei_growth.py
+++ pybamm/models/submodels/interface/sei/sei_growth.py
@@ -71,7 +71,7 @@ class SEIGrowth(BaseModel):
 
         L_inner, L_outer = Ls
 
-        SEI_option = getattr(self.options, domain)["SEI"]
+        SEI_option = getattr(getattr(self.options, domain), self.phase)["SEI"]
         if SEI_option.startswith("ec reaction limited"):
             L_inner = 0 * L_inner  # Set L_inner to zero, copying domains
 
@@ -83,7 +83,7 @@ class SEIGrowth(BaseModel):
         param = self.param
         phase_param = self.phase_param
         domain, Domain = self.domain_Domain
-        SEI_option = getattr(self.options, domain)["SEI"]
+        SEI_option = getattr(getattr(self.options, domain), self.phase)["SEI"]
         T = variables[f"{Domain} electrode temperature [K]"]
         # delta_phi = phi_s - phi_e
         if self.reaction_loc == "interface":
@@ -256,7 +256,7 @@ class SEIGrowth(BaseModel):
         )
 
         # we have to add the spreading rate to account for cracking
-        SEI_option = getattr(self.options, domain)["SEI"]
+        SEI_option = getattr(getattr(self.options, domain), self.phase)["SEI"]
         if SEI_option.startswith("ec reaction limited"):
             self.rhs = {L_outer: -dLdt_SEI_outer + spreading_outer}
         else:
@@ -284,7 +284,7 @@ class SEIGrowth(BaseModel):
         else:
             L_inner_0 = self.phase_param.L_inner_0
             L_outer_0 = self.phase_param.L_outer_0
-        SEI_option = getattr(self.options, domain)["SEI"]
+        SEI_option = getattr(getattr(self.options, domain), self.phase)["SEI"]
         if SEI_option.startswith("ec reaction limited"):
             self.initial_conditions = {L_outer: L_inner_0 + L_outer_0}
         else:
