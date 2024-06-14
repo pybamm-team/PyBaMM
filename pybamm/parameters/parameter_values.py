f1ea8226daabd06f1db072197ae87e5f8430d85b
Brady Planden <brady.planden@gmail.com>
2024-06-14 10:21:01 +0100

    fix: revert logic change for failing 'particle diffusivity' key, updt. check_parameter_values to rename depreciated key

--- pybamm/parameters/parameter_values.py
+++ pybamm/parameters/parameter_values.py
@@ -392,7 +392,7 @@ class ParameterValues:
 
     @staticmethod
     def check_parameter_values(values):
-        for param in values:
+        for param in list(values.keys()):
             if "propotional term" in param:
                 raise ValueError(
                     f"The parameter '{param}' has been renamed to "
@@ -405,12 +405,13 @@ class ParameterValues:
                     f"parameter '{param}' has been renamed to " "'Thermodynamic factor'"
                 )
             if "electrode diffusivity" in param:
+                new_param = param.replace("electrode", "particle")
                 warn(
-                    f"The parameter '{param}' has been renamed to '{param.replace('electrode', 'particle')}'",
+                    f"The parameter '{param}' has been renamed to '{new_param}'",
                     DeprecationWarning,
                     stacklevel=2,
                 )
-                param = param.replace("electrode", "particle")
+                values[new_param] = values.pop(param)
 
         return values
 
