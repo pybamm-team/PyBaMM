5f3ff1da999107fd8e57c653224918b6ac4b0cf8
Pradyot Ranjan <99216956+pradyotRanjan@users.noreply.github.com>
2024-06-15 18:02:02 +0530

    adding  back changes
    Signed-off-by: Pradyot Ranjan <99216956+pradyotRanjan@users.noreply.github.com>

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -19,5 +19,6 @@ class TestExamples:
         return file_list
 
     @pytest.mark.parametrize("files", list_of_files())
+    @pytest.mark.examples
     def test_example_scripts(self, files):
         runpy.run_path(files)
