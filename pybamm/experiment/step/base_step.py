b756ba8a4ec43c1124844e2e0251d79119eb80ca
Valentin Sulzer <valentinsulzer@hotmail.com>
2024-07-03 13:33:04 -0400

    typo

--- pybamm/experiment/step/base_step.py
+++ pybamm/experiment/step/base_step.py
@@ -259,7 +259,7 @@ class BaseStep:
             t = value[:, 0]
             return t[-1]
         else:
-            return 1000 * 3600  # 1000 hours in second
+            return 1000 * 3600  # 1000 hours in seconds
 
     def process_model(self, model, parameter_values):
         new_model = model.new_copy()
