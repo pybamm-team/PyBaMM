6173943e85f8edcb54586652136d8984524eab18
Xinlei <gaoxinlei1997@163.com>
2024-07-03 18:44:20 +0100

    Add Okane2022_posmolar_0703 and revise

--- pybamm/input/parameters/lithium_ion/OKane2022_posmolar_0703.py
+++ pybamm/input/parameters/lithium_ion/OKane2022_posmolar_0703.py
@@ -622,8 +622,8 @@ def get_parameter_values():
         "Positive electrode Young's modulus [Pa]": 375000000000.0,
         "Positive electrode reference concentration for free of deformation [mol.m-3]"
         "": 0.0,
-        "Positive electrode partial molar volume [m3.mol-1]": 1.25e-05,
-#        "Positive electrode partial molar volume [m3.mol-1]": 3.1e-06,
+        ##"Positive electrode partial molar volume [m3.mol-1]": 1.25e-05,
+        "Positive electrode partial molar volume [m3.mol-1]": 1e-6, # updated by Xinlei 2024/7/3
         "Positive electrode volume change": volume_change_Ai2020,
         "Positive electrode initial crack length [m]": 2e-08,
         "Positive electrode initial crack width [m]": 1.5e-08,
