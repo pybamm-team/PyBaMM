2dca1a27b599397a27213a13320ecc33140068b2
Pradyot Ranjan <99216956+prady0t@users.noreply.github.com>
2024-07-08 22:21:24 +0530

    Update conftest.py
    Co-authored-by: Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>

--- conftest.py
+++ conftest.py
@@ -22,7 +22,7 @@ def pytest_addoption(parser):
 
 
 def pytest_configure(config):
-    config.addinivalue_line("markers", "scripts: mark test as an example")
+    config.addinivalue_line("markers", "scripts: mark test as an example script")
     config.addinivalue_line("markers", "unit: mark test as a unit test")
     config.addinivalue_line("markers", "integration: mark test as an integration test")
 
