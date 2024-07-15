d4bd2a341ee2a56ff268b639f80d57028b825421
Pradyot Ranjan <99216956+pradyotRanjan@users.noreply.github.com>
2024-07-15 15:19:49 +0530

    adding reason for skips
    Signed-off-by: Pradyot Ranjan <99216956+pradyotRanjan@users.noreply.github.com>

--- tests/unit/test_models/test_full_battery_models/test_lithium_ion/test_newman_tobias.py
+++ tests/unit/test_models/test_full_battery_models/test_lithium_ion/test_newman_tobias.py
@@ -16,22 +16,22 @@ class TestNewmanTobias(BaseUnitTestLithiumIon):
         with pytest.raises(pybamm.OptionError, match="electrolyte conductivity"):
             pybamm.lithium_ion.NewmanTobias(options)
 
-    @pytest.mark.skip
+    @pytest.mark.skip(reason="Test currently not implemented")
     def test_well_posed_particle_phases(self):
         pass  # skip this test
 
-    @pytest.mark.skip
+    @pytest.mark.skip(reason="Test currently not implemented")
     def test_well_posed_particle_phases_thermal(self):
         pass  # Skip this test
 
-    @pytest.mark.skip
+    @pytest.mark.skip(reason="Test currently not implemented")
     def test_well_posed_particle_phases_sei(self):
         pass  # skip this test
 
-    @pytest.mark.skip
+    @pytest.mark.skip(reason="Test currently not implemented")
     def test_well_posed_composite_kinetic_hysteresis(self):
         pass  # skip this test
 
-    @pytest.mark.skip
+    @pytest.mark.skip(reason="Test currently not implemented")
     def test_well_posed_composite_diffusion_hysteresis(self):
         pass  # skip this test
