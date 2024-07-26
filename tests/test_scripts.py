2b4c07ccd45c674e083025783d9ccc337550c69f
Pradyot Ranjan <99216956+pradyotRanjan@users.noreply.github.com>
2024-07-26 08:27:27 +0530

    removing -s flag and getting file names in scripts log
    Signed-off-by: Pradyot Ranjan <99216956+pradyotRanjan@users.noreply.github.com>

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -14,7 +14,7 @@ class TestExamples:
 
     def list_of_files():
         file_list = (ROOT_DIR / "examples" / "scripts").rglob("*.py")
-        return file_list
+        return [pytest.param(file, id=file.name) for file in file_list]
 
     @pytest.mark.parametrize("files", list_of_files())
     @pytest.mark.scripts
