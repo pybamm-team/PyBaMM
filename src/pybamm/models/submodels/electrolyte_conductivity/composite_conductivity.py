655b0faba4e55383df49e768c904f33163e611a9
Ferran Brosa Planella <Ferran.Brosa-Planella@warwick.ac.uk>
2024-08-09 17:33:01 +0100

    fixed bugs composite conductivity

--- src/pybamm/models/submodels/electrolyte_conductivity/composite_conductivity.py
+++ src/pybamm/models/submodels/electrolyte_conductivity/composite_conductivity.py
@@ -86,24 +86,19 @@ class Composite(BaseElectrolyteConductivity):
 
         # electrolyte potential
         if self.options.electrode_types["negative"] == "planar":
-            phi_e_n_right = variables[
-                "Lithium metal interface electrolyte potential [V]"
-            ]
-            eta_c_n = pybamm.PrimaryBroadcast(pybamm.Scalar(0), "negative electrode")
-            delta_phi_e_n = pybamm.PrimaryBroadcast(
-                pybamm.Scalar(0), "negative electrode"
-            )
+            phi_e_const = variables["Lithium metal interface electrolyte potential [V]"]
+            eta_c_n = pybamm.Scalar(0)
+            delta_phi_e_n = pybamm.Scalar(0)
         else:
             # need to have current evaluated on edge as integral will make it
             # evaluate on node
             x_n_edge = pybamm.standard_spatial_vars.x_n_edge
             i_e_n_edge = i_boundary_cc * x_n_edge / L_n
-            c_e_n_edge = pybamm.FullBroadcastToEdges(
-                c_e_n, broadcast_domains=c_e_n.domains
-            )
 
             eta_c_n = -RT_F_av_n * pybamm.IndefiniteIntegral(
-                param.chi(c_e_n, T_av_n) * c_e_n_edge.diff(x_n) / c_e_n_edge,
+                param.chi(c_e_n, T_av_n)
+                * self._higher_order_macinnes_function(c_e_n).diff(c_e_n)
+                * pybamm.grad(c_e_n),
                 x_n,
             )
 
@@ -118,43 +113,64 @@ class Composite(BaseElectrolyteConductivity):
             )
 
             phi_e_n = phi_e_const - (delta_phi_e_n + eta_c_n)
-            phi_e_n_right = pybamm.boundary_value(phi_e_n, "right")
             phi_e_dict["negative electrode"] = phi_e_n
 
         i_e_s_edge = pybamm.PrimaryBroadcastToEdges(i_boundary_cc, "separator")
-        c_e_s_edge = pybamm.FullBroadcastToEdges(c_e_s, broadcast_domains=c_e_s.domains)
         eta_c_s = -RT_F_av_s * pybamm.IndefiniteIntegral(
-            param.chi(c_e_s, T_av_s) * c_e_s_edge.diff(x_s) / c_e_s_edge,
+            param.chi(c_e_s, T_av_s)
+            * self._higher_order_macinnes_function(c_e_s).diff(c_e_s)
+            * pybamm.grad(c_e_s),
             x_s,
         )
         delta_phi_e_s = pybamm.IndefiniteIntegral(
             i_e_s_edge / (param.kappa_e(c_e_s, T_av_s) * tor_s), x_s
         )
 
-        phi_e_s = phi_e_n_right - (delta_phi_e_s + eta_c_s)
+        phi_e_s = (
+            phi_e_const
+            - pybamm.boundary_value(delta_phi_e_n, "right")
+            - pybamm.boundary_value(eta_c_n, "right")
+            - (delta_phi_e_s + eta_c_s)
+        )
 
         x_p_edge = pybamm.standard_spatial_vars.x_p_edge
         i_e_p_edge = i_boundary_cc * (L_x - x_p_edge) / L_p
-        c_e_p_edge = pybamm.FullBroadcastToEdges(c_e_p, broadcast_domains=c_e_p.domains)
         eta_c_p = -RT_F_av_p * pybamm.IndefiniteIntegral(
-            param.chi(c_e_p, T_av_p) * c_e_p_edge.diff(x_p) / c_e_p_edge,
+            param.chi(c_e_p, T_av_p)
+            * self._higher_order_macinnes_function(c_e_p).diff(c_e_p)
+            * pybamm.grad(c_e_p),
             x_p,
         )
         delta_phi_e_p = pybamm.IndefiniteIntegral(
             i_e_p_edge / (param.kappa_e(c_e_p, T_av_p) * tor_p), x_p
         )
 
-        phi_e_p = pybamm.boundary_value(phi_e_s, "right") - (delta_phi_e_p + eta_c_p)
+        phi_e_p = (
+            phi_e_const
+            - pybamm.boundary_value(delta_phi_e_n, "right")
+            - pybamm.boundary_value(eta_c_n, "right")
+            - pybamm.boundary_value(delta_phi_e_s, "right")
+            - pybamm.boundary_value(eta_c_s, "right")
+            - (delta_phi_e_p + eta_c_p)
+        )
 
         phi_e_dict["separator"] = phi_e_s
         phi_e_dict["positive electrode"] = phi_e_p
 
         # concentration overpotential
-        eta_c_av = pybamm.x_average(eta_c_p) - pybamm.x_average(eta_c_n)
+        eta_c_av = (
+            -pybamm.x_average(eta_c_p)
+            + pybamm.x_average(eta_c_n)
+            - pybamm.boundary_value(eta_c_s, "right")
+            - pybamm.boundary_value(eta_c_n, "right")
+        )
 
         # average electrolyte ohmic losses
-        delta_phi_e_av = -pybamm.x_average(delta_phi_e_p) + pybamm.x_average(
-            delta_phi_e_n
+        delta_phi_e_av = (
+            -pybamm.x_average(delta_phi_e_p)
+            + pybamm.x_average(delta_phi_e_n)
+            - pybamm.boundary_value(delta_phi_e_s, "right")
+            - pybamm.boundary_value(delta_phi_e_n, "right")
         )
 
         variables.update(self._get_standard_potential_variables(phi_e_dict))
@@ -165,3 +181,22 @@ class Composite(BaseElectrolyteConductivity):
         i_e.print_name = "i_e"
 
         return variables
+
+    def set_boundary_conditions(self, variables):
+        if self.options.electrode_types["negative"] == "planar":
+            domains = ["separator", "positive"]
+        else:
+            domains = ["negative", "separator", "positive"]
+
+        for domain in domains:
+            c_e = variables[
+                domain.capitalize() + " electrolyte concentration [mol.m-3]"
+            ]
+            self.boundary_conditions.update(
+                {
+                    c_e: {
+                        "left": (pybamm.boundary_gradient(c_e, "left"), "Neumann"),
+                        "right": (pybamm.boundary_gradient(c_e, "right"), "Neumann"),
+                    }
+                }
+            )
