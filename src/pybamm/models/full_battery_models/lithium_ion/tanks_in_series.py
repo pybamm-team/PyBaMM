e121f328191455ee80bf48a482da23ec2117c8b4
Caitlin Parke <caitlin@ionworks.com>
2024-08-19 19:10:35 -0400

    plotting fixes, still in progress

--- src/pybamm/models/full_battery_models/lithium_ion/tanks_in_series.py
+++ src/pybamm/models/full_battery_models/lithium_ion/tanks_in_series.py
@@ -29,13 +29,13 @@ class TanksInSeries(BaseModel):
         c_s_n = pybamm.Variable("X-averaged negative particle concentration [mol.m-3]")
         c_s_p = pybamm.Variable("X-averaged positive particle concentration [mol.m-3]")
 
-        c_e_n = pybamm.Variable(
+        c_e_av_n = pybamm.Variable(
             "X-averaged negative electrolyte concentration [mol.m-3]"
         )
-        c_e_s = pybamm.Variable(
+        c_e_av_s = pybamm.Variable(
             "X-averaged separator electrolyte concentration [mol.m-3]"
         )
-        c_e_p = pybamm.Variable(
+        c_e_av_p = pybamm.Variable(
             "X-averaged positive electrolyte concentration [mol.m-3]"
         )
 
@@ -53,9 +53,9 @@ class TanksInSeries(BaseModel):
             "X-averaged positive particle concentration gradient [mol.m-4]"
         )
 
-        phi_e_n = pybamm.Variable("X-averaged negative electrolyte potential [V]")
-        phi_e_s = pybamm.Variable("X-averaged separator electrolyte potential [V]")
-        phi_e_p = pybamm.Variable("X-averaged positive electrolyte potential [V]")
+        phi_e_av_n = pybamm.Variable("X-averaged negative electrolyte potential [V]")
+        phi_e_av_s = pybamm.Variable("X-averaged separator electrolyte potential [V]")
+        phi_e_av_p = pybamm.Variable("X-averaged positive electrolyte potential [V]")
 
         phi_s_p = pybamm.Variable("X-averaged positive electrode potential [V]")
         phi_s_n = pybamm.Variable("X-averaged negative electrode potential [V]")
@@ -75,15 +75,15 @@ class TanksInSeries(BaseModel):
 
         # Interfacial reactions
         sto_surf_n = c_surf_ave_n / param.n.prim.c_max
-        j0_n = param.n.prim.j0(c_e_n, c_surf_ave_n, T)
-        eta_n = phi_s_n - phi_e_n - param.n.prim.U(sto_surf_n, T)
+        j0_n = param.n.prim.j0(c_e_av_n, c_surf_ave_n, T)
+        eta_n = phi_s_n - phi_e_av_n - param.n.prim.U(sto_surf_n, T)
         Feta_RT_n = param.F * eta_n / (param.R * T)
         j_n = 2 * j0_n * pybamm.sinh(param.n.prim.ne / 2 * Feta_RT_n)
         pore_wall_flux_n = j_n / param.F
 
         sto_surf_p = c_surf_ave_p / param.p.prim.c_max
-        j0_p = param.p.prim.j0(c_e_p, c_surf_ave_p, T)
-        eta_p = phi_s_p - phi_e_p - param.p.prim.U(sto_surf_p, T)
+        j0_p = param.p.prim.j0(c_e_av_p, c_surf_ave_p, T)
+        eta_p = phi_s_p - phi_e_av_p - param.p.prim.U(sto_surf_p, T)
         Feta_RT_p = param.F * eta_p / (param.R * T)
         j_p = 2 * j0_p * pybamm.sinh(param.p.prim.ne / 2 * Feta_RT_p)
         pore_wall_flux_p = j_p / param.F
@@ -130,13 +130,13 @@ class TanksInSeries(BaseModel):
 
         # 12: boundary between positive electrode and separator
         c_12 = (
-            eps_p**param.p.b_e / param.p.L * c_e_p
-            + eps_sep**param.s.b_e / param.s.L * c_e_s
+            eps_p**param.p.b_e / param.p.L * c_e_av_p
+            + eps_sep**param.s.b_e / param.s.L * c_e_av_s
         ) / (eps_p**param.p.b_e / param.p.L + eps_sep**param.s.b_e / param.s.L)
         # 23: boundary between separator and negative electrode
         c_23 = (
-            eps_n**param.n.b_e / param.n.L * c_e_n
-            + eps_sep**param.s.b_e / param.s.L * c_e_s
+            eps_n**param.n.b_e / param.n.L * c_e_av_n
+            + eps_sep**param.s.b_e / param.s.L * c_e_av_s
         ) / (eps_n**param.n.b_e / param.n.L + eps_sep**param.s.b_e / param.s.L)
 
         # Grouped thickness/porosity variables
@@ -149,22 +149,30 @@ class TanksInSeries(BaseModel):
         # Electrolyte concentration
         ######################
 
-        self.rhs[c_e_n] = (
-            -2 * param.D_e(c_23, T) * (c_e_n - c_e_s) / leps23 / (eps_n * param.n.L)
-            - (1 - param.t_plus(c_e_n, T)) * iapp / param.F / eps_n / param.n.L
+        self.rhs[c_e_av_n] = (
+            -2
+            * param.D_e(c_23, T)
+            * (c_e_av_n - c_e_av_s)
+            / leps23
+            / (eps_n * param.n.L)
+            - (1 - param.t_plus(c_e_av_n, T)) * iapp / param.F / eps_n / param.n.L
         )
-        self.rhs[c_e_s] = (
-            -2 * param.D_e(c_12, T) * (c_e_s - c_e_p) / leps12
-            + 2 * param.D_e(c_23, T) * (c_e_n - c_e_s) / leps23
+        self.rhs[c_e_av_s] = (
+            -2 * param.D_e(c_12, T) * (c_e_av_s - c_e_av_p) / leps12
+            + 2 * param.D_e(c_23, T) * (c_e_av_n - c_e_av_s) / leps23
         ) / (eps_sep * param.s.L)
-        self.rhs[c_e_p] = (
-            2 * param.D_e(c_12, T) * (c_e_s - c_e_p) / leps12 / (eps_p * param.p.L)
-            + (1 - param.t_plus(c_e_p, T)) * iapp / param.F / eps_p / param.p.L
+        self.rhs[c_e_av_p] = (
+            2
+            * param.D_e(c_12, T)
+            * (c_e_av_s - c_e_av_p)
+            / leps12
+            / (eps_p * param.p.L)
+            + (1 - param.t_plus(c_e_av_p, T)) * iapp / param.F / eps_p / param.p.L
         )
 
-        self.initial_conditions[c_e_n] = param.c_e_init
-        self.initial_conditions[c_e_s] = param.c_e_init
-        self.initial_conditions[c_e_p] = param.c_e_init
+        self.initial_conditions[c_e_av_n] = param.c_e_init
+        self.initial_conditions[c_e_av_s] = param.c_e_init
+        self.initial_conditions[c_e_av_p] = param.c_e_init
 
         ######################
         # Algebraic Variables
@@ -198,21 +206,21 @@ class TanksInSeries(BaseModel):
         # Current in the electrolyte
         ######################
 
-        self.algebraic[phi_e_p] = (
+        self.algebraic[phi_e_av_p] = (
             -iapp
-            - 2 * param.kappa_e(c_12, T) * (phi_e_s - phi_e_p) / leps12
-            + 2 * param.chiRT_over_Fc(c_12, T) * (c_e_s - c_e_p) / leps12
+            - 2 * param.kappa_e(c_12, T) * (phi_e_av_s - phi_e_av_p) / leps12
+            + 2 * param.chiRT_over_Fc(c_12, T) * (c_e_av_s - c_e_av_p) / leps12
         )
-        self.algebraic[phi_e_n] = (
+        self.algebraic[phi_e_av_n] = (
             -iapp
-            - 2 * param.kappa_e(c_23, T) * (phi_e_n - phi_e_s) / leps23
-            + 2 * param.chiRT_over_Fc(c_23, T) * (c_e_n - c_e_s) / leps23
+            - 2 * param.kappa_e(c_23, T) * (phi_e_av_n - phi_e_av_s) / leps23
+            + 2 * param.chiRT_over_Fc(c_23, T) * (c_e_av_n - c_e_av_s) / leps23
         )
-        self.algebraic[phi_e_s] = iapp / param.F / a_n / param.n.L + pore_wall_flux_n
+        self.algebraic[phi_e_av_s] = iapp / param.F / a_n / param.n.L + pore_wall_flux_n
 
-        self.initial_conditions[phi_e_p] = pybamm.Scalar(0)
-        self.initial_conditions[phi_e_n] = pybamm.Scalar(0)
-        self.initial_conditions[phi_e_s] = pybamm.Scalar(0)
+        self.initial_conditions[phi_e_av_p] = pybamm.Scalar(0)
+        self.initial_conditions[phi_e_av_n] = pybamm.Scalar(0)
+        self.initial_conditions[phi_e_av_s] = pybamm.Scalar(0)
 
         ######################
         # Current in the solid
@@ -237,20 +245,93 @@ class TanksInSeries(BaseModel):
         )
 
         # The `variables` dictionary contains all variables that might be useful for
-        # visualising the solution of the model
+        # visualising the solution of the model.
+        # To compare the tanks-in-series model with other models,
+        # the average variables are broadcast to the x or r domain of relevance.
+
+        # Broadcasting the electrolyte concentration for compatibility with built-in plotting
+        c_e_n = pybamm.Variable("Negative electrolyte concentration [mol.m-3]")
+        c_e_n = pybamm.PrimaryBroadcast(c_e_av_n, "negative electrode")
+        c_e_s = pybamm.Variable("Separator electrolyte concentration [mol.m-3]")
+        c_e_s = pybamm.PrimaryBroadcast(c_e_av_s, "separator")
+        c_e_p = pybamm.Variable("Positive electrolyte concentration [mol.m-3]")
+        c_e_p = pybamm.PrimaryBroadcast(c_e_av_p, "positive electrode")
+        c_e = pybamm.concatenation(c_e_n, c_e_s, c_e_p)
+
+        # Electrolyte potential
+        # for plotting from average to broadcasted values
+
+        phi_e_n = pybamm.Variable("Negative electrolyte potential [V]")
+        phi_e_n = pybamm.PrimaryBroadcast(phi_e_av_n, "negative electrode")
+        phi_e_s = pybamm.Variable("Separator electrolyte potential [V]")
+        phi_e_s = pybamm.PrimaryBroadcast(phi_e_av_s, "separator")
+        phi_e_p = pybamm.Variable("Positive electrolyte potential [V]")
+        phi_e_p = pybamm.PrimaryBroadcast(phi_e_av_p, "positive electrode")
+        phi_e = pybamm.concatenation(phi_e_n, phi_e_s, phi_e_p)
+        #
+        # {"Negative electrolyte "
+        #     "potential [V]": pybamm.PrimaryBroadcast(
+        #         phi_e_av_n, "negative electrode"
+        #     )}
+        # phi_e = pybamm.concatenation(
+        #     {"Negative electrolyte "
+        #     "potential [V]": pybamm.PrimaryBroadcast(
+        #         phi_e_av_n, "negative electrode"
+        #     )},
+        #     {"Positive electrolyte "
+        #     "potential [V]": pybamm.PrimaryBroadcast(
+        #         phi_e_av_p, "positive electrode"
+        #     )},
+        #     {"Separator electrolyte "
+        #     "potential [V]": pybamm.PrimaryBroadcast(
+        #         phi_e_av_s, "separator"
+        #     )}
+        # )
+
         self.variables = {
-            "X-averaged negative electrolyte concentration [mol.m-3]": c_e_n,
-            "X-averaged positive electrolyte concentration [mol.m-3]": c_e_p,
-            "X-averaged separator electrolyte concentration [mol.m-3]": c_e_s,
+            # 'Negative particle concentration [mol.m-3]': pybamm.FullBroadcast(
+            #     c_s_n, 'negative particle', 'negative electrode'
+            # ),
+            "Electrolyte concentration [mol.m-3]": c_e,
+            "Negative electrolyte concentration [mol.m-3]": c_e_n,
+            "Separator electrolyte concentration [mol.m-3]": c_e_s,
+            "Positive electrolyte concentration [mol.m-3]": c_e_p,
+            "Negative particle surface "
+            "concentration [mol.m-3]": pybamm.PrimaryBroadcast(
+                c_surf_ave_n, "negative electrode"
+            ),
+            "Positive particle surface "
+            "concentration [mol.m-3]": pybamm.PrimaryBroadcast(
+                c_surf_ave_p, "positive electrode"
+            ),
+            "Electrolyte potential [V]": phi_e,
+            "Negative electrolyte potential [V]": phi_e_n,
+            "Separator electrolyte potential [V]": phi_e_s,
+            "Positive electrolyte potential [V]": phi_e_p,
+            # "Negative electrolyte "
+            # "potential [V]": pybamm.PrimaryBroadcast(
+            #     phi_e_av_n, "negative electrode"
+            # ),
+            # "Positive electrolyte "
+            # "potential [V]": pybamm.PrimaryBroadcast(
+            #     phi_e_av_p, "positive electrode"
+            # ),
+            # "Separator electrolyte "
+            # "potential [V]": pybamm.PrimaryBroadcast(
+            #     phi_e_av_s, "separator"
+            # ),
+            # "X-averaged negative electrolyte concentration [mol.m-3]": c_e_av_n,
+            # "X-averaged positive electrolyte concentration [mol.m-3]": c_e_av_p,
+            # "X-averaged separator electrolyte concentration [mol.m-3]": c_e_av_s,
             "X-averaged negative particle concentration [mol.m-3]": c_s_n,
             "X-averaged positive particle concentration [mol.m-3]": c_s_p,
-            "X-averaged negative particle surface concentration [mol.m-3]": c_surf_ave_n,
-            "X-averaged positive particle surface concentration [mol.m-3]": c_surf_ave_p,
+            # "X-averaged negative particle surface concentration [mol.m-3]": c_surf_ave_n,
+            # "X-averaged positive particle surface concentration [mol.m-3]": c_surf_ave_p,
             "X-averaged negative particle concentration gradient [mol.m-4]": q_ave_n,
             "X-averaged positive particle concentration gradient [mol.m-4]": q_ave_p,
-            "X-averaged positive electrolyte potential [V]": phi_e_p,
-            "X-averaged negative electrolyte potential [V]": phi_e_n,
-            "X-averaged separator electrolyte potential [V]": phi_e_s,
+            # "X-averaged positive electrolyte potential [V]": phi_e_av_p,
+            # "X-averaged negative electrolyte potential [V]": phi_e_av_n,
+            # "X-averaged separator electrolyte potential [V]": phi_e_av_s,
             "X-averaged positive electrode potential [V]": phi_s_p,
             "X-averaged negative electrode potential [V]": phi_s_n,
             "Current [A]": I,
