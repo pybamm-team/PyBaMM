5d7bac4a8626ea9c0ccd4a860463e90e53712722
kratman <kratmandu@gmail.com>
2024-08-20 13:57:01 -0400

    Add new base class

---
+++ src/pybamm/input/parameters/base_parameter_set.py
@@ -0,0 +1,25 @@
+
+class AbstractBaseParameters:
+    _details = {}
+    _sei = {}
+    _cell = {}
+    _negative_electrode = {}
+    _positive_electrode = {}
+    _seperator = {}
+    _electrolyte = {}
+    _experiment = {}
+
+    def get_param_set(self):
+        full_set = {}
+        for sub_set in [
+            self._details,
+            self._sei,
+            self._cell,
+            self._negative_electrode,
+            self._positive_electrode,
+            self._seperator,
+            self._electrolyte,
+            self._experiment,
+        ]:
+            full_set.update(sub_set)
+        return full_set
