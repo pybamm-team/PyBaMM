9a996c4006e2cf94ab9f39f0e96f7813f710d0bb
Caitlin Parke <caitlin@ionworks.com>
2024-08-21 16:46:28 -0400

    updated for pytest and better coverage

--- tests/unit/test_models/test_full_battery_models/test_lithium_ion/test_ecm_split_OCV.py
+++ tests/unit/test_models/test_full_battery_models/test_lithium_ion/test_ecm_split_OCV.py
@@ -8,3 +8,8 @@ class TestECMSplitOCV:
     def test_ecmsplitocv_well_posed(self):
         model = pybamm.lithium_ion.ECMsplitOCV()
         model.check_well_posedness()
+
+    def test_get_default_quick_plot_variables(self):
+        model = pybamm.lithium_ion.ECMsplitOCV()
+        variables = model.default_quick_plot_variables
+        assert "Current [A]" in variables
