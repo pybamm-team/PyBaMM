7ec445bbcb6a890129c6d2e170e41822100341c0
cringeyburger <medaankit18@gmail.com>
2024-08-24 01:00:00 +0530

    feat: add new `.bat` script to set environment variables permanently

---
+++ scripts/windows_setup.bat
@@ -0,0 +1,11 @@
+@echo off
+
+echo Setting environment variables...
+
+setx PYBAMM_USE_VCPKG ON
+setx VCPKG_ROOT_DIR C:\vcpkg
+setx VCPKG_DEFAULT_TRIPLET x64-windows-static-md
+setx VCPKG_FEATURE_FLAGS manifests,registries
+
+echo Environment variables have been set. Please open a new Command Prompt to see the changes.
+pause
