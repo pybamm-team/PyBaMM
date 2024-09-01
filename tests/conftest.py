d1e6a5a0938fee19fd6c61268930291476a8a40e
Pradyot Ranjan <99216956+pradyotRanjan@users.noreply.github.com>
2024-09-01 18:38:06 +0530

    Trying to remove shared.py
    Signed-off-by: Pradyot Ranjan <99216956+pradyotRanjan@users.noreply.github.com>

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+from tests.shared import (
+    SpatialMethodForTesting,
+    get_mesh_for_testing,
+)
+import pybamm
+
+
+@pytest.fixture(scope="module")
+def get_discretisation_for_testing(
+        xpts=None, rpts=10, mesh=None, cc_method=SpatialMethodForTesting
+):
+    if mesh is None:
+        mesh = get_mesh_for_testing(xpts=xpts, rpts=rpts)
+    spatial_methods = {
+        "macroscale": SpatialMethodForTesting(),
+        "negative particle": SpatialMethodForTesting(),
+        "positive particle": SpatialMethodForTesting(),
+        "negative particle size": SpatialMethodForTesting(),
+        "positive particle size": SpatialMethodForTesting(),
+        "current collector": cc_method(),
+    }
+    return pybamm.Discretisation(mesh, spatial_methods)
