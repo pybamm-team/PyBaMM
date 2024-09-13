568f9f87d8d0e7ab4750e475acfbf1af570613c9
Pradyot Ranjan <99216956+prady0t@users.noreply.github.com>
2024-09-13 15:41:33 +0530

    Update tests/unit/test_parameters/test_process_parameter_data.py
    Co-authored-by: Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>

--- tests/unit/test_parameters/test_process_parameter_data.py
+++ tests/unit/test_parameters/test_process_parameter_data.py
@@ -41,7 +41,7 @@ class TestProcessParameterData:
         assert isinstance(processed[1][0][1], np.ndarray)
         assert isinstance(processed[1][1], np.ndarray)
 
-    def test_process_3_d_data_csv(self):
+    def test_process_3D_data_csv(self):
         name = "data_for_testing_3D"
         path = os.path.abspath(os.path.dirname(__file__))
         processed = pybamm.parameters.process_3D_data_csv(name, path)
