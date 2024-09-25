0819a7f734422d08761916cd316c644dbfd8272a
Arjun Verma <arjunverma.oc@gmail.com>
2024-09-25 21:54:39 +0530

    Update tests/unit/test_parameters/test_parameter_values.py

--- tests/unit/test_parameters/test_parameter_values.py
+++ tests/unit/test_parameters/test_parameter_values.py
@@ -1049,4 +1049,4 @@ class TestParameterValues:
             values={"Negative particle radius [m]": 1e-6}
         )
         pv = [i for i in parameter_values]
-        assert len(pv) == 5, "Should have 5 keys "
+        assert len(pv) == 5, "Should have 5 keys"
