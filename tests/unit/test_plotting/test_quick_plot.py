e387495ccf4ad842673889df48579ef74b733e94
Marc Berliner <34451391+MarcBerliner@users.noreply.github.com>
2024-10-10 12:24:39 -0400

    move `_solver_args` inside class

--- tests/unit/test_plotting/test_quick_plot.py
+++ tests/unit/test_plotting/test_quick_plot.py
@@ -5,12 +5,12 @@ import pytest
 import numpy as np
 from tempfile import TemporaryDirectory
 
-_solver_args = [pybamm.CasadiSolver()]
-if pybamm.has_idaklu():
-    _solver_args.append(pybamm.IDAKLUSolver())
-
 
 class TestQuickPlot:
+    _solver_args = [pybamm.CasadiSolver()]
+    if pybamm.has_idaklu():
+        _solver_args.append(pybamm.IDAKLUSolver())
+
     @pytest.mark.parametrize("solver", _solver_args)
     def test_simple_ode_model(self, solver):
         model = pybamm.lithium_ion.BaseModel(name="Simple ODE Model")
