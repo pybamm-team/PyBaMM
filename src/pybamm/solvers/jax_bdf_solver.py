017a92723865418f90b59c94a7ee4aca2d49a7ca
kratman <kratmandu@gmail.com>
2024-10-11 11:42:12 -0400

    Fix other jax links

--- src/pybamm/solvers/jax_bdf_solver.py
+++ src/pybamm/solvers/jax_bdf_solver.py
@@ -711,7 +711,7 @@ if pybamm.has_jax():
         return onp.block(blocks)
 
     # NOTE: the code below (except the docstring on jax_bdf_integrate and other minor
-    # edits), has been modified from the JAX library at https://github.com/google/jax.
+    # edits), has been modified from the JAX library at https://github.com/jax-ml/jax.
     # The main difference is the addition of support for semi-explicit dae index 1
     # problems via the addition of a mass matrix.
     # This is under an Apache license, a short form of which is given here:
