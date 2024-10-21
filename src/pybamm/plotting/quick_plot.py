173d514901d480879877baa58a20a0904b124cd8
medha-14 <medha140904@gmail.com>
2024-10-21 18:48:37 +0530

    added different colours to different time plots

--- src/pybamm/plotting/quick_plot.py
+++ src/pybamm/plotting/quick_plot.py
@@ -313,11 +313,11 @@ class QuickPlot:
             first_variable = first_solution[0]
             domain = first_variable.domain
             # check all other solutions against the first solution
-            for idx, variable in enumerate(first_solution):
+            for _idx, variable in enumerate(first_solution):
                 if variable.domain != domain:
                     raise ValueError(
                         "Mismatching variable domains. "
-                        f"'{variable_tuple[0]}' has domain '{domain}', but '{variable_tuple[idx]}' has domain '{variable.domain}'"
+                        f"'{variable_tuple[0]}' has domain '{domain}', but '{variable_tuple[_idx]}' has domain '{variable.domain}'"
                     )
                 self.spatial_variable_dict[variable_tuple] = {}
 
@@ -480,7 +480,7 @@ class QuickPlot:
                 raise ValueError(f"Axis limits cannot be NaN for variables '{key}'")
 
     def plot(self, t, dynamic=False):
-        """Produces a quick plot with the internal states at time t or a list of times.
+        """Produces a quick plot with the internal states at time t.
 
         Parameters
         ----------
@@ -493,20 +493,12 @@ class QuickPlot:
 
         plt = import_optional_dependency("matplotlib.pyplot")
         gridspec = import_optional_dependency("matplotlib.gridspec")
-        cm = import_optional_dependency("matplotlib", "cm")
-        colors = import_optional_dependency("matplotlib", "colors")
 
-        # Check if `t` is a float (single time) or a list (multiple times)
         if isinstance(t, float):
-            t_list = [t]
-        elif isinstance(t, list):
-            t_list = t
-        else:
-            raise ValueError("t should be either a float or a list of floats")
-
-        t_in_seconds_list = [time * self.time_scaling_factor for time in t_list]
+            t = [t]
 
         self.fig = plt.figure(figsize=self.figsize)
+
         self.gridspec = gridspec.GridSpec(self.n_rows, self.n_cols)
         self.plots = {}
         self.time_lines = {}
@@ -515,6 +507,11 @@ class QuickPlot:
 
         # initialize empty handles, to be created only if the appropriate plots are made
         solution_handles = []
+
+        # Generate distinct colors for each time point
+        time_colors = plt.cm.coolwarm(
+            np.linspace(0, 1, len(t))
+        )  # Use a colormap for distinct colors
 
         for k, (key, variable_lists) in enumerate(self.variables.items()):
             ax = self.fig.add_subplot(self.gridspec[k])
@@ -526,158 +523,127 @@ class QuickPlot:
             ax.xaxis.set_major_locator(plt.MaxNLocator(3))
             self.plots[key] = defaultdict(dict)
             variable_handles = []
-            # Set labels for the first subplot only (avoid repetition)
+
             if variable_lists[0][0].dimensions == 0:
-                # 0D plot: plot as a function of time, indicating time t with a single line
+                # 0D plot: plot as a function of time, indicating multiple times with lines
                 ax.set_xlabel(f"Time [{self.time_unit}]")
                 for i, variable_list in enumerate(variable_lists):
                     for j, variable in enumerate(variable_list):
-                        if len(variable_list) == 1:
-                            # single variable -> use linestyle to differentiate model
-                            linestyle = self.linestyles[i]
-                        else:
-                            # multiple variables -> use linestyle to differentiate
-                            # variables (color differentiates models)
-                            linestyle = self.linestyles[j]
+                        linestyle = (
+                            self.linestyles[i]
+                            if len(variable_list) == 1
+                            else self.linestyles[j]
+                        )
                         full_t = self.ts_seconds[i]
                         (self.plots[key][i][j],) = ax.plot(
                             full_t / self.time_scaling_factor,
                             variable(full_t),
-                            color=self.colors[i],
+                            color=self.colors[i],  # Different colors for plot lines
                             linestyle=linestyle,
                         )
                         variable_handles.append(self.plots[key][0][j])
                     solution_handles.append(self.plots[key][i][0])
-
                 y_min, y_max = ax.get_ylim()
                 ax.set_ylim(y_min, y_max)
 
-                # Plot a single vertical time line for the first time `t`
-                (self.time_lines[key],) = ax.plot(
-                    [t_in_seconds_list[0] / self.time_scaling_factor] * 2,
-                    [y_min, y_max],
-                    "k--",
-                    lw=1.5,
-                )
+                # Add vertical lines for each time in the list, using different colors for each time
+                for _idx, t_single in enumerate(t):
+                    t_in_seconds = t_single * self.time_scaling_factor
+                    (self.time_lines[key],) = ax.plot(
+                        [
+                            t_in_seconds / self.time_scaling_factor,
+                            t_in_seconds / self.time_scaling_factor,
+                        ],
+                        [y_min, y_max],
+                        "--",  # Dashed lines
+                        lw=1.5,
+                        color=time_colors[_idx],  # Different color for each time
+                        label=f"t = {t_single:.2f} {self.time_unit}",
+                    )
+                ax.legend()
 
             elif variable_lists[0][0].dimensions == 1:
-                # 1D plot: plot as a function of x at time t
-                # Read dictionary of spatial variables
+                # 1D plot: plot as a function of x at different times
                 spatial_vars = self.spatial_variable_dict[key]
                 spatial_var_name = next(iter(spatial_vars.keys()))
                 ax.set_xlabel(f"{spatial_var_name} [{self.spatial_unit}]")
 
-                # Plot the variable at each time in the list
-                for i, variable_list in enumerate(variable_lists):
-                    for j, variable in enumerate(variable_list):
-                        if len(variable_list) == 1:
-                            # single variable -> use linestyle to differentiate model
-                            linestyle = self.linestyles[i]
-                        else:
-                            # multiple variables -> use linestyle to differentiate
-                            # variables (color differentiates models)
-                            linestyle = self.linestyles[j]
+                for _idx, t_single in enumerate(t):
+                    t_in_seconds = t_single * self.time_scaling_factor
 
-                        for t_in_seconds in t_in_seconds_list:
+                    for i, variable_list in enumerate(variable_lists):
+                        for j, variable in enumerate(variable_list):
+                            linestyle = (
+                                self.linestyles[i]
+                                if len(variable_list) == 1
+                                else self.linestyles[j]
+                            )
                             (self.plots[key][i][j],) = ax.plot(
                                 self.first_spatial_variable[key],
                                 variable(t_in_seconds, **spatial_vars),
-                                color=self.colors[i],
+                                color=time_colors[
+                                    _idx
+                                ],  # Different color for each time
                                 linestyle=linestyle,
+                                label=f"t = {t_single:.2f} {self.time_unit}",  # Add time label
                                 zorder=10,
-                                label=f"time = {t_in_seconds / self.time_scaling_factor} {self.time_unit}",
                             )
                             variable_handles.append(self.plots[key][0][j])
-                    solution_handles.append(self.plots[key][i][0])
+                        solution_handles.append(self.plots[key][i][0])
 
-                # Add lines for boundaries between subdomains
-                for boundary in variable_lists[0][0].internal_boundaries:
-                    boundary_scaled = boundary * self.spatial_factor
-                    ax.axvline(boundary_scaled, color="0.5", lw=1, zorder=0)
+                # Add a legend to indicate which plot corresponds to which time
+                ax.legend()
 
             elif variable_lists[0][0].dimensions == 2:
-                # 2D variables do not support plotting for multiple times
-                if len(t_list) > 1:
-                    raise ValueError("Cannot plot 2D variables for multiple times")
-
-                # Continue with 2D plotting as before for a single time
+                # 2D plot: superimpose plots at different times
                 spatial_vars = self.spatial_variable_dict[key]
                 variable = variable_lists[0][0]
-                if self.x_first_and_y_second[key] is False:
-                    x_name = list(spatial_vars.keys())[1][0]
-                    y_name = next(iter(spatial_vars.keys()))[0]
-                    x = self.second_spatial_variable[key]
-                    y = self.first_spatial_variable[key]
-                    var = variable(t_in_seconds_list[0], **spatial_vars)
-                else:
-                    x_name = next(iter(spatial_vars.keys()))[0]
-                    y_name = list(spatial_vars.keys())[1][0]
+
+                for _idx, t_single in enumerate(t):
+                    t_in_seconds = t_single * self.time_scaling_factor
                     x = self.first_spatial_variable[key]
                     y = self.second_spatial_variable[key]
-                    var = variable(t_in_seconds_list[0], **spatial_vars).T
-                ax.set_xlabel(f"{x_name} [{self.spatial_unit}]")
-                ax.set_ylabel(f"{y_name} [{self.spatial_unit}]")
-                vmin, vmax = self.variable_limits[key]
-                # store the plot and the var data (for testing) as cant access
-                # z data from QuadMesh or QuadContourSet object
-                if self.is_y_z[key] is True:
-                    self.plots[key][0][0] = ax.pcolormesh(
-                        x,
-                        y,
-                        var,
-                        vmin=vmin,
-                        vmax=vmax,
-                        shading=self.shading,
-                    )
-                else:
-                    self.plots[key][0][0] = ax.contourf(
-                        x, y, var, levels=100, vmin=vmin, vmax=vmax
-                    )
-                self.plots[key][0][1] = var
-                self.colorbars[key] = self.fig.colorbar(
-                    cm.ScalarMappable(colors.Normalize(vmin=vmin, vmax=vmax)),
-                    ax=ax,
-                )
+                    var = variable(t_in_seconds, **spatial_vars).T
 
-            # Set either y label or legend entries
-            if len(key) == 1:
-                title = split_long_string(key[0])
-                ax.set_title(title, fontsize="medium")
-            else:
-                ax.legend(
-                    variable_handles,
-                    [split_long_string(s, 6) for s in key],
-                    bbox_to_anchor=(0.5, 1),
-                    loc="lower center",
-                )
+                    ax.set_xlabel(
+                        f"{next(iter(spatial_vars.keys()))[0]} [{self.spatial_unit}]"
+                    )
+                    ax.set_ylabel(
+                        f"{list(spatial_vars.keys())[1][0]} [{self.spatial_unit}]"
+                    )
+                    vmin, vmax = self.variable_limits[key]
 
-        # Set global legend
+                    # Use contourf and colorbars to represent the values
+                    contour_plot = ax.contourf(
+                        x, y, var, levels=100, vmin=vmin, vmax=vmax, cmap="coolwarm"
+                    )
+                    self.plots[key][0][0] = contour_plot
+                    self.colorbars[key] = self.fig.colorbar(contour_plot, ax=ax)
+
+                    # Add time to the colorbar title or axis title to indicate which time it represents
+                    ax.set_title(f"t = {t_single:.2f} {self.time_unit}")
+
+        # Set global legend if there are multiple models
         if len(self.labels) > 1:
             fig_legend = self.fig.legend(
                 solution_handles, self.labels, loc="lower right"
             )
-            # Get the position of the top of the legend in relative figure units
-            # There may be a better way ...
-            try:
-                legend_top_inches = fig_legend.get_window_extent(
-                    renderer=self.fig.canvas.get_renderer()
-                ).get_points()[1, 1]
-                fig_height_inches = (self.fig.get_size_inches() * self.fig.dpi)[1]
-                legend_top = legend_top_inches / fig_height_inches
-            except AttributeError:  # pragma: no cover
-                # When testing the examples we set the matplotlib backend to "Template"
-                # which means that the above code doesn't work. Since this is just for
-                # that particular test we can just skip it
-                legend_top = 0
         else:
-            legend_top = 0
+            fig_legend = None
 
-        # Fix layout
+        # Fix layout for sliders if dynamic
         if dynamic:
             slider_top = 0.05
         else:
             slider_top = 0
-        bottom = max(legend_top, slider_top)
+        bottom = max(
+            fig_legend.get_window_extent(
+                renderer=self.fig.canvas.get_renderer()
+            ).get_points()[1, 1]
+            if fig_legend
+            else 0,
+            slider_top,
+        )
         self.gridspec.tight_layout(self.fig, rect=[0, bottom, 1, 1])
 
     def dynamic_plot(self, show_plot=True, step=None):
