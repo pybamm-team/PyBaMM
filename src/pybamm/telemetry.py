b739a62afd9e27fe649d6e894a988410f4358686
kratman <kratmandu@gmail.com>
2024-11-14 21:05:41 -0500

    Mock telemetry object

--- src/pybamm/telemetry.py
+++ src/pybamm/telemetry.py
@@ -3,21 +3,45 @@ import os
 import pybamm
 import sys
 
-_posthog = Posthog(
-    # this is the public, write only API key, so it's ok to include it here
-    project_api_key="phc_bLZKBW03XjgiRhbWnPsnKPr0iw0z03fA6ZZYjxgW7ej",
-    host="https://us.i.posthog.com",
-)
+
+def check_opt_out():
+    opt_env_out = os.getenv("PYBAMM_DISABLE_TELEMETRY", "false").lower() != "false"
+    config_opt_out = True
+    config = pybamm.config.read()
+    if config:
+        config_opt_out = config["enable_telemetry"]
+    return opt_env_out or config_opt_out
+
+
+class MockTelemetry:
+    def __init__(self):
+        class MockLog:
+            @staticmethod
+            def setLevel(_: str):
+                pass
+
+        self.disabled = True
+        self.log = MockLog()
+
+    @staticmethod
+    def capture(**kwargs):
+        pass
+
+
+if check_opt_out():
+    _posthog = MockTelemetry()
+else:
+    _posthog = Posthog(
+        # this is the public, write only API key, so it's ok to include it here
+        project_api_key="phc_bLZKBW03XjgiRhbWnPsnKPr0iw0z03fA6ZZYjxgW7ej",
+        host="https://us.i.posthog.com",
+    )
 
 _posthog.log.setLevel("CRITICAL")
 
 
 def disable():
     _posthog.disabled = True
-
-
-def check_opt_out():
-    return os.getenv("PYBAMM_DISABLE_TELEMETRY", "false").lower() != "false"
 
 
 if check_opt_out():  # pragma: no cover
@@ -33,13 +57,11 @@ def capture(event):  # pragma: no cover
         disable()
         return
 
-    properties = {
-        "python_version": sys.version,
-        "pybamm_version": pybamm.__version__,
-    }
-
     config = pybamm.config.read()
     if config:
-        if config["enable_telemetry"]:
-            user_id = config["uuid"]
-            _posthog.capture(user_id, event, properties=properties)
+        properties = {
+            "python_version": sys.version,
+            "pybamm_version": pybamm.__version__,
+        }
+        user_id = config["uuid"]
+        _posthog.capture(user_id, event, properties=properties)
