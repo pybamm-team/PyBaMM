006f3c5748808b431446863231ee8a06ab1967d9
Alec Bills <48105066+aabills@users.noreply.github.com>
2025-01-02 13:12:18 -0800

    Geometric input params (#4665)
    adds a symbolic mesh which enables geometric parameters to be used as `pybamm.InputParameter`

--- tests/unit/test_parameters/test_parameter_values.py
+++ tests/unit/test_parameters/test_parameter_values.py
@@ -914,12 +914,6 @@ class TestParameterValues:
         exp_param = param.process_symbol(expression)
         assert exp_param == 3.0 * (2.0**var2) / ((-4.0 + var1) + var2)
 
-    def test_process_geometry(self):
-        var = pybamm.Variable("var")
-        geometry = {"negative electrode": {"x": {"min": 0, "max": var}}}
-        with pytest.raises(ValueError, match="Geometry parameters must be Scalars"):
-            pybamm.ParameterValues({}).process_geometry(geometry)
-
     def test_process_model(self):
         model = pybamm.BaseModel()
         a = pybamm.Parameter("a")
@@ -976,6 +970,12 @@ class TestParameterValues:
         model.boundary_conditions = {var1: {"left": (x, "Dirichlet")}}
         with pytest.raises(KeyError):
             parameter_values.process_model(model)
+
+    def test_process_geometry(self):
+        var = pybamm.Variable("var")
+        geometry = {"negative electrode": {"x": {"min": 0, "max": var}}}
+        with pytest.raises(ValueError, match="Geometry parameters must be Scalars"):
+            pybamm.ParameterValues({}).process_geometry(geometry)
 
     def test_inplace(self):
         model = pybamm.lithium_ion.SPM()
