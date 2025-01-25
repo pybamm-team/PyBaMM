5e6678bb691bb383e8ae5746eb5ca7f251a73d4e
R Aswin <135364633+Aswinr24@users.noreply.github.com>
2025-01-25 21:52:32 +0530

    chore: switch to np.testing.assert_allclose for better precision in tests (#4789)
    * chore: switch to np.testing.assert_allclose for better precision in tests
    
    * fix: adjust tolerance values in upwind/downwind tests
    
    * chore: switch to np.testing.assert_allclose for better precision in tests-v2
    
    * fix: replace 10** notation with exponential notation for consistency
    
    * Fix: adjust absolute tolerance to a consistent value of 1e-6
    
    * Retrigger CI

--- tests/integration/test_models/test_full_battery_models/test_lithium_ion/base_lithium_ion_tests.py
+++ tests/integration/test_models/test_full_battery_models/test_lithium_ion/base_lithium_ion_tests.py
@@ -45,11 +45,11 @@ class BaseIntegrationTestLithiumIon:
 
         original = optimtest.evaluate_model()
         to_python = optimtest.evaluate_model(to_python=True)
-        np.testing.assert_array_almost_equal(original, to_python)
+        np.testing.assert_allclose(original, to_python, rtol=1e-7, atol=1e-7)
 
         if pybamm.has_jax():
             to_jax = optimtest.evaluate_model(to_jax=True)
-            np.testing.assert_array_almost_equal(original, to_jax)
+            np.testing.assert_allclose(original, to_jax, rtol=1e-7, atol=1e-7)
 
     def test_set_up(self):
         model = self.model()
