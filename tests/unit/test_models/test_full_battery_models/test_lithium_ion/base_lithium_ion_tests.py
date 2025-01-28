83ac3c98970437ca9b7bc9af24fcd6692b950651
Alexander Bills <alexanderallenbills@gmail.com>
2025-01-28 15:18:24 -0800

    coverage

--- tests/unit/test_models/test_full_battery_models/test_lithium_ion/base_lithium_ion_tests.py
+++ tests/unit/test_models/test_full_battery_models/test_lithium_ion/base_lithium_ion_tests.py
@@ -525,10 +525,18 @@ class BaseUnitTestLithiumIon:
         options = {"particle size": "distribution", "surface form": "algebraic"}
         self.check_well_posedness(options)
 
-    def test_well_posed_psd_mechanics(self):
+    def test_well_posed_psd_swelling_and_cracking(self):
         options = {
             "particle size": "distribution",
             "particle mechanics": "swelling and cracking",
+            "surface form": "algebraic",
+        }
+        self.check_well_posedness(options)
+
+    def test_well_posed_psd_swelling_only(self):
+        options = {
+            "particle size": "distribution",
+            "particle mechanics": "swelling only",
             "surface form": "algebraic",
         }
         self.check_well_posedness(options)
