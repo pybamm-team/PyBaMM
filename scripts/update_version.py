6eff86186ee022a43f5bb9f95210ce1ac5672072
Vidip Singh <112854574+vidipsingh@users.noreply.github.com>
2025-02-03 14:07:00 +0530

    integrate hatch-vcs for automated versioning
    - Move hatch-vcs config to `tool.hatch` in `pyproject.toml`
    - Switch to `_version.py` for versioning with hatch-vcs
    - Remove manual `version.py` updates, using git tags instead

--- scripts/update_version.py
+++ scripts/update_version.py
@@ -1,5 +1,5 @@
 """
-Automatically update the version number
+Automatically update version numbers in various files for releases
 """
 
 import os
@@ -10,32 +10,10 @@ import pybamm
 
 def update_version():
     """
-    Opens file and updates the version number
+    Updates version numbers and release information across project files
     """
     release_version = os.getenv("VERSION")[1:]
     release_date = date.today()
-
-    # pybamm/version.py
-    with open(
-        os.path.join(pybamm.root_dir(), "src", "pybamm", "version.py"), "r+"
-    ) as file:
-        output = file.read()
-        replace_version = re.sub(
-            '(?<=__version__ = ")(.+)(?=")', release_version, output
-        )
-        file.truncate(0)
-        file.seek(0)
-        file.write(replace_version)
-
-    # pyproject.toml
-    with open(os.path.join(pybamm.root_dir(), "pyproject.toml"), "r+") as file:
-        output = file.read()
-        replace_version = re.sub(
-            r'(?<=\bversion = ")(.+)(?=")', release_version, output
-        )
-        file.truncate(0)
-        file.seek(0)
-        file.write(replace_version)
 
     # CITATION.cff
     with open(os.path.join(pybamm.root_dir(), "CITATION.cff"), "r+") as file:
@@ -45,10 +23,10 @@ def update_version():
         file.seek(0)
         file.write(replace_version)
 
+    # CHANGELOG.md
     changelog_line1 = "# [Unreleased](https://github.com/pybamm-team/PyBaMM/)\n"
     changelog_line2 = f"# [v{release_version}](https://github.com/pybamm-team/PyBaMM/tree/v{release_version}) - {release_date}\n\n"
 
-    # CHANGELOG.md
     with open(os.path.join(pybamm.root_dir(), "CHANGELOG.md"), "r+") as file:
         output_list = file.readlines()
         output_list[0] = changelog_line1
@@ -56,6 +34,10 @@ def update_version():
         file.truncate(0)
         file.seek(0)
         file.writelines(output_list)
+
+    version_file_path = os.path.join(pybamm.root_dir(), "src", "pybamm", "_version.py")
+    with open(version_file_path, "w") as f:
+        f.write(f'__version__ = "{release_version}"\n')
 
 
 if __name__ == "__main__":
