28e792813975086627d20cd92e1940ac36099214
kratman <kratmandu@gmail.com>
2025-02-14 14:55:47 -0500

    Bug fix

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -7,15 +7,15 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).parent.parent
 
 
+def list_of_files():
+    file_list = (ROOT_DIR / "examples" / "scripts").rglob("*.py")
+    return [pytest.param(file, id=file.name) for file in file_list]
+
+
 class TestExamples:
     """
     A class to test the example scripts.
     """
-
-    @staticmethod
-    def list_of_files():
-        file_list = (ROOT_DIR / "examples" / "scripts").rglob("*.py")
-        return [pytest.param(file, id=file.name) for file in file_list]
 
     @pytest.mark.parametrize("files", list_of_files())
     @pytest.mark.scripts
