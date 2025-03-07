9b4dcfb327534f5f1d84ccb2b181d722d0ac1120
tanishac25 <tanishac25@gmail.com>
2025-03-08 01:47:38 +0530

    Imported numpy.typing as npt, replaced np.ndarray with npt.NDArray for better type hinting.

--- src/pybamm/type_definitions.py
+++ src/pybamm/type_definitions.py
@@ -3,14 +3,15 @@ from __future__ import annotations
 from typing import Union
 from typing_extensions import TypeAlias
 import numpy as np
+import numpy.typing as npt
 import pybamm
 
 # numbers.Number should not be used for type hints
 Numeric: TypeAlias = Union[int, float, np.number]
 
 # expression tree
-ChildValue: TypeAlias = Union[float, np.ndarray]
-ChildSymbol: TypeAlias = Union[float, np.ndarray, pybamm.Symbol]
+ChildValue: TypeAlias = Union[float, npt.NDArray]
+ChildSymbol: TypeAlias = Union[float, npt.NDArray, pybamm.Symbol]
 
 DomainType: TypeAlias = Union[list[str], str, None]
 AuxiliaryDomainType: TypeAlias = Union[dict[str, str], None]
