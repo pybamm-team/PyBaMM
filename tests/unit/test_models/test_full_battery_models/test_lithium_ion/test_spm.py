7187871df4e10f8699dc25d5c542f173d927efe7
rishab <kumarjharishab@gmail.com>
2025-07-01 15:43:39 +0530

    adding unit tests

--- tests/unit/test_models/test_full_battery_models/test_lithium_ion/test_spm.py
+++ tests/unit/test_models/test_full_battery_models/test_lithium_ion/test_spm.py
@@ -73,3 +73,29 @@ class TestSPM(BaseUnitTestLithiumIon):
         new_model_cs_eqn = list(new_model.rhs.values())[1]
         model_cs_eqn = list(model.rhs.values())[1]
         assert new_model_cs_eqn == model_cs_eqn
+
+    def test_basic_spm_with_3d_thermal_box(self):
+        options = {"cell geometry": "box", "dimensionality": 3}
+        self.model = pybamm.lithium_ion.BasicSPM_with_3DThermal
+        self.check_well_posedness(options)
+
+    def test_basic_spm_with_3d_thermal_cylinder(self):
+        options = {"cell geometry": "cylindrical", "dimensionality": 3}
+        self.model = pybamm.lithium_ion.BasicSPM_with_3DThermal
+        self.check_well_posedness(options)
+
+    def test_basic_spm_with_3d_thermal_incompatible_options(self):
+        options = {"cell geometry": "box", "dimensionality": 2}
+        self.model = pybamm.lithium_ion.BasicSPM_with_3DThermal
+        with pytest.raises(
+            pybamm.OptionError,
+            match="cell geometry must be 3D for box or cylindrical geometries",
+        ):
+            self.check_well_posedness(options)
+
+        options = {"cell geometry": "pouch", "dimensionality": 3}
+        with pytest.raises(
+            pybamm.OptionError,
+            match="cell geometry must be box or cylindrical in 3D model",
+        ):
+            self.check_well_posedness(options)
