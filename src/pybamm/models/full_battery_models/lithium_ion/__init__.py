c06726d2bf7e32ccf7a896dea7077332786b99db
Alec Bills <48105066+aabills@users.noreply.github.com>
2025-08-08 11:24:38 -0700

    2D FVM (#5106)

--- src/pybamm/models/full_battery_models/lithium_ion/__init__.py
+++ src/pybamm/models/full_battery_models/lithium_ion/__init__.py
@@ -18,6 +18,7 @@ from .spme import SPMe
 from .dfn import DFN
 from .newman_tobias import NewmanTobias
 from .basic_dfn import BasicDFN
+from .basic_dfn_2d import BasicDFN2D
 from .basic_spm import BasicSPM
 from .basic_spm_with_3d_thermal import Basic3DThermalSPM
 from .basic_dfn_half_cell import BasicDFNHalfCell
