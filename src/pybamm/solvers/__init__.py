808c1c82cfcafaf7f0f7f78a25505e42e6aaa4ed
Gregor Decristoforo <gregor.decristoforo@gmail.com>
2025-11-27 15:44:03 +0100

    Move DiffslExport to evaluate_python.py
    Co-authored-by: martinjrobins <martinjrobins@gmail.com>

--- src/pybamm/solvers/__init__.py
+++ src/pybamm/solvers/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ['algebraic_solver', 'base_solver', 'c_solvers',
            'casadi_algebraic_solver', 'casadi_solver', 'dummy_solver',
-           'export_to_diffsl', 'idaklu_jax', 'idaklu_solver', 'jax_bdf_solver', 'jax_solver',
+           'idaklu_jax', 'idaklu_solver', 'jax_bdf_solver', 'jax_solver',
            'lrudict', 'processed_variable', 'processed_variable_computed',
            'scipy_solver', 'solution', 'processed_variable_time_integral']
