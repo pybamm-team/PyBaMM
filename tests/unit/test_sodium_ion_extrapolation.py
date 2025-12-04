4f1142a545d0dca84b062ae575d92dc41c7faa25
AARYAN <153413051+aaryan-iiitu@users.noreply.github.com>
2025-12-04 05:23:26 +0000

    Fix interpolation errors in sodium-ion DFN and add regression test

---
+++ tests/unit/test_sodium_ion_extrapolation.py
@@ -0,0 +1,14 @@
+import pybamm
+
+def test_sodium_ion_interpolants_allow_extrapolation():
+    
+    param = pybamm.ParameterValues("Chayambuka2022")
+    model = pybamm.sodium_ion.BasicDFN()
+
+    # short nsolve to ensure extrapolation works
+    sim = pybamm.Simulation(model, parameter_values=param)
+
+    # This previously crashed with interpolation bounds exceeded
+    sol = sim.solve([0, 5]) 
+
+    assert sol is not None
