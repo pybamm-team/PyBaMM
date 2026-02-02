1d756f1e1c72450689325667ed0172a25246b574
Marc Berliner <34451391+MarcBerliner@users.noreply.github.com>
2026-02-02 16:15:57 -0500

    Export regularisation classes

--- src/pybamm/__init__.py
+++ src/pybamm/__init__.py
@@ -60,6 +60,7 @@ from .expression_tree.operations.jacobian import Jacobian
 from .expression_tree.operations.convert_to_casadi import CasadiConverter
 from .expression_tree.operations.unpack_symbols import SymbolUnpacker
 from .expression_tree.operations.serialise import Serialise,ExpressionFunctionParameter
+from .expression_tree.operations.regularise import RegulariseSqrtAndPower
 
 # Model classes
 from .models.base_model import BaseModel, ModelSolutionObservability
