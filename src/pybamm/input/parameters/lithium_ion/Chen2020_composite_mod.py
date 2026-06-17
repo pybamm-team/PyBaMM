24bfa92668f72ce6a313126ebfd45756950b6265
lorenzplank <lorenz@sphere-energy.eu>
2026-06-17 15:59:21 +0200

    electrolyte scaling

--- src/pybamm/input/parameters/lithium_ion/Chen2020_composite_mod.py
+++ src/pybamm/input/parameters/lithium_ion/Chen2020_composite_mod.py
@@ -516,18 +516,17 @@ def electrolyte_diffusivity_Nyman2008(c_e, T):
         Solid diffusivity
     """
 
-    c2 = pybamm.Parameter("Electrolyte diffusivity coefficient c2 [m2.s-1]")
-    c1 = pybamm.Parameter("Electrolyte diffusivity coefficient c1 [m2.s-1]")
-    c0 = pybamm.Parameter("Electrolyte diffusivity coefficient c0 [m2.s-1]")
-
-    D_c_e = c2 * (c_e / 1000) ** 2 + c1 * (c_e / 1000) + c0
+    D_c_e = 8.794e-11 * (c_e / 1000) ** 2 - 3.972e-10 * (c_e / 1000) + 4.862e-10
 
     # Nyman et al. (2008) does not provide temperature dependence, so the
     # temperature dependence is added through an Arrhenius activation energy
     E_D_c_e = pybamm.Parameter("Electrolyte diffusivity activation energy [J.mol-1]")
     arrhenius = np.exp(E_D_c_e / pybamm.constants.R * (1 / 298.15 - 1 / T))
 
-    return D_c_e * arrhenius
+    # the overall magnitude can be scaled directly via a single parameter
+    scale = pybamm.Parameter("Electrolyte diffusivity scaling factor")
+
+    return scale * D_c_e * arrhenius
 
 
 def electrolyte_conductivity_Nyman2008(c_e, T):
@@ -554,12 +553,8 @@ def electrolyte_conductivity_Nyman2008(c_e, T):
         Solid diffusivity
     """
 
-    c3 = pybamm.Parameter("Electrolyte conductivity coefficient c3 [S.m-1]")
-    c1p5 = pybamm.Parameter("Electrolyte conductivity coefficient c1.5 [S.m-1]")
-    c1 = pybamm.Parameter("Electrolyte conductivity coefficient c1 [S.m-1]")
-
     sigma_e = (
-        c3 * (c_e / 1000) ** 3 + c1p5 * (c_e / 1000) ** 1.5 + c1 * (c_e / 1000)
+        0.1297 * (c_e / 1000) ** 3 - 2.51 * (c_e / 1000) ** 1.5 + 3.329 * (c_e / 1000)
     )
 
     # Nyman et al. (2008) does not provide temperature dependence, so the
@@ -567,7 +562,10 @@ def electrolyte_conductivity_Nyman2008(c_e, T):
     E_sigma_e = pybamm.Parameter("Electrolyte conductivity activation energy [J.mol-1]")
     arrhenius = np.exp(E_sigma_e / pybamm.constants.R * (1 / 298.15 - 1 / T))
 
-    return sigma_e * arrhenius
+    # the overall magnitude can be scaled directly via a single parameter
+    scale = pybamm.Parameter("Electrolyte conductivity scaling factor")
+
+    return scale * sigma_e * arrhenius
 #plating
 def graphite_plating_exchange_current_density_OKane2020(c_e, c_Li, T):
     """
@@ -1007,14 +1005,10 @@ def get_parameter_values():
         "Cation transference number": 0.2594,
         "Thermodynamic factor": 1.0,
         "Electrolyte diffusivity [m2.s-1]": electrolyte_diffusivity_Nyman2008,
-        "Electrolyte diffusivity coefficient c2 [m2.s-1]": 8.794e-11,
-        "Electrolyte diffusivity coefficient c1 [m2.s-1]": -3.972e-10,
-        "Electrolyte diffusivity coefficient c0 [m2.s-1]": 4.862e-10,
+        "Electrolyte diffusivity scaling factor": 1.0,
         "Electrolyte diffusivity activation energy [J.mol-1]": 17000.0,
         "Electrolyte conductivity [S.m-1]": electrolyte_conductivity_Nyman2008,
-        "Electrolyte conductivity coefficient c3 [S.m-1]": 0.1297,
-        "Electrolyte conductivity coefficient c1.5 [S.m-1]": -2.51,
-        "Electrolyte conductivity coefficient c1 [S.m-1]": 3.329,
+        "Electrolyte conductivity scaling factor": 1.0,
         "Electrolyte conductivity activation energy [J.mol-1]": 17000.0,
         # experiment
         "Reference temperature [K]": 298.15,
--- src/pybamm/input/parameters/lithium_ion/Prada2013.py
+++ src/pybamm/input/parameters/lithium_ion/Prada2013.py
@@ -453,18 +453,17 @@ def electrolyte_diffusivity_Nyman2008(c_e, T):
         Solid diffusivity
     """
 
-    c2 = pybamm.Parameter("Electrolyte diffusivity coefficient c2 [m2.s-1]")
-    c1 = pybamm.Parameter("Electrolyte diffusivity coefficient c1 [m2.s-1]")
-    c0 = pybamm.Parameter("Electrolyte diffusivity coefficient c0 [m2.s-1]")
-
-    D_c_e = c2 * (c_e / 1000) ** 2 + c1 * (c_e / 1000) + c0
+    D_c_e = 8.794e-11 * (c_e / 1000) ** 2 - 3.972e-10 * (c_e / 1000) + 4.862e-10
 
     # Nyman et al. (2008) does not provide temperature dependence, so the
     # temperature dependence is added through an Arrhenius activation energy
     E_D_c_e = pybamm.Parameter("Electrolyte diffusivity activation energy [J.mol-1]")
     arrhenius = np.exp(E_D_c_e / pybamm.constants.R * (1 / 298.15 - 1 / T))
 
-    return D_c_e * arrhenius
+    # the overall magnitude can be scaled directly via a single parameter
+    scale = pybamm.Parameter("Electrolyte diffusivity scaling factor")
+
+    return scale * D_c_e * arrhenius
 
 
 def electrolyte_conductivity_Nyman2008(c_e, T):
@@ -492,12 +491,8 @@ def electrolyte_conductivity_Nyman2008(c_e, T):
         Solid conductivity
     """
 
-    c3 = pybamm.Parameter("Electrolyte conductivity coefficient c3 [S.m-1]")
-    c1p5 = pybamm.Parameter("Electrolyte conductivity coefficient c1.5 [S.m-1]")
-    c1 = pybamm.Parameter("Electrolyte conductivity coefficient c1 [S.m-1]")
-
     sigma_e = (
-        c3 * (c_e / 1000) ** 3 + c1p5 * (c_e / 1000) ** 1.5 + c1 * (c_e / 1000)
+        0.1297 * (c_e / 1000) ** 3 - 2.51 * (c_e / 1000) ** 1.5 + 3.329 * (c_e / 1000)
     )
 
     # Nyman et al. (2008) does not provide temperature dependence, so the
@@ -505,7 +500,10 @@ def electrolyte_conductivity_Nyman2008(c_e, T):
     E_sigma_e = pybamm.Parameter("Electrolyte conductivity activation energy [J.mol-1]")
     arrhenius = np.exp(E_sigma_e / pybamm.constants.R * (1 / 298.15 - 1 / T))
 
-    return sigma_e * arrhenius
+    # the overall magnitude can be scaled directly via a single parameter
+    scale = pybamm.Parameter("Electrolyte conductivity scaling factor")
+
+    return scale * sigma_e * arrhenius
 
 
 # Call dict via a function to avoid errors when editing in place
@@ -643,14 +641,10 @@ def get_parameter_values():
         "Cation transference number": 0.36,
         "Thermodynamic factor": 1.0,
         "Electrolyte diffusivity [m2.s-1]": electrolyte_diffusivity_Nyman2008,
-        "Electrolyte diffusivity coefficient c2 [m2.s-1]": 8.794e-11,
-        "Electrolyte diffusivity coefficient c1 [m2.s-1]": -3.972e-10,
-        "Electrolyte diffusivity coefficient c0 [m2.s-1]": 4.862e-10,
+        "Electrolyte diffusivity scaling factor": 1.0,
         "Electrolyte diffusivity activation energy [J.mol-1]": 17000.0,
         "Electrolyte conductivity [S.m-1]": electrolyte_conductivity_Nyman2008,
-        "Electrolyte conductivity coefficient c3 [S.m-1]": 0.1297,
-        "Electrolyte conductivity coefficient c1.5 [S.m-1]": -2.51,
-        "Electrolyte conductivity coefficient c1 [S.m-1]": 3.329,
+        "Electrolyte conductivity scaling factor": 1.0,
         "Electrolyte conductivity activation energy [J.mol-1]": 17000.0,
         # experiment
         "Reference temperature [K]": 298,
