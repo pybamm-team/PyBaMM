e2f232419eefe5ff1db6eb1a9cbbe14c643662c8
martinjrobins <martinjrobins@gmail.com>
2026-07-16 12:10:30 +0000

    turn on pydiffsol in unit tests

--- noxfile.py
+++ noxfile.py
@@ -147,7 +147,7 @@ def run_doctests(session):
 def run_unit(session):
     """Run the unit tests."""
     set_environment_variables(PYBAMM_ENV, session=session)
-    install_locked(session, extras=["all", "jax"], groups=["dev"])
+    install_locked(session, extras=["all", "jax", "pydiffsol"], groups=["dev"])
     session.run("python", "-m", "pytest", "-m", "unit", "packages/pybamm/tests")
 
 
